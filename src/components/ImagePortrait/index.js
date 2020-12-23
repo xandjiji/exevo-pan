35942146f4bdad179b4c7e06a16b72527c6ccc02
xandjiji <xandjiji@gmail.com>
2020-12-23 08:59:48 -0300

    useEffect for ImagePortrait img src

--- src/components/ImagePortrait/index.js
+++ src/components/ImagePortrait/index.js
@@ -4,28 +4,23 @@ import ImagePortrait from './ImagePortrait.styled';
 export default ({ src, alt, title }) => {
 
     const [loaded, setLoaded] = useState(false);
-    const [error, setError] = useState(false);
+    const [imgSrc, setImgSrc] = useState(null);
+
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            if(!error) {
-                setLoaded(true);
-            }
-        }, 500);
-
-        return () => clearTimeout(timeout);
-    },[error])
+        setImgSrc(src);
+    },[src])
 
 
     return (
         <ImagePortrait className="shadow">
             <img
                 className={loaded ? 'loaded' : 'loading'}
-                src={src}
+                src={imgSrc}
                 alt={alt}
                 title={title}
                 onLoad={() => setLoaded(true)}
-                onError={() => setError(true)}
+                onError={() => setLoaded(false)}
             />
             <div className={loaded ? 'loader hidden' : 'loader'}></div>
         </ImagePortrait>
