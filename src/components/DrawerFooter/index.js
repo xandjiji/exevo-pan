5a1dcd7c21063bed9ef7c2a329fe67a38e8737a0
xandjiji <xandjiji@gmail.com>
2021-03-02 20:25:04 -0300

    toggle theme button complete

--- src/components/DrawerFooter/index.js
+++ src/components/DrawerFooter/index.js
@@ -3,19 +3,25 @@ import DrawerFooter from './DrawerFooter.styled';
 import ThemeContext from '../../contexts/Theme/context';
 
 import { ReactComponent as GithubIcon } from '../../assets/svgs/github.svg';
+import { ReactComponent as MoonIcon } from '../../assets/svgs/moon.svg';
 
 export default () => {
     const { toggleTheme } = useContext(ThemeContext);
 
     return (
         <DrawerFooter className="inner-container">
-            <a className="gitrepo" href="https://github.com/xandjiji/exevo-pan" target="_blank" rel="noreferrer">
-                <GithubIcon className="clickable" />
+            <div className="left-wrapper">
+                <a className="gitrepo" href="https://github.com/xandjiji/exevo-pan" target="_blank" rel="noreferrer">
+                    <GithubIcon className="clickable" />
                 Go to this repository
             </a>
             made by <a className="gituser" href="https://github.com/xandjiji" target="_blank" rel="noreferrer">xandjiji</a>
+            </div>
 
-            <button onClick={toggleTheme}>dsadsa</button>
+            <div className="theme-wrapper clickable" onClick={toggleTheme}>
+                dark theme
+                <MoonIcon />
+            </div>
         </DrawerFooter>
     )
 }
