d096f6d10574be0f74fa1bdc5cbe4825bfd66a86
xandjiji <xandjiji@gmail.com>
2021-03-03 20:30:46 -0300

    changing metaBar color with theme toggle

--- src/contexts/Theme/index.js
+++ src/contexts/Theme/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import ThemeContext from './context';
 
 export default ({ children }) => {
@@ -9,9 +9,19 @@ export default ({ children }) => {
         setTheme((prevTheme) => {
             const newValue = prevTheme === 'light-theme' ? 'dark-theme' : 'light-theme';
             localStorage.setItem('theme', newValue);
+
             return newValue;
         });
     }, []);
+
+    useEffect(() => {
+        const primaryColor = (theme === 'light-theme' ? '#3F51B5' : '#9857E7');
+        let addressBar;
+        addressBar = document.querySelector('meta[name=theme-color]');
+        addressBar.setAttribute('content', primaryColor);
+        addressBar = document.querySelector('meta[name=msapplication-navbutton-color]')
+        addressBar.setAttribute('content', primaryColor);
+    }, [theme]);
 
     return (
         <ThemeContext.Provider
