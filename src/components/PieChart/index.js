6b73dd489603dc47f8f39f6bac30697575700b34
xandjiji <xandjiji@gmail.com>
2021-04-17 18:18:29 -0300

    changing PieCHart colors

--- src/components/PieChart/index.js
+++ src/components/PieChart/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Wrapper, Title, ChartWrapper } from './PieChart.styled';
 import { Doughnut } from 'react-chartjs-2';
 
+const colorArray = [
+    '#8338EC',
+    '#FFD166',
+    '#118AB2',
+    '#06D6A0',
+    '#EF476F'
+]
+
 export default ({ data, chartLabel }) => {
 
     const currentTheme = localStorage.getItem('theme') || 'light-theme';
@@ -40,22 +48,8 @@ export default ({ data, chartLabel }) => {
             label: chartLabel,
             data: Object.keys(data).map(item => Number(data[item])),
             fill: false,
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
+            backgroundColor: colorArray,
+            borderColor: colorArray,
             borderWidth: 0
         }]
     }
