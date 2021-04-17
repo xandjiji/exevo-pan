482908f0a4dd4e987ea26903f460eba29fe4101c
xandjiji <xandjiji@gmail.com>
2021-04-17 19:31:48 -0300

    QA for StatisticsGrid

--- src/components/PercentageCard/PercentageCard.styled.js
+++ src/components/PercentageCard/PercentageCard.styled.js
@@ -10,6 +10,12 @@ export const Wrapper = styled.div`
     border-radius: 5px;
     background-color: var(--surface);
     box-shadow: 2px 2px 4px 2px rgba(0,0,0,0.14);
+
+    @media(max-width: 440px) {
+        width: 100%;
+        margin-right: 0;
+        align-items: center;
+    }
 `;
 
 export const Title = styled.h2`
--- src/components/PieChart/PieChart.styled.js
+++ src/components/PieChart/PieChart.styled.js
@@ -7,6 +7,10 @@ export const Wrapper = styled.div`
     border-radius: 5px;
     background-color: var(--surface);
     box-shadow: 2px 2px 4px 2px rgba(0,0,0,0.14);
+
+    @media(max-width: 440px) {
+        width: 100%;
+    }
 `;
 
 export const Title = styled.h2`
