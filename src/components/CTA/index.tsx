0fdf85f73fb6606f03afc6eeb075332dfd1e6aa7
xandjiji <xandjiji@gmail.com>
2021-06-03 10:21:25 -0300

    feature(component): CTA component refactor

---
+++ src/components/CTA/index.tsx
@@ -0,0 +1,21 @@
+import coinsGif from '../../assets/tibiaCoins.gif'
+import * as S from './styles'
+
+const CTA = ({
+  ...props
+}: React.HTMLAttributes<HTMLAnchorElement>): JSX.Element => {
+  return (
+    <S.Button
+      className="clickable shadow"
+      href="https://www.reidoscoins.com.br/?tracking=60b8120a1ab43"
+      target="_blank"
+      rel="external"
+      {...props}
+    >
+      <S.Img src={coinsGif as string} alt="Tibia Coins" />
+      Buy Tibia Coins
+    </S.Button>
+  )
+}
+
+export default CTA
