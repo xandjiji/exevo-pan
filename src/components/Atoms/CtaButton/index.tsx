a33b6af251632f556f9fbb084cbb47fc55a2c0c7
xandjiji <xandjiji@gmail.com>
2021-06-08 22:37:17 -0300

    refactor(component): using types.ts file for CtaButton component

--- src/components/Atoms/CtaButton/index.tsx
+++ src/components/Atoms/CtaButton/index.tsx
@@ -1,9 +1,8 @@
 import coinsGif from '../../../assets/tibiaCoins.gif'
+import { CtaProps } from './types'
 import * as S from './styles'
 
-const CtaButton = ({
-  ...props
-}: React.HTMLAttributes<HTMLAnchorElement>): JSX.Element => {
+const CtaButton = ({ ...props }: CtaProps): JSX.Element => {
   return (
     <S.Button
       href="https://www.reidoscoins.com.br/?tracking=60b8120a1ab43"
---
+++ src/components/Atoms/CtaButton/types.ts
@@ -0,0 +1,5 @@
+import { HTMLAttributes } from 'react'
+
+export interface CtaProps {
+  props?: HTMLAttributes<HTMLAnchorElement>
+}
