64192b12f1f51916c87f00acfeec58a14194577e
xandjiji <xandjiji@gmail.com>
2021-06-14 11:25:34 -0300

    refactor(RangeSlider): using hidden input as a truth source for value

--- src/components/RangeSlider/RangeSlider.tsx
+++ src/components/RangeSlider/RangeSlider.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
+import { debounce } from 'lodash'
 import useDrag from './useDrag'
 import { RangeSliderProps } from './types'
 import * as S from './styles'
@@ -10,15 +11,23 @@ const RangeSlider = ({
   onChange,
   ...props
 }: RangeSliderProps): JSX.Element => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
   const [value, setValue] = useState(initialValue ?? min)
+
+  const dispatchSyntheticEvent = useMemo(
+    () =>
+      debounce(() => {
+        const event = new Event('input', { bubbles: true })
+        inputRef.current?.dispatchEvent(event)
+      }, 250),
+    [],
+  )
 
   const { binders, isMousePressed, percentagePosition, setPercentagePosition } =
     useDrag(value / max)
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     /* @ToDo: prevent the user from typing NaN with regex*/
-
-    onChange?.(event)
     /* @ToDo: granularity of increments */
     /* @ToDo: if NaN -> set to Min */
     const newValue: number = parseInt(event.target.value, 10)
@@ -29,6 +38,10 @@ const RangeSlider = ({
   useEffect(() => {
     setValue(Math.round(max * percentagePosition))
   }, [max, percentagePosition])
+
+  useEffect(() => {
+    dispatchSyntheticEvent()
+  }, [value, dispatchSyntheticEvent])
 
   return (
     <S.Wrapper>
@@ -41,6 +54,14 @@ const RangeSlider = ({
         max={max}
         value={value}
         onChange={handleInputChange}
+      />
+      <input
+        hidden
+        value={value}
+        onInput={event =>
+          onChange?.(event as React.ChangeEvent<HTMLInputElement>)
+        }
+        ref={inputRef}
         {...props}
       />
     </S.Wrapper>
