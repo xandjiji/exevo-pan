869538010fc2eee68b0da55cf324ad14809e14aa
xandjiji <xandjiji@gmail.com>
2021-06-23 21:43:42 -0300

    test(RangeSliderInput): testing cursor styling and display valus

--- src/components/Atoms/RangeSliderInput/__tests__/index.tsx
+++ src/components/Atoms/RangeSliderInput/__tests__/index.tsx
@@ -6,4 +6,15 @@ describe('<RangeSliderInput />', () => {
     render(<RangeSliderInput data-testid="test" min={0} max={100} />)
     expect(screen.getByTestId('test')).toBeInTheDocument()
   })
+
+  test('should render cursors correctly', async () => {
+    render(<RangeSliderInput min={0} max={100} value={[10, 90]} />)
+
+    const [cursorA, cursorB] = screen.getAllByRole('slider')
+
+    expect(cursorA).toHaveStyle('left: 10%')
+    expect(cursorB).toHaveStyle('left: 90%')
+    expect(screen.getByText('10')).toBeInTheDocument()
+    expect(screen.getByText('90')).toBeInTheDocument()
+  })
 })
