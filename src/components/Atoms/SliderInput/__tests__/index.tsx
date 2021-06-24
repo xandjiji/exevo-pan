45af9679fb16920e7c573b161d706b9bdc20ed68
xandjiji <xandjiji@gmail.com>
2021-06-23 21:20:23 -0300

    test(SliderInput): testing invalidity

--- src/components/Atoms/SliderInput/__tests__/index.tsx
+++ src/components/Atoms/SliderInput/__tests__/index.tsx
@@ -24,4 +24,19 @@ describe('<SliderInput />', () => {
     userEvent.type(hiddenInput, '-50')
     expect(mockedOnChange).toBeCalledTimes(3)
   })
+
+  test('should be invalid', async () => {
+    render(<SliderInput data-testid="test" min={0} max={100} />)
+
+    const displayInput = screen.getAllByDisplayValue(0)[0]
+    const hiddenInput = screen.getByTestId('test')
+
+    userEvent.clear(displayInput)
+    userEvent.type(displayInput, '-50')
+    expect(displayInput).toHaveValue('-50')
+    expect(displayInput).toBeInvalid()
+
+    expect(hiddenInput).not.toHaveValue('-50')
+    expect(hiddenInput).toBeInvalid()
+  })
 })
