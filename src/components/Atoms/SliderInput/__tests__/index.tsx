273fd841d1a7ba2cbb292fbc8239acce489777db
xandjiji <xandjiji@gmail.com>
2021-06-23 21:20:05 -0300

    test(SliderInput): testing onChange calls

--- src/components/Atoms/SliderInput/__tests__/index.tsx
+++ src/components/Atoms/SliderInput/__tests__/index.tsx
@@ -9,4 +9,19 @@ describe('<SliderInput />', () => {
     render(<SliderInput data-testid="test" min={0} max={100} />)
     expect(screen.getByTestId('test')).toBeInTheDocument()
   })
+
+  test('onChange should be called', async () => {
+    render(
+      <SliderInput
+        data-testid="test"
+        min={0}
+        max={100}
+        onChange={mockedOnChange}
+      />,
+    )
+
+    const hiddenInput = screen.getByTestId('test')
+    userEvent.type(hiddenInput, '-50')
+    expect(mockedOnChange).toBeCalledTimes(3)
+  })
 })
