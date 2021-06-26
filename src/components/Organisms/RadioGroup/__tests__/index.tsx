dc72fdd2b7b49a4c8bec6ddaabe641c5d6659628
xandjiji <xandjiji@gmail.com>
2021-06-26 19:15:33 -0300

    test(RadioGroup): more tests

--- src/components/Organisms/RadioGroup/__tests__/index.tsx
+++ src/components/Organisms/RadioGroup/__tests__/index.tsx
@@ -3,7 +3,13 @@ import userEvent from '@testing-library/user-event'
 import { RadioButton } from 'components/Atoms'
 import RadioGroup from '..'
 
+const mockedOnChange = jest.fn()
+
 describe('<RadioGroup />', () => {
+  beforeEach(() => {
+    mockedOnChange.mockReset()
+  })
+
   describe('renders children correctly', () => {
     test('RadioButtons as children', () => {
       render(
@@ -37,5 +43,67 @@ describe('<RadioGroup />', () => {
 
       expect(screen.queryAllByTestId('child')).toHaveLength(2)
     })
+  })
+
+  test('should implicitly control its children', () => {
+    render(
+      <RadioGroup>
+        <RadioButton />
+        <RadioButton />
+      </RadioGroup>,
+    )
+
+    const [radioA, radioB] = screen.getAllByRole('radio')
+
+    expect(radioA).toBeChecked()
+    expect(radioB).not.toBeChecked()
+    userEvent.click(radioB)
+    expect(radioA).not.toBeChecked()
+    expect(radioB).toBeChecked()
+    userEvent.click(radioA)
+    expect(radioA).toBeChecked()
+    expect(radioB).not.toBeChecked()
+  })
+
+  test('should be controlled', () => {
+    render(
+      <RadioGroup indexValue={0}>
+        <RadioButton />
+        <RadioButton />
+      </RadioGroup>,
+    )
+
+    const [radioA, radioB] = screen.getAllByRole('radio')
+
+    expect(radioA).toBeChecked()
+    expect(radioB).not.toBeChecked()
+    userEvent.click(radioB)
+    expect(radioA).toBeChecked()
+    expect(radioB).not.toBeChecked()
+    userEvent.click(radioA)
+    expect(radioA).toBeChecked()
+    expect(radioB).not.toBeChecked()
+  })
+
+  test('should call onChange', () => {
+    render(
+      <RadioGroup onChange={mockedOnChange}>
+        <RadioButton />
+        <RadioButton />
+      </RadioGroup>,
+    )
+
+    const [radioA, radioB] = screen.getAllByRole('radio')
+
+    userEvent.click(radioB)
+    expect(mockedOnChange).toBeCalledTimes(1)
+    expect(mockedOnChange).toBeCalledWith(1)
+
+    userEvent.click(radioA)
+    expect(mockedOnChange).toBeCalledTimes(2)
+    expect(mockedOnChange).toBeCalledWith(0)
+
+    userEvent.click(radioA)
+    expect(mockedOnChange).not.toBeCalledTimes(3)
   })
 })
