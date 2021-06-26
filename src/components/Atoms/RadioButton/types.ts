f658b838cef2d719074f9f3160b36a87a792c647
xandjiji <xandjiji@gmail.com>
2021-06-26 11:32:49 -0300

    fix(RadioButton): children is now optional

--- src/components/Atoms/RadioButton/types.ts
+++ src/components/Atoms/RadioButton/types.ts
@@ -1,7 +1,7 @@
 import React, { HTMLAttributes } from 'react'
 
 export interface RadioButtonProps {
-  children: React.ReactNode
+  children?: React.ReactNode
   active?: boolean
   onClick?: (event?: React.MouseEvent) => void
   props?: HTMLAttributes<HTMLDivElement>
