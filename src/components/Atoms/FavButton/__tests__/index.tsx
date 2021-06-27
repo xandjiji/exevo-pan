1fb70756394f0c92f3c51d21558cca712a7f99b6
xandjiji <xandjiji@gmail.com>
2021-06-27 10:43:21 -0300

    test(FavButton): adding tests

---
+++ src/components/Atoms/FavButton/__tests__/index.tsx
@@ -0,0 +1,151 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { getFavArray, saveToLocalStorage } from 'utils'
+import FavButton from '..'
+
+jest.mock('utils', () => ({
+  getFavArray: jest.fn(),
+  saveToLocalStorage: jest.fn(),
+}))
+
+const getFavArrayMock = getFavArray as jest.MockedFunction<typeof getFavArray>
+const saveToLocalStorageMock = saveToLocalStorage as jest.MockedFunction<
+  typeof saveToLocalStorage
+>
+
+const mockedCharacterObject = {
+  id: 123456,
+  nickname: 'Ksu',
+  auctionEnd: 1624812300,
+  currentBid: 9500,
+  hasBeenBidded: false,
+  outfitId: '130_0',
+  serverId: 32,
+  vocationId: 4,
+  level: 607,
+  skills: {
+    magic: 104.41,
+    club: 14.27,
+    fist: 16.65,
+    sword: 14.2,
+    fishing: 10,
+    axe: 12.49,
+    distance: 13.13,
+    shielding: 33.61,
+  },
+  items: [29423, 29426, 30400, 30403],
+  charms: ['Freeze', 'Poison', 'Wound', 'Zap'],
+  transfer: false,
+  imbuements: [
+    'Critical Hit',
+    'Death Damage',
+    'Death Protection',
+    'Energy Damage',
+    'Energy Protection',
+    'Fire Damage',
+    'Fire Protection',
+    'Life Leech',
+    'Magic Level',
+    'Mana Leech',
+    'Speed',
+  ],
+  hasSoulwar: true,
+} as CharacterObject
+
+describe('<FavButton />', () => {
+  beforeEach(() => {
+    getFavArrayMock.mockReset()
+    saveToLocalStorageMock.mockReset()
+  })
+
+  describe('lazy state initialization should work correctly', () => {
+    test('if the character IS favorited, it should render with an ACTIVE button', () => {
+      getFavArrayMock.mockReturnValueOnce([mockedCharacterObject])
+      render(<FavButton characterObject={mockedCharacterObject} />)
+
+      expect(
+        screen.getByLabelText(
+          `Remove ${mockedCharacterObject.nickname} from favorites`,
+        ),
+      ).toBeInTheDocument()
+      expect(screen.getByRole('button')).toHaveAttribute('aria-pressed', 'true')
+    })
+
+    test('if the character ISNT favorited, it should render with an INACTIVE button', () => {
+      getFavArrayMock.mockReturnValueOnce([
+        { ...mockedCharacterObject, id: 987654 },
+      ])
+      render(<FavButton characterObject={mockedCharacterObject} />)
+
+      expect(
+        screen.getByLabelText(
+          `Add ${mockedCharacterObject.nickname} to favorites`,
+        ),
+      ).toBeInTheDocument()
+      expect(screen.getByRole('button')).toHaveAttribute(
+        'aria-pressed',
+        'false',
+      )
+    })
+
+    test('if there are no favorited characters, it should render with an INACTIVE button', () => {
+      getFavArrayMock.mockReturnValueOnce([])
+      render(<FavButton characterObject={mockedCharacterObject} />)
+
+      expect(
+        screen.getByLabelText(
+          `Add ${mockedCharacterObject.nickname} to favorites`,
+        ),
+      ).toBeInTheDocument()
+      expect(screen.getByRole('button')).toHaveAttribute(
+        'aria-pressed',
+        'false',
+      )
+    })
+  })
+
+  test('should toggle correctly', () => {
+    getFavArrayMock.mockReturnValue([])
+    render(<FavButton characterObject={mockedCharacterObject} />)
+
+    const buttonElement = screen.getByRole('button')
+
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'false')
+
+    userEvent.click(buttonElement)
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'true')
+    expect(saveToLocalStorageMock).toBeCalledTimes(1)
+    expect(saveToLocalStorageMock).toHaveBeenLastCalledWith(
+      'initialFavCharacterData',
+      [mockedCharacterObject],
+    )
+
+    getFavArrayMock.mockReturnValueOnce([mockedCharacterObject])
+    userEvent.click(buttonElement)
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'false')
+    expect(saveToLocalStorageMock).toBeCalledTimes(2)
+    expect(saveToLocalStorageMock).toHaveBeenLastCalledWith(
+      'initialFavCharacterData',
+      [],
+    )
+  })
+
+  test('keyboard navigation should toggle the button', () => {
+    getFavArrayMock.mockReturnValue([])
+    render(<FavButton characterObject={mockedCharacterObject} />)
+
+    const buttonElement = screen.getByRole('button')
+
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'false')
+
+    userEvent.tab()
+    userEvent.keyboard('{enter}')
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'true')
+    expect(saveToLocalStorageMock).toBeCalledTimes(1)
+
+    getFavArrayMock.mockReturnValueOnce([mockedCharacterObject])
+    userEvent.keyboard('{enter}')
+    expect(buttonElement).toHaveAttribute('aria-pressed', 'false')
+    expect(saveToLocalStorageMock).toBeCalledTimes(2)
+  })
+})
