6e063b87a1977f1385294bac7a031bf6844645ed
xandjiji <xandjiji@gmail.com>
2021-06-29 16:21:28 -0300

    refactor(AuctionTimer): using theme provider props

--- src/components/Atoms/AuctionTimer/styles.ts
+++ src/components/Atoms/AuctionTimer/styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 import { CountdownProps } from './types'
 
 export const Countdown = styled.span<CountdownProps>`
-  ${({ endingSoon }) => endingSoon && 'color: var(--red)'}
+  ${({ endingSoon, theme }) => endingSoon && `color: ${theme.colors.red};`}
+  transition: color 0.2s ease-out;
 `
