15163d9af81bcc8ceabadb6c62d22f175312f2bd
xandjiji <xandjiji@gmail.com>
2021-06-30 10:25:54 -0300

    feature(LoadingAlert): using Spinner styled-component

--- src/components/Atoms/LoadingAlert/styles.ts
+++ src/components/Atoms/LoadingAlert/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { Shadow } from 'styles'
+import { Shadow, Spinner as BaseSpinner } from 'styles'
 
 export const FloatingLabel = styled.div`
   position: absolute;
@@ -20,53 +20,13 @@ export const FloatingLabel = styled.div`
 
   ${Shadow}
 `
-export const Spinner = styled.div`
+export const Spinner = styled(BaseSpinner)`
   margin-right: 8px;
-  position: relative;
   width: 12px;
   height: 12px;
-  border-radius: 50%;
-  background: linear-gradient(
-    to right,
-    ${({ theme }) => theme.colors.primary} 10%,
-    rgba(255, 255, 255, 0) 42%
-  );
-  animation: spinAnimation 1.4s infinite ease-out;
-  transform: translateZ(0);
-  transition: opacity 0.2s ease-out;
-
-  &:before {
-    content: '';
-    width: 50%;
-    height: 50%;
-    background: ${({ theme }) => theme.colors.primary};
-    border-radius: 100% 0 0 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+  background-color: ${({ theme }) => theme.colors.alert};
 
   &:after {
-    content: '';
     background: ${({ theme }) => theme.colors.alert};
-    width: 75%;
-    height: 75%;
-    border-radius: 50%;
-    margin: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-  }
-
-  @keyframes spinAnimation {
-    0% {
-      transform: rotate(0deg);
-    }
-
-    100% {
-      transform: rotate(360deg);
-    }
   }
 `
