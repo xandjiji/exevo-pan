218997e287df6d49ddb457d4ea9d2e062fcbb768
xandjiji <xandjiji@gmail.com>
2021-06-30 10:24:49 -0300

    feature(style): Spinner styled component

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,3 +1,4 @@
 export { default as Clickable } from './Clickable'
 export { default as Shadow } from './Shadow'
 export { default as Smooth } from './Smooth'
+export { default as Spinner } from './Spinner'
