a310d7a61fe2dee9fda846cb41ca5e3186297be5
xandjiji <xandjiji@gmail.com>
2021-06-29 22:21:34 -0300

    test(CtaButton): using theme provider wrapper

--- src/components/Atoms/CtaButton/__tests__/index.tsx
+++ src/components/Atoms/CtaButton/__tests__/index.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
 import CtaButton from '..'
 
 describe('<CtaButton />', () => {
   test('it renders correctly', () => {
-    render(<CtaButton data-testid="testid" />)
+    renderWithProviders(<CtaButton data-testid="testid" />)
     expect(screen.getByTestId('testid')).toBeInTheDocument()
   })
 })
