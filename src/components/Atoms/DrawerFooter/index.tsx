1ff0e7f65904e63cb5f8c58a2782a4a4b3e68f6b
xandjiji <xandjiji@gmail.com>
2021-07-04 18:00:23 -0300

    feature(DrawerFooter): component

---
+++ src/components/Atoms/DrawerFooter/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles'
+
+const DrawerFooter = ({
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>): JSX.Element => (
+  <S.Wrapper {...props}>
+    <S.RepositoryLink
+      href="https://github.com/xandjiji/exevo-pan"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <S.GithubIcon />
+      Go to this repository
+    </S.RepositoryLink>
+    made by
+    <S.ProfileLink
+      href="https://github.com/xandjiji"
+      target="_blank"
+      rel="noreferrer"
+    >
+      xandjiji
+    </S.ProfileLink>
+  </S.Wrapper>
+)
+
+export default DrawerFooter
