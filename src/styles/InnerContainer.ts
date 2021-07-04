61a547702bc0c3419c364e93ac67f89e7e604a7d
xandjiji <xandjiji@gmail.com>
2021-07-04 17:57:10 -0300

    feature(styles): creating Container, InnerContainer and NegativeContainer

---
+++ src/styles/InnerContainer.ts
@@ -0,0 +1,11 @@
+import { css } from 'styled-components'
+
+export default css`
+  padding-left: 20px;
+  padding-right: 20px;
+
+  @media (min-width: 768px) {
+    padding-left: 40px;
+    padding-right: 40px;
+  }
+`
