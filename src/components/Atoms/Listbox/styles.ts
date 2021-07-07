1eac1a1668e9a0c96c7f18d90dcef28a7acefb31
xandjiji <xandjiji@gmail.com>
2021-07-07 11:07:38 -0300

    feature(Listbox): adding shadow, and custom scrollbar

--- src/components/Atoms/Listbox/styles.ts
+++ src/components/Atoms/Listbox/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
+import { Shadow, CustomScrollbar } from 'styles'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  border-radius: 5px;
+  overflow: auto;
+  ${Shadow}
+  ${CustomScrollbar}
+`
