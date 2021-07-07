9104e10dd3cd3193db175125526b88023d988834
xandjiji <xandjiji@gmail.com>
2021-07-07 14:14:51 -0300

    fix(Listbox): fixing indexToId method

--- src/components/Atoms/Listbox/utils.ts
+++ src/components/Atoms/Listbox/utils.ts
@@ -1,2 +1,2 @@
 export const indexToId = (index: number | undefined): string | undefined =>
-  index ? `listbox-item-${index}` : undefined
+  index === undefined ? undefined : `listbox-item-${index}`
