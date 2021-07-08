7aa4cb6fa72dd5887aa3c2ecfeb330fdeb4402ae
xandjiji <xandjiji@gmail.com>
2021-07-08 16:11:35 -0300

    fix(AutocompleteInput): fixing regex search

--- src/components/Organisms/AutocompleteInput/utils.ts
+++ src/components/Organisms/AutocompleteInput/utils.ts
@@ -1,5 +1,5 @@
 export const filterByTerm = (term: string, array: Option[]): Option[] => {
-  const regex = new RegExp(term, 'ig')
+  const regex = new RegExp(term, 'i')
   return array.filter(item => regex.test(item.name))
 }
 
