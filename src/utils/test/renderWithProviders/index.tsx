0c25ccc669f1e4d40baab03554baaca1a197c890
xandjiji <xandjiji@gmail.com>
2021-07-09 10:58:33 -0300

    feature(utils): improving renderWithProviders()

--- src/utils/test/renderWithProviders/index.tsx
+++ src/utils/test/renderWithProviders/index.tsx
@@ -1,6 +1,21 @@
-import { render, RenderResult } from '@testing-library/react'
+import { ReactElement } from 'react'
+import { render, RenderResult, RenderOptions } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 import Themes from 'styles/themes'
 
-export const renderWithProviders = (ui: JSX.Element): RenderResult =>
-  render(<ThemeProvider theme={Themes.default}>{ui}</ThemeProvider>)
+export const renderWithThemeProvider = (ui: ReactElement): ReactElement => (
+  <ThemeProvider theme={Themes.default}>{ui}</ThemeProvider>
+)
+
+export const renderWithProviders = (
+  ui: ReactElement,
+  options?: RenderOptions,
+): RenderResult => {
+  const renderResult = render(renderWithThemeProvider(ui), options)
+
+  return {
+    ...renderResult,
+    rerender: (rerenderedUi): void =>
+      renderResult.rerender(renderWithThemeProvider(rerenderedUi)),
+  }
+}
