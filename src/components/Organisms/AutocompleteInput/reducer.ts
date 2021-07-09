f71d78bcc0e972e31bf84be13fdbaec719938385
xandjiji <xandjiji@gmail.com>
2021-07-09 09:35:30 -0300

    refactor(AutocompleteInput): if no highlightedIndex, it should be treated as 0

--- src/components/Organisms/AutocompleteInput/reducer.ts
+++ src/components/Organisms/AutocompleteInput/reducer.ts
@@ -24,26 +24,18 @@ export default (
         listboxStatus: false,
       }
 
-    case 'arrowNavigation':
-      if (highlightedIndex === undefined) {
-        return {
-          ...state,
-          listboxStatus: true,
-          highlightedIndex: 0,
-          inputValue: action.list[0].name,
-        }
-      } else {
-        const newIndex = circularArrayIndex(
-          highlightedIndex + action.value,
-          action.list,
-        )
-        return {
-          ...state,
-          listboxStatus: true,
-          highlightedIndex: newIndex,
-          inputValue: action.list[newIndex].name,
-        }
+    case 'arrowNavigation': {
+      const newIndex = circularArrayIndex(
+        (highlightedIndex ?? 0) + action.value,
+        action.list,
+      )
+      return {
+        ...state,
+        listboxStatus: true,
+        highlightedIndex: newIndex,
+        inputValue: action.list[newIndex].name,
       }
+    }
 
     case 'setListboxStatus':
       return { ...state, listboxStatus: action.value }
