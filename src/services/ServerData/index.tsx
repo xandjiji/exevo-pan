706f8936001358cbe7f3b0d326ee38da78c5af66
xandjiji <xandjiji@gmail.com>
2021-07-11 12:14:10 -0300

    feature(services): ServerData

---
+++ src/services/ServerData/index.tsx
@@ -0,0 +1,20 @@
+import { getFromLocalStorage } from 'utils'
+import { BASE_DATA_ENDPOINT, SERVER_DATA_PATH } from '../../constants'
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export default class ServerDataClient {
+  static baseUrl = `${BASE_DATA_ENDPOINT}sss${SERVER_DATA_PATH}`
+
+  static async fetch(): Promise<ServerObject[]> {
+    try {
+      const response = await fetch(this.baseUrl)
+      const data = (await response.json()) as Record<string, ServerObject>
+      const serverArray = Object.values(data)
+
+      return serverArray
+    } catch (error: unknown) {
+      console.log(error)
+      return getFromLocalStorage<ServerObject[]>('serverData', [])
+    }
+  }
+}
---
+++ src/services/index.ts
@@ -0,0 +1,1 @@
+export { default as ServerDataClient } from './ServerData'
