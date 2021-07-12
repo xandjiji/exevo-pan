5380af4428554901afc4a96a0755226dabe01406
xandjiji <xandjiji@gmail.com>
2021-07-11 21:46:30 -0300

    chore(tests): moving mock to a separate file

--- src/components/Atoms/FavButton/__tests__/index.tsx
+++ src/components/Atoms/FavButton/__tests__/index.tsx
@@ -3,6 +3,7 @@ import userEvent from '@testing-library/user-event'
 import { getFavArray, saveToLocalStorage } from 'utils'
 import { renderWithProviders } from 'utils/test'
 import FavButton from '..'
+import { mockedCharacterObject } from './mock'
 
 jest.mock('utils', () => ({
   getFavArray: jest.fn(),
@@ -13,45 +14,6 @@ const getFavArrayMock = getFavArray as jest.MockedFunction<typeof getFavArray>
 const saveToLocalStorageMock = saveToLocalStorage as jest.MockedFunction<
   typeof saveToLocalStorage
 >
-
-const mockedCharacterObject = {
-  id: 123456,
-  nickname: 'Ksu',
-  auctionEnd: 1624812300,
-  currentBid: 9500,
-  hasBeenBidded: false,
-  outfitId: '130_0',
-  serverId: 32,
-  vocationId: 4,
-  level: 607,
-  skills: {
-    magic: 104.41,
-    club: 14.27,
-    fist: 16.65,
-    sword: 14.2,
-    fishing: 10,
-    axe: 12.49,
-    distance: 13.13,
-    shielding: 33.61,
-  },
-  items: [29423, 29426, 30400, 30403],
-  charms: ['Freeze', 'Poison', 'Wound', 'Zap'],
-  transfer: false,
-  imbuements: [
-    'Critical Hit',
-    'Death Damage',
-    'Death Protection',
-    'Energy Damage',
-    'Energy Protection',
-    'Fire Damage',
-    'Fire Protection',
-    'Life Leech',
-    'Magic Level',
-    'Mana Leech',
-    'Speed',
-  ],
-  hasSoulwar: true,
-} as CharacterObject
 
 describe('<FavButton />', () => {
   beforeEach(() => {
