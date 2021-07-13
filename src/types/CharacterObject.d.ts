c062f1f36af4bdd1651cc0014c6cf89e43e7d4f0
xandjiji <xandjiji@gmail.com>
2021-07-13 20:32:00 -0300

    fix(types): adding missing attributes to PartialCharacterObject

--- src/types/CharacterObject.d.ts
+++ src/types/CharacterObject.d.ts
@@ -9,6 +9,22 @@ declare interface CharacterSkillsObject {
   shielding: number
 }
 
+declare interface PartialCharacterObject {
+  id: number
+  nickname: string
+  auctionEnd: number
+  currentBid: number
+  hasBeenBidded: boolean
+  outfitId: string
+  serverId: number
+  vocationId: number
+  level: number
+  imbuements: string[]
+  items: number[]
+  hasSoulwar: boolean
+  skills: CharacterSkillsObject
+}
+
 declare interface CharacterObject extends PartialCharacterObject {
   serverData: ServerObject
 }
