3905e683e2df3a7b17ff78a6e9227be8fc1a8d0b
xandjiji <xandjiji@gmail.com>
2021-07-15 22:13:19 -0300

    refactor(constants): adding KEY suffix to LocalStorageKeys

--- src/constants/LocalStorageKeys.ts
+++ src/constants/LocalStorageKeys.ts
@@ -1,7 +1,7 @@
 export const SERVER_DATA_KEY = 'serverData'
-export const AUCTION_CHARACTER_DATA = 'auctionCharacterData'
-export const RARE_ITEM_DATA = 'rareItemData'
+export const AUCTION_CHARACTER_DATA_KEY = 'auctionCharacterData'
+export const RARE_ITEM_DATA_KEY = 'rareItemData'
 
-export const FAV_CHARACTER_DATA = 'initialFavCharacterData'
+export const FAV_CHARACTER_DATA_KEY = 'initialFavCharacterData'
 
-export const HISTORY_HASH_PREFIX = 'historyHash'
+export const HISTORY_HASH_KEY_PREFIX = 'historyHash'
