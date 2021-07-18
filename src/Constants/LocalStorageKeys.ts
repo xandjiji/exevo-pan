94b1ba4ec1a349cb2f59d984d63e1ee0bae6568c
xandjiji <xandjiji@gmail.com>
2021-07-18 11:41:26 -0300

    chore(constants): changing FAV_CHARACTER_DATA_KEY value

--- src/Constants/LocalStorageKeys.ts
+++ src/Constants/LocalStorageKeys.ts
@@ -2,7 +2,7 @@ export const SERVER_DATA_KEY = 'serverData'
 export const AUCTION_CHARACTER_DATA_KEY = 'auctionCharacterData'
 export const RARE_ITEM_DATA_KEY = 'rareItemData'
 
-export const FAV_CHARACTER_DATA_KEY = 'initialFavCharacterData'
+export const FAV_CHARACTER_DATA_KEY = 'favCharacterData'
 
 export const HISTORY_HASH_KEY_PREFIX = 'historyHash'
 export const HISTORY_DATA_KEY_PREFIX = 'historyData'
