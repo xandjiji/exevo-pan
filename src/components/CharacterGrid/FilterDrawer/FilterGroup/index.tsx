38aeb94849d2f8f831c7e289ae63a53f488c50ae
xandjiji <xandjiji@gmail.com>
2021-07-26 21:48:51 -0300

    feature(FilterGroup): initial component

---
+++ src/components/CharacterGrid/FilterDrawer/FilterGroup/index.tsx
@@ -0,0 +1,20 @@
+import * as S from './styles'
+import { FilterGroupProps } from './types'
+
+const FilterGroup = ({
+  label,
+  labelSuffix,
+  htmlFor,
+  children,
+  ...props
+}: FilterGroupProps): JSX.Element => (
+  <S.Wrapper {...props}>
+    <S.LabelWrapper>
+      <S.Label htmlFor={htmlFor}>{label}</S.Label>
+      {labelSuffix}
+    </S.LabelWrapper>
+    {children}
+  </S.Wrapper>
+)
+
+export default FilterGroup
