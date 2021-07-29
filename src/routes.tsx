bcf82d534da05be2bed2bc46a80690731d021c8c
xandjiji <xandjiji@gmail.com>
2021-07-29 15:17:20 -0300

    fix(routes): adding key to each route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,16 +2,19 @@ import { BazaarAuctions, Statistics, ErrorPage } from 'modules'
 
 export default [
   {
+    key: 'Bazaar Auctions',
     path: ['/', '/bazaar-history'],
     exact: true,
     component: BazaarAuctions,
   },
   {
+    key: 'Statistics',
     path: '/statistics',
     exact: true,
     component: Statistics,
   },
   {
+    key: 'Error',
     path: undefined,
     component: ErrorPage,
   },
