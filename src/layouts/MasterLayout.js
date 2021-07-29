ff8865e4f6d15e7b32602787b20dde931d467a8f
xandjiji <xandjiji@gmail.com>
2021-07-29 14:54:48 -0300

    refactor: refactoring MasterLayout

--- src/layouts/MasterLayout.js
+++ src/layouts/MasterLayout.js
@@ -1,44 +1,15 @@
-import React, { useContext, useEffect } from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import { useTheme } from 'contexts/useTheme';
-import CharGridSwitcher from '../Routes/CharGridSwitcher';
-import SideDrawerSwitcher from '../Routes/SideDrawerSwitcher';
-
-import StatisticsGrid from '../components/StatisticsGrid';
-
-import ErrorPage from '../components/ErrorPage';
-
-const pageTitle = {
-    '/': 'Exevo Pan - Current Auctions',
-    '/bazaar-history': 'Exevo Pan - Bazaar History',
-    '/statistics': 'Exevo Pan - Statistics'
-}
+import routes from '../routes'
 
 const MasterLayout = () => {
     const { currentTheme } = useTheme()
-    const { pathname } = useLocation()
-    useEffect(() => {
-        document.title = pageTitle[pathname] || 'Exevo Pan';
-    }, [pathname]);
-
     return (
         <div className={`body-container ${currentTheme}`}>
+            <Header />
             <Switch>
-                <Route exact path={["/", "/bazaar-history"]}>
-                    <Header />
-                    <CharGridSwitcher />
-                    <SideDrawerSwitcher />
-                </Route>
-
-                <Route exact path="/statistics">
-                    <Header />
-                    <StatisticsGrid />
-                </Route>
-
-                <Route>
-                    <ErrorPage mainMessage="404" paragraphs={['oops!', 'page not found']} />
-                </Route>
+                {routes.map(route => (<Route {...route} />))}
             </Switch>
         </div>
     );
