0c3995ae890ed081ae0600194616714220d7fcf1
xandjiji <xandjiji@gmail.com>
2021-08-01 10:27:43 -0300

    feature(constants): adding routes

---
+++ src/Constants/Routes.ts
@@ -0,0 +1,5 @@
+export const routes = {
+  HOME: '/',
+  BAZAAR_HISTORY: '/bazaar-history',
+  STATISTICS: '/statistics',
+}
--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -1,3 +1,4 @@
 export * from './Endpoints'
 export * from './LocalStorageKeys'
 export * from './Paths'
+export * from './Routes'
