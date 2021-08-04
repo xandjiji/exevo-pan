7fe0e1f728f8c5bf182d6afda2bade6ecb409485
xandjiji <xandjiji@gmail.com>
2021-08-04 20:38:07 -0300

    test(PercentageCard): testing styling and value display

--- src/components/PercentageCard/__tests__/index.tsx
+++ src/components/PercentageCard/__tests__/index.tsx
@@ -14,6 +14,52 @@ describe('<PercentageCard />', () => {
     )
   })
 
-  test.todo('should display values correctly')
-  test.todo('should style values correctly')
+  test('should display values correctly', () => {
+    const { rerender } = renderWithProviders(
+      <PercentageCard title="Auction success rate" percentage={0} />,
+    )
+
+    expect(screen.getByText(/0/)).toBeInTheDocument()
+
+    rerender(<PercentageCard title="Auction success rate" percentage={1} />)
+    expect(screen.getByText(/1/)).toBeInTheDocument()
+
+    rerender(<PercentageCard title="Auction success rate" percentage={10} />)
+    expect(screen.getByText(/10/)).toBeInTheDocument()
+
+    rerender(<PercentageCard title="Auction success rate" percentage={0.1} />)
+    expect(screen.getByText(/0\.1/)).toBeInTheDocument()
+
+    rerender(<PercentageCard title="Auction success rate" percentage={100} />)
+    expect(screen.getByText(/100/)).toBeInTheDocument()
+
+    rerender(<PercentageCard title="Auction success rate" percentage={1000} />)
+    expect(screen.getByText(/1000/)).toBeInTheDocument()
+  })
+
+  test('should style values correctly', () => {
+    const { rerender } = renderWithProviders(
+      <PercentageCard title="Auction success rate" percentage={0} />,
+    )
+
+    const percentageElement = screen.getByText(/0/)
+    expect(percentageElement).toHaveStyle('color: #C51313;')
+
+    rerender(<PercentageCard title="Auction success rate" percentage={50} />)
+    expect(percentageElement).toHaveStyle('color: #377712;')
+
+    rerender(<PercentageCard title="Auction success rate" percentage={100} />)
+    expect(percentageElement).toHaveStyle('color: #377712;')
+
+    rerender(<PercentageCard title="Auction success rate" percentage={1000} />)
+    expect(percentageElement).toHaveStyle('color: #377712;')
+
+    rerender(<PercentageCard title="Auction success rate" percentage={0.1} />)
+    expect(percentageElement).toHaveStyle('color: #C51313;')
+
+    rerender(
+      <PercentageCard title="Auction success rate" percentage={49.9999} />,
+    )
+    expect(percentageElement).toHaveStyle('color: #C51313;')
+  })
 })
