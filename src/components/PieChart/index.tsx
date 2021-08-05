87f8e090f9ef158a3d71a7627afab2d8b8584a5c
xandjiji <xandjiji@gmail.com>
2021-08-04 21:13:34 -0300

    feature(PieChart): adding aria-describedby

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { useMemo } from 'react'
+import { useRef, useMemo } from 'react'
 import { useTheme } from 'styled-components'
 import { Doughnut } from 'react-chartjs-2'
+import { v4 as uuidv4 } from 'uuid'
 import * as S from './styles'
 import { PieChartProps } from './types'
 
 const colorArray = ['#8338EC', '#FFD166', '#118AB2', '#06D6A0', '#EF476F']
 
 const PieChart = ({ title, pieDataSet }: PieChartProps): JSX.Element => {
+  const { current: titleId } = useRef(uuidv4())
   const { colors } = useTheme()
 
   const options = useMemo(
@@ -60,8 +62,8 @@ const PieChart = ({ title, pieDataSet }: PieChartProps): JSX.Element => {
 
   return (
     <S.Wrapper>
-      <S.Title>{title}</S.Title>
-      <S.ChartWrapper>
+      <S.Title id={titleId}>{title}</S.Title>
+      <S.ChartWrapper aria-describedby={titleId}>
         <Doughnut data={chartData} options={options} />
       </S.ChartWrapper>
     </S.Wrapper>
