77fd73cb75f5ab47f3db6155c2a234f070464bc2
xandjiji <xandjiji@gmail.com>
2021-08-05 22:48:00 -0300

    feature(Summary): styling

--- src/components/Chart/Summary/styles.ts
+++ src/components/Chart/Summary/styles.ts
@@ -4,10 +4,39 @@ import { PositiveStyleProps } from './types'
 
 export const Wrapper = styled.div``
 
-export const Title = styled.h3``
+export const Title = styled.h3`
+  font-size: 14px;
+  font-weight: 300;
+  color: ${({ theme }) => theme.colors.onSurface};
+`
 
-export const Value = styled.span``
+export const Value = styled.span`
+  margin: 6px 0 4px 0;
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.onSurface};
+`
 
-export const Percentage = styled.span<PositiveStyleProps>``
+export const TrendIcon = styled(TrendingIconSvg)`
+  margin-right: 4px;
+  width: 16px;
+  height: 16px;
+`
 
-export const TrendIcon = styled(TrendingIconSvg)``
+export const Percentage = styled.span<PositiveStyleProps>`
+  display: flex;
+  align-items: center;
+
+  font-size: 12px;
+  font-weight: 600;
+  color: ${({ theme, positive }) =>
+    positive ? theme.colors.green : theme.colors.red};
+
+  ${TrendIcon} {
+    fill: ${({ theme, positive }) =>
+      positive ? theme.colors.green : theme.colors.red};
+    transform: ${({ positive }) => (positive ? 'none' : 'scaleY(-1)')};
+  }
+`
