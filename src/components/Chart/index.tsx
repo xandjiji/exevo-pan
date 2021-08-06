12dfd3881af5c63934553ab346f9c13643e652f6
xandjiji <xandjiji@gmail.com>
2021-08-05 22:41:28 -0300

    feature(Chart): initial component

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,143 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { useState, useMemo } from 'react'
+import { Line } from 'react-chartjs-2'
+import { Chip } from 'components/Atoms'
+import { useTheme } from 'styled-components'
+import { formatNumberWithCommas } from 'utils/formatNumberWithCommas'
+import Summary from './Summary'
+import * as S from './styles'
+import { ChartProps } from './types'
+
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+
+const Chart = ({
+  totalLabel,
+  yesterdayLabel,
+  tooltipLabel,
+  chartData,
+  ...props
+}: ChartProps): JSX.Element => {
+  const { colors } = useTheme()
+
+  const [dataSize, setDataSize] = useState(7)
+
+  const { current, lastMonth } = chartData
+  const todayValue = lastMonth[lastMonth.length - 1]
+  const yesterdayValue = lastMonth[lastMonth.length - 2]
+  const dailyDifference = todayValue - yesterdayValue
+
+  const options = useMemo(
+    () => ({
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        duration: 400,
+        easing: 'easeOutCubic',
+      },
+      elements: {
+        line: {
+          tension: 0,
+        },
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              fontColor: colors.onSurface,
+            },
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              callback: value => formatNumberWithCommas(value),
+              fontColor: colors.onSurface,
+            },
+            gridLines: {
+              color: colors.separator,
+            },
+          },
+        ],
+      },
+      tooltips: {
+        callbacks: {
+          title: tooltipItem => {
+            return `Day ${tooltipItem[0].xLabel}`
+          },
+          label: tooltipItem => {
+            return `${tooltipLabel}: ${formatNumberWithCommas(
+              tooltipItem.yLabel,
+            )} TC`
+          },
+        },
+        displayColors: false,
+      },
+    }),
+    [colors, tooltipLabel],
+  )
+
+  const chartDataObject = useMemo(
+    () => ({
+      labels: lastMonth
+        .slice(lastMonth.length - dataSize)
+        .map((_, index) => {
+          const date = new Date()
+          date.setDate(date.getDate() - index)
+          return `${date.getDate()}/${date.getMonth() + 1}, ${
+            weekdays[date.getDay()]
+          }`
+        })
+        .reverse(),
+      datasets: [
+        {
+          label: tooltipLabel,
+          data: lastMonth.slice(lastMonth.length - dataSize),
+          fill: false,
+          backgroundColor: colors.primary,
+          borderColor: colors.primary,
+        },
+      ],
+    }),
+    [colors, tooltipLabel, dataSize, lastMonth],
+  )
+
+  return (
+    <S.Wrapper {...props}>
+      <S.SummaryWrapper>
+        <Summary
+          title={totalLabel}
+          value={current}
+          percentage={(todayValue / (current - todayValue)) * 100}
+        />
+        <Summary
+          title={yesterdayLabel}
+          value={todayValue}
+          percentage={(Math.abs(dailyDifference) / yesterdayValue) * 100}
+          positive={dailyDifference > 0}
+        />
+      </S.SummaryWrapper>
+
+      <S.ChartWrapper>
+        <Line data={chartDataObject} options={options} />
+      </S.ChartWrapper>
+
+      <S.ChipWrapper>
+        <Chip overrideStatus={dataSize === 28} onClick={() => setDataSize(28)}>
+          28 days
+        </Chip>
+        <Chip overrideStatus={dataSize === 7} onClick={() => setDataSize(7)}>
+          7 days
+        </Chip>
+      </S.ChipWrapper>
+    </S.Wrapper>
+  )
+}
+
+export default Chart
---
+++ src/components/Chart/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { MaterialCard } from 'styles'
+
+export const Wrapper = styled.div`
+  ${MaterialCard}
+`
+
+export const SummaryWrapper = styled.div``
+
+export const ChartWrapper = styled.div``
+
+export const ChipWrapper = styled.div``
