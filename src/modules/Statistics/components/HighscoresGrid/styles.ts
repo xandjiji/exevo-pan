15ed50da3db5bdcf25fe96a51a42c096377f90c1
xandjiji <xandjiji@gmail.com>
2021-08-08 00:34:24 -0300

    refactor(HighscoresGrid): refactoring grid-template-columns

--- src/modules/Statistics/components/HighscoresGrid/styles.ts
+++ src/modules/Statistics/components/HighscoresGrid/styles.ts
@@ -13,27 +13,11 @@ export const Wrapper = styled.article`
   ${CustomScrollbar}
 
   display: grid;
-  grid-template-columns: 1fr;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
   grid-gap: 24px;
 
   background-color: ${({ theme }) => theme.colors.background};
   ${Smooth}
-
-  @media (min-width: 600px) {
-    grid-template-columns: 1fr 1fr;
-  }
-
-  @media (min-width: 1024px) {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-
-  @media (min-width: 1400px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-
-  @media (min-width: 1600px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  }
 
   &::before {
     content: '';
