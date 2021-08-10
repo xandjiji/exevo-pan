2b534f25736e9f0abff5732b47eb3eef62e8f5f5
xandjiji <xandjiji@gmail.com>
2021-08-09 21:57:19 -0300

    chore: using eslint-disable

--- src/utils/debounce/index.ts
+++ src/utils/debounce/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const debounce = <F extends (...args: any[]) => any>(
   func: F,
   waitFor: number,
