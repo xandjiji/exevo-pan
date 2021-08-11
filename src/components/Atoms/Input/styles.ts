1c498c0fddf929567328f065a023362b83566ea1
xandjiji <xandjiji@gmail.com>
2021-08-10 21:26:47 -0300

    fix(Input): fixing clear button spacing

--- src/components/Atoms/Input/styles.ts
+++ src/components/Atoms/Input/styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div``
 
 export const Input = styled.input`
   padding: 10px 16px;
-  flex-grow: 1;
+  width: 100%;
 
   border: none;
   outline: none;
@@ -35,6 +35,7 @@ export const ErrorMessage = styled.span`
 export const ClearButton = styled.button`
   position: relative;
   width: 42px;
+  flex-shrink: 0;
   pointer-events: none;
   opacity: 0;
   transition: opacity 0.2s ease-out;
