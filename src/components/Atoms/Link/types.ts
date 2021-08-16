8a967cfd2af384be137fdd760696954a83c75522
xandjiji <xandjiji@gmail.com>
2021-08-15 22:04:05 -0300

    feature(Link): typing 'exact' prop

--- src/components/Atoms/Link/types.ts
+++ src/components/Atoms/Link/types.ts
@@ -4,4 +4,5 @@ export interface LinkProps {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   children: ReactElement<any, string | JSXElementConstructor<any>>
   href: string
+  exact?: boolean
 }
