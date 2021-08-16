e60d53edd3cfbac8722ebb5ad38f7e6b02a55f30
xandjiji <xandjiji@gmail.com>
2021-08-15 22:16:15 -0300

    feature(analytics): triggering pageviews on route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,28 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import ErrorBoundary from 'components/ErrorBoundary'
 import { ThemeProvider } from 'contexts/useTheme'
 import { DatabaseProvider } from 'contexts/useDatabase'
 import { GlobalStyles } from 'styles'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { google } from 'Constants'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter()
+
+  const handleRouteChange = (url: string) => {
+    ;(window as WindowObject).gtag('config', google.GTM_ID, {
+      page_path: url,
+    })
+  }
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <ErrorBoundary>
       <ThemeProvider>
