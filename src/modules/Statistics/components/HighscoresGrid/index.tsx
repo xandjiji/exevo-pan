d01cdc88950b232aa23dcef0dae713790bd3fb59
xandjiji <xandjiji@gmail.com>
2021-08-16 22:42:18 -0300

    feature(Highscores): Overall Statistics page is now SSG

--- src/modules/Statistics/components/HighscoresGrid/index.tsx
+++ src/modules/Statistics/components/HighscoresGrid/index.tsx
@@ -1,79 +1,70 @@
 import { memo } from 'react'
-import { useStatisticsData } from 'contexts/useDatabase'
 import { formatNumberWithCommas } from 'utils'
 import List from '../List'
 import * as S from './styles'
+import { HighscoresGridProps } from './types'
 
 const HighscoresGrid = ({
+  statisticsData,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>): JSX.Element => {
-  const { statisticsData, loading } = useStatisticsData()
-
-  return (
-    <S.Wrapper {...props}>
-      <S.PageTitle>
-        Tibia Bazaar top 10 characters, ranking and highscores
-      </S.PageTitle>
-      {!loading && statisticsData ? (
-        <>
-          <List
-            title="Bid"
-            charactersList={statisticsData.top10Bid}
-            displayedDataKey="currentBid"
-            format={formatNumberWithCommas}
-          />
-          <List
-            title="Level"
-            charactersList={statisticsData.top10Level}
-            displayedDataKey="level"
-            format={formatNumberWithCommas}
-          />
-          <List
-            title="Magic"
-            charactersList={statisticsData.top10Magic}
-            displayedDataKey="magic"
-          />
-          <List
-            title="Distance"
-            charactersList={statisticsData.top10Distance}
-            displayedDataKey="distance"
-          />
-          <List
-            title="Sword"
-            charactersList={statisticsData.top10Sword}
-            displayedDataKey="sword"
-          />
-          <List
-            title="Axe"
-            charactersList={statisticsData.top10Axe}
-            displayedDataKey="axe"
-          />
-          <List
-            title="Club"
-            charactersList={statisticsData.top10Club}
-            displayedDataKey="club"
-          />
-          <List
-            title="Fist"
-            charactersList={statisticsData.top10Fist}
-            displayedDataKey="fist"
-          />
-          <List
-            title="Shielding"
-            charactersList={statisticsData.top10Shielding}
-            displayedDataKey="shielding"
-          />
-          <List
-            title="Fishing"
-            charactersList={statisticsData.top10Fishing}
-            displayedDataKey="fishing"
-          />
-        </>
-      ) : (
-        <S.Loading />
-      )}
-    </S.Wrapper>
-  )
-}
+}: HighscoresGridProps): JSX.Element => (
+  <S.Wrapper {...props}>
+    <S.PageTitle>
+      Tibia Bazaar top 10 characters, ranking and highscores
+    </S.PageTitle>
+    <List
+      title="Bid"
+      charactersList={statisticsData.top10Bid}
+      displayedDataKey="currentBid"
+      format={formatNumberWithCommas}
+    />
+    <List
+      title="Level"
+      charactersList={statisticsData.top10Level}
+      displayedDataKey="level"
+      format={formatNumberWithCommas}
+    />
+    <List
+      title="Magic"
+      charactersList={statisticsData.top10Magic}
+      displayedDataKey="magic"
+    />
+    <List
+      title="Distance"
+      charactersList={statisticsData.top10Distance}
+      displayedDataKey="distance"
+    />
+    <List
+      title="Sword"
+      charactersList={statisticsData.top10Sword}
+      displayedDataKey="sword"
+    />
+    <List
+      title="Axe"
+      charactersList={statisticsData.top10Axe}
+      displayedDataKey="axe"
+    />
+    <List
+      title="Club"
+      charactersList={statisticsData.top10Club}
+      displayedDataKey="club"
+    />
+    <List
+      title="Fist"
+      charactersList={statisticsData.top10Fist}
+      displayedDataKey="fist"
+    />
+    <List
+      title="Shielding"
+      charactersList={statisticsData.top10Shielding}
+      displayedDataKey="shielding"
+    />
+    <List
+      title="Fishing"
+      charactersList={statisticsData.top10Fishing}
+      displayedDataKey="fishing"
+    />
+  </S.Wrapper>
+)
 
 export default memo(HighscoresGrid)
--- src/pages/statistics/highscores.tsx
+++ src/pages/statistics/highscores.tsx
@@ -2,8 +2,14 @@ import Head from 'next/head'
 import { Title, Description } from 'components/Meta'
 import { Main } from 'templates'
 import { Header, HighscoresGrid } from 'modules/Statistics'
+import { ManageDataClient } from 'services'
+import { GetStaticProps } from 'next'
 
-export default function Statistics(): JSX.Element {
+export default function Highscores({
+  statisticsData,
+}: {
+  statisticsData: StatisticsData
+}): JSX.Element {
   return (
     <div>
       <Head>
@@ -14,9 +20,17 @@ export default function Statistics(): JSX.Element {
       <Main>
         <main>
           <Header />
-          <HighscoresGrid />
+          <HighscoresGrid statisticsData={statisticsData} />
         </main>
       </Main>
     </div>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const statisticsData = await ManageDataClient.fetchStatisticsData()
+
+  return {
+    props: { statisticsData },
+  }
 }
