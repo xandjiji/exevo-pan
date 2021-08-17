4da60e551265167c971c6db1d84092ca5ecd99a3
xandjiji <xandjiji@gmail.com>
2021-08-16 23:13:57 -0300

    test(Statistics): fixing tests

--- src/modules/Statistics/components/HighscoresGrid/__tests__/index.tsx
+++ src/modules/Statistics/components/HighscoresGrid/__tests__/index.tsx
@@ -1,18 +1,16 @@
 import { screen } from '@testing-library/react'
-import { ManageDataClient } from 'services'
 import { renderWithProviders } from 'utils/test'
 import HighscoresGrid from '..'
 import { mockedStatisticsData } from './mock'
 
 describe('<HighscoresGrid />', () => {
-  beforeEach(() => {
-    jest
-      .spyOn(ManageDataClient, 'fetchStatisticsData')
-      .mockResolvedValueOnce(mockedStatisticsData)
-  })
-
   test('should render correctly', () => {
-    renderWithProviders(<HighscoresGrid data-testid="grid-element" />)
+    renderWithProviders(
+      <HighscoresGrid
+        data-testid="grid-element"
+        statisticsData={mockedStatisticsData}
+      />,
+    )
 
     expect(screen.getByTestId('grid-element')).toBeInTheDocument()
   })
