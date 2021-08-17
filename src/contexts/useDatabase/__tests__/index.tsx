383f3edfce75a78a855e2303bd21e1770846d323
xandjiji <xandjiji@gmail.com>
2021-08-16 23:18:11 -0300

    test(useDatabase): removing initial statistics data fetch

--- src/contexts/useDatabase/__tests__/index.tsx
+++ src/contexts/useDatabase/__tests__/index.tsx
@@ -628,35 +628,4 @@ describe('useDatabase()', () => {
       dispatch: expect.any(Function),
     })
   })
-
-  test(`checking ${routes.STATISTICS} path initial data load`, async () => {
-    mockedUseRouter.mockReturnValue({
-      pathname: routes.STATISTICS,
-    } as NextRouter)
-    const { result, waitForNextUpdate } = renderHook(() => useDatabase(), {
-      wrapper: ComponentWrapper,
-    })
-
-    expect(result.current).toEqual({
-      loading: true,
-      characterData: [],
-      serverData: [],
-      rareItemData: {},
-      historyData: [],
-      statisticsData: null,
-      dispatch: expect.any(Function),
-    })
-
-    await waitForNextUpdate()
-
-    expect(result.current).toEqual({
-      loading: false,
-      characterData: [],
-      serverData: [],
-      rareItemData: {},
-      historyData: [],
-      statisticsData: mockStatisticsData,
-      dispatch: expect.any(Function),
-    })
-  })
 })
