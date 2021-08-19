8b5073e6d6f91f251b23841eee55b889e72e54be
xandjiji <xandjiji@gmail.com>
2021-08-18 23:31:11 -0300

    feature(SpritePortrait): lazyloading

--- src/components/Atoms/SpritePortrait/index.tsx
+++ src/components/Atoms/SpritePortrait/index.tsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
+import { useOnScreen } from 'hooks'
 import * as S from './styles'
 
 const SpritePortrait = ({
   src,
   ...props
 }: React.ImgHTMLAttributes<HTMLImageElement>): JSX.Element => {
+  const ref = useRef<HTMLDivElement | null>()
+
   const [loaded, setLoaded] = useState<boolean>(!src)
+  const [currentSrc, setCurrentSrc] = useState<string | undefined>()
+
+  const onScreen = useOnScreen<HTMLDivElement>(ref)
+  useEffect(() => {
+    if (src && onScreen) setCurrentSrc(src)
+  }, [onScreen])
 
   return (
-    <S.Wrapper>
+    <S.Wrapper ref={ref as React.RefObject<HTMLDivElement>}>
       <S.Img
-        src={src}
-        aria-hidden={!src || !loaded}
-        hidden={!src}
+        src={currentSrc}
+        aria-hidden={!currentSrc || !loaded}
+        hidden={!currentSrc}
         onLoad={() => setLoaded(true)}
         onError={() => setLoaded(false)}
         {...props}
