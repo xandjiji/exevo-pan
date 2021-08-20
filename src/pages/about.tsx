bd5a50117c33e12adcfb0ce5d74bdfab8702b332
xandjiji <xandjiji@gmail.com>
2021-08-19 21:04:58 -0300

    feature(About): about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import { Main } from 'templates'
+
+export default function About(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Exevo Pan - About us</title>
+        <meta name="title" content="Exevo Pan - About us" />
+        <meta property="og:site_name" content="Exevo Pan - About us" />
+        <meta property="og:title" content="Exevo Pan - About us" />
+        <meta property="twitter:title" content="Exevo Pan - About us" />
+
+        <meta
+          name="description"
+          content="About Exevo Pan and our contact info"
+        />
+        <meta
+          property="twitter:description"
+          content="About Exevo Pan and our contact info"
+        />
+        <meta
+          property="og:description"
+          content="About Exevo Pan and our contact info"
+        />
+        <meta property="og:type" content="website" />
+      </Head>
+
+      <Main>{/* @ ToDo: add module */}</Main>
+    </div>
+  )
+}
