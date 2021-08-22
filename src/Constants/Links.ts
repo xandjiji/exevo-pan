5f4c078047c5531d42271a2345390910dd7601c8
xandjiji <xandjiji@gmail.com>
2021-08-22 16:50:33 -0300

    feature(Constants): adding links

---
+++ src/Constants/Links.ts
@@ -0,0 +1,9 @@
+export const links = {
+  EMAIL: 'mailto:xandjiji@gmail.com',
+  LINKEDIN: 'https://www.linkedin.com/in/xandjiji',
+  GITHUB_PROFILE: 'https://github.com/xandjiji',
+  GITHUB_REPOSITORY: 'https://github.com/xandjiji/exevo-pan',
+  TIBIACLIPS: 'https://www.youtube.com/channel/UC2srmu0R5yNikl3cnfqcomQ',
+  TIBIA: 'https://www.tibia.com',
+  CIPSOFT: 'https://www.cipsoft.com',
+}
