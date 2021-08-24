5a220693ec5c1cd70fad1d77f0d1f305d22e8580
xandjiji <xandjiji@gmail.com>
2021-08-23 21:15:46 -0300

    chore(i18n): adding config files

---
+++ next-i18next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    i18n: {
+        locales: ['en', 'pt'],
+        defaultLocale: 'en',
+        domains: [
+            { domain: 'exevopan.com', defaultLocale: 'en' },
+            { domain: 'exevopan.com.br', defaultLocale: 'pt' }
+        ]
+    },
+}
