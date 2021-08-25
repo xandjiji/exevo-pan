8959d7c6401adda840403325067fb6ea35249aa6
xandjiji <xandjiji@gmail.com>
2021-08-24 21:17:00 -0300

    feature(i18n): 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,32 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 import Head from 'next/head'
 import { Main } from 'templates'
 import ErrorPage from 'modules/ErrorPage'
 import { GetStaticProps } from 'next'
 
 export default function Custom404(): JSX.Element {
+  const { t } = useTranslation()
+
   return (
     <div>
       <Head>
-        <title>Exevo Pan - 404</title>
-        <meta name="title" content="Exevo Pan - 404" />
-        <meta property="og:site_name" content="Exevo Pan - 404" />
-        <meta property="og:title" content="Exevo Pan - 404" />
-        <meta property="twitter:title" content="Exevo Pan - 404" />
+        <title>{t('ErrorPage.Meta.title')}</title>
+        <meta name="title" content={t('ErrorPage.Meta.title')} />
+        <meta property="og:site_name" content={t('ErrorPage.Meta.title')} />
+        <meta property="og:title" content={t('ErrorPage.Meta.title')} />
+        <meta property="twitter:title" content={t('ErrorPage.Meta.title')} />
+
+        <meta name="description" content={t('ErrorPage.Meta.description')} />
+        <meta
+          property="twitter:description"
+          content={t('ErrorPage.Meta.description')}
+        />
+        <meta
+          property="og:description"
+          content={t('ErrorPage.Meta.description')}
+        />
+        <meta property="og:type" content="website" />
       </Head>
 
       <Main>
