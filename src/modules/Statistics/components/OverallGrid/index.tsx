a665a178ee28a2c45dec83a9b5228bcff36d18e2
xandjiji <xandjiji@gmail.com>
2021-08-25 21:16:56 -0300

    feature(i18n): adding translations to Overall Statistics

--- src/modules/Statistics/components/OverallGrid/index.tsx
+++ src/modules/Statistics/components/OverallGrid/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next'
 import { memo } from 'react'
 import Chart from '../Chart'
 import PieChart from '../PieChart'
@@ -8,35 +9,39 @@ import { OverallGridProps } from './types'
 const OverallGrid = ({
   statisticsData,
   ...props
-}: OverallGridProps): JSX.Element => (
-  <S.Wrapper {...props}>
-    <S.PageTitle>Tibia Bazaar statistics data and analytics</S.PageTitle>
-    <S.ChartWrapper>
-      <Chart
-        totalLabel="Total volume"
-        yesterdayLabel="Yesterday's volume"
-        tooltipLabel="Tibia Coins volume"
-        chartData={statisticsData.totalTibiaCoins}
-      />
-      <Chart
-        totalLabel="Cipsoft's total revenue"
-        yesterdayLabel="Yesterday's revenue"
-        tooltipLabel="Cipsoft revenue"
-        chartData={statisticsData.totalRevenue}
-      />
-    </S.ChartWrapper>
-    <S.ItemWrapper>
-      <PercentageCard
-        title="Auction success rate"
-        /* @ ToDo: change this assertion after backend refactor */
-        percentage={statisticsData.successRate as unknown as number}
-      />
-      <PieChart
-        title="Vocation distribution"
-        pieDataSet={statisticsData.vocationPercentage}
-      />
-    </S.ItemWrapper>
-  </S.Wrapper>
-)
+}: OverallGridProps): JSX.Element => {
+  const { t } = useTranslation('statistics')
+
+  return (
+    <S.Wrapper {...props}>
+      <S.PageTitle>{t('OverallGrid.title')}</S.PageTitle>
+      <S.ChartWrapper>
+        <Chart
+          totalLabel={t('OverallGrid.Chart1.totalLabel')}
+          yesterdayLabel={t('OverallGrid.Chart1.yesterdayLabel')}
+          tooltipLabel={t('OverallGrid.Chart1.tooltipLabel')}
+          chartData={statisticsData.totalTibiaCoins}
+        />
+        <Chart
+          totalLabel={t('OverallGrid.Chart2.totalLabel')}
+          yesterdayLabel={t('OverallGrid.Chart2.yesterdayLabel')}
+          tooltipLabel={t('OverallGrid.Chart2.tooltipLabel')}
+          chartData={statisticsData.totalRevenue}
+        />
+      </S.ChartWrapper>
+      <S.ItemWrapper>
+        <PercentageCard
+          title={t('OverallGrid.PercentageCard1.title')}
+          /* @ ToDo: change this assertion after backend refactor */
+          percentage={statisticsData.successRate as unknown as number}
+        />
+        <PieChart
+          title={t('OverallGrid.PieChart1.title')}
+          pieDataSet={statisticsData.vocationPercentage}
+        />
+      </S.ItemWrapper>
+    </S.Wrapper>
+  )
+}
 
 export default memo(OverallGrid)
