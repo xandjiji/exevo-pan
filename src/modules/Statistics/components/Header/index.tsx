95220b185909c34129a720ed9f54e772ce0c6413
xandjiji <xandjiji@gmail.com>
2021-08-25 21:30:28 -0300

    feature(i18n): translating Header component

--- src/modules/Statistics/components/Header/index.tsx
+++ src/modules/Statistics/components/Header/index.tsx
@@ -1,29 +1,34 @@
+import { useTranslation } from 'next-i18next'
 import { memo } from 'react'
 import { Link } from 'components/Atoms'
 import { routes } from 'Constants'
 import * as S from './styles'
 
-const Header = (): JSX.Element => (
-  <S.Nav>
-    <S.Ul>
-      <S.Li>
-        <Link href={routes.STATISTICS} exact>
-          <S.A>
-            <S.OverallIcon />
-            <S.H3>Overall</S.H3>
-          </S.A>
-        </Link>
-      </S.Li>
-      <S.Li>
-        <Link href={routes.HIGHSCORES} exact>
-          <S.A>
-            <S.HighscoresIcon />
-            <S.H3>Highscores</S.H3>
-          </S.A>
-        </Link>
-      </S.Li>
-    </S.Ul>
-  </S.Nav>
-)
+const Header = (): JSX.Element => {
+  const { t } = useTranslation('statistics')
+
+  return (
+    <S.Nav>
+      <S.Ul>
+        <S.Li>
+          <Link href={routes.STATISTICS} exact>
+            <S.A>
+              <S.OverallIcon />
+              <S.H3>{t('Header.Overall')}</S.H3>
+            </S.A>
+          </Link>
+        </S.Li>
+        <S.Li>
+          <Link href={routes.HIGHSCORES} exact>
+            <S.A>
+              <S.HighscoresIcon />
+              <S.H3>{t('Header.Highscores')}</S.H3>
+            </S.A>
+          </Link>
+        </S.Li>
+      </S.Ul>
+    </S.Nav>
+  )
+}
 
 export default memo(Header)
