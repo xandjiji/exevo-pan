23e9edbe505d9b488e91dc54baf100ddddccfc21
xandjiji <xandjiji@gmail.com>
2021-08-25 21:46:25 -0300

    feature(i18n): translating List component

--- src/modules/Statistics/components/List/index.tsx
+++ src/modules/Statistics/components/List/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next'
 import { memo } from 'react'
 import * as S from './styles'
 import { ListProps } from './types'
@@ -8,46 +9,52 @@ const List = ({
   displayedDataKey,
   format,
   ...props
-}: ListProps): JSX.Element => (
-  <S.Wrapper {...props}>
-    <S.CardHead>
-      Top 10
-      <S.Title>{title}</S.Title>
-    </S.CardHead>
+}: ListProps): JSX.Element => {
+  const { t } = useTranslation('statistics')
 
-    <S.Table>
-      <S.Caption>{`Top 10 ${title} players ranking`}</S.Caption>
-      <S.TableHead>
-        <tr>
-          <S.HeadItem aria-label="Ranking position">#</S.HeadItem>
-          <S.HeadItem>Nickname</S.HeadItem>
-          <S.HeadItem>{title}</S.HeadItem>
-        </tr>
-      </S.TableHead>
+  return (
+    <S.Wrapper {...props}>
+      <S.CardHead>
+        Top 10
+        <S.Title>{title}</S.Title>
+      </S.CardHead>
 
-      <tbody>
-        {charactersList.map((character, index) => (
-          <tr key={character.id}>
-            <S.RowItem>{index + 1}</S.RowItem>
-            <S.RowItem>
-              <S.Link
-                href={`https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades&page=details&auctionid=${character.id}&source=overview`}
-                target="_blank"
-                rel="noreferrer noopener external"
-              >
-                {character.nickname}
-              </S.Link>
-            </S.RowItem>
-            <S.RowItem>
-              {format
-                ? format(character[displayedDataKey] as never)
-                : character[displayedDataKey]}
-            </S.RowItem>
+      <S.Table>
+        <S.Caption>{`${t('List.captionTop10')} ${title} ${t(
+          'List.captionDescription',
+        )}`}</S.Caption>
+        <S.TableHead>
+          <tr>
+            <S.HeadItem aria-label={t('List.titleLabel')}>#</S.HeadItem>
+            <S.HeadItem>Nickname</S.HeadItem>
+            <S.HeadItem>{title}</S.HeadItem>
           </tr>
-        ))}
-      </tbody>
-    </S.Table>
-  </S.Wrapper>
-)
+        </S.TableHead>
+
+        <tbody>
+          {charactersList.map((character, index) => (
+            <tr key={character.id}>
+              <S.RowItem>{index + 1}</S.RowItem>
+              <S.RowItem>
+                <S.Link
+                  href={`https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades&page=details&auctionid=${character.id}&source=overview`}
+                  target="_blank"
+                  rel="noreferrer noopener external"
+                >
+                  {character.nickname}
+                </S.Link>
+              </S.RowItem>
+              <S.RowItem>
+                {format
+                  ? format(character[displayedDataKey] as never)
+                  : character[displayedDataKey]}
+              </S.RowItem>
+            </tr>
+          ))}
+        </tbody>
+      </S.Table>
+    </S.Wrapper>
+  )
+}
 
 export default memo(List)
