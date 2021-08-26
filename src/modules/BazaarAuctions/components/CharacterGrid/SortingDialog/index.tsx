1d2c6cf9ce5c5d61f18d09ebbad341926fcf43f5
xandjiji <xandjiji@gmail.com>
2021-08-26 20:49:17 -0300

    feature(i18n): SortingDialog

--- src/modules/BazaarAuctions/components/CharacterGrid/SortingDialog/index.tsx
+++ src/modules/BazaarAuctions/components/CharacterGrid/SortingDialog/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next'
 import { memo } from 'react'
 import { Switch, RadioButton } from 'components/Atoms'
 import { Tooltip } from 'components/Organisms'
@@ -5,6 +6,12 @@ import * as S from './styles'
 import { SortingDialogProps } from './types'
 
 const sortModes = ['Auction end', 'Level', 'Price', 'Price (bidded only)']
+const sortModesTranslationKey = {
+  'Auction end': 'auctionEnd',
+  Level: 'level',
+  Price: 'price',
+  'Price (bidded only)': 'priceBidded',
+} as Record<string, string>
 
 const SortingDialog = ({
   sortMode,
@@ -12,36 +19,40 @@ const SortingDialog = ({
   descendingOrder,
   setDescendingOrder,
   ...props
-}: SortingDialogProps): JSX.Element => (
-  <Tooltip
-    role="dialog"
-    trigger="click"
-    content={
-      <S.Dialog {...props}>
-        <Switch
-          active={descendingOrder}
-          onClick={() => setDescendingOrder((prev) => !prev)}
-          aria-label="Sort by descending order"
-        >
-          Descending
-        </Switch>
-        {sortModes.map((mode, index) => (
-          <RadioButton
-            key={mode}
-            onClick={() => setSortMode(index)}
-            active={sortMode === index}
+}: SortingDialogProps): JSX.Element => {
+  const { t } = useTranslation('homepage')
+
+  return (
+    <Tooltip
+      role="dialog"
+      trigger="click"
+      content={
+        <S.Dialog {...props}>
+          <Switch
+            active={descendingOrder}
+            onClick={() => setDescendingOrder((prev) => !prev)}
+            aria-label={t('CharacterGrid.descendingSwitchLabel')}
           >
-            {mode}
-          </RadioButton>
-        ))}
-      </S.Dialog>
-    }
-  >
-    <S.SortIcon
-      aria-label="Set the sorting order and criteria"
-      aria-haspopup="dialog"
-    />
-  </Tooltip>
-)
+            {t('CharacterGrid.descending')}
+          </Switch>
+          {sortModes.map((mode, index) => (
+            <RadioButton
+              key={mode}
+              onClick={() => setSortMode(index)}
+              active={sortMode === index}
+            >
+              {t(`CharacterGrid.sortModes.${sortModesTranslationKey[mode]}`)}
+            </RadioButton>
+          ))}
+        </S.Dialog>
+      }
+    >
+      <S.SortIcon
+        aria-label={t('CharacterGrid.sortingButtonLabel')}
+        aria-haspopup="dialog"
+      />
+    </Tooltip>
+  )
+}
 
 export default memo(SortingDialog)
