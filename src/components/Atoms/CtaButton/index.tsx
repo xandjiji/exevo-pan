f853ed1ee324d98dc71e58e8558d70a28bf76aa8
xandjiji <xandjiji@gmail.com>
2021-08-26 21:31:55 -0300

    feature(i18n): CtaButton

--- src/components/Atoms/CtaButton/index.tsx
+++ src/components/Atoms/CtaButton/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import coinsGif from 'assets/tibiaCoins.gif'
 import { links } from 'Constants'
@@ -5,18 +6,28 @@ import * as S from './styles'
 
 const CtaButton = ({
   ...props
-}: React.HTMLAttributes<HTMLAnchorElement>): JSX.Element => (
-  <S.Button
-    href={links.COINS_REF}
-    target="_blank"
-    rel="noopener external nofollow"
-    {...props}
-  >
-    <S.ImgWrapper>
-      <Image src={coinsGif} priority alt="Tibia Coins" width="24" height="24" />
-    </S.ImgWrapper>
-    Buy Tibia Coins
-  </S.Button>
-)
+}: React.HTMLAttributes<HTMLAnchorElement>): JSX.Element => {
+  const { t } = useTranslation('common')
+
+  return (
+    <S.Button
+      href={links.COINS_REF}
+      target="_blank"
+      rel="noopener external nofollow"
+      {...props}
+    >
+      <S.ImgWrapper>
+        <Image
+          src={coinsGif}
+          priority
+          alt="Tibia Coins"
+          width="24"
+          height="24"
+        />
+      </S.ImgWrapper>
+      {t('TibiaCoinsCta')}
+    </S.Button>
+  )
+}
 
 export default CtaButton
