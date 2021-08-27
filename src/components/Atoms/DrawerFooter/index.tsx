868747f907e9a6f107b397368c86499b404f6990
xandjiji <xandjiji@gmail.com>
2021-08-26 21:37:09 -0300

    feature(i18n): DrawerFooter

--- src/components/Atoms/DrawerFooter/index.tsx
+++ src/components/Atoms/DrawerFooter/index.tsx
@@ -1,28 +1,33 @@
+import { useTranslation } from 'next-i18next'
 import { memo } from 'react'
 import { links } from 'Constants'
 import * as S from './styles'
 
 const DrawerFooter = ({
   ...props
-}: React.HTMLAttributes<HTMLDivElement>): JSX.Element => (
-  <S.Wrapper {...props}>
-    <S.RepositoryLink
-      href={links.GITHUB_REPOSITORY}
-      target="_blank"
-      rel="noreferrer noopener external"
-    >
-      <S.GithubIcon />
-      Go to this repository
-    </S.RepositoryLink>
-    made by
-    <S.ProfileLink
-      href={links.GITHUB_PROFILE}
-      target="_blank"
-      rel="noopener noreferrer author external"
-    >
-      xandjiji
-    </S.ProfileLink>
-  </S.Wrapper>
-)
+}: React.HTMLAttributes<HTMLDivElement>): JSX.Element => {
+  const { t } = useTranslation('common')
+
+  return (
+    <S.Wrapper {...props}>
+      <S.RepositoryLink
+        href={links.GITHUB_REPOSITORY}
+        target="_blank"
+        rel="noreferrer noopener external"
+      >
+        <S.GithubIcon />
+        {t('RepoLinkText')}
+      </S.RepositoryLink>
+      {t('MadeBy')}
+      <S.ProfileLink
+        href={links.GITHUB_PROFILE}
+        target="_blank"
+        rel="noopener noreferrer author external"
+      >
+        xandjiji
+      </S.ProfileLink>
+    </S.Wrapper>
+  )
+}
 
 export default memo(DrawerFooter)
