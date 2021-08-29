1e1218e8fe2d8c5c40cea36b37c891ecc295a69b
xandjiji <xandjiji@gmail.com>
2021-08-29 09:36:04 -0300

    feature(Constants): adding libertabra-war route

--- src/Constants/Routes.ts
+++ src/Constants/Routes.ts
@@ -3,5 +3,6 @@ export const routes = {
   BAZAAR_HISTORY: '/bazaar-history',
   STATISTICS: '/statistics',
   HIGHSCORES: '/statistics/highscores',
+  LIBERTABRA_WAR: '/libertabra-war',
   ABOUT: '/about',
 }
