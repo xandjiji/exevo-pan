112a82262bc36053150f5020d378d23adea5866a
xandjiji <xandjiji@gmail.com>
2021-09-01 15:04:24 -0300

    feature(LibertabraWar): registering GuildXP route

--- src/modules/LibertabraWar/components/Header/index.tsx
+++ src/modules/LibertabraWar/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { SubHeader } from 'templates'
 import { routes } from 'Constants'
 import OverallIcon from 'assets/svgs/charts.svg'
 import HighscoresIcon from 'assets/svgs/trophy.svg'
+import GuildXPIcon from 'assets/svgs/group.svg'
 
 const navItems = [
   { title: 'Overall', href: routes.LIBERTABRA_WAR, icon: <OverallIcon /> },
@@ -9,6 +10,11 @@ const navItems = [
     title: 'Top 10',
     href: routes.LIBERTABRA_WAR_TOP_10,
     icon: <HighscoresIcon />,
+  },
+  {
+    title: 'Guild XP',
+    href: routes.LIBERTABRA_WAR_GUILD_XP,
+    icon: <GuildXPIcon />,
   },
 ]
 
