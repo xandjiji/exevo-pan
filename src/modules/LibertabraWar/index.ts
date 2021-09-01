99092aa4f1946fd9b0a1c6743734c9a08a9aade9
xandjiji <xandjiji@gmail.com>
2021-09-01 15:18:54 -0300

    feature(pages): Guild XP

--- src/modules/LibertabraWar/index.ts
+++ src/modules/LibertabraWar/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './components/Header'
 export { default as OverallGrid } from './components/OverallGrid'
 export { default as Top10Grid } from './components/Top10Grid'
+export { default as GuildXPGrid } from './components/GuildXPGrid'
---
+++ src/pages/libertabra-war/guild-xp.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import { Main } from 'templates'
+import { Header, GuildXPGrid } from 'modules/LibertabraWar'
+import { endpoints, paths } from 'Constants'
+
+export default function LibertabraWar(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Exevo Pan - Libertabra War</title>
+        <meta name="title" content="Exevo Pan - Libertabra War" />
+        <meta property="og:site_name" content="Exevo Pan - Libertabra War" />
+        <meta property="og:title" content="Exevo Pan - Libertabra War" />
+        <meta property="twitter:title" content="Exevo Pan - Libertabra War" />
+
+        <meta
+          name="description"
+          content="Follow live data and statistics from Libertabra War!"
+        />
+        <meta
+          property="twitter:description"
+          content="Follow live data and statistics from Libertabra War!"
+        />
+        <meta
+          property="og:description"
+          content="Follow live data and statistics from Libertabra War!"
+        />
+        <meta property="og:type" content="website" />
+
+        <link
+          rel="preload"
+          href={`${endpoints.WAR_DATA}${paths.WAR_STATISTICS}`}
+          as="fetch"
+          crossOrigin="anonymous"
+        />
+      </Head>
+
+      <Main>
+        <main>
+          <Header />
+          <GuildXPGrid />
+        </main>
+      </Main>
+    </div>
+  )
+}
