0918756305d68175fafed9a3eb1cb288d515bd52
xandjiji <xandjiji@gmail.com>
2021-09-01 15:01:19 -0300

    feature(Constants): adding LIBERTABRA_WAR_GUILD_XP

--- src/Constants/Routes.ts
+++ src/Constants/Routes.ts
@@ -5,5 +5,6 @@ export const routes = {
   HIGHSCORES: '/statistics/highscores',
   LIBERTABRA_WAR: '/libertabra-war',
   LIBERTABRA_WAR_TOP_10: '/libertabra-war/top-10',
+  LIBERTABRA_WAR_GUILD_XP: '/libertabra-war/guild-xp',
   ABOUT: '/about',
 }
