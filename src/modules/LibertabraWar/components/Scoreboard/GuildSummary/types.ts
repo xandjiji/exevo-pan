66543ef94a21a77571e18b1b7fe403c773af92c9
xandjiji <xandjiji@gmail.com>
2021-09-01 14:56:33 -0300

    chore: removing unused code

--- src/modules/LibertabraWar/components/Scoreboard/GuildSummary/types.ts
+++
@@ -1,8 +0,0 @@
-export interface GuildSummaryProps
-  extends React.HTMLAttributes<HTMLDivElement> {
-  guildName: string
-  href: string
-  totalKills: number
-  diff: number
-  winning: boolean
-}
