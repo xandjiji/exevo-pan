94ef8794f0debce62b88d657258e8775d9ed6ad4
xandjiji <xandjiji@gmail.com>
2021-09-07 19:05:26 -0300

    feature(SearchGrid): EmptyState

---
+++ src/modules/LibertabraWar/components/SearchGrid/MembersTable/EmptyState/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+import Image from 'next/image'
+import NotFoundImage from 'assets/notFound.png'
+
+export const Wrapper = styled.div`
+  margin-top: 16px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`
+
+export const Text = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+
+  font-size: 32px;
+  font-weight: 400;
+  color: var(--onSurface);
+  text-align: center;
+`
+
+export const NotFound = styled(Image).attrs({
+  src: NotFoundImage,
+  alt: 'No character was found',
+})`
+  filter: grayscale(0.5);
+  opacity: 0.15;
+`
