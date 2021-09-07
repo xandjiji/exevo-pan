8b53a902967c1c31676315f4e2c641efca1abcd5
xandjiji <xandjiji@gmail.com>
2021-09-07 09:09:53 -0300

    feature(LibertabraWar): adding search members to Header

--- src/modules/LibertabraWar/components/Header/index.tsx
+++ src/modules/LibertabraWar/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { routes } from 'Constants'
 import OverallIcon from 'assets/svgs/charts.svg'
 import HighscoresIcon from 'assets/svgs/trophy.svg'
 import GuildXPIcon from 'assets/svgs/group.svg'
+import SearchMembersIcon from 'assets/svgs/search.svg'
 
 const navItems = [
   { title: 'Overall', href: routes.LIBERTABRA_WAR, icon: <OverallIcon /> },
@@ -15,6 +16,11 @@ const navItems = [
     title: 'Guild XP',
     href: routes.LIBERTABRA_WAR_GUILD_XP,
     icon: <GuildXPIcon />,
+  },
+  {
+    title: 'Search Members',
+    href: routes.LIBERTABRA_WAR_SEARCH,
+    icon: <SearchMembersIcon />,
   },
 ]
 
