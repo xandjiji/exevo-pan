b2d940ef242d5fdda4c24b17a0d351998038904e
xandjiji <xandjiji@gmail.com>
2021-09-10 11:07:08 -0300

    test(CharacterInfoColumn): adding tests

---
+++ src/modules/LibertabraWar/components/CharacterInfoColumn/__tests__/index.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
+import CharacterInfoColumn from '..'
+
+describe('<CharacterInfoColumn />', () => {
+  test('should render all content correctly', () => {
+    renderWithProviders(
+      <CharacterInfoColumn
+        nickname="Master Ksu"
+        level={400}
+        vocation="Master Sorcerer"
+      />,
+    )
+
+    const linkElement = screen.getByRole('link')
+    expect(linkElement).toHaveTextContent('Master Ksu')
+    expect(linkElement).toHaveAttribute(
+      'href',
+      'https://www.tibia.com/community/?name=Master%20Ksu',
+    )
+    expect(screen.getByText('Level 400 - Master Sorcerer')).toBeInTheDocument()
+  })
+})
