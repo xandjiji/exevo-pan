c8e8024c1b7519d94fb99f9dfe74de7190abc4eb
xandjiji <xandjiji@gmail.com>
2021-09-10 08:00:14 -0300

    feature(Main): removing ads

--- src/templates/Main/index.tsx
+++ src/templates/Main/index.tsx
@@ -1,14 +1,10 @@
 import Header from './Header'
-import KwaiVerticalBanner from './KwaiVerticalBanner'
-import InPagePush from './InPagePush'
 import * as S from './styles'
 import { MainProps } from './types'
 
 const MasterLayout = ({ children, ...props }: MainProps): JSX.Element => (
   <S.Wrapper {...props}>
     <Header />
-    <KwaiVerticalBanner />
-    <InPagePush />
     {children}
   </S.Wrapper>
 )
