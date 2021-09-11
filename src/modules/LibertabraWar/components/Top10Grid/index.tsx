db67680ce3888005c0601450c4c8a6b604cca31f
xandjiji <xandjiji@gmail.com>
2021-09-11 18:17:54 -0300

    feature(Top10Grid): adding captions to table elements

--- src/modules/LibertabraWar/components/Top10Grid/index.tsx
+++ src/modules/LibertabraWar/components/Top10Grid/index.tsx
@@ -6,22 +6,26 @@ const Top10Grid = ({ warData }: Top10GridProps): JSX.Element => (
     <S.KillsTable
       title="Most kills ⚔️"
       subtitle="Libertabra Pune"
+      caption="Libertabra Pune members with the most kills"
       characterList={warData.top10Kills.guildA}
     />
     <S.KillsTable
       title="Most kills ⚔️"
       subtitle="Bones Alliance"
+      caption="Bones Alliance members with the most kills"
       characterList={warData.top10Kills.guildB}
     />
 
     <S.DeathsTable
       title="Most deaths 💀"
       subtitle="Libertabra Pune"
+      caption="Libertabra Pune members with the most deaths"
       characterList={warData.top10Deaths.guildA}
     />
     <S.DeathsTable
       title="Most deaths 💀"
       subtitle="Bones Alliance"
+      caption="Bones Alliance members with the most deaths"
       characterList={warData.top10Deaths.guildB}
     />
   </S.Wrapper>
