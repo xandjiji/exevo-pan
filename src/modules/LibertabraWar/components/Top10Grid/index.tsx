9bf8224281c22736cf06977d0b5bff33c1ec892f
xandjiji <xandjiji@gmail.com>
2021-09-12 11:31:04 -0300

    feature(i18n): translating Top10Grid

--- src/modules/LibertabraWar/components/Top10Grid/index.tsx
+++ src/modules/LibertabraWar/components/Top10Grid/index.tsx
@@ -1,34 +1,39 @@
+import { useTranslation } from 'next-i18next'
 import * as S from './styles'
 import { Top10GridProps } from './types'
 
-const Top10Grid = ({ warData }: Top10GridProps): JSX.Element => (
-  <S.Wrapper>
-    <S.KillsTable
-      title="Most kills ⚔️"
-      subtitle="Libertabra Pune"
-      caption="Libertabra Pune members with the most kills"
-      characterList={warData.top10Kills.guildA}
-    />
-    <S.KillsTable
-      title="Most kills ⚔️"
-      subtitle="Bones Alliance"
-      caption="Bones Alliance members with the most kills"
-      characterList={warData.top10Kills.guildB}
-    />
+const Top10Grid = ({ warData }: Top10GridProps): JSX.Element => {
+  const { t } = useTranslation('war')
 
-    <S.DeathsTable
-      title="Most deaths 💀"
-      subtitle="Libertabra Pune"
-      caption="Libertabra Pune members with the most deaths"
-      characterList={warData.top10Deaths.guildA}
-    />
-    <S.DeathsTable
-      title="Most deaths 💀"
-      subtitle="Bones Alliance"
-      caption="Bones Alliance members with the most deaths"
-      characterList={warData.top10Deaths.guildB}
-    />
-  </S.Wrapper>
-)
+  return (
+    <S.Wrapper>
+      <S.KillsTable
+        title={`${t('Top10Grid.mostKillsTitle')} ⚔️`}
+        subtitle="Libertabra Pune"
+        caption={`Libertabra Pune ${t('Top10Grid.mostKillsCaptionSuffix')}`}
+        characterList={warData.top10Kills.guildA}
+      />
+      <S.KillsTable
+        title={`${t('Top10Grid.mostKillsTitle')} ⚔️`}
+        subtitle="Bones Alliance"
+        caption={`Bones Alliance ${t('Top10Grid.mostKillsCaptionSuffix')}`}
+        characterList={warData.top10Kills.guildB}
+      />
+
+      <S.DeathsTable
+        title={`${t('Top10Grid.mostDeathsTitle')} 💀`}
+        subtitle="Libertabra Pune"
+        caption={`Libertabra Pune ${t('Top10Grid.mostDeathsCaptionSuffix')}`}
+        characterList={warData.top10Deaths.guildA}
+      />
+      <S.DeathsTable
+        title={`${t('Top10Grid.mostDeathsTitle')} 💀`}
+        subtitle="Bones Alliance"
+        caption={`Bones Alliance ${t('Top10Grid.mostDeathsCaptionSuffix')}`}
+        characterList={warData.top10Deaths.guildB}
+      />
+    </S.Wrapper>
+  )
+}
 
 export default Top10Grid
