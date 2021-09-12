4faf72f1eca2a5a59f6d3e9a5ceeedd4bca683ad
xandjiji <xandjiji@gmail.com>
2021-09-12 10:47:33 -0300

    feature(i18n): translating Header

--- src/modules/LibertabraWar/components/Header/index.tsx
+++ src/modules/LibertabraWar/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next'
 import { SubHeader } from 'templates'
 import { routes } from 'Constants'
 import OverallIcon from 'assets/svgs/charts.svg'
@@ -5,25 +6,33 @@ import HighscoresIcon from 'assets/svgs/trophy.svg'
 import GuildXPIcon from 'assets/svgs/group.svg'
 import SearchMembersIcon from 'assets/svgs/search.svg'
 
-const navItems = [
-  { title: 'Overall', href: routes.LIBERTABRA_WAR, icon: <OverallIcon /> },
-  {
-    title: 'Top 10',
-    href: routes.LIBERTABRA_WAR_TOP_10,
-    icon: <HighscoresIcon />,
-  },
-  {
-    title: 'Guild XP',
-    href: routes.LIBERTABRA_WAR_GUILD_XP,
-    icon: <GuildXPIcon />,
-  },
-  {
-    title: 'Search Members',
-    href: routes.LIBERTABRA_WAR_SEARCH,
-    icon: <SearchMembersIcon />,
-  },
-]
+const Header = (): JSX.Element => {
+  const { t } = useTranslation('war')
 
-const Header = (): JSX.Element => <SubHeader navItems={navItems} />
+  const navItems = [
+    {
+      title: t('Header.Overall'),
+      href: routes.LIBERTABRA_WAR,
+      icon: <OverallIcon />,
+    },
+    {
+      title: t('Header.Top10'),
+      href: routes.LIBERTABRA_WAR_TOP_10,
+      icon: <HighscoresIcon />,
+    },
+    {
+      title: t('Header.GuildXP'),
+      href: routes.LIBERTABRA_WAR_GUILD_XP,
+      icon: <GuildXPIcon />,
+    },
+    {
+      title: t('Header.Search'),
+      href: routes.LIBERTABRA_WAR_SEARCH,
+      icon: <SearchMembersIcon />,
+    },
+  ]
+
+  return <SubHeader navItems={navItems} />
+}
 
 export default Header
