1ffe0265faa89cb0080523af8082dc9c474c2010
xandjiji <xandjiji@gmail.com>
2021-09-12 10:55:31 -0300

    feature(i18n): translating LastFrags

--- src/modules/LibertabraWar/components/OverallGrid/LastFrags/utils.ts
+++ src/modules/LibertabraWar/components/OverallGrid/LastFrags/utils.ts
@@ -1,6 +1,6 @@
 const MILLISECONDS_IN_A_MINUTE = 60000
 
-export const getTimeDiff = (timeStamp: number): string => {
+export const getTimeDiff = (timeStamp: number): number => {
   const timeStampDiff = +new Date() - timeStamp
-  return `${Math.trunc(timeStampDiff / MILLISECONDS_IN_A_MINUTE)} minutes ago`
+  return Math.trunc(timeStampDiff / MILLISECONDS_IN_A_MINUTE)
 }
