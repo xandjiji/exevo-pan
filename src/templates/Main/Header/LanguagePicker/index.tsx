7cfe08043b08777b74c9cc4d7b9c5f9b559f243e
xandjiji <xandjiji@gmail.com>
2021-09-13 11:10:08 -0300

    feature(LanguagePicker): positioning

--- src/templates/Main/Header/LanguagePicker/index.tsx
+++ src/templates/Main/Header/LanguagePicker/index.tsx
@@ -1,30 +1,54 @@
+import { useTranslation } from 'next-i18next'
+import { memo, useState, useRef } from 'react'
 import { useRouter } from 'next/router'
 import { RadioButton } from 'components/Atoms'
 import * as S from './styles'
 
 const LanguagePicker = (): JSX.Element => {
+  const { t } = useTranslation('common')
+
   const { locale, push, pathname } = useRouter()
 
-  const handleLocaleSelect = (selectedLocale: 'en' | 'pt') =>
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleLocaleSelect = (selectedLocale: 'en' | 'pt') => {
     push(pathname, pathname, { locale: selectedLocale })
+    setIsVisible(false)
+  }
+
+  const wrapperRef = useRef<HTMLDivElement | null>()
+  const absoluteWrapperPosition =
+    wrapperRef.current?.getBoundingClientRect().right
 
   return (
-    <S.Wrapper>
-      <S.LanguageIcon aria-label="Select preferred language" />
-      <S.FixedWrapper>
-        <S.Picker>
-          <S.Item onClick={() => handleLocaleSelect('en')}>
-            <RadioButton active={locale === 'en'} />
-            English
-          </S.Item>
-          <S.Item onClick={() => handleLocaleSelect('pt')}>
-            <RadioButton active={locale === 'pt'} />
-            Português
-          </S.Item>
-        </S.Picker>
-      </S.FixedWrapper>
+    <S.Wrapper ref={wrapperRef as React.RefObject<HTMLDivElement>}>
+      <S.LanguageIcon
+        aria-label={t('PreferredLanguageLabel')}
+        onClick={() => setIsVisible(true)}
+      />
+      <S.Picker
+        role="dialog"
+        aria-hidden={!isVisible}
+        style={{
+          left: absoluteWrapperPosition ? absoluteWrapperPosition - 12 : '100%',
+        }}
+      >
+        <S.Item onClick={() => handleLocaleSelect('en')}>
+          <RadioButton active={locale === 'en'} />
+          English
+        </S.Item>
+        <S.Item onClick={() => handleLocaleSelect('pt')}>
+          <RadioButton active={locale === 'pt'} />
+          Português
+        </S.Item>
+      </S.Picker>
+      <S.Backdrop
+        aria-label={t('PopoverCloseLabel')}
+        aria-hidden={!isVisible}
+        onClick={() => setIsVisible(false)}
+      />
     </S.Wrapper>
   )
 }
 
-export default LanguagePicker
+export default memo(LanguagePicker)
--- src/templates/Main/Header/LanguagePicker/styles.ts
+++ src/templates/Main/Header/LanguagePicker/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import LanguageSvg from 'assets/svgs/language.svg'
-import { Clickable, MaterialCard, Smooth } from 'styles'
+import { Clickable, MaterialCard } from 'styles'
+
+export const Wrapper = styled.div`
+  height: 24px;
+`
 
 export const LanguageIcon = styled(LanguageSvg)`
   border-radius: 50%;
@@ -8,30 +12,31 @@ export const LanguageIcon = styled(LanguageSvg)`
   ${Clickable}
 `
 
-export const FixedWrapper = styled.div`
-  position: fixed;
-  z-index: 10;
-  transform: translate(calc(-50% + 12px), -50%);
-  height: 10px;
-  width: 100px;
-  ${Smooth}
-`
+const visibilityStyle = css`
+  transition: opacity 0.2s ease-out;
 
-export const Wrapper = styled.div`
-  position: relative;
-  height: 24px;
-
-  :not(:hover) ${FixedWrapper} {
+  &[aria-hidden='true'] {
     opacity: 0;
     pointer-events: none;
   }
 `
 
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(0, 0, 0, 0.1);
+
+  ${visibilityStyle}
+`
+
 export const Picker = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 50%;
-  transform: translateX(-50%);
+  position: fixed;
+  transform: translate(-50%, 6px);
   z-index: 10;
 
   ${MaterialCard}
@@ -40,6 +45,8 @@ export const Picker = styled.div`
   > *:not(:last-child) {
     margin-bottom: 6px;
   }
+
+  ${visibilityStyle}
 `
 
 export const Item = styled.div`
