7ffc6f0c8dc36a15dcd72dcc42686c1956866aa9
xandjiji <xandjiji@gmail.com>
2021-09-14 11:23:36 -0300

    test(i18n): fixing test

--- src/modules/BazaarAuctions/components/FilterDrawer/__tests__/index.tsx
+++ src/modules/BazaarAuctions/components/FilterDrawer/__tests__/index.tsx
@@ -43,7 +43,7 @@ describe('<FilterDrawer />', () => {
     renderWithProviders(<FilterDrawer {...defaultArgs} />)
 
     expect(mockOnClose).toHaveBeenCalledTimes(0)
-    userEvent.click(screen.getByLabelText('Close drawer'))
+    userEvent.click(screen.getByLabelText('CloseDrawerLabel'))
     expect(mockOnClose).toHaveBeenCalledTimes(1)
   })
 
@@ -61,15 +61,18 @@ describe('<FilterDrawer />', () => {
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(4)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(1)
 
-    userEvent.type(screen.getByLabelText('Search nickname'), 'K')
+    userEvent.type(
+      screen.getByLabelText('FilterDrawer.searchNicknameLabel'),
+      'K',
+    )
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(5)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(2)
 
-    userEvent.click(screen.getByText('Rare nicknames'))
+    userEvent.click(screen.getByText('FilterDrawer.rareNicknamesButton'))
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(6)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(3)
 
-    userEvent.click(screen.getByText('Rare nicknames'))
+    userEvent.click(screen.getByText('FilterDrawer.rareNicknamesButton'))
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(7)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(2)
 
@@ -81,7 +84,9 @@ describe('<FilterDrawer />', () => {
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(9)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(1)
 
-    userEvent.click(screen.getByPlaceholderText('Select imbuements'))
+    userEvent.click(
+      screen.getByPlaceholderText('FilterDrawer.imbuementsPlaceholder'),
+    )
     userEvent.click(screen.getByRole('option', { name: 'Critical Hit' }))
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(10)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(2)
@@ -90,22 +95,24 @@ describe('<FilterDrawer />', () => {
   test('autocompleteInputs should control its chips/options correctly', () => {
     renderWithProviders(<FilterDrawer {...defaultArgs} />)
 
-    const inputElement = screen.getByPlaceholderText('Select imbuements')
+    const inputElement = screen.getByPlaceholderText(
+      'FilterDrawer.imbuementsPlaceholder',
+    )
 
     userEvent.click(inputElement)
     userEvent.click(screen.getByRole('option', { name: 'Critical Hit' }))
     expect(
       screen.queryByRole('option', { name: 'Critical Hit' }),
     ).not.toBeInTheDocument()
-    expect(screen.getByLabelText('Remove item')).toBeInTheDocument()
+    expect(screen.getByLabelText('RemoveItem')).toBeInTheDocument()
 
     userEvent.click(inputElement)
     expect(
       screen.queryByRole('option', { name: 'Critical Hit' }),
     ).not.toBeInTheDocument()
 
-    userEvent.click(screen.getByLabelText('Remove item'))
-    expect(screen.queryByLabelText('Remove item')).not.toBeInTheDocument()
+    userEvent.click(screen.getByLabelText('RemoveItem'))
+    expect(screen.queryByLabelText('RemoveItem')).not.toBeInTheDocument()
     userEvent.click(inputElement)
     expect(
       screen.getByRole('option', { name: 'Critical Hit' }),
@@ -115,40 +122,46 @@ describe('<FilterDrawer />', () => {
   test('toggle all items/imbuements should control filters correctly', () => {
     renderWithProviders(<FilterDrawer {...defaultArgs} />)
 
-    const inputElement = screen.getByPlaceholderText('Select imbuements')
+    const inputElement = screen.getByPlaceholderText(
+      'FilterDrawer.imbuementsPlaceholder',
+    )
     userEvent.click(inputElement)
     expect(screen.queryAllByRole('option')).toHaveLength(23)
-    expect(screen.queryByLabelText('Remove item')).not.toBeInTheDocument()
+    expect(screen.queryByLabelText('RemoveItem')).not.toBeInTheDocument()
 
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(2)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(0)
 
-    const toggleElement = screen.getByRole('switch', { name: 'All imbuements' })
+    const toggleElement = screen.getByRole('switch', {
+      name: 'FilterDrawer.allImbuementsButton',
+    })
     userEvent.click(toggleElement)
     userEvent.click(inputElement)
     expect(screen.queryByRole('option')).not.toBeInTheDocument()
-    expect(screen.queryAllByLabelText('Remove item')).toHaveLength(23)
+    expect(screen.queryAllByLabelText('RemoveItem')).toHaveLength(23)
 
     expect(mockSetActiveFilterCount).toHaveBeenCalledTimes(3)
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(1)
 
-    screen.queryAllByLabelText('Remove item').forEach((element) => {
+    screen.queryAllByLabelText('RemoveItem').forEach((element) => {
       userEvent.click(element)
     })
 
     userEvent.click(inputElement)
     expect(screen.queryAllByRole('option')).toHaveLength(23)
-    expect(screen.queryByLabelText('Remove item')).not.toBeInTheDocument()
+    expect(screen.queryByLabelText('RemoveItem')).not.toBeInTheDocument()
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(0)
   })
 
   test('should reset filters correctly', () => {
     renderWithProviders(<FilterDrawer {...defaultArgs} />)
 
-    const nickInput = screen.getByLabelText('Search nickname')
+    const nickInput = screen.getByLabelText('FilterDrawer.searchNicknameLabel')
     const knightChip = screen.getByText('Knight')
-    const toggleChip = screen.getByRole('switch', { name: 'All imbuements' })
-    const rareNickChip = screen.getByText('Rare nicknames')
+    const toggleChip = screen.getByRole('switch', {
+      name: 'FilterDrawer.allImbuementsButton',
+    })
+    const rareNickChip = screen.getByText('FilterDrawer.rareNicknamesButton')
 
     userEvent.type(nickInput, 'Ksu')
     userEvent.click(knightChip)
@@ -159,7 +172,7 @@ describe('<FilterDrawer />', () => {
     expect(knightChip).toBeChecked()
     expect(toggleChip).toBeChecked()
     expect(rareNickChip).toBeChecked()
-    expect(screen.getAllByLabelText('Remove item')).toHaveLength(23)
+    expect(screen.getAllByLabelText('RemoveItem')).toHaveLength(23)
 
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(4)
 
@@ -169,7 +182,7 @@ describe('<FilterDrawer />', () => {
     expect(knightChip).not.toBeChecked()
     expect(toggleChip).not.toBeChecked()
     expect(rareNickChip).not.toBeChecked()
-    expect(screen.queryByLabelText('Remove item')).not.toBeInTheDocument()
+    expect(screen.queryByLabelText('RemoveItem')).not.toBeInTheDocument()
 
     expect(mockSetActiveFilterCount).toHaveBeenLastCalledWith(0)
   })
