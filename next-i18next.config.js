74ca16ad18357de2951dfc264bd9e69e01131079
xandjiji <xandjiji@gmail.com>
2021-09-14 09:46:49 -0300

    feature(i18n): changing config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,10 +2,6 @@ module.exports = {
   i18n: {
     locales: ['en', 'pt'],
     defaultLocale: 'en',
-    domains: [
-      { domain: 'exevopan.com', defaultLocale: 'en' },
-      { domain: 'exevopan.com.br', defaultLocale: 'pt' },
-    ],
   },
   react: {
     useSuspense: false,
