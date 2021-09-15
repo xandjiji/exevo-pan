a47f762f72ec8d2c2857b9c4fc7990d0fec48253
xandjiji <xandjiji@gmail.com>
2021-09-15 15:48:07 -0300

    feature(locales): adding all locales exports

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,19 +2,42 @@ import errorEN from './en/404'
 import errorES from './es/404'
 import errorPT from './pt/404'
 
+import aboutEN from './en/about'
+import aboutES from './es/about'
+import aboutPT from './pt/about'
+
 import commonEN from './en/common'
 import commonES from './es/common'
 import commonPT from './pt/common'
 
+import bazaarHistoryEN from './en/bazaarHistory'
+import bazaarHistoryES from './es/bazaarHistory'
+import bazaarHistoryPT from './pt/bazaarHistory'
+
 import homepageEN from './en/homepage'
 import homepageES from './es/homepage'
 import homepagePT from './pt/homepage'
+
+import statisticsEN from './en/statistics'
+import statisticsES from './es/statistics'
+import statisticsPT from './pt/statistics'
 
 import warEN from './en/war'
 import warES from './es/war'
 import warPT from './pt/war'
 
 export const error = { en: errorEN, es: errorES, pt: errorPT }
+export const about = { en: aboutEN, es: aboutES, pt: aboutPT }
+export const bazaarHistory = {
+  en: bazaarHistoryEN,
+  es: bazaarHistoryES,
+  pt: bazaarHistoryPT,
+}
 export const common = { en: commonEN, es: commonES, pt: commonPT }
 export const homepage = { en: homepageEN, es: homepageES, pt: homepagePT }
+export const statistics = {
+  en: statisticsEN,
+  es: statisticsES,
+  pt: statisticsPT,
+}
 export const war = { en: warEN, es: warES, pt: warPT }
