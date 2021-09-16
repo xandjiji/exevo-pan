55b2571b940b945b967e0c2c9673ec9c3eb69888
xandjiji <xandjiji@gmail.com>
2021-09-16 18:01:14 -0300

    test(LastFrags): fixing test

--- src/modules/LibertabraWar/components/OverallGrid/LastFrags/__tests__/index.tsx
+++ src/modules/LibertabraWar/components/OverallGrid/LastFrags/__tests__/index.tsx
@@ -12,6 +12,6 @@ describe('<LastFrags />', () => {
     expect(container.querySelectorAll('tbody tr')).toHaveLength(
       mockedFragsList.length < 40 ? mockedFragsList.length : 40,
     )
-    expect(screen.getByTitle('Sorted by character level')).toBeInTheDocument()
+    expect(screen.getByTitle('Sorted by Level')).toBeInTheDocument()
   })
 })
