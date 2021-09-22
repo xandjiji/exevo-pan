224b2d03aa8fb0de3b2816c6107fa7587a1fc9a4
xandjiji <xandjiji@gmail.com>
2021-09-22 11:31:19 -0300

    refactor(Advertise): now using Form

--- src/modules/Advertise/index.tsx
+++ src/modules/Advertise/index.tsx
@@ -1,13 +1,23 @@
-import { SelectedCharacterProvider } from './contexts/SelectedCharacter'
+import { FormProvider, useForm } from './contexts/Form'
 import { AuctionSearch, CharacterCard } from './components'
 import * as S from './styles'
 
-const AdvertiseGrid = (): JSX.Element => (
-  <S.Wrapper>
-    <SelectedCharacterProvider>
+const Form = (): JSX.Element => {
+  const { formStep } = useForm()
+  console.log(formStep)
+  return (
+    <>
       <AuctionSearch />
       <CharacterCard />
-    </SelectedCharacterProvider>
+    </>
+  )
+}
+
+const AdvertiseGrid = (): JSX.Element => (
+  <S.Wrapper>
+    <FormProvider>
+      <Form />
+    </FormProvider>
   </S.Wrapper>
 )
 
