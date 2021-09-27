278c29e68415967901492281ae02625523c35cbd
xandjiji <xandjiji@gmail.com>
2021-09-27 17:27:30 -0300

    feature(UserData): NicknameInput is rendered only if neccessary

--- src/modules/Advertise/components/Checkout/UserData/index.tsx
+++ src/modules/Advertise/components/Checkout/UserData/index.tsx
@@ -1,13 +1,18 @@
+import { useForm } from '../../../contexts/Form'
 import EmailInput from './EmailInput'
 import NicknameInput from './NicknameInput'
 import * as S from './styles'
 
-const UserData = (): JSX.Element => (
-  <S.Wrapper>
-    <S.Title>Your information</S.Title>
-    <EmailInput />
-    <NicknameInput />
-  </S.Wrapper>
-)
+const UserData = (): JSX.Element => {
+  const { paymentMethod } = useForm()
+
+  return (
+    <S.Wrapper>
+      <S.Title>Your information</S.Title>
+      <EmailInput />
+      {paymentMethod === 'TIBIA_COINS' && <NicknameInput />}
+    </S.Wrapper>
+  )
+}
 
 export default UserData
