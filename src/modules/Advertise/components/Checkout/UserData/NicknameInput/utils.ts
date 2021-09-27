d43a891cadf87a87c0d3e97e5e81e882aa8b6ae6
xandjiji <xandjiji@gmail.com>
2021-09-27 17:23:37 -0300

    feature(NicknameInput): handling validation error

--- src/modules/Advertise/components/Checkout/UserData/NicknameInput/utils.ts
+++ src/modules/Advertise/components/Checkout/UserData/NicknameInput/utils.ts
@@ -2,7 +2,11 @@ const buildUrl = (nickname: string): string =>
   `https://api.tibiadata.com/v2/characters/${nickname}.json`
 
 export const isCharacterValid = async (nickname: string): Promise<boolean> => {
-  const response = await fetch(buildUrl(nickname))
-  const data = await response.json()
-  return !data.characters.error
+  try {
+    const response = await fetch(buildUrl(nickname))
+    const data = await response.json()
+    return !data.characters.error
+  } catch (error) {
+    return true
+  }
 }
