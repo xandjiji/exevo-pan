6d7a387d27eb2acacd83927a41ed33e884416357
xandjiji <xandjiji@gmail.com>
2021-10-02 10:04:40 -0300

    chore: testing email

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -14,7 +14,23 @@ const mailCreds = {
 
 const mailer = nodemailer.createTransport(mailCreds)
 
-export default (request: VercelRequest, response: VercelResponse): void => {
+export default async (
+  request: VercelRequest,
+  response: VercelResponse,
+): Promise<void> => {
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    mailer.verify((error, success) => {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log('Server is ready to take our messages')
+        resolve(success)
+      }
+    })
+  })
+
   const { name = 'World' } = request.query
 
   const mailOptions = {
@@ -24,7 +40,20 @@ export default (request: VercelRequest, response: VercelResponse): void => {
     html: 'deu certo',
   }
 
-  mailer.sendMail(mailOptions)
+  await new Promise((resolve, reject) => {
+    // send mail
+    mailer.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err)
+        reject(err)
+      } else {
+        console.log(info)
+        resolve(info)
+      }
+    })
+  })
+
+  /* mailer.sendMail(mailOptions) */
 
   response.status(200).send(`Hello ${name}!`)
 }
