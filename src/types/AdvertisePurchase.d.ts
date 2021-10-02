5b220a55cf214e28b770d1d2942490484ed4ee23
xandjiji <xandjiji@gmail.com>
2021-10-02 11:29:32 -0300

    feature(types): AdvertisePurchase

---
+++ src/types/AdvertisePurchase.d.ts
@@ -0,0 +1,8 @@
+declare type AdvertisePurchase = {
+  uuid: string
+  selectedCharacter: CharacterObject
+  selectedDates: string[]
+  paymentMethod: string
+  email: string
+  paymentCharacter: string
+}
