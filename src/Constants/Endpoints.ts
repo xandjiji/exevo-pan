2e53346c93e156878ccd8f5e9d2576ecc2b6f5ae
xandjiji <xandjiji@gmail.com>
2021-10-13 21:18:38 -0300

    feature(constants): adding endpoints and paths for highlighted auctions

--- src/Constants/Endpoints.ts
+++ src/Constants/Endpoints.ts
@@ -2,6 +2,7 @@ export const endpoints = {
   BASE_DATA: 'https://exevopan-data.netlify.app',
   BASE_HISTORY_DATA: 'https://exevopan-history-data.netlify.app',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
+  HIGHLIGHTED_DATA: 'https://exevo-pan-highlighted.netlify.app',
   TIBIADATA: 'https://api.tibiadata.com/v2/characters',
   MAIL_CHECKOUT: '/api/mail-checkout',
 }
