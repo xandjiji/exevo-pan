c60f77707d75a6b463ef1ee7d22e92c4c50d0710
xandjiji <xandjiji@gmail.com>
2021-10-18 22:51:57 -0300

    test(Stepper): adding more tests

--- src/components/Atoms/Stepper/__tests__/index.tsx
+++ src/components/Atoms/Stepper/__tests__/index.tsx
@@ -38,9 +38,60 @@ describe('<Stepper />', () => {
     expect(screen.getByText('fourth icon')).toBeInTheDocument()
   })
 
-  test.todo('should call click functions onClick')
+  test('should call click functions onClick', () => {
+    renderWithProviders(<Stepper currentStep={1} steps={mockedSteps} />)
 
-  test.todo('should call click functions on keyboard navigation')
+    mockedSteps.forEach((step) => {
+      userEvent.click(screen.getByRole('heading', { name: step.title }))
+      if (step.onClick) {
+        expect(step.onClick).toHaveBeenCalledTimes(1)
+      }
+    })
+  })
 
-  test.todo('should mark completed steps')
+  test('should call click functions on keyboard navigation', () => {
+    renderWithProviders(<Stepper currentStep={1} steps={mockedSteps} />)
+
+    mockedSteps.forEach((step) => {
+      userEvent.tab()
+      userEvent.keyboard('{space}')
+      if (step.onClick) {
+        expect(step.onClick).toHaveBeenCalled()
+      }
+    })
+  })
+
+  test('should mark completed steps', () => {
+    let currentStep = 0
+    const { rerender } = renderWithProviders(
+      <Stepper currentStep={currentStep} steps={mockedSteps} />,
+    )
+
+    const buttons = screen.getAllByRole('button')
+
+    mockedSteps.forEach((step, index) => {
+      expect(buttons[index]).toHaveAttribute(
+        'aria-labelledby',
+        `step-item-${step.title}`,
+      )
+    })
+
+    const verifyCurrentAria = () => {
+      mockedSteps.forEach((_, index) => {
+        const currentButton = buttons[index]
+        if (index === currentStep) {
+          expect(currentButton).toHaveAttribute('aria-current', 'step')
+        } else {
+          expect(currentButton).not.toHaveAttribute('aria-current', 'step')
+        }
+      })
+      currentStep += 1
+      rerender(<Stepper currentStep={currentStep} steps={mockedSteps} />)
+    }
+
+    verifyCurrentAria()
+    verifyCurrentAria()
+    verifyCurrentAria()
+    verifyCurrentAria()
+  })
 })
