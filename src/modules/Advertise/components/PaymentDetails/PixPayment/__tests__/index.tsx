03e99ffabde5b1197246cf4142c5185eaa02bfba
xandjiji <xandjiji@gmail.com>
2021-10-29 23:01:26 -0300

    test(PixPayment): adding test

---
+++ src/modules/Advertise/components/PaymentDetails/PixPayment/__tests__/index.tsx
@@ -0,0 +1,22 @@
+import { screen, waitFor } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
+import PixPayment from '..'
+
+jest.mock('../../../../contexts/Form', () => ({
+  useForm: () => ({
+    paymentCharacter: {
+      value: 'Bubble',
+    },
+    selectedDates: ['10/21/2021', '10/22/2021'],
+  }),
+}))
+
+describe('<PixPayment />', () => {
+  test('should render qr code correctly', async () => {
+    renderWithProviders(<PixPayment />)
+
+    await waitFor(() => {
+      expect(screen.getByRole('img')).toHaveAttribute('src')
+    })
+  })
+})
