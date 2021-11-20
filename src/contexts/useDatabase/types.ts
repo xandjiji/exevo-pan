bc7a97621c43e0ab3c4f729841aec5d5a207183f
xandjiji <xandjiji@gmail.com>
2021-11-20 10:49:51 -0300

    chore: removing unused code

--- src/contexts/useDatabase/types.ts
+++ src/contexts/useDatabase/types.ts
@@ -1,49 +1,12 @@
-export type Action =
-  | {
-      type: 'APPLY_FILTERS'
-      filters: FilterState
-      isHistory: boolean
-    }
-  | {
-      type: 'INITIAL_DATA_LOAD'
-      characterData: CharacterObject[]
-      highlightedAuctions: HighlightedAuction[]
-      serverData: ServerObject[]
-      rareItemData: RareItemData
-      isHistory: boolean
-    }
-  | {
-      type: 'RESET_TO_BASE_DATA'
-    }
-  | {
-      type: 'WAR_GUILD_DATA_LOAD'
-      warGuildData: MemberWarData[]
-    }
+export type Action = {
+  type: 'WAR_GUILD_DATA_LOAD'
+  warGuildData: MemberWarData[]
+}
 
 export interface DatabaseContextValues {
   loading: boolean
-  characterData: CharacterObject[]
-  serverData: ServerObject[]
-  rareItemData: RareItemData
-  historyData: CharacterObject[]
-  highlightedAuctions: CharacterObject[]
   warGuildData: MemberWarData[]
   dispatch: (action: Action) => void
-}
-
-export interface CharactersContextValues {
-  loading: boolean
-  characterData: CharacterObject[]
-  historyData: CharacterObject[]
-  baseCharacterData: CharacterObject[]
-  baseHistoryData: CharacterObject[]
-  highlightedAuctions: CharacterObject[]
-}
-
-export interface DrawerFieldsContextValues {
-  loading: boolean
-  serverData: ServerObject[]
-  rareItemData: RareItemData
 }
 
 export interface WarGuildDataContextValues {
@@ -56,12 +19,5 @@ export interface DatabaseDispatchContextValues {
 }
 
 export interface DatabaseReducerState {
-  baseCharacterData: CharacterObject[]
-  characterData: CharacterObject[]
-  serverData: ServerObject[]
-  rareItemData: RareItemData
-  baseHistoryData: CharacterObject[]
-  historyData: CharacterObject[]
-  highlightedAuctions: CharacterObject[]
   warGuildData: MemberWarData[]
 }
