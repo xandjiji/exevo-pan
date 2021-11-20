2015e4cecd1b1de0be78229fa4ee549276714308
xandjiji <xandjiji@gmail.com>
2021-11-20 11:07:43 -0300

    chore: removing unused code

--- src/contexts/useDatabase/__tests__/mock.ts
+++ src/contexts/useDatabase/__tests__/mock.ts
@@ -1,56 +1,12 @@
 import { randomDataset } from 'utils/test'
 
-const {
-  partialCharacterData,
-  serverData,
-  itemData,
-  characterData,
-  statisticsData,
-  puneMembersData,
-  bonesMembersData,
-  allGuildMembers,
-} = randomDataset()
+const { statisticsData, puneMembersData, bonesMembersData, allGuildMembers } =
+  randomDataset()
 
-export const mockedPartialCharacterData = partialCharacterData
-export const mockedServerData = serverData
-export const mockedItemData = itemData
 export const mockStatisticsData = statisticsData
-export const mockedCharacterData = characterData
-
-export const initialFilter = {
-  nicknameFilter: '',
-  vocation: new Set([]),
-  pvp: new Set([]),
-  battleye: new Set([]),
-  location: new Set([]),
-  serverSet: new Set([]),
-  minLevel: 8,
-  maxLevel: 2000,
-  minSkill: 10,
-  skillKey: new Set([]),
-  itemSet: new Set([]),
-  fav: false,
-  rareNick: false,
-  soulwarFilter: false,
-  imbuementsSet: new Set([]),
-} as FilterState
-
-const favEK: CharacterObject = { ...characterData[0], vocationId: 1 }
-const favED: CharacterObject = { ...characterData[1], vocationId: 4 }
-
-export const mockFavArray = [favEK, favED] as CharacterObject[]
-export const filteredFavArray = [favEK] as CharacterObject[]
 
 export const mockedGuildData = {
   puneMembersData,
   bonesMembersData,
   allGuildMembers,
 }
-
-export const mockedHighlightedAuctions: CharacterObject[] = characterData.slice(
-  0,
-  20,
-)
-
-export const mockedHighlightedAuctionsData: HighlightedAuction[] =
-  mockedHighlightedAuctions.map((auction) => ({ id: auction.id, days: [] }))
