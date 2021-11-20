d48159a5d435454c70ce5b48b04f1b09f4a13ffa
xandjiji <xandjiji@gmail.com>
2021-11-20 11:07:50 -0300

    chore: removing unused code

--- src/Constants/Paths.ts
+++ src/Constants/Paths.ts
@@ -2,8 +2,6 @@ export const paths = {
   SERVER_DATA: '/ServerData.json',
   ITEMS_DATA: '/ItemsData.json',
   OVERALL_STATISTICS: '/overallStatistics.json',
-  CHARACTER_DATA: '/MinifiedCharacterData.json',
-  HISTORY_HASH: '/hash.json',
   HIGHLIGHTED_AUCTIONS: '/HighlightedAuctions.json',
   WAR_STATISTICS: '/statistics.json',
   PUNE_DATA: '/miniLibertabraPuneData.json',
