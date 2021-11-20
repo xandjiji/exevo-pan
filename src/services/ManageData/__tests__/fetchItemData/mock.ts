78f8d60a22910136045822c26be0b97f58308ba5
xandjiji <xandjiji@gmail.com>
2021-11-20 11:07:32 -0300

    chore: removing unused code

--- src/services/ManageData/__tests__/fetchItemData/mock.ts
+++
@@ -1,6 +0,0 @@
-import { randomDataset } from 'utils/test'
-
-const { rawItemData, itemData } = randomDataset()
-
-export const mockedRareItemData = rawItemData
-export const filteredRareItemData = itemData
