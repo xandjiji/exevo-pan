d289f46107e7d610917a833d960871e57f4158af
xandjiji <xandjiji@gmail.com>
2021-11-23 07:54:58 -0300

    feature(constants): adding HISTORY_AUCTIONS

--- src/Constants/Endpoints.ts
+++ src/Constants/Endpoints.ts
@@ -1,5 +1,6 @@
 export const endpoints = {
   CURRENT_AUCTIONS: 'https://worker-exevopan.exevopan.workers.dev',
+  HISTORY_AUCTIONS: 'http://history.exevopan.com',
   BASE_DATA: 'https://exevopan-data.netlify.app',
   BASE_HISTORY_DATA: 'https://exevopan-history-data.netlify.app',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
