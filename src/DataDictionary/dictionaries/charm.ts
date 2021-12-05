8d38bc37e8ad36c1654806ae33c8956585243abf
xandjiji <xandjiji@gmail.com>
2021-12-05 18:41:32 -0300

    feature(DataDictionary): adding charms

--- src/DataDictionary/dictionaries/charm.ts
+++ src/DataDictionary/dictionaries/charm.ts
@@ -1,25 +1,27 @@
 import { dictionaryFactory } from '../utils'
 
-export const tokens = [
-  'Dodge',
-  'Wound',
-  'Curse',
-  'Zap',
-  'Enflame',
-  'Freeze',
-  'Low Blow',
-  'Parry',
-  'Poison',
-  'Divine Wrath',
-  'Cripple',
-  'Cleanse',
-  'Adrenaline Burst',
-  'Vampiric Embrace',
-  'Numb',
-  "Void's Call",
-  'Scavenge',
-  'Gut',
-  'Bless',
-]
+export const scrapingTokens = {
+  Dodge: 'Dodge',
+  Wound: 'Wound',
+  Curse: 'Curse',
+  Zap: 'Zap',
+  Enflame: 'Enflame',
+  Freeze: 'Freeze',
+  'Low Blow': 'Low Blow',
+  Parry: 'Parry',
+  Poison: 'Poison',
+  'Divine Wrath': 'Divine Wrath',
+  Cripple: 'Cripple',
+  Cleanse: 'Cleanse',
+  'Adrenaline Burst': 'Adrenaline Burst',
+  'Vampiric Embrace': 'Vampiric Embrace',
+  Numb: 'Numb',
+  "Void's Call": "Void's Call",
+  Scavenge: 'Scavenge',
+  Gut: 'Gut',
+  Bless: 'Bless',
+}
+
+export const tokens = Object.values(scrapingTokens)
 
 export const dictionary = dictionaryFactory(tokens)
