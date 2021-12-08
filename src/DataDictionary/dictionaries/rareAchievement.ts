96147a6d921615e2b07b705170221b92b597f132
xandjiji <xandjiji@gmail.com>
2021-12-08 11:02:16 -0300

    feature(DataDictionary): adding more achievements

--- src/DataDictionary/dictionaries/rareAchievement.ts
+++ src/DataDictionary/dictionaries/rareAchievement.ts
@@ -1,6 +1,9 @@
 import { dictionaryFactory } from '../utils'
 
 export const scrapingTokens = {
+  Biodegradable: 'Biodegradable',
+  Fearless: 'Fearless',
+  Goldhunter: 'Goldhunter',
   'His True Face': 'His True Face',
   'Razing!': 'Razing!',
   'The More the Merrier': 'The More the Merrier',
