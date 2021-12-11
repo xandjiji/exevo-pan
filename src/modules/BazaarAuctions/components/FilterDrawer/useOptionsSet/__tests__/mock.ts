f8c170072a3abc58ef2f112a3bdffdd5dc973e24
xandjiji <xandjiji@gmail.com>
2021-12-11 09:01:24 -0300

    test(useOptionsSet): adding tests

---
+++ src/modules/BazaarAuctions/components/FilterDrawer/useOptionsSet/__tests__/mock.ts
@@ -0,0 +1,5 @@
+export const options: Option[] = [
+  { name: 'Blue Banana', value: 'blue-banana' },
+  { name: 'Green Apple', value: 'green-apple' },
+  { name: 'Red Orange', value: 'red-orange' },
+]
