7ed5c671dd93aa99bdb9a8c2c3b09e20b50b2f42
xandjiji <xandjiji@gmail.com>
2021-12-28 15:59:55 -0300

    chore: changing tunnel endpoint

--- src/Constants/Endpoints.ts
+++ src/Constants/Endpoints.ts
@@ -1,10 +1,8 @@
 export const endpoints = {
   CURRENT_AUCTIONS: 'https://auctions.exevopan.workers.dev',
-  /* HISTORY_AUCTIONS: 'https://exevopan.pagekite.me', */
-  HISTORY_AUCTIONS: 'https://compatibility-karen-are-sphere.trycloudflare.com',
+  HISTORY_AUCTIONS: 'https://history.service-exevopan.com',
   STATIC_DATA: 'https://static-exevopan.surge.sh',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
-  HIGHLIGHTED_DATA: 'https://exevo-pan-highlighted.netlify.app',
   TIBIADATA: 'https://api.tibiadata.com/v2/characters',
   MAIL_CHECKOUT: '/api/mail-checkout',
   FCM_SEND: 'https://fcm.googleapis.com/fcm/send',
