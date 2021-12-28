dee1f29b2f23fd7fc8af1a21d63c93a83a4bca92
xandjiji <xandjiji@gmail.com>
2021-12-28 11:28:34 -0300

    chore: trying cloudflare tunnel

--- src/Constants/Endpoints.ts
+++ src/Constants/Endpoints.ts
@@ -1,6 +1,7 @@
 export const endpoints = {
   CURRENT_AUCTIONS: 'https://auctions.exevopan.workers.dev',
-  HISTORY_AUCTIONS: 'https://exevopan.pagekite.me',
+  /* HISTORY_AUCTIONS: 'https://exevopan.pagekite.me', */
+  HISTORY_AUCTIONS: 'https://compatibility-karen-are-sphere.trycloudflare.com',
   STATIC_DATA: 'https://static-exevopan.surge.sh',
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
   HIGHLIGHTED_DATA: 'https://exevo-pan-highlighted.netlify.app',
