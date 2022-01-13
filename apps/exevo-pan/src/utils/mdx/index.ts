22dce18382a8d0ec4a5aef25fa1cc00a058ba787
xandjiji <xandjiji@gmail.com>
2022-01-13 09:30:14 -0300

    chore: removing unused code

--- apps/exevo-pan/src/utils/mdx/index.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-export const POSTS_PATH = path.join(process.cwd(), 'src/posts')
-
-export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
-  .filter((filename) => /\.mdx?$/.test(filename))
