7b4d64c37af069701ce3d5efce4776d93d4d7c66
xandjiji <xandjiji@gmail.com>
2022-01-15 08:40:24 -0300

    fix(PostCard): displaying correct day

--- apps/exevo-pan/src/modules/Blog/components/Home/PostGrid/PostCard/utils.ts
+++ apps/exevo-pan/src/modules/Blog/components/Home/PostGrid/PostCard/utils.ts
@@ -4,7 +4,7 @@ export const extractDate = (timestamp: number): DateObject => {
   const date = new Date(timestamp)
 
   const month = date.getMonth()
-  const day = date.getDay() + 1
+  const day = date.getDate()
   const year = date.getFullYear()
   return { month, day, year }
 }
