f5d6564c42b42705531c3ce32f6235991e2866ce
xandjiji <xandjiji@gmail.com>
2022-01-19 11:29:43 -0300

    feature(blockquote): updating style

--- apps/exevo-pan/src/modules/Blog/components/Post/blockquote.ts
+++ apps/exevo-pan/src/modules/Blog/components/Post/blockquote.ts
@@ -2,9 +2,10 @@ import { css } from 'styled-components'
 
 export const blockquote = css`
   blockquote {
+    border-left: solid 6px var(--primary);
     margin: 0;
     padding: 16px 24px;
-    border-radius: 5px;
+    border-radius: 4px;
     background-color: var(--background);
 
     font-size: 19px;
@@ -12,6 +13,7 @@ export const blockquote = css`
     color: var(--primary);
 
     p {
+      font-weight: 400;
       filter: brightness(130%);
     }
   }
