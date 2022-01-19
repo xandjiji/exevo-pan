3c7192ebeca18594dbc3fa0b03a3b2a189007966
xandjiji <xandjiji@gmail.com>
2022-01-19 07:47:07 -0300

    feature(Newsletter): initial component

--- apps/exevo-pan/src/modules/Blog/components/Newsletter/index.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Newsletter/index.tsx
@@ -1,14 +1,42 @@
+import { Input } from 'components/Atoms'
 import * as S from './styles'
 
 const Newsletter = (
   props: React.HTMLAttributes<HTMLDivElement>,
-): JSX.Element => {
-  console.log('Newsletter')
-  return (
-    <S.Wrapper {...props}>
-      <h2>Newsletter</h2>
-    </S.Wrapper>
-  )
-}
+): JSX.Element => (
+  <S.Wrapper {...props}>
+    <S.Title>
+      Get our <strong>newsletter</strong>
+      <br />
+      and receive every new content!
+      <S.ImageWrapper>
+        <S.MailboxImage
+          alt="Royal mail"
+          layout="fixed"
+          width={64}
+          height={64}
+        />
+      </S.ImageWrapper>
+    </S.Title>
+
+    <S.FormGroup>
+      <S.Label htmlFor="newsletter-email-input">Email</S.Label>
+      <Input
+        id="newsletter-email-input"
+        placeholder="your@email.com"
+        allowClear
+      />
+    </S.FormGroup>
+    <S.Button type="button">
+      Sign up
+      <S.LetterImage
+        alt="Stamped letter"
+        layout="fixed"
+        width={26}
+        height={15}
+      />
+    </S.Button>
+  </S.Wrapper>
+)
 
 export default Newsletter
--- apps/exevo-pan/src/modules/Blog/components/Newsletter/styles.ts
+++ apps/exevo-pan/src/modules/Blog/components/Newsletter/styles.ts
@@ -1,9 +1,53 @@
 import styled from 'styled-components'
+import Image from 'next/image'
+import { Button as BaseButton } from 'components/Atoms'
 import { MaterialCard } from 'styles'
+import MailboxPng from 'assets/mailbox.png'
+import LetterPng from 'assets/letter.png'
 
 export const Wrapper = styled.section`
   ${MaterialCard}
-  height: 320px;
-  background-color: var(--primary);
-  color: var(--onPrimary);
+  padding: 16px;
+  background-color: var(--primaryVariant);
+
+  display: grid;
+  gap: 24px;
 `
+
+export const Title = styled.h4`
+  position: relative;
+  font-size: 18px;
+  font-weight: 300;
+`
+
+export const ImageWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: -1;
+`
+
+export const MailboxImage = styled(Image).attrs({
+  src: MailboxPng,
+})`
+  filter: grayscale(0.75);
+  opacity: 0.1;
+`
+
+export const FormGroup = styled.div`
+  display: grid;
+  gap: 6px;
+`
+
+export const Label = styled.label``
+
+export const Button = styled(BaseButton)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+`
+
+export const LetterImage = styled(Image).attrs({
+  src: LetterPng,
+})``
