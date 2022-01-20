e3d883e6f728c54bc5bc650c4470b84c6352f50e
xandjiji <xandjiji@gmail.com>
2022-01-20 10:56:20 -0300

    feature(components): CharacterMiniCard

---
+++ apps/exevo-pan/src/components/CharacterMiniCard/index.tsx
@@ -0,0 +1,47 @@
+import { useTranslations } from 'contexts/useTranslation'
+import * as S from './styles'
+import { CharacterMiniCardProps } from './types'
+
+const DEFAULT_OUTFIT_SRC =
+  'https://static.tibia.com/images/charactertrade/outfits/128_0.gif'
+
+const CharacterMiniCard = ({
+  isCard = false,
+  displayLink = false,
+  displayServer = false,
+  outfitSrc = DEFAULT_OUTFIT_SRC,
+  characterData,
+  ...props
+}: CharacterMiniCardProps): JSX.Element => {
+  const {
+    translations: { common },
+  } = useTranslations()
+
+  const { name, level, vocation, world } = characterData
+
+  return (
+    <S.Wrapper isCard={isCard} {...props}>
+      <S.SpritePortrait src={outfitSrc} alt={name} title={name} />
+      <div>
+        <S.Nickname>
+          {name}
+          {displayLink && (
+            <S.Link
+              href={`https://www.tibia.com/community/?name=${name}`}
+              target="_blank"
+              rel="noreferrer noopener external"
+            >
+              <S.ExternalIcon />
+              {common.CharacterTooltipLabel}
+            </S.Link>
+          )}
+        </S.Nickname>
+        <S.Description>
+          Level {level} - {vocation} {displayServer && `(${world})`}
+        </S.Description>
+      </div>
+    </S.Wrapper>
+  )
+}
+
+export default CharacterMiniCard
---
+++ apps/exevo-pan/src/components/CharacterMiniCard/types.ts
@@ -0,0 +1,8 @@
+export interface CharacterMiniCardProps
+  extends React.HTMLAttributes<HTMLDivElement> {
+  isCard?: boolean
+  displayLink?: boolean
+  displayServer?: boolean
+  outfitSrc?: string
+  characterData: SingleCharacterData
+}
