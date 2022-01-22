433db8557e22310be89c1a2e846184067b618f63
xandjiji <xandjiji@gmail.com>
2022-01-22 08:12:46 -0300

    feature(components): FadeImage

---
+++ apps/exevo-pan/src/components/Atoms/FadeImage/index.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import { useOnImageLoad } from 'hooks'
+import * as S from './styles'
+import { FadeImageProps } from './types'
+
+const FadeImage = ({
+  className,
+  id,
+  ...props
+}: FadeImageProps): JSX.Element => {
+  const [loaded, onLoad] = useOnImageLoad()
+
+  return (
+    <S.Wrapper className={className} id={id} data-loaded={loaded}>
+      <Image {...props} onLoad={onLoad} />
+    </S.Wrapper>
+  )
+}
+
+export default FadeImage
