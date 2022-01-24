5b00260250caa48b90c9becd282744b980f7829b
xandjiji <xandjiji@gmail.com>
2022-01-24 16:33:03 -0300

    feature: parsing encoded characters

--- apps/exevo-pan/src/modules/Blog/utils/parseMarkdownSections.ts
+++ apps/exevo-pan/src/modules/Blog/utils/parseMarkdownSections.ts
@@ -1,15 +1,27 @@
+type Parser = (value: string) => string
+
 const HEX_BASE = 16
 
+/* parsing emojis */
 const emojiRegex = new RegExp(/\\\\u{[\dA-F]{5}}/g)
 const unicodeGroup = new RegExp(/\\\\u{(.*)}/)
-
-const unicodeToChar = (text: string) =>
+const decodeEmoji = (text: string) =>
   text.replace(unicodeGroup, (_, group) =>
     String.fromCodePoint(parseInt(group, HEX_BASE)),
   )
 
-const parseEmojis = (text: string) =>
-  text.replace(emojiRegex, (match) => unicodeToChar(match))
+const parseEmojis: Parser = (text: string) =>
+  text.replace(emojiRegex, (match) => decodeEmoji(match))
+
+/* parsing encoded characters */
+const encodedCharacterRegex = new RegExp(/\\\\x[\dA-F]{2}/g)
+const decodeHex = (text: string) => {
+  const hexString = text.replace(/\\\\/g, '')
+  return String.fromCodePoint(+`0${hexString}`)
+}
+
+const parseCharacters: Parser = (text: string) =>
+  text.replace(encodedCharacterRegex, (match) => decodeHex(match))
 
 export const parseMarkdownSections = (src: string): string[] =>
   src
@@ -17,5 +29,5 @@ export const parseMarkdownSections = (src: string): string[] =>
     .filter((value) => value.includes('\\"h2\\"'))
     .map((value) => {
       const [, , , title] = value.split('\\"')
-      return parseEmojis(title)
+      return parseCharacters(parseEmojis(title))
     })
