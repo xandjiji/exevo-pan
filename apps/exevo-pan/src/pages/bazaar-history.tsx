d4adb911232e32fd8f6f66c2b92b6918f10f85a7
xandjiji <xandjiji@gmail.com>
2022-01-25 06:50:27 -0300

    fix: sticking the correct history endpoint

--- apps/exevo-pan/src/pages/bazaar-history.tsx
+++ apps/exevo-pan/src/pages/bazaar-history.tsx
@@ -97,7 +97,7 @@ export default function BazaarHistory({
         >
           <FiltersProvider>
             <AuctionsProvider
-              endpoint={endpoints.CURRENT_AUCTIONS}
+              endpoint={endpoints.HISTORY_AUCTIONS}
               highlightedAuctions={[]}
               initialPage={page}
               initialPageData={pageData}
