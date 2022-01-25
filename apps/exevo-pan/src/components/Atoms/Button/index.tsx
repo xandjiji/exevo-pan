f9899dc3082bc2f8a3bed1c68228f104b9653e37
xandjiji <xandjiji@gmail.com>
2022-01-25 18:12:51 -0300

    feature(Button): loading state

--- apps/exevo-pan/src/components/Atoms/Button/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Button/index.tsx
@@ -1,8 +1,19 @@
 import * as S from './styles'
 import { ButtonProps } from './types'
 
-const Button = ({ ...props }: ButtonProps): JSX.Element => (
-  <S.Button {...props} />
+const Button = ({
+  children,
+  loading,
+  disabled,
+  ...props
+}: ButtonProps): JSX.Element => (
+  <S.Button
+    {...props}
+    data-loading={loading}
+    disabled={loading ? true : disabled}
+  >
+    {loading ? <S.LoadingState role="alert" /> : children}
+  </S.Button>
 )
 
 export default Button
