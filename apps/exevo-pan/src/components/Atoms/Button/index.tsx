6c446dfc6daee0132309a294dd90dfc997155b3d
xandjiji <xandjiji@gmail.com>
2022-01-26 07:36:10 -0300

    feature(i18n): translating button loading label

--- apps/exevo-pan/src/components/Atoms/Button/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Button/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from 'contexts/useTranslation'
 import * as S from './styles'
 import { ButtonProps } from './types'
 
@@ -6,14 +7,23 @@ const Button = ({
   loading,
   disabled,
   ...props
-}: ButtonProps): JSX.Element => (
-  <S.Button
-    {...props}
-    data-loading={loading}
-    disabled={loading ? true : disabled}
-  >
-    {loading ? <S.LoadingState role="alert" /> : children}
-  </S.Button>
-)
+}: ButtonProps): JSX.Element => {
+  const {
+    translations: { common },
+  } = useTranslations()
+  return (
+    <S.Button
+      {...props}
+      data-loading={loading}
+      disabled={loading ? true : disabled}
+    >
+      {loading ? (
+        <S.LoadingState role="alert" aria-label={common.LoadingLabel} />
+      ) : (
+        children
+      )}
+    </S.Button>
+  )
+}
 
 export default Button
