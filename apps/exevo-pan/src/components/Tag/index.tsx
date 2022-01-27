f252bc5fd2cebb1a67df3a0ca846b40c1c79f10a
xandjiji <xandjiji@gmail.com>
2022-01-27 10:48:06 -0300

    feature(Tag): adding fallbacks

--- apps/exevo-pan/src/components/Tag/index.tsx
+++ apps/exevo-pan/src/components/Tag/index.tsx
@@ -5,6 +5,7 @@ import * as S from './styles'
 import { TagProps } from './types'
 
 const { tagById } = blogTags
+const FALLBACK_COLOR = 0
 
 const Tag = ({
   clickable = false,
@@ -20,16 +21,18 @@ const Tag = ({
 
   const type = clickable ? 'button' : undefined
 
+  console.log(tagId)
+
   return (
     <S.Wrapper
       as={type}
       type={type}
       role={clickable ? 'switch' : undefined}
       aria-checked={clickable && active}
-      tagColor={tagColor ?? tagById[tagId as string].color}
+      tagColor={tagColor ?? tagById[tagId as string]?.color ?? FALLBACK_COLOR}
       {...props}
     >
-      {children ?? common.BlogTags[tagById[tagId as string].id]}
+      {children ?? common.BlogTags[tagById[tagId as string]?.id] ?? tagId}
     </S.Wrapper>
   )
 }
