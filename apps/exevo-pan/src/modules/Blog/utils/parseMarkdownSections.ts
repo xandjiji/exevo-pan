3d41c190aa8a00179d0c23d5d9dc78e5acfb7ba6
xandjiji <xandjiji@gmail.com>
2022-01-27 15:40:23 -0300

    feature: decoding ASCII characters

--- apps/exevo-pan/src/modules/Blog/utils/parseMarkdownSections.ts
+++ apps/exevo-pan/src/modules/Blog/utils/parseMarkdownSections.ts
@@ -4,24 +4,35 @@ const HEX_BASE = 16
 
 /* parsing emojis */
 const emojiRegex = new RegExp(/\\\\u{[\dA-F]{5}}/g)
-const unicodeGroup = new RegExp(/\\\\u{(.*)}/)
+const emojiUnicodeGroup = new RegExp(/\\\\u{(.*)}/)
 const decodeEmoji = (text: string) =>
-  text.replace(unicodeGroup, (_, group) =>
+  text.replace(emojiUnicodeGroup, (_, group) =>
     String.fromCodePoint(parseInt(group, HEX_BASE)),
   )
 
 const parseEmojis: Parser = (text: string) =>
   text.replace(emojiRegex, (match) => decodeEmoji(match))
 
-/* parsing encoded characters */
-const encodedCharacterRegex = new RegExp(/\\\\x[\dA-F]{2}/g)
-const decodeHex = (text: string) => {
+/* parsing ASCII encoded characters */
+const ASCIIRegex = new RegExp(/\\\\x[\dA-F]{2}/g)
+const decodeASCII = (text: string) => {
   const hexString = text.replace(/\\\\/g, '')
   return String.fromCodePoint(+`0${hexString}`)
 }
 
-const parseCharacters: Parser = (text: string) =>
-  text.replace(encodedCharacterRegex, (match) => decodeHex(match))
+const parseASCIICharacters: Parser = (text: string) =>
+  text.replace(ASCIIRegex, (match) => decodeASCII(match))
+
+/* parsing UNICODE encoded characters */
+const unicodeRegex = new RegExp(/\\\\u[\dA-F]{4}/g)
+const unicodeGroup = new RegExp(/\\\\u(.*)/)
+const decodeUnicodes = (text: string) =>
+  text.replace(unicodeGroup, (_, group) =>
+    String.fromCodePoint(parseInt(group, HEX_BASE)),
+  )
+
+const parseUnicode: Parser = (text: string) =>
+  text.replace(unicodeRegex, (match) => decodeUnicodes(match))
 
 export const parseMarkdownSections = (src: string): string[] =>
   src
@@ -29,5 +40,5 @@ export const parseMarkdownSections = (src: string): string[] =>
     .filter((value) => value.includes('\\"h2\\"'))
     .map((value) => {
       const [, , , title] = value.split('\\"')
-      return parseCharacters(parseEmojis(title))
+      return parseUnicode(parseASCIICharacters(parseEmojis(title)))
     })
