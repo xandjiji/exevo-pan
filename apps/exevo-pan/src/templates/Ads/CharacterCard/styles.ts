31891a040524df33d847fd39f802825155ec86e2
xandjiji <xandjiji@gmail.com>
2022-02-01 09:57:02 -0300

    feature(Ads): adding unfilled state

--- apps/exevo-pan/src/templates/Ads/CharacterCard/styles.ts
+++ apps/exevo-pan/src/templates/Ads/CharacterCard/styles.ts
@@ -1,5 +1,20 @@
 import styled from 'styled-components'
 
+export const UnfilledState = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  opacity: 0;
+  transition: opacity 0.2s ease-out;
+  mix-blend-mode: overlay;
+`
+
 export const Wrapper = styled.div<{ estimatedHeight: number }>`
   min-height: ${({ estimatedHeight }) => estimatedHeight}px;
 
@@ -7,5 +22,9 @@ export const Wrapper = styled.div<{ estimatedHeight: number }>`
     margin: 0 auto;
     flex: none;
     flex-shrink: 0;
+  }
+
+  ins[data-ad-status='unfilled'] ${UnfilledState} {
+    opacity: 1;
   }
 `
