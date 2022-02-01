7ab6346a35dee8254637467892dfd1479598223b
xandjiji <xandjiji@gmail.com>
2022-02-01 10:23:21 -0300

    refactor(Ads): removing unfilled state

--- apps/exevo-pan/src/templates/Ads/CharacterCard/styles.ts
+++ apps/exevo-pan/src/templates/Ads/CharacterCard/styles.ts
@@ -1,19 +1,5 @@
 import styled from 'styled-components'
 
-export const UnfilledState = styled.div`
-  display: none;
-  width: 100%;
-  height: 100%;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 16px;
-
-  mix-blend-mode: overlay;
-`
-
 export const Wrapper = styled.div<{ estimatedHeight: number }>`
   min-height: ${({ estimatedHeight }) => estimatedHeight}px;
 
@@ -21,14 +7,5 @@ export const Wrapper = styled.div<{ estimatedHeight: number }>`
     margin: 0 auto;
     flex: none;
     flex-shrink: 0;
-  }
-
-  ins[data-ad-status='unfilled'] {
-    display: none !important;
-
-    ${UnfilledState} {
-      display: block;
-      opacity: 1;
-    }
   }
 `
