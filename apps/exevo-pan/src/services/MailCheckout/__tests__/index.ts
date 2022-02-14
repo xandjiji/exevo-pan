1e7f399abafe1e165086849e3347afd528b247b3
xandjiji <xandjiji@gmail.com>
2022-02-14 15:12:25 -0300

    test(Advertise): fixing tests

--- apps/exevo-pan/src/services/MailCheckout/__tests__/index.ts
+++ apps/exevo-pan/src/services/MailCheckout/__tests__/index.ts
@@ -4,6 +4,10 @@ import MailCheckoutClient from '..'
 global.fetch = jest.fn()
 const mockedFetch = fetch as jest.MockedFunction<typeof fetch>
 
+const mockPurchase = {
+  selectedDates: [],
+} as unknown as AdvertisePurchase
+
 describe('services/MailCheckout', () => {
   beforeEach(async () => {
     mockedFetch.mockClear()
@@ -16,7 +20,7 @@ describe('services/MailCheckout', () => {
   })
 
   test('if promise resolves, should return a string value', async () => {
-    const result = await MailCheckoutClient.postMail({} as AdvertisePurchase)
+    const result = await MailCheckoutClient.postMail(mockPurchase)
 
     expect(result).toEqual('mocked value')
   })
@@ -24,7 +28,7 @@ describe('services/MailCheckout', () => {
   test('on error, should return a new uuid', async () => {
     mockedFetch.mockRejectedValueOnce(undefined)
 
-    const result = await MailCheckoutClient.postMail({} as AdvertisePurchase)
+    const result = await MailCheckoutClient.postMail(mockPurchase)
 
     expect(validateUuid(result)).toBeTruthy()
   })
