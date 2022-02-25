cc399f2db93001af3a901fdf4b4c3e8f13bf9470
xandjiji <xandjiji@gmail.com>
2022-02-25 16:44:20 -0300

    feature(CharacterCard): calculating store value

--- apps/exevo-pan/src/components/CharacterCard/utils.ts
+++ apps/exevo-pan/src/components/CharacterCard/utils.ts
@@ -1,3 +1,5 @@
+import { storeItems } from 'data-dictionary/dist/dictionaries/store'
+
 const CHARM_EXPANSION_VALUE = 450
 const HUNTING_SLOT_VALUE = 750
 const PREY_SLOT_VALUE = 900
@@ -7,18 +9,40 @@ const PREY_SLOT_VALUE = 900
 /* outfits */
 /* mounts */
 /* hirelings */
-/* store items */
+
+const getStoreValue = (characterStoreItems: CharacterItem[]): number => {
+  let sum = 0
+
+  characterStoreItems.forEach(({ name, amount }) => {
+    const foundItem = storeItems[name]
+    if (foundItem) {
+      sum += foundItem.value * amount
+    } else {
+      /* @ ToDo: notify */
+      console.log(`Store Item: [${name}] not found`)
+    }
+  })
+
+  return sum
+}
 
 export const calculateTotalInvestment = (
   character: CharacterObject,
 ): number => {
-  const { charmInfo, huntingSlot, preySlot } = character
+  const {
+    charmInfo,
+    huntingSlot,
+    preySlot,
+    storeItems: characterStoreItems,
+  } = character
 
   let sum = 0
 
   if (charmInfo.expansion) sum += CHARM_EXPANSION_VALUE
   if (huntingSlot) sum += HUNTING_SLOT_VALUE
   if (preySlot) sum += PREY_SLOT_VALUE
+
+  sum += getStoreValue(characterStoreItems)
 
   return sum
 }
