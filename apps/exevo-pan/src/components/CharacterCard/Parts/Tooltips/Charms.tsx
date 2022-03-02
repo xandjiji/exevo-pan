e58a326e51825d7c396ccdbc9e47d4fec42130c5
xandjiji <xandjiji@gmail.com>
2022-03-02 09:00:07 -0300

    feature(Charms): formatting numbers

--- apps/exevo-pan/src/components/CharacterCard/Parts/Tooltips/Charms.tsx
+++ apps/exevo-pan/src/components/CharacterCard/Parts/Tooltips/Charms.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react'
 import { useTranslations } from 'contexts/useTranslation'
 import { Tooltip } from 'components/Organisms'
 import { tokens } from 'data-dictionary/dist/dictionaries/charm'
+import { formatNumberWithCommas } from 'utils'
 import Lister from './Lister'
 import * as S from './styles'
 import { CharacterCharmsProps } from './types'
@@ -26,9 +27,15 @@ const CharacterCharms = ({
         {charmInfo && (
           <>
             {' '}
-            (<strong style={{ marginRight: 2 }}>{charmInfo.total}</strong> total
-            points,
-            <strong style={{ margin: 2 }}>{charmInfo.unspent}</strong> unspent)
+            (
+            <strong style={{ marginRight: 2 }}>
+              {formatNumberWithCommas(charmInfo.total)}
+            </strong>{' '}
+            total points,
+            <strong style={{ margin: 2 }}>
+              {formatNumberWithCommas(charmInfo.unspent)}
+            </strong>{' '}
+            unspent)
           </>
         )}
       </S.TitleWrapper>
