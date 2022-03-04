4a9261d88453dcd0d2a52a2ef17122ed0036669f
xandjiji <xandjiji@gmail.com>
2022-03-04 06:47:39 -0300

    feature(Tabs): forwarding ref

--- apps/exevo-pan/src/components/Atoms/Tabs/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Tabs/index.tsx
@@ -1,4 +1,5 @@
 import {
+  forwardRef,
   useState,
   useCallback,
   Children,
@@ -9,63 +10,68 @@ import useIds from './useIds'
 import * as S from './styles'
 import { TabsProps, PanelProps } from './types'
 
-const Tabs = ({
-  activeIndex: indexProp,
-  initialActive = 0,
-  onChange,
-  children,
-  'aria-label': ariaLabelProp,
-  ...props
-}: TabsProps): JSX.Element => {
-  const [innerIndex, setInnerIndex] = useState(indexProp ?? initialActive)
-  const activeIndex = indexProp ?? innerIndex
+const Tabs = forwardRef(
+  (
+    {
+      activeIndex: indexProp,
+      initialActive = 0,
+      onChange,
+      children,
+      'aria-label': ariaLabelProp,
+      ...props
+    }: TabsProps,
+    ref,
+  ): JSX.Element => {
+    const [innerIndex, setInnerIndex] = useState(indexProp ?? initialActive)
+    const activeIndex = indexProp ?? innerIndex
 
-  const { getTabId, getPanelId } = useIds()
+    const { getTabId, getPanelId } = useIds()
 
-  const handleClick = useCallback(
-    (newIndex: number) =>
-      setInnerIndex((prevIndex) => {
-        if (prevIndex !== newIndex) onChange?.(newIndex)
-        return newIndex
-      }),
-    [onChange],
-  )
+    const handleClick = useCallback(
+      (newIndex: number) =>
+        setInnerIndex((prevIndex) => {
+          if (prevIndex !== newIndex) onChange?.(newIndex)
+          return newIndex
+        }),
+      [onChange],
+    )
 
-  return (
-    <S.Wrapper {...props}>
-      <S.TabWrapper aria-label={ariaLabelProp} role="tablist">
+    return (
+      <S.Wrapper {...props} ref={ref as React.RefObject<HTMLDivElement>}>
+        <S.TabWrapper aria-label={ariaLabelProp} role="tablist">
+          {Children.map(children, (child, childIndex) => {
+            if (!isValidElement(child)) return child
+            const { label } = child.props as PanelProps
+
+            return (
+              <S.Tab
+                type="button"
+                role="tab"
+                aria-controls={getPanelId(childIndex)}
+                id={getTabId(childIndex)}
+                tabIndex={0}
+                aria-selected={childIndex === activeIndex}
+                onClick={() => handleClick(childIndex)}
+              >
+                {label}
+              </S.Tab>
+            )
+          })}
+        </S.TabWrapper>
+
         {Children.map(children, (child, childIndex) => {
           if (!isValidElement(child)) return child
-          const { label } = child.props as PanelProps
 
-          return (
-            <S.Tab
-              type="button"
-              role="tab"
-              aria-controls={getPanelId(childIndex)}
-              id={getTabId(childIndex)}
-              tabIndex={0}
-              aria-selected={childIndex === activeIndex}
-              onClick={() => handleClick(childIndex)}
-            >
-              {label}
-            </S.Tab>
-          )
+          return cloneElement(child, {
+            id: getPanelId(childIndex),
+            'aria-labelledby': getTabId(childIndex),
+            active: childIndex === activeIndex,
+          })
         })}
-      </S.TabWrapper>
-
-      {Children.map(children, (child, childIndex) => {
-        if (!isValidElement(child)) return child
-
-        return cloneElement(child, {
-          id: getPanelId(childIndex),
-          'aria-labelledby': getTabId(childIndex),
-          active: childIndex === activeIndex,
-        })
-      })}
-    </S.Wrapper>
-  )
-}
+      </S.Wrapper>
+    )
+  },
+)
 
 const Panel = ({ active, children, ...props }: PanelProps): JSX.Element => (
   <S.Panel data-active={active} {...props}>
@@ -73,6 +79,8 @@ const Panel = ({ active, children, ...props }: PanelProps): JSX.Element => (
   </S.Panel>
 )
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 Tabs.Panel = Panel
 
 export default Tabs
