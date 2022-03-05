e5d835ebcdb179a2fc8b8a0271d3773a78e67ae0
xandjiji <xandjiji@gmail.com>
2022-03-05 09:44:57 -0300

    feature(utils): memoize()

---
+++ apps/exevo-pan/src/utils/memoize/index.ts
@@ -0,0 +1,14 @@
+export const memoize = <R, T extends (...args: any[]) => R>(fn: T): T => {
+  const cache = {} as Record<string, R>
+
+  const result = (...args: any[]) => {
+    const argsKey = JSON.stringify(args)
+    if (!cache[argsKey]) {
+      cache[argsKey] = fn(...args)
+    }
+
+    return cache[argsKey]
+  }
+
+  return result as T
+}
