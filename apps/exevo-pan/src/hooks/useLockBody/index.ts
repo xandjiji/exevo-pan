f051cb219a78431aaa81e12c43766d41706fa403
xandjiji <xandjiji@gmail.com>
2022-03-10 09:03:06 -0300

    feature(useLockBody): preventing layout shift on removing scrollbar from document

--- apps/exevo-pan/src/hooks/useLockBody/index.ts
+++ apps/exevo-pan/src/hooks/useLockBody/index.ts
@@ -1,13 +1,18 @@
 import { useEffect } from 'react'
 
+const getScrollbarWidth = () =>
+  `${window.innerWidth - document.body.clientWidth}px`
+
 const useLockBody = (open = true): void => {
   useEffect(() => {
     if (document && document.body) {
+      document.body.style.paddingRight = open ? getScrollbarWidth() : 'unset'
       document.body.style.overflow = open ? 'hidden' : 'unset'
     }
 
     return (): void => {
       document.body.style.overflow = 'unset'
+      document.body.style.paddingRight = 'unset'
     }
   }, [open])
 }
