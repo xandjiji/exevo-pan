68039c46ed5b4f23eb40c6dc51e76a8574082371
xandjiji <xandjiji@gmail.com>
2022-03-13 19:30:54 -0300

    chore: adding temporary placeholders

--- packages/data-dictionary/src/dictionaries/store.ts
+++ packages/data-dictionary/src/dictionaries/store.ts
@@ -106,6 +106,32 @@ export const ignoreList = new Set<string>([
 ])
 
 export const storeItems: Record<string, StoreItem> = {
+  /* @ ToDo: remove this once the database is cleaned up */
+  'lasting exercise axe': {
+    name: 'Lasting exercise axe',
+    value: 0,
+  },
+  'lasting exercise bow': {
+    name: 'Lasting exercise bow',
+    value: 0,
+  },
+  'lasting exercise club': {
+    name: 'Lasting exercise club',
+    value: 0,
+  },
+  'lasting exercise sword': {
+    name: 'Lasting exercise sword',
+    value: 0,
+  },
+  'lasting exercise rod': {
+    name: 'Lasting exercise rod',
+    value: 0,
+  },
+  'lasting exercise wand': {
+    name: 'Lasting exercise wand',
+    value: 0,
+  },
+  /*  */
   'gold pouch': {
     name: 'Gold Pouch',
     value: 900,
