e4a3a73793086d7cf603d7468aa119dedcff98b0
xandjiji <xandjiji@gmail.com>
2022-03-17 11:47:57 -0300

    test(utils): adding fetch() setup

--- apps/exevo-pan/src/utils/test/setup/index.ts
+++ apps/exevo-pan/src/utils/test/setup/index.ts
@@ -1,9 +1,13 @@
 export const setup = {
-  setTimeout: () => {
+  setTimeout: (): void => {
     jest.useFakeTimers()
 
     jest
       .spyOn(window, 'setTimeout')
       .mockImplementationOnce((fn) => fn() as unknown as NodeJS.Timeout)
+  },
+  fetch: (): jest.MockedFunction<typeof fetch> => {
+    global.fetch = jest.fn()
+    return fetch as jest.MockedFunction<typeof fetch>
   },
 }
