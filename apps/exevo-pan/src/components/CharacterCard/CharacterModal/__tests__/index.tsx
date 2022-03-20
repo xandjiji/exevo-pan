32a4d9dc1a609e98684d0308ca5918ba1e67f732
xandjiji <xandjiji@gmail.com>
2022-03-20 09:35:56 -0300

    test(CharacterModal): testing elements

--- apps/exevo-pan/src/components/CharacterCard/CharacterModal/__tests__/index.tsx
+++ apps/exevo-pan/src/components/CharacterCard/CharacterModal/__tests__/index.tsx
@@ -3,6 +3,7 @@ import userEvent from '@testing-library/user-event'
 import { renderWithProviders, randomDataset, setup } from 'utils/test'
 import { formatNumberWithCommas, calculateTotalInvestment } from 'utils'
 import CharacterModal from '..'
+import { tabCounter } from '../utils'
 
 const { characterData } = randomDataset()
 const characterList = characterData.slice(0, 10)
@@ -17,7 +18,87 @@ describe('<CharacterModal />', () => {
     setup.setTimeout()
   })
 
-  test.todo('should render every element correctly')
+  test.each(characterList)(
+    'should render every element correctly',
+    (character) => {
+      const {
+        outfits,
+        storeOutfits,
+        mounts,
+        storeMounts,
+        storeItems,
+        charms,
+        charmInfo,
+        hirelings,
+        achievementPoints,
+      } = character
+
+      renderWithProviders(
+        <CharacterModal characterData={character} onClose={mockOnClose} />,
+      )
+
+      const totalInvestment = formatNumberWithCommas(
+        calculateTotalInvestment(character),
+      )
+
+      if (totalInvestment === '0') {
+        expect(screen.queryByText(totalInvestment)).not.toBeInTheDocument()
+      } else {
+        expect(
+          screen.getByText(`${totalInvestment} Tibia Coins`, { exact: false }),
+        ).toBeInTheDocument()
+      }
+
+      expect(
+        screen.getByText(
+          `Outfits ${tabCounter(outfits.length, storeOutfits.length)}`,
+          { exact: false },
+        ),
+      ).toBeInTheDocument()
+
+      expect(
+        screen.getByText(
+          `Mounts ${tabCounter(mounts.length, storeMounts.length)}`,
+          { exact: false },
+        ),
+      ).toBeInTheDocument()
+
+      if (storeItems.length > 0) {
+        expect(
+          screen.getByText(`Store Items (${storeItems.length})`, {
+            exact: false,
+          }),
+        ).toBeInTheDocument()
+      } else {
+        expect(
+          screen.queryByText(`Store Items (${storeItems.length})`, {
+            exact: false,
+          }),
+        ).not.toBeInTheDocument()
+      }
+
+      expect(screen.getByText('Charms:', { exact: false }).textContent).toEqual(
+        `Charms: ${charms.length}/19 (${formatNumberWithCommas(
+          charmInfo.total,
+        )} total points,${formatNumberWithCommas(charmInfo.unspent)} unspent)`,
+      )
+
+      expect(
+        screen.getByText('Hirelings:', { exact: false }).textContent,
+      ).toEqual(
+        `Hirelings: ${hirelings.count} (${hirelings.jobs}/4 jobs, ${hirelings.outfits} outfits)`,
+      )
+
+      expect(
+        screen.getByText(
+          `Achievement points: ${formatNumberWithCommas(achievementPoints)}`,
+          {
+            exact: false,
+          },
+        ),
+      ).toBeInTheDocument()
+    },
+  )
 
   test('if a sprite is not found, it should call `NotifyErrorClient`', async () => {
     const [character] = characterList
