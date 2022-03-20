baf60d7800fcd90186fed88ee1e19865cca33d3b
xandjiji <xandjiji@gmail.com>
2022-03-20 08:51:36 -0300

    test(CharacterModal): testing  call

--- apps/exevo-pan/src/components/CharacterCard/CharacterModal/__tests__/index.tsx
+++ apps/exevo-pan/src/components/CharacterCard/CharacterModal/__tests__/index.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithProviders, randomDataset, setup } from 'utils/test'
 import { formatNumberWithCommas, calculateTotalInvestment } from 'utils'
@@ -7,18 +7,37 @@ import CharacterModal from '..'
 const { characterData } = randomDataset()
 const characterList = characterData.slice(0, 10)
 
+const mockedFetch = setup.fetch()
 const mockOnClose = jest.fn()
 
 describe('<CharacterModal />', () => {
   beforeEach(() => {
     mockOnClose.mockClear()
+    mockedFetch.mockClear()
+    setup.setTimeout()
   })
 
   test.todo('should render every element correctly')
 
   test.todo('should render every element correctly')
 
-  test.todo('if a sprite is not found, it should call `NotifyErrorClient`')
+  test('if a sprite is not found, it should call `NotifyErrorClient`', () => {
+    const [character] = characterList
+
+    expect(mockedFetch).toHaveBeenCalledTimes(0)
+
+    renderWithProviders(
+      <CharacterModal
+        characterData={{
+          ...character,
+          outfits: [{ name: 'weird outfit', type: 0 }],
+        }}
+        onClose={mockOnClose}
+      />,
+    )
+
+    expect(mockedFetch).toHaveBeenCalledTimes(1)
+  })
 
   test('should call `onClose` handler', () => {
     const [character] = characterList
