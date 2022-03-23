450f6844c457b1ff4b4f7d4e36bd42ec0783a2ff
xandjiji <xandjiji@gmail.com>
2022-03-23 10:47:16 -0300

    perf(fonts): preconnecting to google endpoints

--- apps/exevo-pan/src/pages/_document.tsx
+++ apps/exevo-pan/src/pages/_document.tsx
@@ -47,12 +47,17 @@ export default class MyDocument extends Document {
           <meta name="theme-color" content="#3F51B5" />
           <meta name="msapplication-navbutton-color" content="#3F51B5" />
 
-          <link rel="preconnect" href="https://fonts.gstatic.com/" />
-          <link rel="dns-prefetch" href="//fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
             rel="stylesheet"
           />
+
           <link rel="icon" href="/favicon.ico" />
 
           <meta
