ab3bed61e639362fb76254fe66ad6f3ae81d8d9e
xandjiji <xandjiji@gmail.com>
2022-03-23 11:04:43 -0300

    refactor(fonts): using old css endpoint

--- apps/exevo-pan/src/pages/_document.tsx
+++ apps/exevo-pan/src/pages/_document.tsx
@@ -54,7 +54,7 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap"
             rel="stylesheet"
           />
 
