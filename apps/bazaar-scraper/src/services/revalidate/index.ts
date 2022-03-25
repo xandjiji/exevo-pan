ba8dd5b76e0ed5a36c13842c21052cca53510e7e
xandjiji <xandjiji@gmail.com>
2022-03-25 09:54:25 -0300

    feature(services): error handling

--- apps/bazaar-scraper/src/services/revalidate/index.ts
+++ apps/bazaar-scraper/src/services/revalidate/index.ts
@@ -2,14 +2,23 @@ import fetch from 'node-fetch'
 
 const REQUEST_TIMEOUT = 15000
 
+const ENDPOINT = 'https://exevopan.com/api/revalidate'
+
+const buildRoute = (route?: string): string => {
+  const token = process.env.REVALIDATION_AUTH
+
+  if (!token) {
+    throw new Error(
+      'Invalid token! Add `export REVALIDATION_AUTH="cf943cab-a1d2-4447-a0c5-bd910aaac4d2"` to your `~/.bashrc` file',
+    )
+  }
+
+  return `${ENDPOINT}?secret=${token}${route ? `&route=${route}` : ''}`
+}
+
 export default class RevalidateClient {
-  private static ENDPOINT = `https://exevo-pan-i8ojiafg2-exevopan.vercel.app/api/revalidate?secret=${process.env}`
-
-  private static buildRoute = (route?: string): string =>
-    `${this.ENDPOINT}${route ? `&route=${route}` : ''}`
-
   static async route(route?: string): Promise<void> {
-    await fetch(this.buildRoute(route), {
+    await fetch(buildRoute(route), {
       timeout: REQUEST_TIMEOUT,
     })
   }
