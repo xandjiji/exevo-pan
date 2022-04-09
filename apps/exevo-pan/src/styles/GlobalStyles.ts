1a0610a1207ecfb61b5132f0773eb194e4952405
xandjiji <xandjiji@gmail.com>
2022-04-09 11:41:08 -0300

    feature(config): defining `screens` and `colors` theme

--- apps/exevo-pan/src/styles/GlobalStyles.ts
+++ apps/exevo-pan/src/styles/GlobalStyles.ts
@@ -23,6 +23,25 @@ const GlobalStyles = createGlobalStyle`
     --kwai: #ff7705;
     --kwaiSurface: #fff7f0;
     --kwaiVariant: #ffc696;
+
+    /* @ ToDo: remove this 'tw' prefix */
+    --tw-background: 238 238 238;
+    --tw-surface: 255 255 255;
+    --tw-onSurface: 0 0 0;
+    --tw-separator: 180 180 180;
+    --tw-primary: 63 81 181;
+    --tw-onPrimary: 255 255 255;
+    --tw-primaryVariant: 197 202 233;
+    --tw-darkerPrimary: 50 61 142;
+    --tw-green: 55 119 18;
+    --tw-red: 197 19 19;
+    --tw-alert: 249 238 193;
+    --tw-battleGreen: 67 182 0;
+    --tw-battleYellow: 255 221 0;
+    --tw-primaryVariantHighlight: 231 232 238;
+    --tw-kwai: 255 119 5;
+    --tw-kwaiSurface: 255 247 240;
+    --tw-kwaiVariant: 255 198 150;
   }
 
   [data-theme='dark'] {
@@ -43,6 +62,25 @@ const GlobalStyles = createGlobalStyle`
     --kwai: #ff7705;
     --kwaiSurface: #453d3c;
     --kwaiVariant: #995f2e;
+
+    /* @ ToDo: remove this 'tw' prefix */
+    --tw-background: 32 34 37;
+    --tw-surface: 54 57 63;
+    --tw-onSurface: 255 255 255;
+    --tw-separator: 114 118 125;
+    --tw-primary: 152 87 231;
+    --tw-onPrimary: 255 255 255;
+    --tw-primaryVariant: 94 68 128;
+    --tw-darkerPrimary: 88 31 155;
+    --tw-green: 90 153 53;
+    --tw-red: 255 91 91;
+    --tw-alert: 249 238 193;
+    --tw-battleGreen: 67 182 0;
+    --tw-battleYellow: 255 221 0;
+    --tw-primaryVariantHighlight: 113 76 161;
+    --tw-kwai: 255 119 5;
+    --tw-kwaiSurface: 69 61 60;
+    --tw-kwaiVariant: 153 95 46;
   }
 
   html {
--- apps/exevo-pan/tailwind.config.js
+++ apps/exevo-pan/tailwind.config.js
@@ -1,3 +1,12 @@
+const withOpacityValue = (variable) => {
+  return ({ opacityValue }) => {
+    if (opacityValue === undefined) {
+      return `rgb(var(${variable}))`
+    }
+    return `rgb(var(${variable}) / ${opacityValue})`
+  }
+}
+
 module.exports = {
   content: [
     './src/pages/**/*.tsx',
@@ -6,7 +15,32 @@ module.exports = {
     './src/modules/**/*.tsx',
   ],
   theme: {
-    extend: {},
+    screens: {
+      md: '768px',
+      lg: '1024px',
+    },
+    /* @ ToDo: remove this 'tw' prefix */
+    colors: {
+      background: withOpacityValue('(--tw-background)'),
+      surface: withOpacityValue('(--tw-surface)'),
+      onSurface: withOpacityValue('(--tw-onSurface)'),
+      separator: withOpacityValue('(--tw-separator)'),
+      primary: withOpacityValue('(--tw-primary)'),
+      onPrimary: withOpacityValue('(--tw-onPrimary)'),
+      primaryVariant: withOpacityValue('(--tw-primaryVariant)'),
+      darkerPrimary: withOpacityValue('(--tw-darkerPrimary)'),
+      green: withOpacityValue('(--tw-green)'),
+      red: withOpacityValue('(--tw-red)'),
+      alert: withOpacityValue('(--tw-alert)'),
+      battleGreen: withOpacityValue('(--tw-battleGreen)'),
+      battleYellow: withOpacityValue('(--tw-battleYellow)'),
+      primaryVariantHighlight: withOpacityValue(
+        '(--tw-primaryVariantHighlight)',
+      ),
+      kwai: withOpacityValue('(--tw-kwai)'),
+      kwaiSurface: withOpacityValue('(--tw-kwaiSurface)'),
+      kwaiVariant: withOpacityValue('(--tw-kwaiVariant)'),
+    },
   },
   plugins: [require('@tailwindcss/line-clamp')],
 }
