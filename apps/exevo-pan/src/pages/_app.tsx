7a794f60e9e9fdf9de342f351ada03bfe8abf8c6
xandjiji <xandjiji@gmail.com>
2022-04-10 09:26:07 -0300

    refactor(styles): splitting `GlobalStyles.ts` into `reset.css` and `globals.css`

--- apps/exevo-pan/src/pages/_app.tsx
+++ apps/exevo-pan/src/pages/_app.tsx
@@ -5,9 +5,9 @@ import ErrorBoundary from 'components/ErrorBoundary'
 import { ThemeProvider } from 'contexts/useTheme'
 import { TranslationsProvider } from 'contexts/useTranslation'
 import { gtag } from 'utils'
-import { GlobalStyles } from 'styles'
 import { AppProps } from 'next/app'
 import 'styles/globals.css'
+import 'styles/reset.css'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const { translations } = pageProps
@@ -45,7 +45,6 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           content="https://i.imgur.com/obDJJOI.png"
         />
       </Head>
-      <GlobalStyles />
       <TranslationsProvider value={{ translations }}>
         <ErrorBoundary>
           <ThemeProvider>
