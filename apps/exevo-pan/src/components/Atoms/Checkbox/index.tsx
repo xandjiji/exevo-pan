88f057fc65b209142c780a4a37f4e50ddaa6a608
xandjiji <xandjiji@gmail.com>
2022-04-15 16:02:56 -0300

    refactor(Checkbox): using tailwind

--- apps/exevo-pan/src/components/Atoms/Checkbox/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Checkbox/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import clsx from 'clsx'
 import { useState, useCallback } from 'react'
-import * as S from './styles'
+import TickIcon from 'assets/svgs/check.svg'
 import { CheckboxProps } from './types'
 
 const Checkbox = ({
@@ -8,6 +10,7 @@ const Checkbox = ({
   disabled = false,
   checked: checkedProp,
   onChange,
+  className,
   ...props
 }: CheckboxProps): JSX.Element => {
   const [innerChecked, setChecked] = useState(checkedProp ?? false)
@@ -22,9 +25,22 @@ const Checkbox = ({
   )
 
   return (
-    <S.Label disabled={disabled}>
-      <S.InputWrapper style={{ marginRight: label ? 6 : undefined }}>
-        <S.Input
+    <label
+      className={clsx(
+        'text-tsm flex cursor-pointer items-center',
+        disabled && 'pointer-events-none',
+      )}
+      style={{ lineHeight: 1 }}
+    >
+      <div
+        className="relative h-4"
+        style={{ marginRight: clsx(label && '6px') }}
+      >
+        <input
+          className={clsx(
+            'border-1 border-separator checked:bg-primary checked:border-primary disabled:bg-separator disabled:border-separator m-0 h-4 w-4 cursor-pointer appearance-none rounded border-solid transition-all active:shadow-inner',
+            className,
+          )}
           id={id}
           disabled={disabled}
           type="checkbox"
@@ -32,10 +48,16 @@ const Checkbox = ({
           onChange={handleChange}
           {...props}
         />
-        <S.TickIcon />
-      </S.InputWrapper>
+        <TickIcon
+          className={clsx(
+            'absolute top-1/2 left-1/2 h-[14px] w-[14px]',
+            checked ? 'fill-onPrimary' : 'fill-transparent',
+          )}
+          style={{ transform: 'translate(-50%,-50%)' }}
+        />
+      </div>
       {label}
-    </S.Label>
+    </label>
   )
 }
 
