3d2bb9df404cfc4cb0451189cbb938071f0da059
xandjiji <xandjiji@gmail.com>
2022-04-15 09:40:35 -0300

    refactor(ActiveCount): using tailwind

--- apps/exevo-pan/src/components/Atoms/ActiveCount/index.tsx
+++ apps/exevo-pan/src/components/Atoms/ActiveCount/index.tsx
@@ -1,9 +1,17 @@
-import * as S from './styles'
+import clsx from 'clsx'
 
 const ActiveCount = ({
+  className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>): JSX.Element => (
-  <S.ActiveIcon {...props} />
+  <div
+    className={clsx(
+      'bg-battleYellow flex h-[17px] w-[17px] cursor-default items-center justify-center rounded-r-full text-[11px] text-black shadow-md transition-opacity',
+      props['aria-hidden'] && 'opacity-0',
+      className,
+    )}
+    {...props}
+  />
 )
 
 export default ActiveCount
