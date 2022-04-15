98fd1854c2334ef74d53a2a2d1ecc7d4bd6ff055
xandjiji <xandjiji@gmail.com>
2022-04-15 16:21:18 -0300

    refactor(ActiveCount): using tailwind

--- apps/exevo-pan/src/components/Atoms/ActiveCount/index.tsx
+++ apps/exevo-pan/src/components/Atoms/ActiveCount/index.tsx
@@ -6,7 +6,7 @@ const ActiveCount = ({
 }: React.HTMLAttributes<HTMLDivElement>): JSX.Element => (
   <div
     className={clsx(
-      'bg-battleYellow flex h-[17px] w-[17px] cursor-default items-center justify-center rounded-r-full text-[11px] text-black shadow-md transition-opacity',
+      'bg-battleYellow flex h-[17px] w-[17px] cursor-default items-center justify-center rounded-full text-[11px] text-black shadow-md transition-opacity',
       props['aria-hidden'] && 'opacity-0',
       className,
     )}
