768b2de73bfeb280d14828e907be737e7660ef59
xandjiji <xandjiji@gmail.com>
2022-04-15 11:09:01 -0300

    fix(Main): wrapping around expanding layout

--- apps/exevo-pan/src/templates/Main/index.tsx
+++ apps/exevo-pan/src/templates/Main/index.tsx
@@ -4,9 +4,9 @@ import Footer from './Footer'
 type MainProps = { children: React.ReactNode }
 
 export default ({ children }: MainProps): JSX.Element => (
-  <>
+  <div className="flex min-h-screen flex-col">
     <Header />
     {children}
     <Footer />
-  </>
+  </div>
 )
