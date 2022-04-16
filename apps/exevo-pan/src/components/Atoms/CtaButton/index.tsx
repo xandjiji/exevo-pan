a03baa61706ede26126e0d5a9fbbda146f216a77
xandjiji <xandjiji@gmail.com>
2022-04-16 09:00:48 -0300

    refactor(CtaButton): using tailwind

--- apps/exevo-pan/src/components/Atoms/CtaButton/index.tsx
+++ apps/exevo-pan/src/components/Atoms/CtaButton/index.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { useTranslations } from 'contexts/useTranslation'
 import Image from 'next/image'
 import coinsGif from 'assets/tibiaCoins.gif'
-import * as S from './styles'
 
 const CtaButton = ({
   ...props
@@ -11,17 +11,22 @@ const CtaButton = ({
   } = useTranslations()
 
   return (
-    <S.Button
+    <a
       href={common.TibiaCoinsCta.link}
       target="_blank"
       rel="noopener external nofollow"
+      className="clickable text-tsm bg-primary text-onPrimary md:text-onSurface md:z-1 hover:highlight-10 md:animated-background fixed bottom-[42px] right-4 z-10 ml-2 flex items-end whitespace-nowrap rounded-2xl py-2 pr-4 pl-10 tracking-wider shadow-lg transition-all md:relative md:bottom-0 md:right-0 md:shadow-md"
       {...props}
     >
-      <S.ImgWrapper>
+      <div
+        className="absolute top-1/2 left-3 h-6 w-6"
+        style={{ transform: 'translateY(-50%)' }}
+      >
         <Image src={coinsGif} alt="Tibia Coins" width="24" height="24" />
-      </S.ImgWrapper>
+      </div>
+
       {common.TibiaCoinsCta.text}
-    </S.Button>
+    </a>
   )
 }
 
