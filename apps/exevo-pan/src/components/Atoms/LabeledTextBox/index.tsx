b6d5fd23ad1921ffe022d5b22665110069a9f87e
xandjiji <xandjiji@gmail.com>
2022-04-18 18:04:12 -0300

    refactor(LabeledTextBox): using tailwind

--- apps/exevo-pan/src/components/Atoms/LabeledTextBox/index.tsx
+++ apps/exevo-pan/src/components/Atoms/LabeledTextBox/index.tsx
@@ -1,12 +1,15 @@
 import { useTranslations } from 'contexts/useTranslation'
+import clsx from 'clsx'
 import { useUuid } from 'hooks'
-import * as S from './styles'
+import Image from 'next/image'
+import warningSrc from 'assets/warning.png'
 import { LabeledTextBoxProps } from './types'
 
 const LabeledTextBox = ({
+  className,
   children,
   labelText,
-  warning,
+  warning = false,
   ...props
 }: LabeledTextBoxProps): JSX.Element => {
   const {
@@ -16,20 +19,39 @@ const LabeledTextBox = ({
   const labelId = useUuid()
 
   return (
-    <S.Wrapper
-      suppressHydrationWarning
+    <div
+      className={clsx(
+        'border-1 relative rounded-md border-solid px-3 pt-[10px] pb-2',
+        warning ? 'border-red' : 'border-separator',
+        className,
+      )}
       aria-labelledby={labelText ? labelId : undefined}
-      warning={warning}
+      suppressHydrationWarning
       {...props}
     >
       {labelText && (
-        <S.Label suppressHydrationWarning id={labelId}>
+        <span
+          id={labelId}
+          suppressHydrationWarning
+          className={clsx(
+            'bg-surface absolute top-0 left-2 flex select-none items-center px-1 text-[9px] font-light uppercase tracking-wider',
+            warning ? 'text-red' : 'text-onSurface',
+          )}
+          style={{ transform: 'translateY(-50%)' }}
+        >
           {labelText}
-          {warning && <S.WarningIcon title={common.WarningLabel} unoptimized />}
-        </S.Label>
+          {warning && (
+            <Image
+              src={warningSrc}
+              title={common.WarningLabel}
+              unoptimized
+              className="warning-icon"
+            />
+          )}
+        </span>
       )}
       {children}
-    </S.Wrapper>
+    </div>
   )
 }
 
--- apps/exevo-pan/src/components/Atoms/LabeledTextBox/types.ts
+++ apps/exevo-pan/src/components/Atoms/LabeledTextBox/types.ts
@@ -3,9 +3,4 @@ import { HTMLAttributes } from 'react'
 export interface LabeledTextBoxProps extends HTMLAttributes<HTMLDivElement> {
   labelText?: string
   warning?: boolean
-  children?: React.ReactNode
-}
-
-export interface WrapperStyleProps {
-  warning?: boolean
 }
