088a4c033dfba157643dde4247bb52cdaddeb70e
xandjiji <xandjiji@gmail.com>
2022-04-19 08:04:28 -0300

    refactor(Option): using tailwind

--- apps/exevo-pan/src/components/Atoms/Option/types.ts
+++ apps/exevo-pan/src/components/Atoms/Option/types.ts
@@ -1,14 +1,13 @@
 import { HTMLAttributes } from 'react'
 
-export interface OptionProps {
+type ExtendedOptionProps = Omit<
+  HTMLAttributes<HTMLOptionElement>,
+  'children' | 'onClick'
+>
+
+export interface OptionProps extends ExtendedOptionProps {
   children: string
   value?: string
   highlighted?: boolean
   onClick?: (option: Option) => void
-  onMouseDown?: (event: React.MouseEvent) => void
-  props?: HTMLAttributes<HTMLOptionElement>
-}
-
-export interface OptionStyleProps {
-  highlighted?: boolean
 }
