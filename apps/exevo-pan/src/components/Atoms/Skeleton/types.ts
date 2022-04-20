2d5f3650c48cea1d8c892abad69291cc90c21aa7
xandjiji <xandjiji@gmail.com>
2022-04-20 10:29:28 -0300

    refactor(Skeleton): using tailwind

--- apps/exevo-pan/src/components/Atoms/Skeleton/types.ts
+++
@@ -1,9 +0,0 @@
-export type Animations = 'pulsate' | 'wave'
-
-export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
-  animation?: Animations
-}
-
-export interface SkeletonStyleProps {
-  animation?: Animations
-}
