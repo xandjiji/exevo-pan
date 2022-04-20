cfe8c1f2c21cab82b8dc0fe02665f5731d3b88c5
xandjiji <xandjiji@gmail.com>
2022-04-20 10:34:28 -0300

    chore: removing unused code

--- apps/exevo-pan/src/components/Atoms/Skeleton/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Skeleton/index.tsx
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 const Skeleton = ({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>): JSX.Element => (
+}: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={clsx(
       'bg-separator relative inline-block overflow-hidden rounded opacity-60 after:absolute after:top-0 after:left-0 after:h-full after:w-full',
