26dbf27bed7284173df7cfe04b6f462f64e876d8
xandjiji <xandjiji@gmail.com>
2022-04-22 11:23:31 -0300

    refactor(Text): using tailwind

--- apps/exevo-pan/src/components/Atoms/Text/TibiaCoin.tsx
+++ apps/exevo-pan/src/components/Atoms/Text/TibiaCoin.tsx
@@ -1,17 +1,25 @@
 import { memo } from 'react'
+import Image from 'next/image'
+import tibiaCoinSrc from 'assets/tibiacoin.png'
 import { formatNumberWithCommas } from 'utils'
-import * as S from './styles'
+import * as S from './atoms'
 
 type TibiaCoinProps = {
   value: number
 } & React.HTMLAttributes<HTMLSpanElement>
 
-const TibiaCoin = ({ value, ...props }: TibiaCoinProps): JSX.Element => {
+const TibiaCoin = ({ value, ...props }: TibiaCoinProps) => {
   const formattedValue = formatNumberWithCommas(value)
 
   return (
     <S.Flex title={`${formattedValue} Tibia Coins`} {...props}>
-      <S.Icon.TibiaCoin />
+      <Image
+        src={tibiaCoinSrc}
+        alt="Tibia Coin"
+        unoptimized
+        width={12}
+        height={12}
+      />
       {formattedValue}
     </S.Flex>
   )
---
+++ apps/exevo-pan/src/components/Atoms/Text/atoms.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx'
+
+export const Flex = ({
+  className,
+  ...props
+}: JSX.IntrinsicElements['span']) => (
+  <span
+    className={clsx(
+      'child:shrink-0 inline-flex items-center justify-center gap-1 whitespace-nowrap',
+      className,
+    )}
+    {...props}
+  />
+)
