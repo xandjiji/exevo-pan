820a0ba7aa33e87466dbdf8c106c16ca2eadd887
xandjiji <xandjiji@gmail.com>
2022-04-23 10:28:37 -0300

    refactor(Constants): adding colors to tags

--- apps/exevo-pan/src/Constants/BlogTags.ts
+++ apps/exevo-pan/src/Constants/BlogTags.ts
@@ -1,25 +1,34 @@
+import { generateTagColors } from 'utils/generateTagColor'
+
 export const tagById: Record<string, BlogTag> = {
   news: {
     id: 'news',
     color: 210,
+    ...generateTagColors(210),
   },
   article: {
     id: 'article',
     color: 40,
+    ...generateTagColors(40),
   },
   tutorial: {
     id: 'tutorial',
     color: 110,
+    ...generateTagColors(110),
   },
   tips: {
     id: 'tips',
     color: 300,
+    ...generateTagColors(300),
   },
 }
 
-export const all: BlogTag[] = Object.values(tagById)
+const all: BlogTag[] = Object.values(tagById)
+
+const fallbackColor = generateTagColors(0)
 
 export const blogTags = {
   all,
   tagById,
+  fallbackColor,
 }
