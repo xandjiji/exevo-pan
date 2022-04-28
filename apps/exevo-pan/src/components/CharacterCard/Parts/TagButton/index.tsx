f3945b6e5e5ff0a99e553eaa2f52120c63680851
xandjiji <xandjiji@gmail.com>
2022-04-28 08:36:11 -0300

    refactor(TagButton): using tailwind

--- apps/exevo-pan/src/components/CharacterCard/Parts/TagButton/index.tsx
+++ apps/exevo-pan/src/components/CharacterCard/Parts/TagButton/index.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useTranslations } from 'contexts/useTranslation'
 import NextLink from 'next/link'
 import { useState, useCallback } from 'react'
+import clsx from 'clsx'
+import TagIcon from 'assets/svgs/tag.svg'
+import AdvertiseIcon from 'assets/svgs/advertise.svg'
 import { useUuid } from 'hooks'
 import { routes } from 'Constants'
-import * as S from './styles'
 import { HoveredState } from './types'
 
 const TagButton = ({
@@ -27,27 +32,60 @@ const TagButton = ({
 
   return (
     <NextLink href={routes.ADVERTISE}>
-      <S.Wrapper
+      <div
         suppressHydrationWarning
         tabIndex={0}
-        animation={hoverState}
         onMouseOver={handleHover}
         onFocus={handleHover}
         onMouseOut={handleUnhover}
         onBlur={handleUnhover}
         aria-describedby={labelId}
         onClick={(event) => event.stopPropagation()}
+        className="relative h-11 w-7 shrink-0 cursor-pointer self-start"
         {...props}
       >
-        <S.IconWrapper>
-          <S.TagIcon />
-          <S.AdvertiseIcon />
-        </S.IconWrapper>
+        <div
+          className="z-2 absolute top-1/2 left-1/2 h-[72px] w-16"
+          style={{ transform: 'translate(-50%, -50%)' }}
+        />
+        <div
+          className={clsx(
+            'animate-swing z-1 relative h-full w-full',
+            hoverState === 'hover' && 'animate-tilt',
+            hoverState === 'off' && 'animate-letGo',
+            hoverState === 'initial' && 'animate-swing',
+          )}
+          style={{
+            transformOrigin: '50% 0',
+            animationDelay: clsx(hoverState === 'initial' && '1s'),
+          }}
+        >
+          <TagIcon
+            className="h-full w-full"
+            style={{ filter: 'drop-shadow(2px 2px 2px rgba(0, 0, 0, 0.3)' }}
+          />
+          <AdvertiseIcon
+            className="z-1 fill-onPrimary absolute h-4 w-4"
+            style={{
+              top: 'calc(50% + 6px)',
+              left: 'calc(50% - 1px)',
+              transform: 'translate(-50%, -50%)',
+            }}
+          />
+        </div>
 
-        <S.Text suppressHydrationWarning id={labelId}>
+        <p
+          suppressHydrationWarning
+          id={labelId}
+          className={clsx(
+            'text-onSurface absolute bottom-0 left-1/2 text-center text-xs transition-opacity',
+            hoverState !== 'hover' && 'opacity-0',
+          )}
+          style={{ transform: 'translateX(-50%)' }}
+        >
           {common.CharacterCard.highlightLabelText}
-        </S.Text>
-      </S.Wrapper>
+        </p>
+      </div>
     </NextLink>
   )
 }
