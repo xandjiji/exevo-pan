91e318145bcec168031f434945ae0a1473ec51cb
xandjiji <xandjiji@gmail.com>
2022-05-04 15:59:33 -0300

    refactor(PercentageCard): using tailwind

--- apps/exevo-pan/src/modules/Statistics/components/PercentageCard/index.tsx
+++ apps/exevo-pan/src/modules/Statistics/components/PercentageCard/index.tsx
@@ -1,16 +1,30 @@
 import { memo } from 'react'
-import * as S from './styles'
+import clsx from 'clsx'
 import { PercentageCardProps } from './types'
 
 const PercentageCard = ({
   title,
   percentage,
+  className,
   ...props
-}: PercentageCardProps): JSX.Element => (
-  <S.Wrapper {...props}>
-    <S.Title>{title}</S.Title>
-    <S.Percentage positive={percentage >= 50}>{percentage}%</S.Percentage>
-  </S.Wrapper>
+}: PercentageCardProps) => (
+  <section
+    className={clsx(
+      'card flex flex-col justify-center p-5 text-center transition-colors',
+      className,
+    )}
+    {...props}
+  >
+    <h4 className="text-onSurface text-base font-light">{title}</h4>
+    <span
+      className={clsx(
+        'text-[46px] font-bold',
+        percentage >= 50 ? 'text-green' : 'text-red',
+      )}
+    >
+      {percentage}%
+    </span>
+  </section>
 )
 
 export default memo(PercentageCard)
