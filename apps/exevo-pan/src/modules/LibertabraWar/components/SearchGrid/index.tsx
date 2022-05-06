42fefce83e1947d38828c1d1065323ff16ba014c
xandjiji <xandjiji@gmail.com>
2022-05-06 09:49:49 -0300

    chore: using brackets

--- apps/exevo-pan/src/modules/LibertabraWar/components/SearchGrid/index.tsx
+++ apps/exevo-pan/src/modules/LibertabraWar/components/SearchGrid/index.tsx
@@ -9,13 +9,15 @@ const SearchGrid = () => {
 
   const { warGuildData } = useWarGuildData()
 
-  if (warGuildData.length === 0)
+  if (warGuildData.length === 0) {
     return (
       <div
         className="loading-spinner z-1 fixed top-1/2 left-1/2"
         style={{ transform: 'translate(-50%, -50%)' }}
       />
     )
+  }
+
   return (
     <article className="inner-container custom-scrollbar bg-background relative max-h-[calc(100%-44px)] overflow-auto py-4 transition-colors">
       <h2 className="hidden">{war.SearchGrid.Title}</h2>
