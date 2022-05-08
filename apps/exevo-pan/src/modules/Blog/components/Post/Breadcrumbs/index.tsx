863c769ad6763884dd1f8fbf995a5f7b6bbd1bc3
xandjiji <xandjiji@gmail.com>
2022-05-08 10:01:59 -0300

    refactor(Breadcrumbs): using tailwind

--- apps/exevo-pan/src/modules/Blog/components/Post/Breadcrumbs/index.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/Breadcrumbs/index.tsx
@@ -1,24 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useTranslations } from 'contexts/useTranslation'
 import NextLink from 'next/link'
 import { routes } from 'Constants'
-import * as S from './styles'
 import { BreadcrumbsProps } from './types'
 
-const Breadcrumbs = ({
-  postTitle,
-  ...props
-}: BreadcrumbsProps): JSX.Element => {
+const Separator = () => <span className="mx-1 font-light after:content-['>']" />
+
+const Breadcrumbs = ({ postTitle, ...props }: BreadcrumbsProps) => {
   const {
     translations: { blog },
   } = useTranslations()
   return (
-    <S.Wrapper {...props}>
-      <NextLink href={routes.HOME}>{blog.Meta.breadcrumbRoot}</NextLink>
-      <S.Separator />
-      <NextLink href={routes.BLOG}>{blog.Meta.title}</NextLink>
-      <S.Separator />
-      <S.PostTitle>{postTitle}</S.PostTitle>
-    </S.Wrapper>
+    <div
+      className="text-tsm max-w-min overflow-hidden text-ellipsis whitespace-nowrap"
+      {...props}
+    >
+      <NextLink href={routes.HOME}>
+        <a className="text-primaryHighlight">{blog.Meta.breadcrumbRoot}</a>
+      </NextLink>
+      <Separator />
+      <NextLink href={routes.BLOG}>
+        <a className="text-primaryHighlight">{blog.Meta.title}</a>
+      </NextLink>
+      <Separator />
+      <span>{postTitle}</span>
+    </div>
   )
 }
 
