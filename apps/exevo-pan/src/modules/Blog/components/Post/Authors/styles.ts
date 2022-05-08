00944489c62aa5b4ec0b9aec91b81f1e08d5ad01
xandjiji <xandjiji@gmail.com>
2022-05-08 09:56:17 -0300

    refactor(Authors): using tailwind

--- apps/exevo-pan/src/modules/Blog/components/Post/Authors/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Title = styled.h3`
-  margin-bottom: 16px;
-  font-size: 19px;
-  font-weight: 300;
-  line-height: 1.3;
-  letter-spacing: 0.5px;
-`
