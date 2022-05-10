f356a2f1b7759ac143bdc58a4069fe64520eebc2
xandjiji <xandjiji@gmail.com>
2022-05-10 09:37:36 -0300

    refactor(Sprites): using tailwind

--- apps/exevo-pan/src/modules/Blog/components/Post/custom/Sprite/index.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/custom/Sprite/index.tsx
@@ -1,5 +1,6 @@
-import * as S from './styles'
 import { SpriteProps } from './types'
+
+const SPACE_GAP = 3
 
 const Sprite = ({
   src,
@@ -7,10 +8,28 @@ const Sprite = ({
   height,
   inline = false,
   children,
-}: SpriteProps): JSX.Element => (
-  <S.Wrapper src={src} width={width} height={height} inline={inline}>
+}: SpriteProps) => (
+  <span
+    className="relative whitespace-nowrap font-normal"
+    style={{
+      display: inline ? 'inline-block' : 'unset',
+      marginLeft: width + SPACE_GAP,
+      marginTop: inline ? SPACE_GAP : 'unset',
+      marginBottom: inline ? SPACE_GAP : 'unset',
+    }}
+  >
     {children}
-  </S.Wrapper>
+    <div
+      className="absolute top-1/2 bg-no-repeat"
+      style={{
+        left: -(width + SPACE_GAP),
+        width,
+        height,
+        backgroundImage: `url("${src}")`,
+        transform: 'translateY(-50%)',
+      }}
+    />
+  </span>
 )
 
 export default Sprite
