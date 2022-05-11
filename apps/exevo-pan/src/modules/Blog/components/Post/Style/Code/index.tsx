ac1c6d337a1eee6b37440475b4b5a42a316cb502
xandjiji <xandjiji@gmail.com>
2022-05-11 10:41:48 -0300

    chore: adding comments

--- apps/exevo-pan/src/modules/Blog/components/Post/Style/Code/index.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/Style/Code/index.tsx
@@ -5,6 +5,7 @@ export const pre = (args: JSX.IntrinsicElements['pre']) => (
   <pre {...args} className={clsx('m-0', styles.pre)} />
 )
 
+/* changes made here should also be added to `.contentWrapper` class */
 export const code = (args: JSX.IntrinsicElements['code']) => (
   <code {...args} className="code text-s py-[2px] px-2 tracking-wider" />
 )
