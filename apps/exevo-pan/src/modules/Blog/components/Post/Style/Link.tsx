5e4ecabe845cfe823517677d326fd8e3445896a1
xandjiji <xandjiji@gmail.com>
2022-05-11 11:44:23 -0300

    fix(Link): fixing cursor

--- apps/exevo-pan/src/modules/Blog/components/Post/Style/Link.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/Style/Link.tsx
@@ -3,7 +3,7 @@
 export const a = (args: JSX.IntrinsicElements['a']) => (
   <a
     {...args}
-    className="text-primaryHighlight selection:bg-primary selection:text-onPrimary bg from-primaryHighlight bg-gradient-to-r bg-left-bottom bg-no-repeat transition-all hover:opacity-75"
+    className="text-primaryHighlight selection:bg-primary selection:text-onPrimary bg from-primaryHighlight cursor-pointer bg-gradient-to-r bg-left-bottom bg-no-repeat transition-all hover:opacity-75"
     style={{ backgroundSize: '14px 1px' }}
   />
 )
