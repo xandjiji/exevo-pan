1fd8de9f1109e09e675ed32d0d6b94b970e6c398
xandjiji <xandjiji@gmail.com>
2022-05-17 09:48:09 -0300

    feature(RangeDatePicker): reviewing shadows

--- apps/exevo-pan/src/modules/Advertise/components/AdConfiguration/RangeDatePicker/atoms.tsx
+++ apps/exevo-pan/src/modules/Advertise/components/AdConfiguration/RangeDatePicker/atoms.tsx
@@ -5,7 +5,7 @@ export const Day = ({ today = false, className, ...props }: DayProps) => (
   <button
     type="button"
     className={clsx(
-      'disabled:text-separator text-onSurface cursor-pointer rounded-md py-2 px-1 text-base shadow transition-all hover:shadow-md active:shadow-inner disabled:pointer-events-none disabled:shadow-none',
+      'disabled:text-separator text-onSurface cursor-pointer rounded-md py-2 px-1 text-base shadow-[2px_2px_2px_0px_rgba(0,0,0,0.09)] transition-all hover:shadow active:shadow-inner disabled:pointer-events-none disabled:shadow-none',
       today ? 'font-bold' : 'font-light',
       props['aria-selected'] && 'bg-primaryVariant',
       className,
