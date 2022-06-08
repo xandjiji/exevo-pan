9994f3a37358a2dabfbcd49f5d1c0e745990d8df
xandjiji <xandjiji@gmail.com>
2022-06-08 11:50:11 -0300

    test(Select): initial tests

---
+++ apps/exevo-pan/src/components/Organisms/Select/__tests__/index.tsx
@@ -0,0 +1,23 @@
+import { screen, fireEvent, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithProviders, setup } from 'utils/test'
+import Select from '..'
+import { mockedOptionList } from './mock'
+
+describe('<Select />', () => {
+  test.todo('should render everything correctly (including placeholder)')
+
+  test.todo('listbox state should be managed correctly')
+
+  test.todo('controlled/uncontrolled (defaultValue too)')
+
+  test.todo('controlled/uncontrolled keyboard navigation')
+
+  test.todo('search type')
+
+  test.todo('error')
+
+  test.todo('disabled')
+
+  test.todo('a11y')
+})
---
+++ apps/exevo-pan/src/components/Organisms/Select/__tests__/mock.ts
@@ -0,0 +1,414 @@
+export const mockedOptionList = [
+  {
+    name: 'Adra',
+    value: 'adra',
+  },
+  {
+    name: 'Antica',
+    value: 'antica',
+  },
+  {
+    name: 'Assombra',
+    value: 'assombra',
+  },
+  {
+    name: 'Astera',
+    value: 'astera',
+  },
+  {
+    name: 'Belluma',
+    value: 'belluma',
+  },
+  {
+    name: 'Belobra',
+    value: 'belobra',
+  },
+  {
+    name: 'Bona',
+    value: 'bona',
+  },
+  {
+    name: 'Calmera',
+    value: 'calmera',
+  },
+  {
+    name: 'Carnera',
+    value: 'carnera',
+  },
+  {
+    name: 'Celebra',
+    value: 'celebra',
+  },
+  {
+    name: 'Celesta',
+    value: 'celesta',
+  },
+  {
+    name: 'Concorda',
+    value: 'concorda',
+  },
+  {
+    name: 'Cosera',
+    value: 'cosera',
+  },
+  {
+    name: 'Damora',
+    value: 'damora',
+  },
+  {
+    name: 'Descubra',
+    value: 'descubra',
+  },
+  {
+    name: 'Dibra',
+    value: 'dibra',
+  },
+  {
+    name: 'Duna',
+    value: 'duna',
+  },
+  {
+    name: 'Emera',
+    value: 'emera',
+  },
+  {
+    name: 'Epoca',
+    value: 'epoca',
+  },
+  {
+    name: 'Estela',
+    value: 'estela',
+  },
+  {
+    name: 'Faluna',
+    value: 'faluna',
+  },
+  {
+    name: 'Ferobra',
+    value: 'ferobra',
+  },
+  {
+    name: 'Firmera',
+    value: 'firmera',
+  },
+  {
+    name: 'Funera',
+    value: 'funera',
+  },
+  {
+    name: 'Furia',
+    value: 'furia',
+  },
+  {
+    name: 'Garnera',
+    value: 'garnera',
+  },
+  {
+    name: 'Gentebra',
+    value: 'gentebra',
+  },
+  {
+    name: 'Gladera',
+    value: 'gladera',
+  },
+  {
+    name: 'Harmonia',
+    value: 'harmonia',
+  },
+  {
+    name: 'Helera',
+    value: 'helera',
+  },
+  {
+    name: 'Honbra',
+    value: 'honbra',
+  },
+  {
+    name: 'Impera',
+    value: 'impera',
+  },
+  {
+    name: 'Inabra',
+    value: 'inabra',
+  },
+  {
+    name: 'Javibra',
+    value: 'javibra',
+  },
+  {
+    name: 'Jonera',
+    value: 'jonera',
+  },
+  {
+    name: 'Kalibra',
+    value: 'kalibra',
+  },
+  {
+    name: 'Karna',
+    value: 'karna',
+  },
+  {
+    name: 'Kenora',
+    value: 'kenora',
+  },
+  {
+    name: 'Libertabra',
+    value: 'libertabra',
+  },
+  {
+    name: 'Lobera',
+    value: 'lobera',
+  },
+  {
+    name: 'Luminera',
+    value: 'luminera',
+  },
+  {
+    name: 'Lutabra',
+    value: 'lutabra',
+  },
+  {
+    name: 'Macabra',
+    value: 'macabra',
+  },
+  {
+    name: 'Menera',
+    value: 'menera',
+  },
+  {
+    name: 'Mitigera',
+    value: 'mitigera',
+  },
+  {
+    name: 'Monza',
+    value: 'monza',
+  },
+  {
+    name: 'Mudabra',
+    value: 'mudabra',
+  },
+  {
+    name: 'Nefera',
+    value: 'nefera',
+  },
+  {
+    name: 'Noctera',
+    value: 'noctera',
+  },
+  {
+    name: 'Nossobra',
+    value: 'nossobra',
+  },
+  {
+    name: 'Olera',
+    value: 'olera',
+  },
+  {
+    name: 'Ombra',
+    value: 'ombra',
+  },
+  {
+    name: 'Optera',
+    value: 'optera',
+  },
+  {
+    name: 'Pacembra',
+    value: 'pacembra',
+  },
+  {
+    name: 'Pacera',
+    value: 'pacera',
+  },
+  {
+    name: 'Peloria',
+    value: 'peloria',
+  },
+  {
+    name: 'Premia',
+    value: 'premia',
+  },
+  {
+    name: 'Pyra',
+    value: 'pyra',
+  },
+  {
+    name: 'Quelibra',
+    value: 'quelibra',
+  },
+  {
+    name: 'Quintera',
+    value: 'quintera',
+  },
+  {
+    name: 'Ragna',
+    value: 'ragna',
+  },
+  {
+    name: 'Refugia',
+    value: 'refugia',
+  },
+  {
+    name: 'Reinobra',
+    value: 'reinobra',
+  },
+  {
+    name: 'Relania',
+    value: 'relania',
+  },
+  {
+    name: 'Relembra',
+    value: 'relembra',
+  },
+  {
+    name: 'Secura',
+    value: 'secura',
+  },
+  {
+    name: 'Serdebra',
+    value: 'serdebra',
+  },
+  {
+    name: 'Serenebra',
+    value: 'serenebra',
+  },
+  {
+    name: 'Solidera',
+    value: 'solidera',
+  },
+  {
+    name: 'Talera',
+    value: 'talera',
+  },
+  {
+    name: 'Torpera',
+    value: 'torpera',
+  },
+  {
+    name: 'Tortura',
+    value: 'tortura',
+  },
+  {
+    name: 'Unica',
+    value: 'unica',
+  },
+  {
+    name: 'Utobra',
+    value: 'utobra',
+  },
+  {
+    name: 'Venebra',
+    value: 'venebra',
+  },
+  {
+    name: 'Vita',
+    value: 'vita',
+  },
+  {
+    name: 'Vunira',
+    value: 'vunira',
+  },
+  {
+    name: 'Wintera',
+    value: 'wintera',
+  },
+  {
+    name: 'Wizera',
+    value: 'wizera',
+  },
+  {
+    name: 'Xandebra',
+    value: 'xandebra',
+  },
+  {
+    name: 'Xylona',
+    value: 'xylona',
+  },
+  {
+    name: 'Yonabra',
+    value: 'yonabra',
+  },
+  {
+    name: 'Ysolera',
+    value: 'ysolera',
+  },
+  {
+    name: 'Zenobra',
+    value: 'zenobra',
+  },
+  {
+    name: 'Zuna',
+    value: 'zuna',
+  },
+  {
+    name: 'Zunera',
+    value: 'zunera',
+  },
+  {
+    name: 'Fera',
+    value: 'fera',
+  },
+  {
+    name: 'Fervora',
+    value: 'fervora',
+  },
+  {
+    name: 'Juva',
+    value: 'juva',
+  },
+  {
+    name: 'Mercera',
+    value: 'mercera',
+  },
+  {
+    name: 'Nexa',
+    value: 'nexa',
+  },
+  {
+    name: 'Unisera',
+    value: 'unisera',
+  },
+  {
+    name: 'Visabra',
+    value: 'visabra',
+  },
+  {
+    name: 'Alumbra',
+    value: 'alumbra',
+  },
+  {
+    name: 'Famosa',
+    value: 'famosa',
+  },
+  {
+    name: 'Marcia',
+    value: 'marcia',
+  },
+  {
+    name: 'Tembra',
+    value: 'tembra',
+  },
+  {
+    name: 'Endebra',
+    value: 'endebra',
+  },
+  {
+    name: 'Endera',
+    value: 'endera',
+  },
+  {
+    name: 'Endura',
+    value: 'endura',
+  },
+  {
+    name: 'Velocera',
+    value: 'velocera',
+  },
+  {
+    name: 'Velocibra',
+    value: 'velocibra',
+  },
+  {
+    name: 'Velocita',
+    value: 'velocita',
+  },
+]
