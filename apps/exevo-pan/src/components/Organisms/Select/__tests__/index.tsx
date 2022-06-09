25926ea2a45fc7c1ee389a4c11173a01916e7e26
xandjiji <xandjiji@gmail.com>
2022-06-09 10:05:28 -0300

    test(Select): testing controlled/uncontrolled component

--- apps/exevo-pan/src/components/Organisms/Select/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Organisms/Select/__tests__/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithProviders, setup } from 'utils/test'
@@ -107,14 +108,137 @@ describe('<Select />', () => {
       userEvent.keyboard('{escape}')
       assertOpen(false)
 
+      userEvent.keyboard('{backspace}')
       userEvent.keyboard('{space}')
       assertOpen()
     })
   })
 
-  test.todo('controlled/uncontrolled (defaultValue too)')
+  describe('should manage its value state correctly', () => {
+    test('uncontrolled', () => {
+      const onChange = jest.fn()
+      const initialOptionIndex = 5
+      const { combobox, open } = setupTest({
+        defaultValue: mockedOptionList[initialOptionIndex].value,
+        onChange,
+      })
 
-  test.todo('controlled/uncontrolled keyboard navigation')
+      const assertSelectedOption = (optionIndex: number) => {
+        const option = mockedOptionList[optionIndex]
+        expect(combobox).toHaveTextContent(option.name)
+      }
+
+      let changeCalls = 0
+      const clickAndAssert = (optionIndex: number) => {
+        userEvent.click(screen.getAllByRole('option')[optionIndex])
+        changeCalls += 1
+        expect(onChange).toHaveBeenCalledTimes(changeCalls)
+
+        assertSelectedOption(optionIndex)
+      }
+
+      assertSelectedOption(initialOptionIndex)
+
+      open()
+      clickAndAssert(1)
+
+      open()
+      clickAndAssert(2)
+
+      open()
+      userEvent.keyboard('{arrowup}')
+      changeCalls += 1
+      assertSelectedOption(1)
+      userEvent.keyboard('{arrowdown}')
+      changeCalls += 1
+      assertSelectedOption(2)
+      userEvent.keyboard('{arrowdown}')
+      changeCalls += 1
+      assertSelectedOption(3)
+
+      clickAndAssert(0)
+    })
+
+    test('controlled', () => {
+      const initialOptionIndex = 5
+      const initialOption = mockedOptionList[initialOptionIndex]
+      const ControlledComponent = () => {
+        const [value, setValue] = useState(initialOption.value)
+
+        return (
+          <Select
+            {...props}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        )
+      }
+
+      renderWithProviders(<ControlledComponent />)
+
+      const combobox = screen.getByRole('combobox', { name: props.label })
+      const assertSelectedOption = (optionIndex: number) => {
+        const option = mockedOptionList[optionIndex]
+        expect(combobox).toHaveTextContent(option.name)
+      }
+
+      const clickAndAssert = (optionIndex: number) => {
+        userEvent.click(screen.getAllByRole('option')[optionIndex])
+        assertSelectedOption(optionIndex)
+      }
+
+      assertSelectedOption(initialOptionIndex)
+      userEvent.click(combobox)
+      clickAndAssert(1)
+      userEvent.click(combobox)
+      clickAndAssert(2)
+
+      userEvent.click(combobox)
+      userEvent.keyboard('{arrowup}')
+      assertSelectedOption(1)
+      userEvent.keyboard('{arrowdown}')
+      assertSelectedOption(2)
+      userEvent.keyboard('{arrowdown}')
+
+      clickAndAssert(0)
+    })
+
+    test('with forced value', () => {
+      let forcedOption = { ...mockedOptionList[5] }
+      const { rerender } = renderWithProviders(
+        <Select {...props} value={forcedOption.value} />,
+      )
+
+      const combobox = screen.getByRole('combobox', { name: props.label })
+
+      const assertForcedOption = () => {
+        expect(combobox).toHaveTextContent(forcedOption.name)
+      }
+
+      assertForcedOption()
+      userEvent.click(combobox)
+      userEvent.click(
+        screen.getByRole('option', { name: mockedOptionList[2].name }),
+      )
+      assertForcedOption()
+      userEvent.click(combobox)
+      userEvent.keyboard('{arrowdown}')
+      assertForcedOption()
+
+      forcedOption = { ...mockedOptionList[1] }
+
+      rerender(<Select {...props} value={forcedOption.value} />)
+
+      assertForcedOption()
+      userEvent.click(
+        screen.getByRole('option', { name: mockedOptionList[2].name }),
+      )
+      assertForcedOption()
+      userEvent.click(combobox)
+      userEvent.keyboard('{arrowup}')
+      assertForcedOption()
+    })
+  })
 
   test.todo('search type')
 
