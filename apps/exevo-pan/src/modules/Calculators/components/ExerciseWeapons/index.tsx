9e7f9779ae51989f89328f175134f7b6f4b0b50c
xandjiji <xandjiji@gmail.com>
2022-06-14 17:05:23 -0300

    feature(ExerciseWeapons): configuring layout

--- apps/exevo-pan/src/modules/Calculators/components/ExerciseWeapons/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/components/ExerciseWeapons/index.tsx
@@ -1,6 +1,8 @@
 import { useState, useMemo } from 'react'
+import clsx from 'clsx'
 import { Input, Slider, Checkbox } from 'components/Atoms'
 import { Select } from 'components/Organisms'
+import ChevronRight from 'assets/svgs/chevronRight.svg'
 import * as CONSTANTS from './constants'
 import { calculateRequiredPoints } from './utils'
 import { TypedOption, Vocation, Skill } from './types'
@@ -65,78 +67,98 @@ export const ExerciseWeapons = () => {
     ],
   )
 
+  const invalidSkill = targetSkill <= currentSkill
+
   return (
     <div>
-      <Select
-        label="Vocation"
-        options={vocationOptions}
-        value={vocation}
-        onChange={(e) => setVocation(e.target.value as Vocation)}
-      />
+      <div className="grid gap-4">
+        <Select
+          label="Vocation"
+          options={vocationOptions}
+          value={vocation}
+          onChange={(e) => setVocation(e.target.value as Vocation)}
+          noAlert
+        />
 
-      <Select
-        label="Skill"
-        options={skillOptions}
-        value={skill}
-        onChange={(e) => setSkill(e.target.value as Skill)}
-      />
+        <Select
+          label="Skill"
+          options={skillOptions}
+          value={skill}
+          onChange={(e) => setSkill(e.target.value as Skill)}
+          noAlert
+        />
 
-      <Input
-        label="Current skill"
-        type="number"
-        value={currentSkill}
-        onChange={(e) => setCurrentSkill(+e.target.value)}
-      />
+        <div className="flex gap-8">
+          <div className="flex items-end gap-2">
+            <Input
+              label="Current skill"
+              type="number"
+              value={currentSkill}
+              onChange={(e) => setCurrentSkill(+e.target.value)}
+              error={invalidSkill}
+              noAlert
+              className="w-20"
+            />
+            <ChevronRight
+              className={clsx(
+                'mb-1.5',
+                invalidSkill ? 'fill-red' : 'fill-onSurface',
+              )}
+            />
+            <Input
+              label="Target skill"
+              type="number"
+              value={targetSkill}
+              onChange={(e) => setTargetSkill(+e.target.value)}
+              error={invalidSkill}
+              noAlert
+              className="w-20"
+            />
+          </div>
 
-      <Slider
-        label="% left"
-        min={0}
-        max={100}
-        step={0.01}
-        showInput
-        invert
-        value={percentageLeft}
-        onChange={(e) => setPercentageLeft(+e.target.value)}
-      />
+          <Slider
+            label="% left"
+            min={0}
+            max={100}
+            step={0.01}
+            showInput
+            invert
+            value={percentageLeft}
+            onChange={(e) => setPercentageLeft(+e.target.value)}
+            className="flex-grow"
+          />
+        </div>
 
-      <div className="my-4" />
+        <Slider
+          label="Loyalty"
+          min={0}
+          max={50}
+          step={5}
+          displayValue
+          transformDisplayedValues={(value) => {
+            if (!value) return 'None'
 
-      <Slider
-        label="Loyalty"
-        min={0}
-        max={50}
-        step={5}
-        displayValue
-        transformDisplayedValues={(value) => {
-          if (!value) return 'None'
+            return `${value}% bonus`
+          }}
+          marks={[
+            { label: 'None', value: 0 },
+            { label: '5%', value: 5 },
+            { label: '10%', value: 10 },
+            { label: '15%', value: 15 },
+            { label: '20%', value: 20 },
+            { label: '25%', value: 25 },
+            { label: '30%', value: 30 },
+            { label: '35%', value: 35 },
+            { label: '40%', value: 40 },
+            { label: '45%', value: 45 },
+            { label: '50%', value: 50 },
+          ]}
+          value={loyaltyBonus}
+          onChange={(e) => setLoyaltyBonus(+e.target.value)}
+        />
+      </div>
 
-          return `${value}% bonus`
-        }}
-        marks={[
-          { label: 'None', value: 0 },
-          { label: '5%', value: 5 },
-          { label: '10%', value: 10 },
-          { label: '15%', value: 15 },
-          { label: '20%', value: 20 },
-          { label: '25%', value: 25 },
-          { label: '30%', value: 30 },
-          { label: '35%', value: 35 },
-          { label: '40%', value: 40 },
-          { label: '45%', value: 45 },
-          { label: '50%', value: 50 },
-        ]}
-        value={loyaltyBonus}
-        onChange={(e) => setLoyaltyBonus(+e.target.value)}
-      />
-
-      <div className="my-4" />
-
-      <Input
-        label="Target skill"
-        type="number"
-        value={targetSkill}
-        onChange={(e) => setTargetSkill(+e.target.value)}
-      />
+      <div className="mb-[200px]" />
 
       <Checkbox
         label="Exercise dummy"
@@ -146,10 +168,7 @@ export const ExerciseWeapons = () => {
       <Checkbox
         label="Double event"
         checked={isDouble}
-        onChange={(e) => {
-          console.log(e)
-          setIsDouble(e.target.checked)
-        }}
+        onChange={(e) => setIsDouble(e.target.checked)}
       />
 
       <p>
