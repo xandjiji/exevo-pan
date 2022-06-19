84a8020642ce431d0c0cc14dcafcad6e10d1fc39
xandjiji <xandjiji@gmail.com>
2022-06-19 11:15:17 -0300

    refactor(Summary): dividing extra configurations

--- apps/exevo-pan/src/modules/Calculators/components/ExerciseWeapons/Summary/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/components/ExerciseWeapons/Summary/index.tsx
@@ -45,110 +45,114 @@ const Summary = ({ pointsRequired }: SummaryProps) => {
   const isClient = !isServer()
 
   return (
-    <LabeledCard labelText="Extra">
-      <div className="flex flex-col items-start justify-between gap-4 sm:flex-row">
-        <ClientComponent className="grid gap-4">
-          <Checkbox
-            label="Exercise dummy"
-            checked={hasDummy}
-            onChange={(e) => setHasDummy(e.target.checked)}
-          />
-          <Checkbox
-            label="Double event"
-            checked={isDouble}
-            onChange={(e) => setIsDouble(e.target.checked)}
-          />
-        </ClientComponent>
+    <div className="grid gap-6">
+      <LabeledCard labelText="Extra">
+        <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
+          <ClientComponent className="grid gap-4">
+            <Checkbox
+              label="Exercise dummy"
+              checked={hasDummy}
+              onChange={(e) => setHasDummy(e.target.checked)}
+            />
+            <Checkbox
+              label="Double event"
+              checked={isDouble}
+              onChange={(e) => setIsDouble(e.target.checked)}
+            />
+          </ClientComponent>
 
-        <Select
-          label="Weapon charges"
-          options={weaponOptions}
-          value={exerciseWeapon}
-          onChange={(e) => setExerciseWeapon(e.target.value as WeaponOption)}
-          noAlert
-          className="w-[180px]"
-        />
-      </div>
-
-      <S.Group>
-        <div className="flex items-center gap-1">
-          <strong>Money cost</strong>
-          <InfoTooltip
-            className="h-3 w-3"
-            content={
-              <span className="block w-36 leading-tight">
-                If the TC price is <strong>higher</strong> than{' '}
-                <Text.GoldCoin value={10500} /> gp then you should buy exercise
-                weapons using <strong>gold</strong>.
-              </span>
-            }
+          <Select
+            label="Weapon charges"
+            options={weaponOptions}
+            value={exerciseWeapon}
+            onChange={(e) => setExerciseWeapon(e.target.value as WeaponOption)}
+            noAlert
+            className="w-[180px]"
           />
         </div>
-        <S.ChipWrapper className="shrink-0 flex-wrap">
-          <S.Chip>
-            <Text.TibiaCoin value={cost.tc} />
-          </S.Chip>
-          <small className="-mx-2.5 font-light">or</small>
-          <S.Chip>
-            <Text.GoldCoin value={cost.gold} />
-          </S.Chip>
-        </S.ChipWrapper>
-      </S.Group>
+      </LabeledCard>
 
-      <S.Group>
-        <p>
-          <strong>Weapons</strong>
-        </p>
-        <ClientComponent>
-          <S.ChipWrapper className="flex-wrap">
-            <S.Chip aria-hidden={!weaponsRequired.lasting}>
-              <S.Weapon.lasting /> lasting weapons
-              <S.ActiveCount>{weaponsRequired.lasting}x</S.ActiveCount>
+      <LabeledCard labelText="Results">
+        <S.Group>
+          <div className="flex items-center gap-1">
+            <strong>Money cost</strong>
+            <InfoTooltip
+              className="h-3 w-3"
+              content={
+                <span className="block w-36 leading-tight">
+                  If the TC price is <strong>higher</strong> than{' '}
+                  <Text.GoldCoin value={10500} /> gp then you should buy
+                  exercise weapons using <strong>gold</strong>.
+                </span>
+              }
+            />
+          </div>
+          <S.ChipWrapper className="shrink-0 flex-wrap">
+            <S.Chip>
+              <Text.TibiaCoin value={cost.tc} />
             </S.Chip>
-            <S.Chip aria-hidden={!weaponsRequired.durable}>
-              <S.Weapon.durable /> durable weapons
-              <S.ActiveCount>{weaponsRequired.durable}x</S.ActiveCount>
+            <small className="-mx-2.5 font-light">or</small>
+            <S.Chip>
+              <Text.GoldCoin value={cost.gold} />
             </S.Chip>
-            <S.Chip aria-hidden={!weaponsRequired.regular}>
-              <S.Weapon.regular /> regular weapons
-              <S.ActiveCount>{weaponsRequired.regular}x</S.ActiveCount>
-            </S.Chip>
-            <S.Empty aria-hidden={!isObjectEmpty(weaponsRequired)}>
-              None
-            </S.Empty>
           </S.ChipWrapper>
-        </ClientComponent>
-      </S.Group>
+        </S.Group>
 
-      <S.Group>
-        <p>
-          <strong>Time required</strong>
-        </p>
-        <ClientComponent>
-          <S.ChipWrapper>
-            <S.TimeBubble
-              time={timeObject.days}
-              aria-hidden={isClient && !timeObject.days}
-            >
-              days
-            </S.TimeBubble>
-            <S.TimeBubble
-              time={timeObject.hours}
-              aria-hidden={!timeObject.hours}
-            >
-              hours
-            </S.TimeBubble>
-            <S.TimeBubble
-              time={timeObject.minutes}
-              aria-hidden={!timeObject.minutes}
-            >
-              minutes
-            </S.TimeBubble>
-            <S.Empty aria-hidden={!isObjectEmpty(timeObject)}>None</S.Empty>
-          </S.ChipWrapper>
-        </ClientComponent>
-      </S.Group>
-    </LabeledCard>
+        <S.Group>
+          <p>
+            <strong>Weapons</strong>
+          </p>
+          <ClientComponent>
+            <S.ChipWrapper className="flex-wrap">
+              <S.Chip aria-hidden={!weaponsRequired.lasting}>
+                <S.Weapon.lasting /> lasting weapons
+                <S.ActiveCount>{weaponsRequired.lasting}x</S.ActiveCount>
+              </S.Chip>
+              <S.Chip aria-hidden={!weaponsRequired.durable}>
+                <S.Weapon.durable /> durable weapons
+                <S.ActiveCount>{weaponsRequired.durable}x</S.ActiveCount>
+              </S.Chip>
+              <S.Chip aria-hidden={!weaponsRequired.regular}>
+                <S.Weapon.regular /> regular weapons
+                <S.ActiveCount>{weaponsRequired.regular}x</S.ActiveCount>
+              </S.Chip>
+              <S.Empty aria-hidden={!isObjectEmpty(weaponsRequired)}>
+                None
+              </S.Empty>
+            </S.ChipWrapper>
+          </ClientComponent>
+        </S.Group>
+
+        <S.Group>
+          <p>
+            <strong>Time required</strong>
+          </p>
+          <ClientComponent>
+            <S.ChipWrapper>
+              <S.TimeBubble
+                time={timeObject.days}
+                aria-hidden={isClient && !timeObject.days}
+              >
+                days
+              </S.TimeBubble>
+              <S.TimeBubble
+                time={timeObject.hours}
+                aria-hidden={!timeObject.hours}
+              >
+                hours
+              </S.TimeBubble>
+              <S.TimeBubble
+                time={timeObject.minutes}
+                aria-hidden={!timeObject.minutes}
+              >
+                minutes
+              </S.TimeBubble>
+              <S.Empty aria-hidden={!isObjectEmpty(timeObject)}>None</S.Empty>
+            </S.ChipWrapper>
+          </ClientComponent>
+        </S.Group>
+      </LabeledCard>
+    </div>
   )
 }
 
