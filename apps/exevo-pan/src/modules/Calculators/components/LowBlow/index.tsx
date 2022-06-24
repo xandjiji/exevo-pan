3c00bf71be79c330ea911d05bc73124b260f7696
xandjiji <xandjiji@gmail.com>
2022-06-24 10:37:28 -0300

    feature(i18n): adding translations

--- apps/exevo-pan/src/modules/Calculators/components/LowBlow/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/components/LowBlow/index.tsx
@@ -17,31 +17,34 @@ const translations = {
     finalAverageDamage: 'Final average damage',
     elementalCharm: 'Elemental charm',
     creatureHP: 'Creature HP',
+    elementalBonus: 'Elemental damage bonus',
   },
   es: {
     yourAverageDamage: 'Tu daño promedio',
     finalAverageDamage: 'Daño promedio final',
     elementalCharm: 'Charm Elemental',
     creatureHP: 'Vida de la criatura',
+    elementalBonus: 'Bono de daño elemental',
   },
   pl: {
     yourAverageDamage: 'Twoje średnie obrażenia',
     finalAverageDamage: 'Końcowe średnie obrażenia',
     elementalCharm: 'Charm ofensywny',
     creatureHP: 'Punkty zdrowia potwora',
+    elementalBonus: 'Premia do obrażeń od żywiołów',
   },
   pt: {
     yourAverageDamage: 'Seu dano médio',
     finalAverageDamage: 'Dano médio final',
     elementalCharm: 'Charm elemental',
     creatureHP: 'HP da criatura',
+    elementalBonus: 'Dano elemental bônus',
   },
 }
 
 /* @ ToDo:
 
 - separar utils
-- i18n
 */
 
 const transformBonusResistance = (value: number) =>
@@ -120,7 +123,7 @@ export const Calculator = () => {
         />
 
         <Slider
-          label="Elemental bonus damage:"
+          label={`${translations[locale].elementalBonus}:`}
           min={0}
           max={100}
           displayValue
