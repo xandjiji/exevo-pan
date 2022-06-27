21a7cba16f643bd49461f4a58e5c0cc465bbfb9f
xandjiji <xandjiji@gmail.com>
2022-06-27 10:53:41 -0300

    fix(TimeInput): clamping values

--- apps/exevo-pan/src/components/Atoms/TimeInput/useTimeInput/index.tsx
+++ apps/exevo-pan/src/components/Atoms/TimeInput/useTimeInput/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback, useMemo } from 'react'
-import { clampValue } from 'utils'
+import { clampValue as baseClampValue } from 'utils'
 import { isNumber, hasNextValue, formatValue } from './utils'
 import { UseTimeInputProps, ValueState } from './types'
 
@@ -16,13 +16,19 @@ const useTimeInput = ({
 
   const formattedValue = useMemo(() => formatValue(value, max), [max, value])
 
+  const clampValue = useCallback(
+    (newValue: string | number): string =>
+      baseClampValue(+newValue, [min, max]).toString(),
+    [min, max],
+  )
+
   const onKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback(
     (e) => {
       onKey?.[e.key]?.()
 
       if (isNumber(e.key)) {
         setState((prev) => {
-          const newValue = prev.buffer + e.key
+          const newValue = clampValue(prev.buffer + e.key)
           const inferValue = !hasNextValue({ min, max, value: newValue })
 
           if (inferValue) onInferredValue?.()
@@ -39,10 +45,7 @@ const useTimeInput = ({
       if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
         setState((prev) => {
           const modifier = e.key === 'ArrowUp' ? 1 : -1
-          const newValue = clampValue(+prev.value + modifier, [
-            min,
-            max,
-          ]).toString()
+          const newValue = clampValue(+prev.value + modifier)
           return { value: newValue, buffer: '' }
         })
       }
