861db02605542fe0e5a67c2e7d25f8e363fed1a8
xandjiji <xandjiji@gmail.com>
2022-06-30 14:26:02 -0300

    feature(Stamina): improving layout

--- apps/exevo-pan/src/modules/Calculators/modules/Stamina/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/Stamina/index.tsx
@@ -1,7 +1,9 @@
 import { useMemo } from 'react'
+import clsx from 'clsx'
 import { useTranslations } from 'contexts/useTranslation'
 import { TimeInput, Button } from 'components/Atoms'
 import { InfoTooltip } from 'components/Organisms'
+import ChevronRight from 'assets/svgs/chevronRight.svg'
 import AddIcon from 'assets/svgs/addPost.svg'
 import useTime from './useTime'
 import StaminaBar from './StaminaBar'
@@ -10,6 +12,7 @@ import { Main, LabeledCard, Group, TimeBubbles } from '../../components'
 
 /* @ ToDo:
 - track feature
+- mark title
 
 -i18n
 */
@@ -43,64 +46,61 @@ const Stamina = () => {
 
   return (
     <Main>
-      <div className="grid gap-6 md:grid-cols-2">
-        <LabeledCard labelText="Stamina">
+      <LabeledCard labelText="Results">
+        <div className="flex items-end gap-2">
           <TimeInput
             label="Current stamina"
             max={42}
             value={currentStamina.time}
             onChange={(e) => setCurrentStamina(e.target.value)}
             error={invalid}
+            className="child:w-full child:whitespace-nowrap w-full"
             noAlert
           />
-
+          <ChevronRight
+            className={clsx(
+              'mb-1.5 shrink-0',
+              invalid ? 'fill-red' : 'fill-onSurface',
+            )}
+          />
           <TimeInput
             label="Desired stamina"
             max={42}
             value={targetStamina.time}
             onChange={(e) => setTargetStamina(e.target.value)}
             error={invalid}
+            className="child:w-full child:whitespace-nowrap w-full"
             noAlert
           />
-        </LabeledCard>
+        </div>
 
-        <LabeledCard labelText="Results">
-          <Button
-            type="button"
-            className="absolute top-0 right-0"
-            style={{
-              padding: '8px 16px',
-              width: 'fit-content',
-              transform: 'translate(12px, -12px)',
-            }}
-          >
-            <div className="flex items-center justify-center gap-1 text-xs font-bold uppercase tracking-wider">
-              <AddIcon class="fill-onPrimary -ml-1 h-4 w-4" /> Track
-            </div>
-          </Button>
+        <StaminaBar time={currentStamina.time} mark={targetStamina.time} />
 
-          <Group>
-            <div className="flex items-center gap-1">
-              <strong>Rest time</strong>
-              <InfoTooltip
-                className="h-3 w-3"
-                content={
-                  <span className="whitespace-nowrap">
-                    {`${common.Month[month]} ${day}, ${hours}:${minutes} (${
-                      typeof weekday === 'number'
-                        ? common.FullWeekdays[weekday]
-                        : common[weekday]
-                    })`}
-                  </span>
-                }
-              />
-            </div>
-            <TimeBubbles seconds={secondsToRegenerate} />
-          </Group>
+        <Group>
+          <div className="flex items-center gap-1">
+            <strong>Rest time</strong>
+            <InfoTooltip
+              className="h-3 w-3"
+              content={
+                <span className="whitespace-nowrap">
+                  {`${common.Month[month]} ${day}, ${hours}:${minutes} (${
+                    typeof weekday === 'number'
+                      ? common.FullWeekdays[weekday]
+                      : common[weekday]
+                  })`}
+                </span>
+              }
+            />
+          </div>
+          <TimeBubbles seconds={secondsToRegenerate} />
+        </Group>
 
-          <StaminaBar time={currentStamina.time} mark={targetStamina.time} />
-        </LabeledCard>
-      </div>
+        <Button type="button" style={{ padding: '5px 16px' }}>
+          <div className="flex items-center justify-center gap-1 text-xs font-bold uppercase tracking-wider">
+            <AddIcon class="fill-onPrimary -ml-1 h-4 w-4" /> Track
+          </div>
+        </Button>
+      </LabeledCard>
     </Main>
   )
 }
