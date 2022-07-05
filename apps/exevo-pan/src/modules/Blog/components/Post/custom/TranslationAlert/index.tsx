b2f7870212207a26c5fbe40f517aa78a1b7ddb77
xandjiji <xandjiji@gmail.com>
2022-07-05 08:47:14 -0300

    feature(TranslationAlert): updating component

--- apps/exevo-pan/src/modules/Blog/components/Post/custom/TranslationAlert/index.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/custom/TranslationAlert/index.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from 'contexts/useTranslation'
 import { links } from 'Constants'
+import { blockquote as Blockquote } from '../../Style/Blockquote'
 import { ExternalLink } from '../Links'
 
 const TranslationAlert = () => {
@@ -8,7 +9,7 @@ const TranslationAlert = () => {
   } = useTranslations()
 
   return (
-    <blockquote>
+    <Blockquote>
       {blog.TranslationAlert.content}{' '}
       <strong>
         <ExternalLink href={links.I18N}>
@@ -16,7 +17,7 @@ const TranslationAlert = () => {
         </ExternalLink>
       </strong>{' '}
       😄
-    </blockquote>
+    </Blockquote>
   )
 }
 
