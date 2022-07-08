152c027fa8d424fae8caab44a91cbf8cd140373c
xandjiji <xandjiji@gmail.com>
2022-07-08 15:29:31 -0300

    feature(ImbuementsCost): improving layout

--- apps/exevo-pan/src/modules/Calculators/modules/ImbuementsCost/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/ImbuementsCost/index.tsx
@@ -20,115 +20,123 @@ const ImbuementsCost = () => {
 
   return (
     <Main>
-      <div className="child:max-w-[50%] child:flex-grow flex items-end gap-4">
-        <Input
-          label={
-            <LabelWrapper>
-              <Icons.GoldToken />
-              Gold Token price
-            </LabelWrapper>
-          }
-          inputMode="numeric"
-          placeholder="GP value"
-          defaultValue={20000}
-          mask={numberWithCommaSeparator}
-          noAlert
-        />
+      <LabeledCard labelText="Configurations">
+        <div className="child:max-w-[50%] child:flex-grow flex items-end gap-4">
+          <Input
+            label={
+              <LabelWrapper>
+                <Icons.GoldToken />
+                Gold Token price
+              </LabelWrapper>
+            }
+            inputMode="numeric"
+            placeholder="GP value"
+            defaultValue={20000}
+            mask={numberWithCommaSeparator}
+            noAlert
+          />
 
-        <Select
-          label="Tier"
-          options={[
-            { name: 'Powerful', value: 'powerful' },
-            { name: 'Intricate', value: 'intricate' },
-            { name: 'Basic', value: 'basic' },
-          ]}
-          defaultValue="powerful"
-          noAlert
-        />
-      </div>
+          <Select
+            label="Tier"
+            options={[
+              { name: 'Powerful', value: 'powerful' },
+              { name: 'Intricate', value: 'intricate' },
+              { name: 'Basic', value: 'basic' },
+            ]}
+            defaultValue="powerful"
+            noAlert
+          />
+        </div>
+      </LabeledCard>
 
-      <Tabs.Group>
-        <Panel label="Vampirism (Life leech)">
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.VampireTeeth />
-                Vampire Teeth price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.BloodyPincers />
-                Bloody Pincers price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.PieceOfDeadBrain />
-                Piece of Dead Brain price
-              </LabelWrapper>
-            }
-          />
-        </Panel>
+      <LabeledCard
+        labelText="Imbuements"
+        className="mt-6"
+        style={{ padding: '16px 12px 24px 12px' }}
+      >
+        <Tabs.Group>
+          <Panel label="Vampirism (life leech)">
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.VampireTeeth />
+                  Vampire Teeth price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.BloodyPincers />
+                  Bloody Pincers price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.PieceOfDeadBrain />
+                  Piece of Dead Brain price
+                </LabelWrapper>
+              }
+            />
+          </Panel>
 
-        <Panel label="Void (Mana leech)">
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.RopeBelt />
-                Rope Belts price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.SilencerClaws />
-                Silencer Claws price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.GrimeleechWings />
-                Some Grimeleech Wings price
-              </LabelWrapper>
-            }
-          />
-        </Panel>
+          <Panel label="Void (mana leech)">
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.RopeBelt />
+                  Rope Belts price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.SilencerClaws />
+                  Silencer Claws price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.GrimeleechWings />
+                  Some Grimeleech Wings price
+                </LabelWrapper>
+              }
+            />
+          </Panel>
 
-        <Panel label="Strike (Critical)">
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.ProtectiveCharm />
-                Protective Charm price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.Sabretooth />
-                Sabretooth price
-              </LabelWrapper>
-            }
-          />
-          <Input
-            label={
-              <LabelWrapper>
-                <Icons.VexclawTalon />
-                Vexclaw Talon price
-              </LabelWrapper>
-            }
-          />
-        </Panel>
-      </Tabs.Group>
+          <Panel label="Strike (critical)">
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.ProtectiveCharm />
+                  Protective Charm price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.Sabretooth />
+                  Sabretooth price
+                </LabelWrapper>
+              }
+            />
+            <Input
+              label={
+                <LabelWrapper>
+                  <Icons.VexclawTalon />
+                  Vexclaw Talon price
+                </LabelWrapper>
+              }
+            />
+          </Panel>
+        </Tabs.Group>
+      </LabeledCard>
     </Main>
   )
 }
