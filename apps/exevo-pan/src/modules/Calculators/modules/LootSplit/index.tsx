53d68d4b57b4d3bbfd5932aca036d8dc5f4ca998
xandjiji <xandjiji@gmail.com>
2022-07-15 16:53:25 -0300

    feature(LootSplit): displaying profits

--- apps/exevo-pan/src/modules/Calculators/modules/LootSplit/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/index.tsx
@@ -1,18 +1,19 @@
+/* eslint-disable no-shadow */
 import { useState, useMemo } from 'react'
-import { TextArea } from 'components/Atoms'
-import { Main, Group } from '../../components'
+import clsx from 'clsx'
+import { TextArea, Text } from 'components/Atoms'
+import { Main, LabeledCard, Group, Chip, ChipWrapper } from '../../components'
 import TransferTable from './TransferTable'
 import { parse, findTransactionsRequired } from './utils'
 import { defaultValue } from './defaultValue'
 
 /* @ ToDo:
-- display transactions
-    copy to clipboard
-    copy all?
 - session summary
+    date timestamp (session lenght)
+    copy all (transactions too)?
 
 - save session
-- invalid state
+- placeholder
 - extra expenses (tibiapal)
 - remove players (tibiapal)
 */
@@ -20,30 +21,56 @@ import { defaultValue } from './defaultValue'
 const LootSplit = () => {
   const [rawSession, setRawSession] = useState(defaultValue)
 
-  const transactions = useMemo(() => {
+  const { teamReceipt, playerReceipts, transactions } = useMemo(() => {
     try {
-      return findTransactionsRequired(parse.PlayerReceipts(rawSession))
+      const teamReceipt = parse.TeamReceipt(rawSession)
+      const playerReceipts = parse.PlayerReceipts(rawSession)
+      const transactions = findTransactionsRequired(playerReceipts)
+
+      return { teamReceipt, playerReceipts, transactions }
     } catch {
-      return []
+      return {
+        teamReceipt: { name: '', balance: 0, loot: 0, supplies: 0 },
+        playerReceipts: [],
+        transactions: [],
+      }
     }
   }, [rawSession])
 
   const isInvalid = rawSession && transactions.length === 0
+  const isWaste = teamReceipt.balance < 0
 
   return (
     <Main>
-      <TextArea
-        label="Party hunt session"
-        onChange={(e) => setRawSession(e.target.value)}
-        value={rawSession}
-        className="h-52"
-        error={isInvalid}
-      />
+      <div className="grid gap-6 lg:grid-cols-2">
+        <TextArea
+          label="Party hunt session"
+          onChange={(e) => setRawSession(e.target.value)}
+          value={rawSession}
+          error={isInvalid}
+        />
 
-      <Group>
-        <strong>Transfers</strong>
-        <TransferTable transactions={transactions} />
-      </Group>
+        <LabeledCard labelText="Transfers">
+          <Group>
+            <strong>Transfers</strong>
+            <TransferTable transactions={transactions} />
+          </Group>
+          <Group>
+            <strong>Total {isWaste ? 'waste' : 'profit'}</strong>
+            <ChipWrapper>
+              <Chip>
+                <Text.GoldCoin
+                  value={Math.floor(
+                    teamReceipt.balance / playerReceipts.length,
+                  )}
+                  displaySign={isWaste}
+                />
+                <span className="-ml-1">each</span>
+              </Chip>
+            </ChipWrapper>
+          </Group>
+        </LabeledCard>
+      </div>
     </Main>
   )
 }
