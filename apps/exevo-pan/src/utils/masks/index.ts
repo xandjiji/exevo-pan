85aa60c347e9cde9e8129c034ec0cdf534649689
xandjiji <xandjiji@gmail.com>
2022-07-15 16:32:56 -0300

    feature(masks): `formatNumberWithCommas` now adds sign

--- apps/exevo-pan/src/utils/masks/index.ts
+++ apps/exevo-pan/src/utils/masks/index.ts
@@ -1,6 +1,11 @@
-export const formatNumberWithCommas = (value: number | string): string =>
-  value
+export const formatNumberWithCommas = (value: number | string): string => {
+  const [sign] = value.toString()
+
+  const formattedValue = value
     .toString()
     .replace(/,/g, '')
     .replace(/\D/g, '')
     .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+  return sign === '-' ? `-${formattedValue}` : formattedValue
+}
