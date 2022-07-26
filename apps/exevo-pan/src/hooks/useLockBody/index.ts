6a34fd2d87015c571810438a46525b6cd48d1cb7
xandjiji <xandjiji@gmail.com>
2022-07-26 10:32:09 -0300

    feature(useLockBody): now supports multiple modals

--- apps/exevo-pan/src/hooks/useLockBody/index.ts
+++ apps/exevo-pan/src/hooks/useLockBody/index.ts
@@ -1,18 +1,25 @@
 import { useEffect } from 'react'
 
+const ATTRIBUTE = 'modals'
+
 const getScrollbarWidth = () =>
   `${window.innerWidth - document.body.clientWidth}px`
 
+const updateState = (open: boolean) => {
+  if (document && document.body) {
+    document.body.setAttribute(ATTRIBUTE, open.toString())
+
+    document.body.style.paddingRight = open ? getScrollbarWidth() : 'unset'
+    document.body.style.overflow = open ? 'hidden' : 'unset'
+  }
+}
+
 const useLockBody = (open = true): void => {
   useEffect(() => {
-    if (document && document.body) {
-      document.body.style.paddingRight = open ? getScrollbarWidth() : 'unset'
-      document.body.style.overflow = open ? 'hidden' : 'unset'
-    }
+    if (open) updateState(true)
 
     return (): void => {
-      document.body.style.overflow = 'unset'
-      document.body.style.paddingRight = 'unset'
+      if (!open) updateState(false)
     }
   }, [open])
 }
