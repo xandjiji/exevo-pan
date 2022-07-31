d4115452a7b3bb207866ddc31d85d94bf4a64435
xandjiji <xandjiji@gmail.com>
2022-07-31 17:36:08 -0300

    refactor(hooks): turning `useLockBody` into a context

---
+++ apps/exevo-pan/src/hooks/useLockBody/index.tsx
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useContext,
+  useCallback,
+  useEffect,
+  useState,
+  useRef,
+} from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { LockBodyProviderProps, Updater } from './types'
+
+const LockBodyContext = createContext<Updater>(() => {})
+
+const getScrollbarWidth = () =>
+  `${window.innerWidth - document.body.clientWidth}px`
+
+export const LockBodyProvider = ({ children }: LockBodyProviderProps) => {
+  const [modals, setModals] = useState<Record<string, boolean>>({})
+
+  useEffect(() => {
+    if (document && document.body) {
+      const isOpen = Object.values(modals).some((state) => state)
+
+      document.body.style.paddingRight = isOpen ? getScrollbarWidth() : 'unset'
+      document.body.style.overflow = isOpen ? 'hidden' : 'unset'
+    }
+  }, [modals])
+
+  const updateModal = useCallback(
+    (key: string, status: boolean) =>
+      setModals((prev) => ({
+        ...prev,
+        [key]: status,
+      })),
+    [],
+  )
+
+  return (
+    <LockBodyContext.Provider value={updateModal}>
+      {children}
+    </LockBodyContext.Provider>
+  )
+}
+
+const useLockBody = (open = true): void => {
+  const updateModal = useContext(LockBodyContext)
+
+  const key = useRef(uuidv4())
+
+  useEffect(() => {
+    updateModal(key.current, open)
+
+    return () => {
+      updateModal(key.current, false)
+    }
+  }, [open])
+}
+
+export default useLockBody
