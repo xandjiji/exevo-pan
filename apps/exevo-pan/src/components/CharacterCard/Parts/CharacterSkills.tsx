2664012b9efadb4f85ed3e5f24d2d9544e38c52b
xandjiji <xandjiji@gmail.com>
2022-08-01 11:31:44 -0300

    refactor(CharacterSkills): using `getHighestSkill` util

--- apps/exevo-pan/src/components/CharacterCard/Parts/CharacterSkills.tsx
+++ apps/exevo-pan/src/components/CharacterCard/Parts/CharacterSkills.tsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx'
+import { getHighestSkill } from 'utils'
 import SkillBar from './SkillBar'
 
 interface CharacterSkillsProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -9,26 +10,23 @@ const CharacterSkills = ({
   skills,
   className,
   ...props
-}: CharacterSkillsProps) => {
-  const highlightedSkillValue = Math.max(...(Object.values(skills) as number[]))
-  return (
-    <div
-      className={clsx('mb-1 grid grid-cols-2 gap-x-4 gap-y-2', className)}
-      {...props}
-    >
-      {Object.keys(skills).map((skillName) => {
-        const skillValue = skills[skillName as keyof CharacterSkillsObject]
-        return (
-          <SkillBar
-            key={skillName}
-            skillName={skillName}
-            skillValue={skillValue}
-            highlight={highlightedSkillValue === skillValue}
-          />
-        )
-      })}
-    </div>
-  )
-}
+}: CharacterSkillsProps) => (
+  <div
+    className={clsx('mb-1 grid grid-cols-2 gap-x-4 gap-y-2', className)}
+    {...props}
+  >
+    {Object.keys(skills).map((skillName) => {
+      const skillValue = skills[skillName as keyof CharacterSkillsObject]
+      return (
+        <SkillBar
+          key={skillName}
+          skillName={skillName}
+          skillValue={skillValue}
+          highlight={getHighestSkill(skills).skill === skillName}
+        />
+      )
+    })}
+  </div>
+)
 
 export default CharacterSkills
