f41bfd0e34e039f4620d1300cdd1040a53c44658
xandjiji <xandjiji@gmail.com>
2022-08-11 14:22:22 -0300

    test(FormError): adding tests

--- apps/exevo-pan/src/components/Atoms/FormError/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/FormError/__tests__/index.tsx
@@ -1,5 +1,27 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
 import FormError from '..'
 
 describe('<FormError />', () => {
-  test.todo('add tests')
+  test('should toggle its visibility', () => {
+    const { rerender } = renderWithProviders(
+      <FormError id="element-id" error="content" />,
+    )
+
+    const errorElement = screen.getByRole('alert')
+    expect(errorElement).toHaveTextContent('content')
+    expect(errorElement).toHaveAttribute('id', 'element-id')
+
+    rerender(<FormError id="element-id" />)
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+
+    rerender(<FormError id="element-id" error />)
+    expect(screen.queryByRole('alert')).toBeInTheDocument()
+
+    rerender(<FormError id="element-id" error={false} />)
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+
+    rerender(<FormError id="element-id" error="" />)
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument()
+  })
 })
