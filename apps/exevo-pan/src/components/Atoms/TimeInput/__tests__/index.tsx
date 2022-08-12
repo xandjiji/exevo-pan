2a553a8739c0180e39ed0ae468b35404820ab0c3
xandjiji <xandjiji@gmail.com>
2022-08-12 15:08:04 -0300

    test(TimeInput): adding tests

--- apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithProviders, assertNoA11yViolations } from 'utils/test'
 import TimeInput from '..'
 
 describe('<TimeInput />', () => {
-  test.todo('add tests')
+  test('should be invalid', () => {
+    renderWithProviders(<TimeInput label="Time" error="Invalid time" />)
+
+    expect(screen.getByRole('alert')).toHaveTextContent('Invalid time')
+  })
+
+  test.todo('the hour should be capped by `max` and `min` props')
+
+  test.todo('should be controlled (keyboard and arrow keys)')
+
+  test.todo('should control its own value (keyboard and arrow keys)')
 })
