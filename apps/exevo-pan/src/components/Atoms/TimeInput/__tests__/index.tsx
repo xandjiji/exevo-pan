561e277e760e841f678c31503dc530f1e7f94acd
xandjiji <xandjiji@gmail.com>
2022-08-12 15:19:43 -0300

    fix(TimeInput): hidden input now recieves current value

--- apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
@@ -1,8 +1,20 @@
-import { useState } from 'react'
+import { useState, memo } from 'react'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithProviders, assertNoA11yViolations } from 'utils/test'
 import TimeInput from '..'
+
+const ControlledComponent: typeof TimeInput = memo((args) => {
+  const [value, setValue] = useState('13:00')
+
+  return (
+    <TimeInput
+      {...args}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  )
+})
 
 describe('<TimeInput />', () => {
   test('should be invalid', () => {
@@ -13,7 +25,16 @@ describe('<TimeInput />', () => {
 
   test.todo('the hour should be capped by `max` and `min` props')
 
-  test.todo('should be controlled (keyboard and arrow keys)')
+  test('should be controlled (keyboard and arrow keys)', () => {
+    renderWithProviders(<ControlledComponent label="Time" />)
+
+    const inputElement = screen.getByLabelText('Time')
+
+    expect(inputElement).toHaveValue('13:00')
+
+    userEvent.type(inputElement, '66')
+    expect(inputElement).toHaveValue('06:06')
+  })
 
   test.todo('should control its own value (keyboard and arrow keys)')
 })
