053d44bb9a97f4ba2012f3ae055f34ecca5d7d31
xandjiji <xandjiji@gmail.com>
2022-08-14 21:54:30 -0300

    test(TimeInput): testing a11y

--- apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/TimeInput/__tests__/index.tsx
@@ -1,20 +1,6 @@
-import { useState, memo } from 'react'
 import { screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import { renderWithProviders, assertNoA11yViolations } from 'utils/test'
 import TimeInput from '..'
-
-const ControlledComponent: typeof TimeInput = memo((args) => {
-  const [value, setValue] = useState('13:00')
-
-  return (
-    <TimeInput
-      {...args}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-    />
-  )
-})
 
 describe('<TimeInput />', () => {
   test('should be invalid', () => {
@@ -23,18 +9,21 @@ describe('<TimeInput />', () => {
     expect(screen.getByRole('alert')).toHaveTextContent('Invalid time')
   })
 
-  test.todo('the hour should be capped by `max` and `min` props')
-
   test('should be controlled', () => {
-    renderWithProviders(<ControlledComponent label="Time" />)
+    const { rerender } = renderWithProviders(
+      <TimeInput label="Time" value="10:30" />,
+    )
 
     const inputElement = screen.getByLabelText('Time')
 
-    expect(inputElement).toHaveValue('13:00')
+    expect(inputElement).toHaveDisplayValue('10:30')
 
-    userEvent.type(inputElement, '66')
-    expect(inputElement).toHaveValue('06:06')
+    rerender(<TimeInput label="Time" value="06:06" />)
+    expect(inputElement).toHaveDisplayValue('06:06')
   })
 
-  test.todo('should control its own value (keyboard and arrow keys)')
+  test('a11y', async () => {
+    const { container } = renderWithProviders(<TimeInput label="Time" />)
+    await assertNoA11yViolations(container)
+  })
 })
