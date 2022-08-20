74478c7fd8dd2156f6173f23e4f6f4859f9afac5
xandjiji <xandjiji@gmail.com>
2022-08-20 11:11:09 -0300

    test(StaminaBar): testing `mark` and `blinking` props

--- apps/exevo-pan/src/modules/Calculators/modules/Stamina/StaminaBar/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/Stamina/StaminaBar/__tests__/index.tsx
@@ -3,5 +3,42 @@ import { renderWithProviders } from 'utils/test'
 import StaminaBar from '..'
 
 describe('<StaminaBar />', () => {
-  test.todo('tests')
+  test.todo('the stamina bar should have the correct width')
+
+  test('the `:` time separator should be blinking', () => {
+    renderWithProviders(<StaminaBar time="32:00" blinking />)
+
+    expect(screen.getByText(':')).toHaveClass('animate-blinking')
+  })
+
+  test('should display a visual mark at the correct position', () => {
+    const { rerender } = renderWithProviders(
+      <StaminaBar time="32:00" mark="36:30" />,
+    )
+
+    {
+      const [, markElement] = screen.getByTitle('36:30').children
+      expect(markElement).toHaveStyle('left: 87%')
+    }
+
+    {
+      rerender(<StaminaBar time="32:00" mark="42:00" />)
+      const [, markElement] = screen.getByTitle('42:00').children
+      expect(markElement).toHaveStyle('left: 100%')
+    }
+
+    {
+      rerender(<StaminaBar time="32:00" mark="00:00" />)
+      const [, markElement] = screen.getByTitle('00:00').children
+      expect(markElement).toHaveStyle('left: 0%')
+    }
+  })
+
+  describe('should have the correct color', () => {
+    test.todo('green')
+
+    test.todo('orange')
+
+    test.todo('yellow')
+  })
 })
