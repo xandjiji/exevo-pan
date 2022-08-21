477e225fd2fa077d79de97311be747bfe645180b
xandjiji <xandjiji@gmail.com>
2022-08-21 17:30:00 -0300

    test(utils): testing `skillCost`

--- apps/exevo-pan/src/utils/calculate/skills/skillCost/__tests__/index.tsx
+++ apps/exevo-pan/src/utils/calculate/skills/skillCost/__tests__/index.tsx
@@ -1,5 +1,76 @@
+import { WeaponsObject } from '../../types'
 import { skillCost } from '..'
 
+type Case = {
+  input: WeaponsObject
+  result: ReturnType<typeof skillCost>
+}
+
+const cases: Case[] = [
+  {
+    input: {
+      regular: 11,
+      durable: 0,
+      lasting: 113,
+    },
+    result: {
+      gold: 857167500,
+      seconds: 3265400,
+      tc: 81635,
+    },
+  },
+  {
+    input: {
+      regular: 0,
+      durable: 0,
+      lasting: 0,
+    },
+    result: {
+      gold: 0,
+      seconds: 0,
+      tc: 0,
+    },
+  },
+  {
+    input: {
+      regular: 65,
+      durable: 0,
+      lasting: 0,
+    },
+    result: {
+      gold: 17062500,
+      seconds: 65000,
+      tc: 1625,
+    },
+  },
+  {
+    input: {
+      regular: 0,
+      durable: 13,
+      lasting: 0,
+    },
+    result: {
+      gold: 12285000,
+      seconds: 46800,
+      tc: 1170,
+    },
+  },
+  {
+    input: {
+      regular: 2,
+      durable: 4,
+      lasting: 8,
+    },
+    result: {
+      gold: 64785000,
+      seconds: 246800,
+      tc: 6170,
+    },
+  },
+]
+
 describe('skillCost()', () => {
-  test.todo('add tests')
+  test.each(cases)('should calculate correctly', ({ input, result }) => {
+    expect(skillCost(input)).toEqual(result)
+  })
 })
