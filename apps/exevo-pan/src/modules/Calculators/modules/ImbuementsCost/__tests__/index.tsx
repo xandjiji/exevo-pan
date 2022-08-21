d95544194ba08c176197b462855cb5b7693c29dd
xandjiji <xandjiji@gmail.com>
2022-08-21 17:40:18 -0300

    chore: adding todos

---
+++ apps/exevo-pan/src/modules/Calculators/modules/ImbuementsCost/__tests__/index.tsx
@@ -0,0 +1,7 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
+import ImbuementsCost from '..'
+
+describe('<ImbuementsCost />', () => {
+  test.todo('tests')
+})
