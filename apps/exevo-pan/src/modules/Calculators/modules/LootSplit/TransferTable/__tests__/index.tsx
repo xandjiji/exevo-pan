dc82e256325fa1bf7cf1817a8f0614c879422091
xandjiji <xandjiji@gmail.com>
2022-08-22 10:36:05 -0300

    test(TransferTable): adding test

--- apps/exevo-pan/src/modules/Calculators/modules/LootSplit/TransferTable/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/TransferTable/__tests__/index.tsx
@@ -3,5 +3,24 @@ import { renderWithProviders } from 'utils/test'
 import TransferTable from '..'
 
 describe('<TransferTable />', () => {
-  test.todo('tests')
+  test('should render everything correctly', () => {
+    renderWithProviders(
+      <TransferTable
+        transactions={[
+          { from: 'me', to: 'you', amount: 1546 },
+          { from: 'Ksu', to: 'Ksa', amount: 123 },
+        ]}
+      />,
+    )
+
+    expect(screen.getByText('me')).toBeInTheDocument()
+    expect(screen.getByText('you')).toBeInTheDocument()
+    expect(screen.getByText('Ksu')).toBeInTheDocument()
+    expect(screen.getByText('Ksa')).toBeInTheDocument()
+
+    expect(screen.getByText('1,546')).toBeInTheDocument()
+    expect(screen.getByText('123')).toBeInTheDocument()
+
+    expect(screen.getAllByRole('button')).toHaveLength(2)
+  })
 })
