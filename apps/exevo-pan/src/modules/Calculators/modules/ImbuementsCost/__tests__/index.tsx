a8dfb6ba71aacfd4b8814fecab9763dde82d31e7
xandjiji <xandjiji@gmail.com>
2022-08-24 14:11:21 -0300

    test(ImbuementsCost): testing tier changes

--- apps/exevo-pan/src/modules/Calculators/modules/ImbuementsCost/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/ImbuementsCost/__tests__/index.tsx
@@ -1,7 +1,9 @@
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { renderWithProviders } from 'utils/test'
+import { renderWithProviders, setup } from 'utils/test'
 import ImbuementsCost from '..'
+
+setup.scrollIntoView()
 
 describe('<ImbuementsCost />', () => {
   const type = (element: Element, text: string) => {
@@ -50,7 +52,40 @@ describe('<ImbuementsCost />', () => {
     expect(screen.getByText('+27,995 gp')).toBeInTheDocument()
   })
 
-  test.todo('should disable inputs according to the current tier')
+  test('should disable inputs according to the current tier', () => {
+    renderWithProviders(<ImbuementsCost />)
+
+    const inputs = [
+      'Vampire Teeth',
+      'Bloody Pincers',
+      'Piece of Dead Brain',
+    ].map((name) => screen.getByRole('textbox', { name }))
+
+    inputs.forEach((input) => {
+      expect(input).toBeEnabled()
+    })
+
+    userEvent.click(screen.getByText('Powerful (III)'))
+    userEvent.keyboard('{arrowdown}')
+
+    inputs.forEach((input, index) => {
+      if (index > 1) {
+        expect(input).toBeDisabled()
+      } else {
+        expect(input).toBeEnabled()
+      }
+    })
+
+    userEvent.keyboard('{arrowdown}')
+
+    inputs.forEach((input, index) => {
+      if (index > 0) {
+        expect(input).toBeDisabled()
+      } else {
+        expect(input).toBeEnabled()
+      }
+    })
+  })
 
   test.todo(
     'navigating between different imbuements should preserve each state',
