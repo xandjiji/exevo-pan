2892e9deaa1b3f18accd67b6475ae778f194a80e
xandjiji <xandjiji@gmail.com>
2022-08-23 21:49:03 -0300

    test(LootSplit): testing advanced options

--- apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
@@ -23,11 +23,45 @@ describe('<LootSplit />', () => {
     expect(secondTransfer.nextSibling).toHaveTextContent('55,219')
     expect(thirdTransfer.nextSibling).toHaveTextContent('61,673')
 
+    expect(firstTransfer.nextSibling?.nextSibling).toHaveTextContent('Guaca')
+    expect(secondTransfer.nextSibling?.nextSibling).toHaveTextContent('Buzz')
+    expect(thirdTransfer.nextSibling?.nextSibling).toHaveTextContent('Buzz')
+
     expect(screen.getByText('Total waste')).toBeInTheDocument()
     expect(screen.getByTitle('-10,628 gp')).toBeInTheDocument()
   })
 
-  test.todo('should apply advanced options')
+  test('should apply advanced options', () => {
+    renderWithProviders(<LootSplit />)
+
+    const textAreaElement = screen.getByLabelText(
+      'Paste your party hunt session',
+    )
+    userEvent.clear(textAreaElement)
+    userEvent.type(textAreaElement, mockedSession)
+
+    userEvent.click(screen.getByRole('button', { name: 'Advanced options' }))
+
+    userEvent.type(screen.getByLabelText('Me'), '123456')
+    userEvent.type(screen.getByLabelText('You'), '5000')
+    userEvent.type(screen.getByLabelText('Buzz'), '999')
+
+    userEvent.click(screen.getByRole('button', { name: 'Done' }))
+
+    const [firstTransfer, secondTransfer, thirdTransfer] =
+      screen.getAllByTitle('You')
+
+    expect(firstTransfer.nextSibling).toHaveTextContent('4,787')
+    expect(secondTransfer.nextSibling).toHaveTextContent('29,419')
+    expect(thirdTransfer.nextSibling).toHaveTextContent('85,527')
+
+    expect(firstTransfer.nextSibling?.nextSibling).toHaveTextContent('Guaca')
+    expect(secondTransfer.nextSibling?.nextSibling).toHaveTextContent('Me')
+    expect(thirdTransfer.nextSibling?.nextSibling).toHaveTextContent('Buzz')
+
+    expect(screen.getByText('Total waste')).toBeInTheDocument()
+    expect(screen.getByTitle('-42,992 gp')).toBeInTheDocument()
+  })
 
   test.todo('should save, list, display and delete previous hunt sesions')
 
