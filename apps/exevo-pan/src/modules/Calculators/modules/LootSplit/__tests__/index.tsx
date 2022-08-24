b10fe0b0a7b8e79296cd09c55e0422ed64cc4086
xandjiji <xandjiji@gmail.com>
2022-08-23 21:42:11 -0300

    test(LootSplit): adding first test

--- apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
@@ -1,7 +1,37 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from 'utils/test'
 import LootSplit from '..'
+import { mockedSession } from './mock'
 
 describe('<LootSplit />', () => {
-  test.todo('tests')
+  test('should summarize the hunt session correctly', () => {
+    renderWithProviders(<LootSplit />)
+
+    const textAreaElement = screen.getByLabelText(
+      'Paste your party hunt session',
+    )
+    userEvent.clear(textAreaElement)
+    userEvent.type(textAreaElement, mockedSession)
+
+    expect(screen.getByText('Feb 11, 2019 - 17:33 (Monday)'))
+
+    const [firstTransfer, secondTransfer] = screen.getAllByTitle('You')
+    const thirdTransfer = screen.getByTitle('Me')
+
+    expect(firstTransfer.nextSibling).toHaveTextContent('37,151')
+    expect(secondTransfer.nextSibling).toHaveTextContent('55,219')
+    expect(thirdTransfer.nextSibling).toHaveTextContent('61,673')
+
+    expect(screen.getByText('Total waste')).toBeInTheDocument()
+    expect(screen.getByTitle('-10,628 gp')).toBeInTheDocument()
+  })
+
+  test.todo('should apply advanced options')
+
+  test.todo('should save, list, display and delete previous hunt sesions')
+
+  test.todo(
+    'if the textarea content is invalid, the save button should be disabled',
+  )
 })
---
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/mock.ts
@@ -0,0 +1,30 @@
+export const mockedSession = `Session data: From 2019-2-11, 17:33:55 to 2019-2-11, 1:44:36
+Session: 01:23h
+Loot Type: Market
+Loot: 624,317
+Supplies: 566,829
+Balance: 57488
+Me (Leader)
+	Loot: 149,363
+	Supplies: 198,318
+	Balance: 51,045
+	Damage: 215,683
+	Healing: 117,408
+You
+	Loot: 205,479
+	Supplies: 123,737
+	Balance: 81,742
+	Damage: 885,460
+	Healing: 332,423
+Buzz
+	Loot: 46,904
+	Supplies: 174,424
+	Balance: -127,520
+	Damage: 628,303
+	Healing: 223
+Guaca
+	Loot: 122,571
+	Supplies: 70,350
+	Balance: -47,779
+	Damage: 564,848
+	Healing: 104,877`
