8cfd77cc55ef4e798c78a0abb555bdd4a6208dae
xandjiji <xandjiji@gmail.com>
2022-08-30 15:12:14 -0300

    chore: removing backend fetch for highlighted auctions

--- apps/bazaar-scraper/src/Scripts/GetHighlightedAuctions/tasks/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './fetchHighlightedAuctionData'
-export * from './buildHighlightedAuctions'
--- automations/scrapAuctions.sh
+++ automations/scrapAuctions.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 cd ~/exevo-pan/apps/bazaar-scraper/
 yarn scrap:auctions
-yarn update:highlighted
 
 cp ~/exevo-pan/apps/bazaar-scraper/Output/ServerData.json ~/exevo-pan/apps/bazaar-scraper/Output/static
 cp ~/exevo-pan/apps/bazaar-scraper/Output/ItemsData.json ~/exevo-pan/apps/bazaar-scraper/Output/static
-cp ~/exevo-pan/apps/bazaar-scraper/Output/HighlightedAuctions.json ~/exevo-pan/apps/bazaar-scraper/Output/static
 sh ~/exevo-pan/apps/bazaar-scraper/Output/static/deployStatic.sh
 
 cp ~/exevo-pan/apps/bazaar-scraper/Output/CurrentAuctions.json ~/exevo-pan/apps/current-auctions-lambda/src/Data
 cp ~/exevo-pan/apps/bazaar-scraper/Output/ServerData.json ~/exevo-pan/apps/current-auctions-lambda/src/Data
-cp ~/exevo-pan/apps/bazaar-scraper/Output/ItemsData.json ~/exevo-pan/apps/current-auctions-lambda/src/Data
 
 cd ~/exevo-pan/apps/current-auctions-lambda
 yarn deploy:fast
