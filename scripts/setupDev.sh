77abe2e1adb8ad919a33cd1a17abf36821084209
xandjiji <xandjiji@gmail.com>
2022-08-31 10:26:19 -0300

    chore: removing unused code

--- scripts/setupDev.sh
+++ scripts/setupDev.sh
@@ -2,7 +2,6 @@ sh scripts/cleanEnvOverrides.sh
 
 # setuping: current-auctions-lambda
 cp apps/bazaar-scraper/Output/CurrentAuctions.json apps/current-auctions-lambda/src/Data/
-cp apps/bazaar-scraper/Output/ItemsData.json apps/current-auctions-lambda/src/Data/
 cp apps/bazaar-scraper/Output/ServerData.json apps/current-auctions-lambda/src/Data/
 
 # setuping: history-server app
