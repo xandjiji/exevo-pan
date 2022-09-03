fc46b473af7cf379c1c12078c35531a69d0067c0
xandjiji <xandjiji@gmail.com>
2022-09-03 19:35:43 -0300

    chore: adding todo

---
+++ apps/exevo-pan/src/modules/BazaarAuctions/components/UrlAuction/__tests__/index.tsx
@@ -0,0 +1,7 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
+import UrlAuction from '..'
+
+describe('<UrlAuction />', () => {
+  test.todo('add tests')
+})
