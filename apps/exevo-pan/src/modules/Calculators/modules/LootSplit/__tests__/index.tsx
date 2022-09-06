6bd6cd4822c6c97d3dbcc895afebdd214d4267f9
xandjiji <xandjiji@gmail.com>
2022-09-06 13:37:35 -0300

    test(LootSplit): testing removed players

--- apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/LootSplit/__tests__/index.tsx
@@ -45,22 +45,22 @@ describe('<LootSplit />', () => {
     userEvent.type(screen.getByLabelText('Me'), '123456')
     userEvent.type(screen.getByLabelText('You'), '5000')
     userEvent.type(screen.getByLabelText('Buzz'), '999')
+    userEvent.click(screen.getAllByLabelText('Remove player')[3])
+
+    expect(screen.getByLabelText('Guaca')).toBeDisabled()
 
     userEvent.click(screen.getByRole('button', { name: 'Done' }))
 
-    const [firstTransfer, secondTransfer, thirdTransfer] =
-      screen.getAllByTitle('You')
+    const [firstTransfer, secondTransfer] = screen.getAllByTitle('You')
 
-    expect(firstTransfer.nextSibling).toHaveTextContent('4,787')
-    expect(secondTransfer.nextSibling).toHaveTextContent('29,419')
-    expect(thirdTransfer.nextSibling).toHaveTextContent('85,527')
+    expect(firstTransfer.nextSibling).toHaveTextContent('31,015')
+    expect(secondTransfer.nextSibling).toHaveTextContent('87,123')
 
-    expect(firstTransfer.nextSibling?.nextSibling).toHaveTextContent('Guaca')
-    expect(secondTransfer.nextSibling?.nextSibling).toHaveTextContent('Me')
-    expect(thirdTransfer.nextSibling?.nextSibling).toHaveTextContent('Buzz')
+    expect(firstTransfer.nextSibling?.nextSibling).toHaveTextContent('Me')
+    expect(secondTransfer.nextSibling?.nextSibling).toHaveTextContent('Buzz')
 
     expect(screen.getByText('Total waste')).toBeInTheDocument()
-    expect(screen.getByTitle('-42,992 gp')).toBeInTheDocument()
+    expect(screen.getByTitle('-41,396 gp')).toBeInTheDocument()
   })
 
   test('should save, list, display and delete previous hunt sesions', () => {
