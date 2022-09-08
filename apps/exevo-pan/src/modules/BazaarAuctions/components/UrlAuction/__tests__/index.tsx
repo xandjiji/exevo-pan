5a154d5d001817dd74b187ac8dab83fdc5376d49
xandjiji <xandjiji@gmail.com>
2022-09-08 09:45:59 -0300

    test(UrlAuction): adding initial test case

--- apps/exevo-pan/src/modules/BazaarAuctions/components/UrlAuction/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/BazaarAuctions/components/UrlAuction/__tests__/index.tsx
@@ -1,7 +1,51 @@
-import { screen } from '@testing-library/react'
-import { renderWithProviders } from 'utils/test'
+import { screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithProviders, randomDataset, setup } from 'utils/test'
 import UrlAuction from '..'
 
+const mockedFetch = setup.fetch()
+
+const [mockedCharacterData] = randomDataset().characterData
+
 describe('<UrlAuction />', () => {
-  test.todo('add tests')
+  beforeEach(() => {
+    mockedFetch.mockClear()
+  })
+
+  test('should fetch and render an auction', async () => {
+    setup.URLSearchParams.get('123456')
+
+    mockedFetch.mockResolvedValueOnce({
+      json: async () => ({
+        page: [mockedCharacterData],
+        sortingMode: 0,
+        descendingOrder: false,
+      }),
+    } as Response)
+
+    renderWithProviders(<UrlAuction endpoint="endpoint" />)
+
+    await waitFor(() =>
+      expect(screen.getByText(/loading/gi)).toBeInTheDocument(),
+    )
+    await waitFor(() =>
+      expect(screen.queryByText(/loading/gi)).not.toBeInTheDocument(),
+    )
+    await waitFor(() =>
+      expect(
+        screen.getByText(mockedCharacterData.nickname),
+      ).toBeInTheDocument(),
+    )
+
+    userEvent.click(screen.getByRole('button', { name: 'Close dialog' }))
+    await waitFor(() =>
+      expect(
+        screen.queryByText(mockedCharacterData.nickname),
+      ).not.toBeInTheDocument(),
+    )
+  })
+
+  test.todo('should fetch and render nothing')
+
+  test.todo('shouldnt either fetch or render anything')
 })
