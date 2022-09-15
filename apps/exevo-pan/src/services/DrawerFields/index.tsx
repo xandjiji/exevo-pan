6a0c364abad09f295f4cb00fed77d31d351c6d61
xandjiji <xandjiji@gmail.com>
2022-09-15 09:15:19 -0300

    feature(services): adding `fetchActiveServers`

--- apps/exevo-pan/src/services/DrawerFields/index.tsx
+++ apps/exevo-pan/src/services/DrawerFields/index.tsx
@@ -2,13 +2,21 @@ import { endpoints, paths } from 'Constants'
 import { buildServerOptions, filterItemData } from './utils'
 
 export default class DrawerFieldsClient {
+  static activeServersUrl = `${endpoints.STATIC_DATA}${paths.ACTIVE_SERVERS}`
+
   static serverDataUrl = `${endpoints.STATIC_DATA}${paths.SERVER_DATA}`
 
   static rareItemDataUrl = `${endpoints.STATIC_DATA}${paths.ITEMS_DATA}`
 
+  static async fetchActiveServers(): Promise<string[]> {
+    const response = await fetch(this.activeServersUrl)
+
+    return response.json()
+  }
+
   static async fetchServerOptions(): Promise<Option[]> {
     const response = await fetch(this.serverDataUrl)
-    const data = (await response.json()) as Record<string, ServerObject>
+    const data: Record<string, ServerObject> = await response.json()
     const serverArray = Object.values(data)
     const serverOptions = buildServerOptions(serverArray)
 
@@ -17,7 +25,7 @@ export default class DrawerFieldsClient {
 
   static async fetchAuctionedItemOptions(): Promise<RareItemData> {
     const response = await fetch(this.rareItemDataUrl)
-    const data = (await response.json()) as RareItemData
+    const data: RareItemData = await response.json()
     return filterItemData(data)
   }
 }
