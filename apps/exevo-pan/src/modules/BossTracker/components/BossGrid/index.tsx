4cc642875ae1647168b2710d5d3cfb770416e390
xandjiji <xandjiji@gmail.com>
2022-09-18 12:12:41 -0300

    feature(BossTracker): adding elevation shadow

--- apps/exevo-pan/src/modules/BossTracker/components/BossGrid/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/components/BossGrid/index.tsx
@@ -31,16 +31,21 @@ const BossGrid = ({ bosses, className, ...props }: BossGridProps) => {
 
   return (
     <section className={clsx('flex flex-col gap-4', className)} {...props}>
-      <ChipGroup
-        label="List bosses by"
-        options={listOptions}
-        value={listingOption}
-        onChange={(e) => setListingOption(e.target.value as ListOption)}
-        className="bg-background z-2 after:bg-background after:-z-1 sticky top-[60px] -mb-3 -mt-6 pb-3 pt-6 after:absolute after:-left-2 after:top-0 after:h-full after:w-[calc(100%_+_16px)]"
-      />
+      <div className="bg-background z-above-tooltip after:bg-background after:-z-1 sticky top-[60px] -mb-3 -mt-6 pb-3 pt-6 after:absolute after:-left-2 after:top-0 after:h-full after:w-[calc(100%_+_16px)]">
+        <ChipGroup
+          label="List bosses by"
+          options={listOptions}
+          value={listingOption}
+          onChange={(e) => setListingOption(e.target.value as ListOption)}
+        />
+        <div
+          role="none"
+          className="z-2 from-background pointer-events-none absolute -bottom-4 -left-2 h-6 w-[calc(100%_+_16px)] bg-gradient-to-b to-transparent"
+        />
+      </div>
 
       {listNotEmpty ? (
-        <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
+        <ul className="grid gap-4 pt-4 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
           {list.map((bossStats) => (
             <BossCard
               key={bossStats.name}
