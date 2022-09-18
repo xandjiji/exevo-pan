897d12cc16d0d370e1ccce4364a947246290b7d6
xandjiji <xandjiji@gmail.com>
2022-09-18 15:47:47 -0300

    chore: removing unused code

--- apps/exevo-pan/src/modules/BossTracker/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/index.tsx
@@ -14,23 +14,19 @@ const BossTracker = ({
   activeServers,
   bossChances,
   recentlyAppeared,
-}: BossTrackerProps) => {
-  console.log(1)
+}: BossTrackerProps) => (
+  <main className="inner-container pb-8">
+    <Hero src={heroSrc} title="Boss Tracker" offset />
 
-  return (
-    <main className="inner-container pb-8">
-      <Hero src={heroSrc} title="Boss Tracker" offset />
-
-      <div className="grid items-start gap-8 md:relative md:flex md:flex-row-reverse md:gap-16 lg:gap-8">
-        <div className="shrink-0 md:sticky md:top-20 md:w-[320px]">
-          <RecentlyAppeared bosses={recentlyAppeared} />
-        </div>
-
-        <div className="bg-separator h-[1px] w-full md:hidden" role="none" />
-        <BossGrid bosses={bossChances.bosses} className="grow" />
+    <div className="grid items-start gap-8 md:relative md:flex md:flex-row-reverse md:gap-16 lg:gap-8">
+      <div className="shrink-0 md:sticky md:top-20 md:w-[320px]">
+        <RecentlyAppeared bosses={recentlyAppeared} />
       </div>
-    </main>
-  )
-}
+
+      <div className="bg-separator h-[1px] w-full md:hidden" role="none" />
+      <BossGrid bosses={bossChances.bosses} className="grow" />
+    </div>
+  </main>
+)
 
 export default BossTracker
