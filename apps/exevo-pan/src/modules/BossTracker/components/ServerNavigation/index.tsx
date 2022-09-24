b0ed82bd9f233cea3edb502a1c4f92135db33136
xandjiji <xandjiji@gmail.com>
2022-09-24 11:16:01 -0300

    feature(ServerNavigation): loading state

--- apps/exevo-pan/src/modules/BossTracker/components/ServerNavigation/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/components/ServerNavigation/index.tsx
@@ -1,5 +1,7 @@
-import { memo, useMemo } from 'react'
+import { memo, useState, useEffect, useMemo } from 'react'
 import { useRouter } from 'next/router'
+import { useTranslations } from 'contexts/useTranslation'
+import { LoadingAlert } from 'components/Atoms'
 import { Select } from 'components/Organisms'
 import { debounce } from 'utils'
 import { routes } from 'Constants'
@@ -12,7 +14,28 @@ const ServerNavigation = ({
   currentServer,
   activeServers,
 }: ServerNavigationProps) => {
-  const { push } = useRouter()
+  const {
+    translations: { common },
+  } = useTranslations()
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { push, events } = useRouter()
+
+  useEffect(() => {
+    const setLoading = () => setIsLoading(true)
+    const setLoaded = () => setIsLoading(false)
+
+    events.on('routeChangeStart', setLoading)
+    events.on('routeChangeComplete', setLoaded)
+    events.on('routeChangeError', setLoaded)
+
+    return () => {
+      events.off('routeChangeStart', setLoading)
+      events.off('routeChangeComplete', setLoaded)
+      events.off('routeChangeError', setLoaded)
+    }
+  }, [events])
 
   const debouncedNav = useMemo(
     () =>
@@ -26,6 +49,7 @@ const ServerNavigation = ({
 
   return (
     <section className="inner-container bg-darkerPrimary z-71 sticky top-[60px] py-3 shadow-md transition-colors">
+      {isLoading && <LoadingAlert>{common.LoadingState}</LoadingAlert>}
       <Select
         label="Current server:"
         options={activeServers.map((server) => ({
