382ea85023f196fc35a77b48582f9b48e6cecb14
xandjiji <xandjiji@gmail.com>
2022-09-25 10:53:28 -0300

    feature(BossTracker): displaying hours since last update

--- apps/exevo-pan/src/modules/BossTracker/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/index.tsx
@@ -1,5 +1,5 @@
 import { Hero } from 'templates'
-import { loadRawSrc } from 'utils'
+import { loadRawSrc, MILLISECONDS_IN } from 'utils'
 import { ServerNavigation, BossGrid, RecentlyAppeared } from './components'
 
 export const heroSrc = loadRawSrc('/bosses.png')
@@ -14,25 +14,36 @@ const BossTracker = ({
   activeServers,
   bossChances,
   recentlyAppeared,
-}: BossTrackerProps) => (
-  <>
-    <ServerNavigation
-      currentServer={bossChances.server}
-      activeServers={activeServers}
-    />
-    <main className="inner-container pb-8">
-      <Hero src={heroSrc} title="Boss Tracker" offset />
+}: BossTrackerProps) => {
+  const hoursSinceLastUpdate = Math.round(
+    (+new Date() - bossChances.lastUpdated) / MILLISECONDS_IN.HOUR,
+  )
 
-      <div className="grid items-start gap-8 md:relative md:flex md:flex-row-reverse md:gap-16 lg:gap-8">
-        <div className="shrink-0 md:sticky md:top-[134px] md:w-[320px]">
-          <RecentlyAppeared bosses={recentlyAppeared} />
+  const subtitle =
+    hoursSinceLastUpdate === 0
+      ? 'Updated a few minutes ago'
+      : `Updated ${hoursSinceLastUpdate} hours ago`
+
+  return (
+    <>
+      <ServerNavigation
+        currentServer={bossChances.server}
+        activeServers={activeServers}
+      />
+      <main className="inner-container pb-8">
+        <Hero src={heroSrc} title="Boss Tracker" offset subtitle={subtitle} />
+
+        <div className="grid items-start gap-8 md:relative md:flex md:flex-row-reverse md:gap-16 lg:gap-8">
+          <div className="shrink-0 md:sticky md:top-[134px] md:w-[320px]">
+            <RecentlyAppeared bosses={recentlyAppeared} />
+          </div>
+
+          <div className="bg-separator h-[1px] w-full md:hidden" role="none" />
+          <BossGrid bosses={bossChances.bosses} className="grow" />
         </div>
-
-        <div className="bg-separator h-[1px] w-full md:hidden" role="none" />
-        <BossGrid bosses={bossChances.bosses} className="grow" />
-      </div>
-    </main>
-  </>
-)
+      </main>
+    </>
+  )
+}
 
 export default BossTracker
