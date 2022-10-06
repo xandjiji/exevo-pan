db14a8731410a3c60368796d289a1bf5388f2175
xandjiji <xandjiji@gmail.com>
2022-10-06 14:11:00 -0300

    feature(api): read method

---
+++ apps/exevo-pan/src/pages/api/read.ts
@@ -0,0 +1,43 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import { PrismaClient } from '@prisma/client'
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+const prisma = new PrismaClient()
+
+type Query = Parameters<typeof prisma.characterObject.count>[0] &
+  Parameters<typeof prisma.characterObject.findMany>[0]
+
+export default async (
+  request: VercelRequest,
+  response: VercelResponse,
+): Promise<void> => {
+  const { method } = request
+  if (method !== 'GET') {
+    response.status(400).send(`${method} not allowed `)
+
+    return
+  }
+
+  try {
+    const query: Query = {
+      where: {
+        nickname: { contains: 'h' },
+      },
+    }
+
+    const results = await prisma.$transaction([
+      prisma.characterObject.count(query),
+      prisma.characterObject.findMany({
+        ...query,
+        take: 1,
+        skip: 1,
+      }),
+    ])
+
+    response.status(200).json(results)
+    return
+  } catch (error) {
+    response.status(400).json({ error })
+  }
+}
