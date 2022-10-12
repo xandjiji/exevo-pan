47e59b7ba4d2e07dc76a53a9c737fa7fa68576fe
xandjiji <xandjiji@gmail.com>
2022-10-12 11:13:15 -0300

    feature: updating prisma client

--- apps/bazaar-scraper/src/services/prisma/index.ts
+++ apps/bazaar-scraper/src/services/prisma/index.ts
@@ -6,6 +6,6 @@ declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = global.prisma || new PrismaClient()
+export const prisma = global.prisma || new PrismaClient({ log: ['error'] })
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma
