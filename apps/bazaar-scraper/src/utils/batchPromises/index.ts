f4ed1f96d3ea766faf28d3f37a06e586c9281fb5
xandjiji <xandjiji@gmail.com>
2022-10-15 17:12:26 -0300

    feature(utils): enabling `batchPromises` configuration

--- apps/bazaar-scraper/src/utils/batchPromises/index.ts
+++ apps/bazaar-scraper/src/utils/batchPromises/index.ts
@@ -2,11 +2,11 @@
 import { sleep } from 'utils'
 import { requests } from 'Constants'
 
-const { MAX_CONCURRENT_REQUESTS, DELAY } = requests
-
 export const batchPromises = async <T>(
   tasks: Array<() => Promise<T>>,
+  config?: Partial<typeof requests>,
 ): Promise<T[]> => {
+  const { MAX_CONCURRENT_REQUESTS, DELAY } = { ...requests, ...config }
   const results: T[] = []
 
   for (let i = 0; i < tasks.length; i += MAX_CONCURRENT_REQUESTS) {
