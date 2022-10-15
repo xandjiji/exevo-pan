5afe96607e621a21fcf41016e3638abcdeda6bc2
xandjiji <xandjiji@gmail.com>
2022-10-15 19:31:47 -0300

    chore: removing unused code

--- apps/bazaar-scraper/src/Constants/file/index.ts
+++ apps/bazaar-scraper/src/Constants/file/index.ts
@@ -2,14 +2,12 @@ import { BOSS_STATISTICS, BOSS_CHANCES } from './bossStatistics'
 import { CURRENT_AUCTIONS } from './currentAuctions'
 import { HISTORY_AUCTIONS, SCRAP_HISTORY_DATA } from './historyAuctions'
 import { HISTORY_STATISTICS } from './historyStatistics'
-import { RARE_ITEM_DATA } from './rareItemData'
 import { SCRAP_RAW_DATA, RAW_DATA_FOLDER } from './rawBazaar'
 
 export const file = {
   BOSS_STATISTICS,
   BOSS_CHANCES,
   CURRENT_AUCTIONS,
-  RARE_ITEM_DATA,
   HISTORY_AUCTIONS,
   SCRAP_HISTORY_DATA,
   HISTORY_STATISTICS,
--- apps/bazaar-scraper/src/Constants/file/rareItemData.ts
+++
@@ -1,8 +0,0 @@
-import { FileConstant, OUTPUT_PATH } from './types'
-
-const FILENAME = 'ItemsData.json'
-
-export const RARE_ITEM_DATA: FileConstant = {
-  name: FILENAME,
-  path: `${OUTPUT_PATH}/${FILENAME}`,
-}
