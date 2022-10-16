19fcb3824d602fcc012565cf60ebbb79ee18dff7
xandjiji <xandjiji@gmail.com>
2022-10-16 11:17:34 -0300

    chore: removing unused code

--- apps/bazaar-scraper/src/Constants/file/index.ts
+++ apps/bazaar-scraper/src/Constants/file/index.ts
@@ -1,13 +1,10 @@
 import { BOSS_STATISTICS, BOSS_CHANCES } from './bossStatistics'
-import { HISTORY_AUCTIONS, SCRAP_HISTORY_DATA } from './historyAuctions'
 import { HISTORY_STATISTICS } from './historyStatistics'
 import { SCRAP_RAW_DATA, RAW_DATA_FOLDER } from './rawBazaar'
 
 export const file = {
   BOSS_STATISTICS,
   BOSS_CHANCES,
-  HISTORY_AUCTIONS,
-  SCRAP_HISTORY_DATA,
   HISTORY_STATISTICS,
   SCRAP_RAW_DATA,
   RAW_DATA_FOLDER,
