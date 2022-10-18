081d414077852a83e18d3b2b0a4de1ba5e93d874
xandjiji <xandjiji@gmail.com>
2022-10-18 15:37:34 -0300

    chore: removing unused code

--- apps/bazaar-scraper/src/Data/index.ts
+++ apps/bazaar-scraper/src/Data/index.ts
@@ -1,4 +1,3 @@
 export { default as BossStatistics } from './BossStatistics'
-export { default as HistoryStatistics } from './HistoryStatistics'
 export { default as RawBazaar } from './RawBazaar'
 export { default as Static } from './Static'
