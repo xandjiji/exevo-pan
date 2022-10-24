9b7041d39161108cfae3fd2e1bf9dee45e80e3c2
xandjiji <xandjiji@gmail.com>
2022-10-24 16:04:48 -0300

    refactor(useDrawerFields): updating `rareItemOptions` API

--- apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/index.tsx
+++ apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/index.tsx
@@ -4,7 +4,7 @@ import { DrawerFieldsContextData, DrawerFieldsProviderProps } from './types'
 
 const DEFAULT_STATE: DrawerFieldsContextData = {
   serverOptions: [],
-  rareItemData: {},
+  rareItemOptions: [],
   imbuementOptions: [],
   charmOptions: [],
   questOptions: [],
@@ -20,11 +20,11 @@ const DrawerFieldsContext =
 
 export const DrawerFieldsProvider = ({
   serverOptions,
-  rareItemData,
+  rareItemOptions,
   children,
 }: DrawerFieldsProviderProps) => (
   <DrawerFieldsContext.Provider
-    value={{ serverOptions, rareItemData, ...options }}
+    value={{ serverOptions, rareItemOptions, ...options }}
   >
     {children}
   </DrawerFieldsContext.Provider>
--- apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/types.ts
+++ apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/types.ts
@@ -2,7 +2,7 @@ import React from 'react'
 
 export interface DrawerFieldsContextData {
   serverOptions: Option[]
-  rareItemData: RareItemData
+  rareItemOptions: Option[]
   imbuementOptions: Option[]
   charmOptions: Option[]
   questOptions: Option[]
@@ -15,7 +15,7 @@ export interface DrawerFieldsContextData {
 
 type ServerSideProps = Pick<
   DrawerFieldsContextData,
-  'serverOptions' | 'rareItemData'
+  'serverOptions' | 'rareItemOptions'
 >
 
 export interface DrawerFieldsProviderProps extends ServerSideProps {
