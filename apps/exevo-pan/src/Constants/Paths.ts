1d675ce352b8a7d5ec0f4b343dcc64e0de8afff4
xandjiji <xandjiji@gmail.com>
2022-10-24 16:25:46 -0300

    refactor(services): updating `fetchAuctionedItemOptions()` API

--- apps/exevo-pan/src/Constants/Paths.ts
+++ apps/exevo-pan/src/Constants/Paths.ts
@@ -1,6 +1,5 @@
 export const paths = {
   BOSS_CHANCES: '/bossChances',
-  ITEMS_DATA: '/ItemsData.json',
   OVERALL_STATISTICS: '/HistoryStatistics.json',
   WAR_STATISTICS: '/statistics.json',
   PUNE_DATA: '/miniLibertabraPuneData.json',
--- apps/exevo-pan/src/services/DrawerFields/index.tsx
+++ apps/exevo-pan/src/services/DrawerFields/index.tsx
@@ -1,10 +1,6 @@
-import { endpoints, paths } from 'Constants'
-import { filterItemData } from './utils'
 import { prisma } from '../prisma'
 
 export default class DrawerFieldsClient {
-  private static rareItemDataUrl = `${endpoints.STATIC_DATA}${paths.ITEMS_DATA}`
-
   static async fetchServerOptions(args?: {
     active?: boolean
   }): Promise<Option[]> {
@@ -20,9 +16,8 @@ export default class DrawerFieldsClient {
     }))
   }
 
-  static async fetchAuctionedItemOptions(): Promise<RareItemData> {
-    const response = await fetch(this.rareItemDataUrl)
-    const data: RareItemData = await response.json()
-    return filterItemData(data)
+  static async fetchAuctionedItemOptions(): Promise<Option[]> {
+    const result = await prisma.currentRareItems.findMany()
+    return result.map(({ name }) => ({ name, value: name }))
   }
 }
