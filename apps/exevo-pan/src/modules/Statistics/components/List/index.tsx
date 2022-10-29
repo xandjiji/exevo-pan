0f62fccd9e0078277c913c1d48f65fd8815d05ca
xandjiji <xandjiji@gmail.com>
2022-10-29 11:25:47 -0300

    feature(List): adding `<CharacterModal />`

--- apps/exevo-pan/src/modules/Statistics/components/List/index.tsx
+++ apps/exevo-pan/src/modules/Statistics/components/List/index.tsx
@@ -1,6 +1,9 @@
 import { useTranslations } from 'contexts/useTranslation'
-import { memo } from 'react'
+import { memo, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Table } from 'components/Atoms'
+import CharacterModal from 'components/CharacterCard/CharacterModal'
+import { permalinkResolver } from 'utils'
 import { ListProps } from './types'
 
 const List = ({
@@ -14,60 +17,80 @@ const List = ({
     translations: { statistics },
   } = useTranslations()
 
+  const { locale } = useRouter()
+
+  const [selectedCharacter, setSelectedCharacter] = useState<
+    CharacterObject | undefined
+  >()
+
   return (
-    <Table {...props} title={title} subtitle="Top 10">
-      <Table.Element>
-        <caption>{`${statistics.List.captionTop10} ${title} ${statistics.List.captionDescription}`}</caption>
-        <Table.Head>
-          <Table.Row>
-            <Table.HeadColumn
-              aria-label={statistics.List.titleLabel}
-              className="w-4 text-center"
-            >
-              #
-            </Table.HeadColumn>
-            <Table.HeadColumn className="w-full px-2 text-left">
-              Nickname
-            </Table.HeadColumn>
-            <Table.HeadColumn
-              highlighted
-              desc
-              className="w-fit whitespace-nowrap text-right"
-            >
-              {title}
-            </Table.HeadColumn>
-          </Table.Row>
-        </Table.Head>
+    <>
+      <Table {...props} title={title} subtitle="Top 10">
+        <Table.Element>
+          <caption>{`${statistics.List.captionTop10} ${title} ${statistics.List.captionDescription}`}</caption>
+          <Table.Head>
+            <Table.Row>
+              <Table.HeadColumn
+                aria-label={statistics.List.titleLabel}
+                className="w-4 text-center"
+              >
+                #
+              </Table.HeadColumn>
+              <Table.HeadColumn className="w-full px-2 text-left">
+                Nickname
+              </Table.HeadColumn>
+              <Table.HeadColumn
+                highlighted
+                desc
+                className="w-fit whitespace-nowrap text-right"
+              >
+                {title}
+              </Table.HeadColumn>
+            </Table.Row>
+          </Table.Head>
 
-        <Table.Body>
-          {charactersList.map((character, index) => {
-            const displayedData = pickFromCharacter(character)
+          <Table.Body>
+            {charactersList.map((character, index) => {
+              const displayedData = pickFromCharacter(character)
 
-            return (
-              <Table.Row key={character.id}>
-                <Table.Column className="w-4 text-center align-top text-xs leading-relaxed">
-                  {index + 1}
-                </Table.Column>
-                <Table.Column className="w-full px-2 text-left">
-                  <a
-                    href={`https://www.tibia.com/charactertrade/?subtopic=currentcharactertrades&page=details&auctionid=${character.id}&source=overview`}
-                    target="_blank"
-                    rel="noreferrer noopener external"
-                  >
-                    {character.nickname}
-                  </a>
-                </Table.Column>
-                <Table.Column className="w-fit whitespace-nowrap text-right">
-                  {formatCharacterValue
-                    ? formatCharacterValue(displayedData)
-                    : displayedData}
-                </Table.Column>
-              </Table.Row>
-            )
+              return (
+                <Table.Row key={character.id}>
+                  <Table.Column className="w-4 text-center align-top text-xs leading-relaxed">
+                    {index + 1}
+                  </Table.Column>
+                  <Table.Column className="w-full px-2 text-left">
+                    <button
+                      type="button"
+                      onClick={() => setSelectedCharacter(character)}
+                      className="text-primaryHighlight text-s cursor-pointer font-normal transition-all hover:opacity-70"
+                    >
+                      {character.nickname}
+                    </button>
+                  </Table.Column>
+                  <Table.Column className="w-fit whitespace-nowrap text-right">
+                    {formatCharacterValue
+                      ? formatCharacterValue(displayedData)
+                      : displayedData}
+                  </Table.Column>
+                </Table.Row>
+              )
+            })}
+          </Table.Body>
+        </Table.Element>
+      </Table>
+
+      {selectedCharacter && (
+        <CharacterModal
+          characterData={selectedCharacter}
+          onClose={() => setSelectedCharacter(undefined)}
+          past
+          permalink={permalinkResolver.history({
+            auctionId: selectedCharacter.id,
+            locale,
           })}
-        </Table.Body>
-      </Table.Element>
-    </Table>
+        />
+      )}
+    </>
   )
 }
 
