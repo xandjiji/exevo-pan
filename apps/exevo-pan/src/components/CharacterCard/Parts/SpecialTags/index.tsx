c246592ae5afc68b236166d266001d6534cc2a57
xandjiji <xandjiji@gmail.com>
2022-11-03 20:39:56 -0300

    chore: removing unused dependency

--- apps/exevo-pan/src/components/CharacterCard/Parts/SpecialTags/index.tsx
+++ apps/exevo-pan/src/components/CharacterCard/Parts/SpecialTags/index.tsx
@@ -1,16 +1,16 @@
 import { useTranslations } from 'contexts/useTranslation'
-import { useMemo } from 'react'
 import clsx from 'clsx'
-import { getCharacterTags } from 'shared-utils/dist/getCharacterTags'
 import { Chip } from 'components/Atoms'
 import { SpecialTagsProps } from './types'
 
-const SpecialTags = ({ character, className, ...props }: SpecialTagsProps) => {
+const SpecialTags = ({
+  character: { tags },
+  className,
+  ...props
+}: SpecialTagsProps) => {
   const {
     translations: { common },
   } = useTranslations()
-
-  const tags = useMemo(() => getCharacterTags(character), [character])
 
   if (tags.length === 0) return null
 
