cf86e967ff7f87f35fc363bafb3982a25a3c6026
xandjiji <xandjiji@gmail.com>
2022-11-04 17:31:14 -0300

    refactor: refactoring `urlParamsState`

--- apps/exevo-pan/src/hooks/useUrlParamsState/__tests__/index.ts
+++ apps/exevo-pan/src/hooks/useUrlParamsState/__tests__/index.ts
@@ -1,19 +1,25 @@
-import { urlParametersState } from '..'
+import {
+  urlParametersStateFactory,
+  SchemaCodec,
+} from 'shared-utils/dist/urlSerializer'
 
-const mockRegister = [
-  {
-    key: 'itemA',
+const mockSchema: SchemaCodec<{
+  itemA: string
+  itemB: string
+}> = {
+  itemA: {
+    urlKey: 'itemA',
     defaultValue: 'default value for,A',
   },
-  {
-    key: 'itemB',
+  itemB: {
+    urlKey: 'itemB',
     defaultValue: 'default value for,B',
   },
-]
+}
 
 describe('urlParameterState', () => {
   test('should have correct default values', () => {
-    const { defaultValues } = urlParametersState(mockRegister)
+    const { defaultValues } = urlParametersStateFactory(mockSchema)
 
     expect(defaultValues).toEqual({
       itemA: 'default value for,A',
@@ -22,58 +28,59 @@ describe('urlParameterState', () => {
   })
 
   test('getUrlValues should have correct values', () => {
-    const { getUrlValues } = urlParametersState(mockRegister)
+    const { get } = urlParametersStateFactory(mockSchema)
 
-    expect(getUrlValues()).toEqual({
+    expect(get.defaultValues()).toEqual({
       itemA: 'default value for,A',
       itemB: 'default value for,B',
     })
   })
 
   test('setUrlValues should set values correctly', () => {
-    const { setUrlValues, getUrlValues } = urlParametersState(mockRegister)
+    const { set, get } = urlParametersStateFactory(mockSchema)
 
-    setUrlValues({ itemA: 'new value for+A' })
-    expect(getUrlValues()).toEqual({
+    set.urlValues({ itemA: 'new value for+A' })
+    expect(get.urlValues()).toEqual({
       itemA: 'new value for+A',
       itemB: 'default value for,B',
     })
 
-    setUrlValues({
+    set.urlValues({
       itemA: 'yet another new value A',
       itemB: 'yet another new value B',
     })
 
-    expect(getUrlValues()).toEqual({
+    expect(get.urlValues()).toEqual({
       itemA: 'yet another new value A',
       itemB: 'yet another new value B',
     })
 
-    setUrlValues({
+    set.urlValues({
       itemA: 'yet AGAIN another new value A',
-      notAKey: 'shouldnt have any effect',
     })
 
-    expect(getUrlValues()).toEqual({
+    expect(get.urlValues()).toEqual({
       itemA: 'yet AGAIN another new value A',
       itemB: 'yet another new value B',
     })
   })
 
   test('should encode/decode value correctly', () => {
-    const { setUrlValues, getUrlValues, defaultValues } = urlParametersState([
-      {
-        key: 'itemC',
+    const { set, get, defaultValues } = urlParametersStateFactory<{
+      itemC: number
+    }>({
+      itemC: {
+        urlKey: 'itemC',
         defaultValue: 10,
         encode: (value: number) => encodeURIComponent(value * 20),
         decode: (value: string) => Number(decodeURIComponent(value)) / 20,
       },
-    ])
+    })
 
     expect(defaultValues).toEqual({ itemC: 10 })
-    expect(getUrlValues()).toEqual({ itemC: 10 })
+    expect(get.urlValues()).toEqual({ itemC: 10 })
 
-    setUrlValues({ itemC: 30 })
-    expect(getUrlValues()).toEqual({ itemC: 30 })
+    set.urlValues({ itemC: 30 })
+    expect(get.urlValues()).toEqual({ itemC: 30 })
   })
 })
