eea78dff0e7bc55f7e4adc462619f98416b8fb68
xandjiji <xandjiji@gmail.com>
2022-11-07 22:22:41 -0300

    feature(api): adding cache strategy

--- apps/exevo-pan/src/pages/api/auctions.ts
+++ apps/exevo-pan/src/pages/api/auctions.ts
@@ -1,6 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { VercelRequest, VercelResponse } from '@vercel/node'
 import { endpoints } from 'Constants'
+import { MILLISECONDS_IN } from 'utils'
+
+const AUCTION_PERIOD = MILLISECONDS_IN.MINUTE * 15
+
+const millisecondsToSeconds = (milliseconds: number) =>
+  Math.floor(milliseconds / MILLISECONDS_IN.MINUTE)
+
+const timeUntilNextCacheBust = () =>
+  millisecondsToSeconds(AUCTION_PERIOD - (+new Date() % AUCTION_PERIOD))
+
+const CACHE_AGE = {
+  current: timeUntilNextCacheBust,
+  history: millisecondsToSeconds(MILLISECONDS_IN.HOUR * 4),
+}
 
 export default async (
   request: VercelRequest,
@@ -26,6 +40,10 @@ export default async (
 
     const result = await fetch(endpoint)
     const parsedResult = await result.json()
+
+    const maxAge = isHistory ? CACHE_AGE.history : CACHE_AGE.current()
+    response.setHeader('Cache-Control', `max-age=${maxAge}, s-maxage=${maxAge}`)
+
     response.status(200).json(parsedResult)
   } catch (error) {
     response.status(400).json(error)
