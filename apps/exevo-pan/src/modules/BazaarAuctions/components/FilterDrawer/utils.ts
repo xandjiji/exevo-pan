1a428484d271c44850594cc7b0c3a3329dc6af63
xandjiji <xandjiji@gmail.com>
2022-11-08 21:14:58 -0300

    fix(FilterDrawer): displaying rare items query only for current auctions

--- apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/utils.ts
+++ apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/utils.ts
@@ -4,6 +4,6 @@ import { routes } from 'Constants'
 export const isHistory = (): boolean => {
   if (isServer()) return false
 
-  if (window.location.pathname === routes.BAZAAR_HISTORY) return true
+  if (window.location.pathname.includes(routes.BAZAAR_HISTORY)) return true
   return false
 }
