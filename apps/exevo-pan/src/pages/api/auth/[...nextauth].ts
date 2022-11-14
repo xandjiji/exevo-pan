f65c1f8e7b64b22394c238b6b68810f793de07f8
xandjiji <xandjiji@gmail.com>
2022-11-14 10:37:47 -0300

    feature(auth): adding prisma adapter

--- apps/exevo-pan/src/pages/api/auth/[...nextauth].ts
+++ apps/exevo-pan/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth/next'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from 'lib/prisma'
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
