2db03c9e4120652c32aeb65c7f2c6484d9e306ab
xandjiji <xandjiji@gmail.com>
2022-11-17 16:25:23 -0300

    feature(UpgradeToPro): abstracting `<Pitch />` component

---
+++ apps/exevo-pan/src/modules/Account/components/UpgradeToPro/Pitch.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx'
+import { CheckIcon } from 'assets/svgs'
+
+const Li = ({ className, children, ...props }: JSX.IntrinsicElements['li']) => (
+  <li className={clsx('flex items-center gap-1', className)} {...props}>
+    <CheckIcon className="fill-greenHighlight" />
+    {children}
+  </li>
+)
+
+const Pitch = () => (
+  <div className="grid place-items-center gap-8">
+    <div className="grid place-items-center gap-2">
+      <p className="flex items-center gap-2">
+        Upgrade now to{' '}
+        <strong className="text-primaryHighlight text-2xl tracking-wider">
+          Exevo Pro 🧙‍♂️
+        </strong>
+      </p>
+      <p>And have access to exclusive features!</p>
+    </div>
+
+    <div className="text-tsm grid gap-4">
+      <ul className="grid gap-2">
+        <Li>Find out how much TC was invested in any Bazaar character</Li>
+        <Li>
+          {' '}
+          Exclusive <strong>auction filters</strong>
+        </Li>
+        <Li>
+          Access to all bosses from <strong>Boss Tracker</strong>
+        </Li>
+      </ul>
+      <p className="text-right">...and more in the future!</p>
+    </div>
+
+    <p className="flex items-center gap-2">
+      Pay once, yours <strong className="text-2xl">forever 🙌</strong>
+    </p>
+  </div>
+)
+
+export default Pitch
--- apps/exevo-pan/src/modules/Account/components/UpgradeToPro/index.tsx
+++ apps/exevo-pan/src/modules/Account/components/UpgradeToPro/index.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx'
 import { LabeledCard, Input, Button, Text } from 'components/Atoms'
-import { CheckIcon } from 'assets/svgs'
+import Pitch from './Pitch'
 
 /* 
 - abstract components
@@ -9,47 +9,13 @@ import { CheckIcon } from 'assets/svgs'
 - payed state (delivery tooltip)
 */
 
-const Li = ({ className, children, ...props }: JSX.IntrinsicElements['li']) => (
-  <li className={clsx('flex items-center gap-1', className)} {...props}>
-    <CheckIcon className="fill-greenHighlight" />
-    {children}
-  </li>
-)
-
 const UpgradeToPro = () => {
   console.log(9)
 
   return (
     <div className="grid gap-8">
-      <div className="grid place-items-center gap-8">
-        <div className="grid place-items-center gap-2">
-          <p className="flex items-center gap-2">
-            Upgrade now to{' '}
-            <strong className="text-primaryHighlight text-2xl tracking-wider">
-              Exevo Pro 🧙‍♂️
-            </strong>
-          </p>
-          <p>And have access to exclusive features!</p>
-        </div>
+      <Pitch />
 
-        <div className="text-tsm grid gap-4">
-          <ul className="grid gap-2">
-            <Li>Find out how much TC was invested in any Bazaar character</Li>
-            <Li>
-              {' '}
-              Exclusive <strong>auction filters</strong>
-            </Li>
-            <Li>
-              Access to all bosses from <strong>Boss Tracker</strong>
-            </Li>
-          </ul>
-          <p className="text-right">...and more in the future!</p>
-        </div>
-
-        <p className="flex items-center gap-2">
-          Pay once, yours <strong className="text-2xl">forever 🙌</strong>
-        </p>
-      </div>
       <LabeledCard labelText="Buy now">
         <span className="text-tsm flex items-center gap-1">
           Transfer
