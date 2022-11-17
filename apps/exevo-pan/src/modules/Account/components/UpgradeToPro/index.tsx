7d9b2880c7a75e63f0919bd2a5cc491ab9e2b231
xandjiji <xandjiji@gmail.com>
2022-11-17 17:07:59 -0300

    feature(UpgradeToPro): initial form action

--- apps/exevo-pan/src/modules/Account/components/UpgradeToPro/index.tsx
+++ apps/exevo-pan/src/modules/Account/components/UpgradeToPro/index.tsx
@@ -1,16 +1,31 @@
 import clsx from 'clsx'
+import { useState, useCallback } from 'react'
 import { LabeledCard, Input, Button, Text } from 'components/Atoms'
+import type { PaymentData } from '@prisma/client'
 import Pitch from './Pitch'
 
 /* 
-- abstract components
-- <FROM> to Ksu
 - confirm ACTION (loading state, api request, etc)
+- <FROM> to Ksu
 - payed state (delivery tooltip)
 */
 
-const UpgradeToPro = () => {
-  console.log(9)
+const UpgradeToPro = ({ character }: Partial<PaymentData>) => {
+  const [inputValue, setInputValue] = useState(character)
+
+  const onSubmit = useCallback(
+    (
+      e: React.FormEvent<
+        ExtendedHTMLFormElement<{ character: HTMLInputElement }>
+      >,
+    ) => {
+      e.preventDefault()
+      const { value } = e.currentTarget.elements.character
+
+      console.log(value)
+    },
+    [],
+  )
 
   return (
     <div className="grid gap-8">
@@ -19,21 +34,30 @@ const UpgradeToPro = () => {
       <LabeledCard labelText="Buy now">
         <span className="text-tsm flex items-center gap-1">
           Transfer
-          <Text.TibiaCoin value={250} /> Tibia Coins to{' '}
-          <strong className="text-primaryHighlight">Ksu</strong>
+          <Text.TibiaCoin value={250} /> Tibia Coins{' '}
+          {inputValue ? (
+            <>
+              from <strong>{inputValue}</strong>
+            </>
+          ) : null}{' '}
+          to <strong className="text-primaryHighlight">Ksu</strong>
         </span>
 
-        <div className="flex items-end gap-4">
+        <form name="test" onSubmit={onSubmit} className="flex items-end gap-4">
           <Input
             className="w-full"
+            name="character"
             label="Sending coins character"
             placeholder="e.g, 'Bubble'"
             noAlert
+            defaultValue={character}
+            onChange={(e) => setInputValue(e.target.value.trim())}
+            enterKeyHint="send"
           />
-          <Button type="button" pill className="mb-[1px] py-3">
+          <Button type="submit" pill className="mb-[1px] py-3">
             Confirm
           </Button>
-        </div>
+        </form>
       </LabeledCard>
     </div>
   )
