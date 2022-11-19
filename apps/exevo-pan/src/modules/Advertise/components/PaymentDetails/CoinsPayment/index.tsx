ef808629edd118395ea99a05a29cc8dc83a67764
xandjiji <xandjiji@gmail.com>
2022-11-19 10:12:49 -0300

    feature(CoinsPayment): using `<Text.Transfer />` component

--- apps/exevo-pan/src/modules/Advertise/components/PaymentDetails/CoinsPayment/index.tsx
+++ apps/exevo-pan/src/modules/Advertise/components/PaymentDetails/CoinsPayment/index.tsx
@@ -1,8 +1,8 @@
 import { useTranslations } from 'contexts/useTranslation'
-import { CopyButton } from 'components/Atoms'
+import { Text, CopyButton } from 'components/Atoms'
 import { advertising } from 'Constants'
 import { useForm } from '../../../contexts/Form'
-import { calculatePrice, readablePrice } from '../../../utils'
+import { calculatePrice } from '../../../utils'
 
 const Strong = (args: JSX.IntrinsicElements['strong']) => (
   <strong {...args} className="whitespace-nowrap" />
@@ -17,27 +17,31 @@ const CoinsPayment = () => {
 
   return (
     <p className="text-s">
-      {advertise.PaymentDetails.CoinsPayment.instruction}{' '}
-      <Strong>
-        {readablePrice.full.TIBIA_COINS(
-          calculatePrice(selectedDates.length, 'TIBIA_COINS').totalPrice,
-        )}
-      </Strong>{' '}
-      {advertise.PaymentDetails.CoinsPayment.from}{' '}
-      <Strong>{paymentCharacter.value}</Strong>{' '}
-      {advertise.PaymentDetails.CoinsPayment.to}{' '}
-      <a
-        href={`https://www.tibia.com/community/?name=${advertising.BANK_CHARACTER}`}
-        target="_blank"
-        rel="noreferrer noopener external"
-        className="text-primaryHighlight font-bold tracking-wide"
-      >
-        {advertising.BANK_CHARACTER}
-      </a>
-      <CopyButton
-        copyString={advertising.BANK_CHARACTER}
-        className="relative top-1.5 ml-[1px] !inline-grid"
-      />
+      {advertise.PaymentDetails.CoinsPayment.instruction}
+      {' Tibia Coins '}
+      {advertise.PaymentDetails.CoinsPayment.from}
+      {': '}
+      <div className="flex items-baseline justify-center gap-2">
+        <Strong>{paymentCharacter.value}</Strong>{' '}
+        <Text.Transfer
+          amount={
+            calculatePrice(selectedDates.length, 'TIBIA_COINS').totalPrice
+          }
+          currency="tc"
+        />
+        <a
+          href={`https://www.tibia.com/community/?name=${advertising.BANK_CHARACTER}`}
+          target="_blank"
+          rel="noreferrer noopener external"
+          className="text-primaryHighlight font-bold tracking-wide"
+        >
+          {advertising.BANK_CHARACTER}
+        </a>
+        <CopyButton
+          copyString={advertising.BANK_CHARACTER}
+          className="relative top-1.5 ml-[1px] !inline-grid"
+        />
+      </div>
     </p>
   )
 }
