0591127fbc4bb640ebf924f42163af0075aff002
xandjiji <xandjiji@gmail.com>
2022-11-22 13:48:47 -0300

    feature(types): declaring `ReducerObject`

--- apps/exevo-pan/src/types/Utilities.d.ts
+++ apps/exevo-pan/src/types/Utilities.d.ts
@@ -3,3 +3,17 @@ declare type KeysOfType<T, V> = {
 }[keyof T]
 
 declare type FilterProperties<T, V> = Pick<T, KeysOfType<T, V>>
+
+type ExtractReducerActionNames<Action extends { type: string }> = Action['type']
+
+type ExtractReducerActionArgs<
+  Actions extends { type: string },
+  ActionName extends ExtractReducerActionNames<Actions>,
+> = Omit<Extract<Actions, { type: ActionName }>, 'type'>
+
+declare type ReducerObject<Actions extends { type: string }, ReducerState> = {
+  [ActionName in ExtractReducerActionNames<Actions>]: (
+    currentState: ReducerState,
+    args: ExtractReducerActionArgs<Actions, ActionName>,
+  ) => ReducerState
+}
