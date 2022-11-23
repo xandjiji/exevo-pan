c6ae6dc302c77154561f6d3fe121c7b17d3f0c3c
xandjiji <xandjiji@gmail.com>
2022-11-23 19:54:14 -0300

    chore: removing unused code

--- apps/exevo-pan/src/modules/BazaarAuctions/index.tsx
+++ apps/exevo-pan/src/modules/BazaarAuctions/index.tsx
@@ -1,4 +1,3 @@
-export { FiltersProvider } from './contexts/useFilters'
 export { DrawerFieldsProvider } from './contexts/useDrawerFields'
 export { AuctionsProvider } from './contexts/useAuctions'
 export { default as AuctionsGrid } from './components/AuctionsGrid'
