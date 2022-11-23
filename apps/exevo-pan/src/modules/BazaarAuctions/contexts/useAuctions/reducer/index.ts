b32ffd53b9d4e7af1d76d7e64e0c84e6ade28e5d
xandjiji <xandjiji@gmail.com>
2022-11-23 19:51:04 -0300

    refactor(useAuctions): better pagination reset

--- apps/exevo-pan/src/modules/BazaarAuctions/contexts/useAuctions/reducer/index.ts
+++ apps/exevo-pan/src/modules/BazaarAuctions/contexts/useAuctions/reducer/index.ts
@@ -6,16 +6,16 @@ import { resetPagination, shouldDisplayHighlightedAuctions } from './utils'
 import FiltersReducer from './filters'
 import { Reducer, Action } from './types'
 
-const Reducer: Reducer<Action> = (state, action) => {
+const ActionsReducer: Reducer<Action> = (state, action) => {
   switch (action.type) {
     case 'SET_LOADING':
       return { ...state, loading: action.loading }
 
     case 'TOGGLE_HISTORY': {
-      const nextState: ReturnType<typeof Reducer> = resetPagination({
+      const nextState: ReturnType<typeof ActionsReducer> = {
         ...state,
         isHistory: !state.isHistory,
-      })
+      }
 
       nextState.sortingOptions =
         DEFAULT_SORT_OPTIONS[nextState.isHistory ? 'history' : 'current']
@@ -40,10 +40,10 @@ const Reducer: Reducer<Action> = (state, action) => {
       }
 
     case 'SET_SORTING':
-      return resetPagination({
+      return {
         ...state,
         sortingOptions: { ...state.sortingOptions, ...action.sortingOptions },
-      })
+      }
 
     case 'SET_PAGINATED_DATA':
       return shouldDisplayHighlightedAuctions({
@@ -55,6 +55,19 @@ const Reducer: Reducer<Action> = (state, action) => {
     default:
       return FiltersReducer(state, action)
   }
+}
+
+const resetPaginationActions: Set<Action['type']> = new Set([
+  'SET_SORTING',
+  'TOGGLE_HISTORY',
+])
+
+const Reducer: Reducer<Action> = (state, action) => {
+  const nextState = ActionsReducer(state, action)
+
+  if (resetPaginationActions.has(action.type)) resetPagination(nextState)
+
+  return nextState
 }
 
 export default Reducer
