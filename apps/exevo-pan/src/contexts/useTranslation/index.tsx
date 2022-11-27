0c6a9c094c50abd620c5117412ee45396e4151dd
xandjiji <xandjiji@gmail.com>
2022-11-27 10:21:11 -0300

    feature(i18n): adding `templateMessage()`

--- apps/exevo-pan/src/contexts/useTranslation/index.tsx
+++ apps/exevo-pan/src/contexts/useTranslation/index.tsx
@@ -9,3 +9,22 @@ export const TranslationsProvider = TranslationsContext.Provider
 
 export const useTranslations = (): TranslationsContextValues =>
   useContext(TranslationsContext)
+
+export const templateMessage = (
+  message: string,
+  template: Record<string, JSX.Element>,
+): JSX.Element => {
+  let messageTail = message
+  const templatedElements: JSX.Element[] = []
+
+  Object.entries(template).forEach(([key, jsx]) => {
+    const [prefix, tail] = messageTail.split(`{{${key}}}`)
+    templatedElements.push(<>{prefix}</>)
+    templatedElements.push(jsx)
+    messageTail = tail
+  })
+
+  if (messageTail) templatedElements.push(<>{messageTail}</>)
+
+  return <>{templatedElements}</>
+}
