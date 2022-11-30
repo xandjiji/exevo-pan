7735e39f8c95b10b82f542bbbc94316dbeabd19c
xandjiji <xandjiji@gmail.com>
2022-11-30 11:09:24 -0300

    feature(api): plucking premium parameters for free members

--- apps/exevo-pan/src/pages/api/auctions.ts
+++ apps/exevo-pan/src/pages/api/auctions.ts
@@ -1,9 +1,37 @@
 import type { VercelRequest } from '@vercel/node'
 import { getToken, JWT } from 'next-auth/jwt'
+import { filterSchema } from 'shared-utils/dist/contracts/Filters/schemas'
+import { dictionary as tags } from 'data-dictionary/dist/dictionaries/characterTags'
 import { endpoints } from 'Constants'
 import { pluckTCInvested } from 'utils'
 
 const isPro = (token: JWT | null) => token && token.proStatus
+
+const pluckPremiumParameters = (searchParams: URLSearchParams) => {
+  searchParams.delete(filterSchema.addon.urlKey)
+  searchParams.delete(filterSchema.sex.urlKey)
+
+  searchParams.delete(filterSchema.outfitSet.urlKey)
+  searchParams.delete(filterSchema.storeOutfitSet.urlKey)
+  searchParams.delete(filterSchema.mountSet.urlKey)
+  searchParams.delete(filterSchema.storeMountSet.urlKey)
+
+  searchParams.delete(filterSchema.tcInvested.urlKey)
+  searchParams.delete(filterSchema.auctionIds.urlKey)
+
+  const tagParams = searchParams.get(filterSchema.tags.urlKey)
+  if (tagParams) {
+    const filteredTagParams = tagParams
+      .split(',')
+      .filter((param) => param !== tags.soulwarAvailable)
+
+    if (filteredTagParams.length === 0) {
+      searchParams.delete(filterSchema.tags.urlKey)
+    } else {
+      searchParams.set(filterSchema.tags.urlKey, filteredTagParams.join(','))
+    }
+  }
+}
 
 export default async (request: VercelRequest) => {
   const { method, url } = request
@@ -25,13 +53,15 @@ export default async (request: VercelRequest) => {
     )
 
     const token = await getToken({ req: request })
+    const userIsPro = isPro(token)
 
     searchParams.delete('history')
+    if (!userIsPro) pluckPremiumParameters(searchParams)
     endpoint.search = searchParams.toString()
 
     const result = await fetch(endpoint.toString())
 
-    if (isPro(token)) {
+    if (userIsPro) {
       return new Response(result.body, {
         status: 200,
         headers: {
