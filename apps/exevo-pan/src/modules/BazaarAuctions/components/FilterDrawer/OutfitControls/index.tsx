ceb8595d0688b3246d7945816cab6c12e57a9b73
xandjiji <xandjiji@gmail.com>
2022-11-30 16:08:15 -0300

    feature(OutfitControls): adding disabled state

--- apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/OutfitControls/index.tsx
+++ apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/OutfitControls/index.tsx
@@ -6,7 +6,10 @@ import { useAuctions } from '../../../contexts/useAuctions'
 import styles from './styles.module.css'
 import { OutfitControlsProps } from './types'
 
-const OutfitControls = ({ disableAddons = false }: OutfitControlsProps) => {
+const OutfitControls = ({
+  isPro = false,
+  disableAddons = false,
+}: OutfitControlsProps) => {
   const { filterState, dispatch } = useAuctions()
 
   const firstAddonSelected = filterState.addon === 1 || filterState.addon === 3
@@ -15,26 +18,31 @@ const OutfitControls = ({ disableAddons = false }: OutfitControlsProps) => {
   return (
     <div className="mt-1.5 mb-2 flex w-full flex-wrap items-center gap-3.5">
       <div
-        onClick={() => dispatch({ type: 'TOGGLE_FILTER', key: 'sex' })}
+        onClick={
+          isPro
+            ? () => dispatch({ type: 'TOGGLE_FILTER', key: 'sex' })
+            : undefined
+        }
         className={clsx(
-          'text-tsm mr-4 flex w-full cursor-pointer items-center gap-2',
+          'text-tsm mr-4 flex w-full items-center gap-2',
+          isPro ? 'cursor-pointer' : 'cursor-not-allowed',
           styles.switchWrapper,
         )}
       >
         Male
-        <Switch active={filterState.sex} />
+        <Switch active={filterState.sex} disabled={!isPro} />
         Female
       </div>
 
       <Checkbox
         label="Addon 1"
-        disabled={disableAddons}
+        disabled={!isPro || disableAddons}
         checked={disableAddons || firstAddonSelected}
         onClick={() => dispatch({ type: 'TOGGLE_ADDON', value: 1 })}
       />
       <Checkbox
         label="Addon 2"
-        disabled={disableAddons}
+        disabled={!isPro || disableAddons}
         checked={disableAddons || secondAddonSelected}
         onClick={() => dispatch({ type: 'TOGGLE_ADDON', value: 2 })}
       />
