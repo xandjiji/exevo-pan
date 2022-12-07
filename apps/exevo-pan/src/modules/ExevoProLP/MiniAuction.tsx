a78e3855ada8f370f93389b802c0bf38736c8d0e
xandjiji <xandjiji@gmail.com>
2022-12-07 11:23:34 -0300

    feature(MiniAuction): improving highlight

--- apps/exevo-pan/src/modules/ExevoProLP/MiniAuction.tsx
+++ apps/exevo-pan/src/modules/ExevoProLP/MiniAuction.tsx
@@ -4,7 +4,7 @@ import { ExevoPanIcon } from 'assets/svgs'
 const MiniAuction = () => (
   <div className="bg-surface flex h-56 w-40 flex-col gap-3 rounded-xl p-4 shadow-md">
     <div className="flex gap-2">
-      <div className="bg-primaryVariant grid h-9 w-9 place-items-center rounded-xl opacity-50">
+      <div className="bg-primaryVariant grid h-9 w-9 place-items-center rounded-xl opacity-40">
         <ExevoPanIcon style={{ height: 18, width: 18 }} />
       </div>
 
@@ -14,19 +14,19 @@ const MiniAuction = () => (
       </div>
     </div>
 
-    <div className="grid grid-cols-2 gap-2 opacity-70">
+    <div className="grid grid-cols-2 gap-2 opacity-50">
       <div className="border-1 border-separator h-3 rounded border-solid" />
       <div className="border-1 border-separator h-3 rounded border-solid" />
     </div>
 
-    <div className="flex justify-center gap-2 opacity-50">
+    <div className="flex justify-center gap-2 opacity-40">
       <div className="bg-primaryVariant h-6 w-6 rounded-md" />
       <div className="bg-primaryVariant h-6 w-6 rounded-md" />
       <div className="bg-primaryVariant h-6 w-6 rounded-md" />
       <div className="bg-primaryVariant h-6 w-6 rounded-md" />
     </div>
 
-    <div className="grid grid-cols-2 gap-2 opacity-30">
+    <div className="grid grid-cols-2 gap-2 opacity-20">
       {[1, 2, 3, 4].map((key) => (
         <div key={key} className="flex items-center gap-1">
           <div className="bg-primary h-3 w-4 shrink-0 rounded" />
@@ -36,13 +36,13 @@ const MiniAuction = () => (
     </div>
 
     <div className="child:h-1 child:rounded grid grid-cols-2 gap-2">
-      <div className="bg-onSurface w-12 opacity-20" />
-      <div className="bg-onSurface  w-14 opacity-20" />
-      <div className="bg-onSurface w-10 opacity-20" />
-      <div className="bg-rare h-1 w-10" />
+      <div className="bg-onSurface/60 w-12 opacity-20" />
+      <div className="bg-onSurface/60  w-14 opacity-20" />
+      <div className="bg-onSurface/60 w-10 opacity-20" />
+      <div className="bg-rare w-10 animate-pulse" />
     </div>
 
-    <div className="flex gap-2 opacity-50">
+    <div className="flex gap-2 opacity-40">
       <div className="bg-primaryVariant h-3 w-7 rounded-md" />
       <div className="bg-primaryVariant h-3 w-7 rounded-md" />
     </div>
