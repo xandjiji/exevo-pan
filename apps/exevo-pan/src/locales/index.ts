85d64c6da66e33dd664dabd1ce0394d5c28225f8
Mathias Bynens <mathias@qiwi.be>
2022-12-12 22:11:23 +0100

    Alpha-sort

--- apps/exevo-pan/src/locales/index.ts
+++ apps/exevo-pan/src/locales/index.ts
@@ -7,150 +7,150 @@ import errorPT from './pt/error'
 import blogEN from './en/blog'
 import blogES from './es/blog'
 import blogNL from './nl/blog'
-import blogPT from './pt/blog'
 import blogPL from './pl/blog'
+import blogPT from './pt/blog'
 
 import advertiseEN from './en/advertise'
 import advertiseES from './es/advertise'
 import advertiseNL from './nl/advertise'
-import advertisePT from './pt/advertise'
 import advertisePL from './pl/advertise'
+import advertisePT from './pt/advertise'
 
 import commonEN from './en/common'
 import commonES from './es/common'
 import commonNL from './nl/common'
-import commonPT from './pt/common'
 import commonPL from './pl/common'
+import commonPT from './pt/common'
 
 import homepageEN from './en/homepage'
 import homepageES from './es/homepage'
 import homepageNL from './nl/homepage'
-import homepagePT from './pt/homepage'
 import homepagePL from './pl/homepage'
+import homepagePT from './pt/homepage'
 
 import statisticsEN from './en/statistics'
 import statisticsES from './es/statistics'
 import statisticsNL from './nl/statistics'
-import statisticsPT from './pt/statistics'
 import statisticsPL from './pl/statistics'
+import statisticsPT from './pt/statistics'
 
 import warEN from './en/war'
 import warES from './es/war'
 import warNL from './nl/war'
-import warPT from './pt/war'
 import warPL from './pl/war'
+import warPT from './pt/war'
 
 import calculatorsEN from './en/calculators'
 import calculatorsES from './es/calculators'
 import calculatorsNL from './nl/calculators'
-import calculatorsPT from './pt/calculators'
 import calculatorsPL from './pl/calculators'
+import calculatorsPT from './pt/calculators'
 
 import bossesEN from './en/bossTracker'
 import bossesES from './es/bossTracker'
 import bossesNL from './nl/bossTracker'
-import bossesPT from './pt/bossTracker'
 import bossesPL from './pl/bossTracker'
+import bossesPT from './pt/bossTracker'
 
 import loginEN from './en/login'
 import loginES from './es/login'
 import loginNL from './nl/login'
-import loginPT from './pt/login'
 import loginPL from './pl/login'
+import loginPT from './pt/login'
 
 import dashboardEN from './en/dashboard'
 import dashboardES from './es/dashboard'
 import dashboardNL from './nl/dashboard'
-import dashboardPT from './pt/dashboard'
 import dashboardPL from './pl/dashboard'
+import dashboardPT from './pt/dashboard'
 
 import exevoproEN from './en/exevopro'
 import exevoproES from './es/exevopro'
 import exevoproNL from './nl/exevopro'
-import exevoproPT from './pt/exevopro'
 import exevoproPL from './pl/exevopro'
+import exevoproPT from './pt/exevopro'
 
 export const error = {
   en: errorEN,
   es: errorES,
   //nl: errorNL,
-  pt: errorPT,
   pl: errorPL,
+  pt: errorPT,
 }
 export const blog = {
   en: blogEN,
   es: blogES,
   //nl: blogNL,
-  pt: blogPT,
   pl: blogPL,
+  pt: blogPT,
 }
 export const advertise = {
   en: advertiseEN,
   es: advertiseES,
   //nl: advertiseNL,
-  pt: advertisePT,
   pl: advertisePL,
+  pt: advertisePT,
 }
 export const common = {
   en: commonEN,
   es: commonES,
   //nl: commonNL,
-  pt: commonPT,
   pl: commonPL,
+  pt: commonPT,
 }
 export const homepage = {
   en: homepageEN,
   es: homepageES,
   //nl: homepageNL,
-  pt: homepagePT,
   pl: homepagePL,
+  pt: homepagePT,
 }
 export const statistics = {
   en: statisticsEN,
   es: statisticsES,
   //nl: statisticsNL,
-  pt: statisticsPT,
   pl: statisticsPL,
+  pt: statisticsPT,
 }
 export const war = {
   en: warEN,
   es: warES,
   //nl: warNL,
-  pt: warPT,
   pl: warPL,
+  pt: warPT,
 }
 export const calculators = {
   en: calculatorsEN,
   es: calculatorsES,
   //nl: calculatorsNL,
-  pt: calculatorsPT,
   pl: calculatorsPL,
+  pt: calculatorsPT,
 }
 export const bosses = {
   en: bossesEN,
   es: bossesES,
   //nl: bossesNL,
-  pt: bossesPT,
   pl: bossesPL,
+  pt: bossesPT,
 }
 export const login = {
   en: loginEN,
   es: loginES,
   //nl: loginNL,
-  pt: loginPT,
   pl: loginPL,
+  pt: loginPT,
 }
 export const dashboard = {
   en: dashboardEN,
   es: dashboardES,
   //nl: dashboardNL,
-  pt: dashboardPT,
   pl: dashboardPL,
+  pt: dashboardPT,
 }
 export const exevopro = {
   en: exevoproEN,
   es: exevoproES,
   //nl: exevoproNL,
-  pt: exevoproPT,
   pl: exevoproPL,
+  pt: exevoproPT,
 }
