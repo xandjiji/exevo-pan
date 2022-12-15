ea7116fb4ce2c4485ffcf68da858da5159182aa5
xandjiji <xandjiji@gmail.com>
2022-12-14 21:14:18 -0300

    test(LanguagePicker): updating tests

--- apps/exevo-pan/src/templates/Main/Header/LanguagePicker/__tests__/index.tsx
+++ apps/exevo-pan/src/templates/Main/Header/LanguagePicker/__tests__/index.tsx
@@ -1,7 +1,9 @@
 import { screen } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
-import { renderWithProviders } from 'utils/test'
+import { renderWithProviders, setup } from 'utils/test'
 import LanguagePicker from '..'
+
+const mockedUseRouter = setup.useRouter()
 
 describe('<LanguagePicker />', () => {
   test('should toggle visibility', () => {
@@ -14,5 +16,28 @@ describe('<LanguagePicker />', () => {
 
     userEvent.click(screen.getByRole('button', { name: 'Close dialog' }))
     expect(screen.queryByRole('menu')).not.toBeInTheDocument()
+  })
+
+  test('should select a language', () => {
+    const mockedArgs = {
+      locale: 'es',
+      push: jest.fn(),
+      pathname: '',
+      query: {},
+    }
+
+    mockedUseRouter.mockReturnValue(mockedArgs as any)
+
+    renderWithProviders(<LanguagePicker />)
+
+    userEvent.click(screen.getByRole('button', { name: 'Change language' }))
+
+    const [, spanish, portuguese] = screen.getAllByRole('menuitemradio')
+
+    expect(spanish).toBeChecked()
+    expect(mockedArgs.push).toHaveBeenCalledTimes(0)
+
+    userEvent.click(portuguese)
+    expect(mockedArgs.push).toHaveBeenCalledTimes(1)
   })
 })
