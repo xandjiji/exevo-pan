fa43e031f908df21239985c9260394060aee31ea
xandjiji <xandjiji@gmail.com>
2022-12-16 17:03:58 -0300

    test(TitledCard): adding tests

--- apps/exevo-pan/src/components/Atoms/TitledCard/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/TitledCard/__tests__/index.tsx
@@ -1,5 +1,17 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from 'utils/test'
 import TitledCard from '..'
 
 describe('<TitledCard />', () => {
-  test.todo('add tests')
+  test('should render all variants correctly', () => {
+    const { rerender } = renderWithProviders(
+      <TitledCard title="Title" variant="rounded" />,
+    )
+
+    expect(screen.getByText('Title')).toBeInTheDocument()
+
+    rerender(<TitledCard title="Title" variant="squared" />)
+
+    expect(screen.getByText('Title')).toBeInTheDocument()
+  })
 })
