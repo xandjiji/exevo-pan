d13470f5b5a5b024d0457559634260e30300c454
xandjiji <xandjiji@gmail.com>
2022-12-17 12:09:11 -0300

    feat(a11y): improving pin button a11y

--- apps/exevo-pan/src/locales/pl/bossTracker.ts
+++ apps/exevo-pan/src/locales/pl/bossTracker.ts
@@ -46,6 +46,8 @@ export default {
       unknown: 'Unknown',
       noChance: 'No chance',
       expectedIn: 'Expected in',
+      pin: 'Favorite this boss',
+      unpin: 'Unfavorite this boss',
     },
     BossDialog: {
       loot: 'Relevant loot',
