748bf1c1197cf44a0a1b48453f912639925bb762
xandjiji <xandjiji@gmail.com>
2022-12-17 10:22:21 -0300

    test(BossGrid): testing grid interactions

--- apps/exevo-pan/src/modules/BossTracker/components/BossGrid/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/components/BossGrid/__tests__/index.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders, setup } from 'utils/test'
+import userEvent from '@testing-library/user-event'
 import { premiumYeti, bossesMock } from './mock'
 import BossGrid from '..'
 
@@ -39,14 +40,40 @@ describe('<BossGrid />', () => {
 
     renderWithProviders(<BossGrid bosses={bossesMock} server="Antica" />)
 
+    expect(screen.queryByText('40.30%')).not.toBeInTheDocument()
     expect(await screen.findByText('40.30%')).toBeInTheDocument()
   })
 
-  test.todo('should sort grid correctly')
+  test('should sort grid correctly', () => {
+    renderWithProviders(<BossGrid bosses={bossesMock} server="Antica" />)
 
-  test.todo('should display an empty state')
+    userEvent.click(screen.getByRole('radio', { name: /last seen/gi }))
 
-  test.todo('should open and close a dialog')
+    const [yeti, zomba, apprentice, valorcrest] = screen.getAllByTitle(
+      'Chance to spawn today',
+    )
 
-  test.todo('should try to pin a boss')
+    expect(yeti).toHaveTextContent('Unknown')
+    expect(zomba).toHaveTextContent('No chance')
+    expect(apprentice).toHaveTextContent('24.70%')
+    expect(valorcrest).toHaveTextContent('73.16%')
+  })
+
+  test('should display an empty state', () => {
+    renderWithProviders(<BossGrid bosses={[]} server="Antica" />)
+
+    expect(screen.getByText('No bosses')).toBeInTheDocument()
+  })
+
+  test('should open and close a dialog', () => {
+    renderWithProviders(<BossGrid bosses={bossesMock} server="Antica" />)
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+
+    userEvent.click(screen.getByRole('heading', { name: 'Yeti' }))
+    expect(screen.getByRole('dialog')).toBeInTheDocument()
+
+    userEvent.click(screen.getAllByRole('button', { name: 'Close dialog' })[0])
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+  })
 })
