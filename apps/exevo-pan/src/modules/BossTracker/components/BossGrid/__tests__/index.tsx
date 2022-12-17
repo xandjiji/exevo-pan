4f52bf6c710a8b44613e4346cc0b8d3bbd084387
xandjiji <xandjiji@gmail.com>
2022-12-17 10:10:39 -0300

    test(BossGrid): adding tests

--- apps/exevo-pan/src/modules/BossTracker/components/BossGrid/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/BossTracker/components/BossGrid/__tests__/index.tsx
@@ -1,5 +1,52 @@
+import { screen } from '@testing-library/react'
+import { renderWithProviders, setup } from 'utils/test'
+import { premiumYeti, bossesMock } from './mock'
 import BossGrid from '..'
 
 describe('<BossGrid />', () => {
-  test.todo('add tests')
+  beforeEach(() => {
+    setup.useSession().mockReturnValue({
+      data: {
+        user: {
+          proStatus: false,
+        },
+      } as any,
+      status: 'unauthenticated',
+    })
+
+    setup.fetch().mockResolvedValue({
+      json: async () => [premiumYeti],
+    } as Response)
+  })
+
+  test('should render every item', () => {
+    renderWithProviders(<BossGrid bosses={bossesMock} server="Antica" />)
+
+    bossesMock.forEach(({ name }) => {
+      expect(screen.getByRole('heading', { name })).toBeInTheDocument()
+    })
+  })
+
+  test('should hydrate premium data', async () => {
+    setup.useSession().mockReturnValue({
+      data: {
+        user: {
+          proStatus: true,
+        },
+      } as any,
+      status: 'unauthenticated',
+    })
+
+    renderWithProviders(<BossGrid bosses={bossesMock} server="Antica" />)
+
+    expect(await screen.findByText('40.30%')).toBeInTheDocument()
+  })
+
+  test.todo('should sort grid correctly')
+
+  test.todo('should display an empty state')
+
+  test.todo('should open and close a dialog')
+
+  test.todo('should try to pin a boss')
 })
