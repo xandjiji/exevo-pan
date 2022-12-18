b76de5bbec1a8d4c3cd2f57445c8d4b4dfcf0197
xandjiji <xandjiji@gmail.com>
2022-12-18 19:13:23 -0300

    feature(automations): updating statistics after history scrap

--- automations/scrapHistory.sh
+++ automations/scrapHistory.sh
@@ -2,6 +2,8 @@
 cd ~/exevo-pan/apps/bazaar-scraper/
 yarn scrap:history
 
+~/exevo-pan/automations/updateStatistics.sh
+
 cp ~/exevo-pan/apps/bazaar-scraper/Output/ServerData.json ~/exevo-pan/apps/history-server/src/Data
 cp ~/exevo-pan/apps/bazaar-scraper/Output/HistoryAuctions.jsonl ~/exevo-pan/apps/history-server/src/Data
 
