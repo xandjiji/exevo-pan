de9fb590425fb240f158de1a48a229809d2eb207
xandjiji <xandjiji@gmail.com>
2022-12-22 10:58:33 -0300

    feature(og): optional image

--- apps/exevo-pan/src/services/PreviewImage.ts
+++ apps/exevo-pan/src/services/PreviewImage.ts
@@ -2,14 +2,14 @@ import { endpoints } from 'Constants'
 
 type GetSrcProps = {
   title: string
-  imgSrc: string
+  imgSrc?: string
   fontSize?: number
 }
 
 export default class PreviewImageClient {
   static getSrc({ title, imgSrc, fontSize }: GetSrcProps): string {
-    return `${endpoints.PREVIEW_IMAGES}?title=${title}&img=${imgSrc}${
-      fontSize ? `&fontSize=${fontSize}` : ''
-    }`
+    return `${endpoints.PREVIEW_IMAGES}?title=${title}${
+      imgSrc ? `&img=${imgSrc}` : ''
+    }${fontSize ? `&fontSize=${fontSize}` : ''}`
   }
 }
