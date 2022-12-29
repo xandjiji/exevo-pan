962d43512db1a001ce72e79b2af590e1ac5f82a0
xandjiji <xandjiji@gmail.com>
2022-12-28 21:15:40 -0300

    feature: adding more rare nickname conditions

--- packages/auction-queries/src/Filtering/schemas/rareNickname.ts
+++ packages/auction-queries/src/Filtering/schemas/rareNickname.ts
@@ -1,5 +1,6 @@
 const filterSkip: FilterSkip = ({ rareNick }): boolean => !rareNick
 
+const RARE_WORD_COUNT = 4
 const MINIMUM_RARE_CHARACTER_COUNT = 3
 const specialCharacters = /[äëïöüÿ'-.,]/i
 const twoConsecutiveUppercase = /[A-Z][A-Z]/
@@ -8,6 +9,7 @@ const isRareNickname = (nickname: string): boolean => {
   if (nickname.length <= MINIMUM_RARE_CHARACTER_COUNT) return true
   if (specialCharacters.test(nickname)) return true
   if (twoConsecutiveUppercase.test(nickname)) return true
+  if (nickname.split(' ').length >= RARE_WORD_COUNT) return true
 
   return false
 }
