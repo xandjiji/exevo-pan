e343033b9ea47de46a252de3f17ae7f2d98e9788
xandjiji <xandjiji@gmail.com>
2022-12-29 20:56:23 -0300

    feature(Menu): initial styling

--- apps/exevo-pan/src/components/Atoms/Menu/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Menu/index.tsx
@@ -1,6 +1,24 @@
+import clsx from 'clsx'
 import { usePopper } from 'react-popper'
 import { Modifier } from '@popperjs/core'
 
-const Menu = () => {}
+const Item = ({ className, ...props }: JSX.IntrinsicElements['div']) => (
+  <div
+    className={clsx('text-tsm hover:bg-primaryVariant px-4 py-2.5', className)}
+    {...props}
+  />
+)
+
+const Menu = () => {
+  console.log(9)
+
+  return (
+    <div className="card w-fit overflow-hidden rounded p-0">
+      <Item>Notify me</Item>
+      <Item>Favorite</Item>
+      <Item>Find similar</Item>
+    </div>
+  )
+}
 
 export default Menu
