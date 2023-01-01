236476ac8a6d3c4065850eeb118462c63ef12763
xandjiji <xandjiji@gmail.com>
2023-01-01 16:38:10 -0300

    test(Menu): adding todos

---
+++ apps/exevo-pan/src/components/Organisms/Menu/__tests__/index.tsx
@@ -0,0 +1,17 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithProviders } from 'utils/test'
+import Menu from '..'
+
+describe('<Menu />', () => {
+  /* disabled, onSelect, highlight */
+  describe('should be controlled correctly with', () => {
+    test.todo('mouse')
+
+    test.todo('keyboard')
+  })
+
+  test.todo('typing should highlight items by text')
+
+  test.todo('a11y')
+})
