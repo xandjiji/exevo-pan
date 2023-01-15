ea7c593143617fa9c82037f704e4425ac1958a19
xandjiji <xandjiji@gmail.com>
2023-01-15 11:01:20 -0300

    test(Alert): adding tests

--- apps/exevo-pan/src/components/Atoms/Alert/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/Alert/__tests__/index.tsx
@@ -3,5 +3,32 @@ import { renderWithProviders } from 'utils/test'
 import Alert from '..'
 
 describe('<Alert />', () => {
-  test.todo('add tests')
+  test('should render correctly', () => {
+    const { container } = renderWithProviders(
+      <Alert variant="alert">content</Alert>,
+    )
+
+    expect(screen.getByRole('alert')).toHaveTextContent('content')
+    expect(container.querySelector('svg')).toBeInTheDocument()
+  })
+
+  test('should render custom icon', () => {
+    renderWithProviders(
+      <Alert variant="alert" icon={() => <svg role="none" />}>
+        content
+      </Alert>,
+    )
+
+    expect(screen.getByRole('none')).toBeInTheDocument()
+  })
+
+  test('should NOT render custom icon', () => {
+    const { container } = renderWithProviders(
+      <Alert variant="alert" noIcon>
+        content
+      </Alert>,
+    )
+
+    expect(container.querySelector('svg')).not.toBeInTheDocument()
+  })
 })
