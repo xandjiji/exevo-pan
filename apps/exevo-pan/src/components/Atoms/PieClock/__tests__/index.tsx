0091eaeda2d90c9b30ade41a86f5c0d6ae2cec6c
xandjiji <xandjiji@gmail.com>
2023-01-15 11:07:59 -0300

    test(PieClock): adding tests

--- apps/exevo-pan/src/components/Atoms/PieClock/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Atoms/PieClock/__tests__/index.tsx
@@ -1,7 +1,14 @@
-import { screen } from '@testing-library/react'
 import { renderWithProviders } from 'utils/test'
 import PieClock from '..'
 
 describe('<PieClock />', () => {
-  test.todo('add tests')
+  test('should render all variants', () => {
+    const { rerender } = renderWithProviders(<PieClock percentage={0.4} />)
+    rerender(<PieClock percentage={0.7} />)
+    rerender(<PieClock percentage={0.7} invert />)
+    rerender(<PieClock percentage={0.4} invert />)
+
+    rerender(<PieClock percentage={0.4} size="small" />)
+    rerender(<PieClock percentage={0.4} size="large" />)
+  })
 })
