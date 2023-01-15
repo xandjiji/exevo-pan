b9e74cb6a402ca9413834d98f101a3ff078c35c6
xandjiji <xandjiji@gmail.com>
2023-01-15 11:04:56 -0300

    test(SuggestedReading): adding tests

--- apps/exevo-pan/src/components/SuggestedReading/__tests__/index.tsx
+++ apps/exevo-pan/src/components/SuggestedReading/__tests__/index.tsx
@@ -1,7 +1,22 @@
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from 'utils/test'
+import { routes } from 'Constants'
 import SuggestedReading from '..'
 
 describe('<SuggestedReading />', () => {
-  test.todo('add tests')
+  test('add tests', () => {
+    renderWithProviders(
+      <SuggestedReading thumbnail="thumb-src" title="post title" slug="slug" />,
+    )
+
+    const thumbElement = screen.getByRole('img')
+    expect(thumbElement).toHaveAccessibleName('post title')
+    expect(thumbElement).toHaveAttribute('src', 'thumb-src')
+
+    expect(screen.getByRole('heading')).toHaveTextContent('post title')
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      `${routes.BLOG}/slug`,
+    )
+  })
 })
