35b9d990d132f29c1366da6d3d76e6a024af1e2d
xandjiji <xandjiji@gmail.com>
2023-01-17 15:00:12 -0300

    feature(trpc): creating `authedProcedure`

--- apps/exevo-pan/src/server/trpc.ts
+++ apps/exevo-pan/src/server/trpc.ts
@@ -4,7 +4,25 @@ import { Context } from './context'
 const t = initTRPC.context<Context>().create()
 
 export const { middleware, router } = t
+
 export const publicProcedure = t.procedure
+
+export const authedProcedure = t.procedure.use(
+  t.middleware(({ next, ctx }) => {
+    if (!ctx.token) {
+      throw new TRPCError({
+        code: 'UNAUTHORIZED',
+      })
+    }
+
+    return next({
+      ctx: {
+        token: ctx.token,
+      },
+    })
+  }),
+)
+
 export const premiumProcedure = t.procedure.use(
   t.middleware(({ next, ctx }) => {
     if (!ctx.token?.proStatus) {
