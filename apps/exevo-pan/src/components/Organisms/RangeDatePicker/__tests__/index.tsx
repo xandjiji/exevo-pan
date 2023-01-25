01580b1bb43cabafa017da995e749228b56ca6c1
xandjiji <xandjiji@gmail.com>
2023-01-25 17:19:02 -0300

    test(RangeDatePicker): adding tests

--- apps/exevo-pan/src/components/Organisms/RangeDatePicker/__tests__/index.tsx
+++ apps/exevo-pan/src/components/Organisms/RangeDatePicker/__tests__/index.tsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react'
 import { screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from 'utils/test'
+import { MILLISECONDS_IN } from 'utils'
 import RangeDatePicker from '..'
-
-import { mockedSelectedCharacter, currentMonth, nextMonth } from './mock'
 
 type DateState = {
   date: string
@@ -12,20 +10,83 @@ type DateState = {
   element: HTMLElement
 }
 
-/* @ ToDo: write tests */
+export const monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
 
-/* describe('<RangeDatePicker />', () => {
+export const monthNameByIndex = (index: number) =>
+  monthNames[index + 1 > 11 ? index - 12 : index]
+
+describe('<RangeDatePicker />', () => {
+  beforeEach(() => {
+    jest.useFakeTimers('modern').setSystemTime(new Date(2020, 9, 1, 7))
+  })
+
   test('should display current and next month correctly', () => {
-    renderWithProviders(<RangeDatePicker />)
+    renderWithProviders(
+      <RangeDatePicker
+        endDate={new Date(+new Date() + MILLISECONDS_IN.DAY * 45)}
+        onDateSelect={jest.fn()}
+        selectedDates={[]}
+      />,
+    )
+
+    const currentMonthIndex = new Date().getMonth()
 
     expect(screen.getByLabelText('Current month')).toHaveTextContent(
-      currentMonth,
+      monthNameByIndex(currentMonthIndex),
     )
-    expect(screen.getByText(nextMonth)).toHaveAccessibleName('Next month')
+    expect(
+      screen.getByText(monthNameByIndex(currentMonthIndex + 1)),
+    ).toHaveAccessibleName('Next month')
   })
 
   test('should toggle dates correctly', () => {
-    renderWithProviders(<RangeDatePicker />)
+    const endDate = new Date(+new Date() + MILLISECONDS_IN.DAY * 45)
+    let selectedDates: Date[] = []
+
+    const onDateSelect = (date: Date) => {
+      const dateSet = new Set(
+        selectedDates.map((dateItem) => dateItem.toISOString()),
+      )
+
+      const selectedIsoDate = date.toISOString()
+      if (dateSet.has(selectedIsoDate)) {
+        dateSet.delete(selectedIsoDate)
+      } else {
+        dateSet.add(selectedIsoDate)
+      }
+
+      selectedDates = [...dateSet].map((isoDate) => new Date(isoDate))
+    }
+
+    const { rerender } = renderWithProviders(
+      <RangeDatePicker
+        endDate={endDate}
+        onDateSelect={onDateSelect}
+        selectedDates={selectedDates}
+      />,
+    )
+
+    const updateState = () =>
+      rerender(
+        <RangeDatePicker
+          endDate={endDate}
+          onDateSelect={onDateSelect}
+          selectedDates={selectedDates}
+        />,
+      )
 
     const dateButtons = screen.getAllByRole('button')
     const dateStates: DateState[] = dateButtons
@@ -45,6 +106,7 @@ type DateState = {
         expect(buttonElement).toHaveAttribute('aria-selected', dateState.active)
 
         userEvent.click(dateState.element)
+        updateState()
 
         expect(buttonElement).not.toHaveAttribute(
           'aria-selected',
@@ -62,4 +124,4 @@ type DateState = {
     toggleAndAssertDates()
     toggleAndAssertDates()
   })
-}) */
+})
