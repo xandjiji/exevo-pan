2b33cd6e9ceb508795fd87459a7a00d7ca01161a
xandjiji <xandjiji@gmail.com>
2023-01-31 11:11:25 -0300

    feature(api): initial `auction-notifications`

---
+++ apps/exevo-pan/src/pages/api/auction-notifications.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from 'lib/prisma'
+import { caller } from 'pages/api/trpc/[trpc]'
+import { MILLISECONDS_IN } from 'utils'
+
+const offsetCurrentDateByMinutes = (minutesOffset: number) =>
+  new Date(+new Date() + MILLISECONDS_IN.MINUTE * minutesOffset)
+
+export default async (
+  request: NextApiRequest,
+  response: NextApiResponse,
+): Promise<void> => {
+  const { secret } = request.query
+  const auth = [process.env.REVALIDATION_AUTH, process.env.BACKOFFICE_TOKEN]
+
+  if (!auth.find((key) => key === secret)) {
+    response.status(401).json({ message: 'Invalid token' })
+    return
+  }
+
+  try {
+    const notifyList = await prisma.auctionNotification.findMany({
+      where: {
+        notifyAt: {
+          gte: offsetCurrentDateByMinutes(-10),
+          lte: offsetCurrentDateByMinutes(2),
+        },
+        scheduleCompleted: false,
+      },
+    })
+
+    /* @ ToDo: notify */
+
+    await prisma.auctionNotification.updateMany({
+      where: { id: { in: notifyList.map(({ id }) => id) } },
+      data: { scheduleCompleted: true },
+    })
+
+    response.send('ok')
+  } catch (error) {
+    response.status(500).json({ error })
+  }
+}
