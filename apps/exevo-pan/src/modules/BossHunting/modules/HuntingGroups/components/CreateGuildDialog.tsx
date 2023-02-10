55df77bab2eae15382b96be243afb5347ed3c75c
xandjiji <xandjiji@gmail.com>
2023-02-10 16:25:49 -0300

    feature(CreateGuildDialog): avatar randomizer

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/components/CreateGuildDialog.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/components/CreateGuildDialog.tsx
@@ -3,6 +3,8 @@ import { Dialog, Button } from 'components/Atoms'
 import { Avatar } from 'components/Organisms'
 import { trpc } from 'lib/trpc'
 import { avatar } from 'utils'
+import { DiceIcon } from 'assets/svgs'
+import { avatar as AVATAR } from 'Constants'
 import type { GuildCreationInput } from 'server/guild/crud'
 
 /* @ ToDo:
@@ -29,24 +31,42 @@ const CreateGuildDialog = ({ onClose }: CreateGuildDialogProps) => {
   })
 
   return (
-    <Dialog isOpen onClose={onClose}>
-      <Avatar
-        alt={formState.name}
-        avatarId={formState.avatarId}
-        avatarDegree={formState.avatarDegree}
-      />
+    <Dialog isOpen onClose={onClose} heading="Create new hunting group">
+      {/* <div className="custom-scrollbar flex max-h-[80vh] max-w-5xl flex-wrap items-center gap-6 overflow-auto">
+        {Array.from({ length: AVATAR.id.max + 1 }, (_, index) => (
+          <div>
+            [{index}]
+            <Avatar
+              alt={formState.name}
+              avatarId={index}
+              avatarDegree={formState.avatarDegree}
+            />
+          </div>
+        ))}
+      </div> */}
 
-      <Button
-        onClick={() =>
-          setFormState((prev) => ({
-            ...prev,
-            avatarId: avatar.getRandom.id(),
-            avatarDegree: avatar.getRandom.degree(),
-          }))
-        }
-      >
-        Re-roll
-      </Button>
+      <div className="flex w-fit flex-col items-center gap-2">
+        <Avatar
+          alt={formState.name}
+          avatarId={formState.avatarId}
+          avatarDegree={formState.avatarDegree}
+          className="grid !h-14 !w-full place-items-center transition-all"
+        />
+        <Button
+          onClick={() =>
+            setFormState((prev) => ({
+              ...prev,
+              avatarId: avatar.getRandom.id(),
+              avatarDegree: avatar.getRandom.degree(),
+            }))
+          }
+          pill
+          className="text-xs"
+        >
+          <DiceIcon className="fill-onPrimary h-4 w-4 shrink-0" />
+          Roll
+        </Button>
+      </div>
     </Dialog>
   )
 }
