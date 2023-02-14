db9b6559ad22b011ebffce1929acf69c030de98d
xandjiji <xandjiji@gmail.com>
2023-02-14 17:03:44 -0300

    feature(GuildList): adding empty state

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/components/GuildList.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/components/GuildList.tsx
@@ -1,13 +1,14 @@
 import { memo } from 'react'
 import clsx from 'clsx'
 import { Button } from 'components/Atoms'
+import EmptyState from 'components/EmptyState'
 import { Avatar, Tooltip } from 'components/Organisms'
 import { LockIcon } from 'assets/svgs'
 
 /* @ ToDo:
 
 - guild link click page
-- i18n
+- i18n (nao esquecer do empty state)
 
 */
 
@@ -16,66 +17,74 @@ type GuildListProps = {
   onApply?: (huntingGroup: PublicHuntingGroup) => void
 }
 
-const GuildList = ({ list, onApply }: GuildListProps) => (
-  <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
-    {list.map((guild) => {
-      const {
-        name,
-        description,
-        private: isPrivate,
-        server,
-        memberCount,
-        avatarId,
-        avatarDegree,
-      } = guild
+const GuildList = ({ list, onApply }: GuildListProps) =>
+  list.length > 0 ? (
+    <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4">
+      {list.map((guild) => {
+        const {
+          name,
+          description,
+          private: isPrivate,
+          server,
+          memberCount,
+          avatarId,
+          avatarDegree,
+        } = guild
 
-      return (
-        <li
-          className={clsx(
-            'card flex flex-col',
-            description ? 'gap-4' : 'gap-2',
-          )}
-        >
-          <div className="flex items-center gap-4">
-            <Avatar
-              alt={name}
-              avatarId={avatarId}
-              avatarDegree={avatarDegree}
-            />
+        return (
+          <li
+            className={clsx(
+              'card flex flex-col',
+              description ? 'gap-4' : 'gap-2',
+            )}
+          >
+            <div className="flex items-center gap-4">
+              <Avatar
+                alt={name}
+                avatarId={avatarId}
+                avatarDegree={avatarDegree}
+              />
 
-            <div className="grid gap-1.5">
-              <h5 className="text-primaryHighlight text-base">{name}</h5>
-              <span className="text-tsm flex items-center gap-1">
-                {server} - {memberCount} members{' '}
-                {isPrivate && (
-                  <div className="h-3 w-3">
-                    <Tooltip
-                      content={<span>This is a private hunting group</span>}
-                      offset={[0, 8]}
-                    >
-                      <LockIcon className="fill-onSurface h-3 w-3" />
-                    </Tooltip>
-                  </div>
-                )}
-              </span>
+              <div className="grid gap-1.5">
+                <h5 className="text-primaryHighlight text-base">{name}</h5>
+                <span className="text-tsm flex items-center gap-1">
+                  {server} - {memberCount} members{' '}
+                  {isPrivate && (
+                    <div className="h-3 w-3">
+                      <Tooltip
+                        content={<span>This is a private hunting group</span>}
+                        offset={[0, 8]}
+                      >
+                        <LockIcon className="fill-onSurface h-3 w-3" />
+                      </Tooltip>
+                    </div>
+                  )}
+                </span>
+              </div>
             </div>
-          </div>
 
-          {!!description && <p className="text-s">{description}</p>}
+            {!!description && <p className="text-s">{description}</p>}
 
-          {!!onApply && (
-            <Button
-              pill
-              className="ml-auto mt-auto"
-              onClick={() => onApply(guild)}
-            >
-              Apply
-            </Button>
-          )}
-        </li>
-      )
-    })}
-  </ul>
-)
+            {!!onApply && (
+              <Button
+                pill
+                className="ml-auto mt-auto"
+                onClick={() => onApply(guild)}
+              >
+                Apply
+              </Button>
+            )}
+          </li>
+        )
+      })}
+    </ul>
+  ) : (
+    <div>
+      <EmptyState
+        text={{ content: 'No hunting groups', size: 42 }}
+        className="mx-auto mt-8"
+      />
+    </div>
+  )
 
 export default memo(GuildList)
