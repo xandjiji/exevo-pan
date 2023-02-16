8d017849f25ae43648f20fab057e238b57c7230b
xandjiji <xandjiji@gmail.com>
2023-02-16 10:27:01 -0300

    feature(pages): serializing an deserializing server side props with `devalue`

--- apps/exevo-pan/src/pages/bosses/hunting-groups/[guildName].tsx
+++ apps/exevo-pan/src/pages/bosses/hunting-groups/[guildName].tsx
@@ -1,10 +1,98 @@
+import Head from 'next/head'
 import { GetServerSideProps, GetServerSidePropsResult } from 'next'
+import { useState } from 'react'
+import { stringify, parse } from 'devalue'
 import { getToken } from 'next-auth/jwt'
+import { useTranslations } from 'contexts/useTranslation'
+import { Hero } from 'templates'
+import { Template } from 'modules/BossHunting'
 import { prisma } from 'lib/prisma'
-import { routes } from 'Constants'
+import type { GuildMember, Guild } from '@prisma/client'
+import { buildPageTitle, avatar } from 'utils'
+import { routes, avatar as AVATAR } from 'Constants'
+import { common, bosses } from 'locales'
 
-export default function GuildPage() {
-  return <div>a</div>
+type GuildPageProps = {
+  serialized: string
+  isMember: boolean
+}
+
+export default function GuildPage({ serialized, isMember }: GuildPageProps) {
+  const { translations } = useTranslations()
+
+  const [{ guildMembers, guild }] = useState<{
+    guildMembers: GuildMember[]
+    guild: Guild
+  }>(parse(serialized))
+
+  /* @ ToDo: add title */
+  /* const pageName = translations.bossTracker.Meta.title */
+  const pageName = guild.name
+  /* const previewSrc = PreviewImageClient.getSrc({
+    title: pageName,
+    imgSrc: heroSrc,
+  }) */
+
+  const pageTitle = buildPageTitle(pageName)
+
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="title" content={pageTitle} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="twitter:title" content={pageTitle} />
+
+        {/* @ ToDo: add meta tags */}
+        {/* <meta
+      name="description"
+      content={translations.bossTracker.Meta.description}
+    />
+    <meta
+      property="twitter:description"
+      content={translations.bossTracker.Meta.description}
+    />
+    <meta
+      property="og:description"
+      content={translations.bossTracker.Meta.description}
+    /> */}
+        {/* <meta property="og:type" content="website" />
+
+        <meta key="preview-1" property="og:image" content={previewSrc} />
+        <meta key="preview-2" property="twitter:image" content={previewSrc} />
+
+        <link rel="canonical" href={pageUrl} />
+        <meta property="og:url" content={pageUrl} />
+        <meta property="twitter:url" content={pageUrl} />
+
+        <link rel="alternate" hrefLang="en" href={pageUrl} />
+        <link rel="alternate" hrefLang="pt" href={buildUrl(pagePath, 'pt')} />
+        <link rel="alternate" hrefLang="es" href={buildUrl(pagePath, 'es')} />
+        <link rel="alternate" hrefLang="pl" href={buildUrl(pagePath, 'pl')} />
+        <link rel="alternate" hrefLang="x-default" href={pageUrl} />
+
+        <script
+          type="application/ld+json"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: jsonld.standard,
+          }}
+        /> */}
+      </Head>
+
+      <Template>
+        <Hero
+          offset
+          title={guild.name}
+          src={avatar.loadSrc(guild.avatarId)}
+          hueRotation={guild.avatarDegree}
+          dimension={guild.avatarId >= AVATAR.itemsBaseIndex ? 192 : undefined}
+        />
+
+        <div className="inner-container grid gap-8 md:-mt-12">a</div>
+      </Template>
+    </>
+  )
 }
 
 const redirect: GetServerSidePropsResult<any> = {
@@ -36,17 +124,19 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   return {
     props: {
-      guildMembers: guild.private && notMember ? [] : guildMembers,
-      guild: notMember
-        ? ({ ...guild, messageBoard: null } as typeof guild)
-        : guild,
+      serialized: stringify({
+        guildMembers: guild.private && notMember ? [] : guildMembers,
+        guild: notMember
+          ? ({ ...guild, messageBoard: null } as typeof guild)
+          : guild,
+      }),
       isMember: !notMember,
-      /*
+
       translations: {
         common: common[locale as RegisteredLocale],
         bosses: bosses[locale as RegisteredLocale],
-        bossTracker: bossTracker[locale as RegisteredLocale],
-      }, */
+        /* bossTracker: bossTracker[locale as RegisteredLocale], */
+      },
       locale,
     },
   }
