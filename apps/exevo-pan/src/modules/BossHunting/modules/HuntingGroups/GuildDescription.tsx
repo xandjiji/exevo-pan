43dedf0dedfc9e94804aa896a35c18ebb6dc774d
xandjiji <xandjiji@gmail.com>
2023-02-17 13:50:12 -0300

    feature(GuildDescription): editor dialog

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/GuildDescription.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/GuildDescription.tsx
@@ -1,44 +1,97 @@
-import { Alert, Button } from 'components/Atoms'
+import { useState, useCallback } from 'react'
+import { Alert, Button, Dialog, TextArea } from 'components/Atoms'
 import { EditIcon } from 'assets/svgs'
 import { useGuildData } from './contexts/useGuildData'
 
 /* @ ToDo:
 
 - add/edit description dialog
+    - consertar caret
+    - length counter
+    - max length (using pie)
+    - submit action
+    - after submit
+    - caracteres especiais, etc?
+
 - i18n
 
 */
 
 const GuildDescription = () => {
   const { guild, isEditor } = useGuildData()
+  const [formState, setFormState] = useState({
+    isOpen: false,
+    description: guild.description ?? '',
+  })
 
-  if (guild.description) {
-    return (
-      <Alert variant="primary" noIcon className="mx-auto max-w-full sm:w-96">
-        {guild.description}
+  const closeForm = useCallback(
+    () =>
+      setFormState({
+        isOpen: false,
+        description: guild.description ?? '',
+      }),
+    [],
+  )
 
-        {isEditor && (
-          <Button hollow pill className="mx-auto mt-3 text-base">
+  if (!guild.description && !isEditor) return null
+
+  return (
+    <>
+      {guild.description ? (
+        <Alert variant="primary" noIcon className="mx-auto max-w-full sm:w-96">
+          {guild.description}
+
+          {isEditor && (
+            <Button
+              hollow
+              pill
+              className="mx-auto mt-3 text-base"
+              onClick={() =>
+                setFormState((prev) => ({ ...prev, isOpen: true }))
+              }
+            >
+              <EditIcon className="h-4 w-4" />
+              Edit description
+            </Button>
+          )}
+        </Alert>
+      ) : (
+        <div className="border-1 border-separator mx-auto grid max-w-full place-items-center rounded-md border-dashed py-2 px-4 sm:w-96">
+          <Button
+            hollow
+            pill
+            className="mx-auto text-base"
+            onClick={() => setFormState((prev) => ({ ...prev, isOpen: true }))}
+          >
             <EditIcon className="h-4 w-4" />
-            Edit description
+            Add description
           </Button>
-        )}
-      </Alert>
-    )
-  }
+        </div>
+      )}
 
-  if (isEditor) {
-    return (
-      <div className="border-1 border-separator mx-auto grid max-w-full place-items-center rounded-md border-dashed py-2 px-4 sm:w-96">
-        <Button hollow pill className="mx-auto text-base">
-          <EditIcon className="h-4 w-4" />
-          Add description
-        </Button>
-      </div>
-    )
-  }
+      <Dialog
+        heading="Edit group description"
+        isOpen={formState.isOpen}
+        onClose={closeForm}
+      >
+        <TextArea
+          label="New description"
+          value={formState.description}
+          className="min-h-[120px]"
+          onChange={(e) =>
+            setFormState((prev) => ({ ...prev, description: e.target.value }))
+          }
+        />
 
-  return null
+        <div className="mt-4 flex items-center justify-end gap-4">
+          <Button pill hollow onClick={closeForm}>
+            Cancel
+          </Button>
+          <Button pill>Save</Button>
+        </div>
+      </Dialog>
+    </>
+  )
 }
 
 export default GuildDescription
