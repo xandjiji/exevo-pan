7716769267357e82d45822ba6fd8ab6dbd61ac9d
xandjiji <xandjiji@gmail.com>
2023-02-20 09:30:36 -0300

    feature(EditGuildDialog): adding form fields

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/EditGuildDialog.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/EditGuildDialog.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import { Dialog, TextArea, Button } from 'components/Atoms'
+import { Dialog, Input, Checkbox, TextArea, Button } from 'components/Atoms'
+import { InfoTooltip } from 'components/Organisms'
 import { trpc } from 'lib/trpc'
-import { EditIcon } from 'assets/svgs'
 import type { GuildEditInput } from 'server/guild/crud'
 import { useGuildData } from './contexts/useGuildData'
 
 /* @ ToDo:
 
-- add/edit description dialog
-    - submit action
-    - after submit
-    - caracteres especiais, etc?
+avatar
+
+- validate fields (disable submit)
+
+- submit action
+- after submit (router push formState.name) (field errors)
+- caracteres especiais, etc?
 
 - i18n
 
@@ -25,8 +28,8 @@ const EditGuildDialog = ({ onClose }: EditGuildDialogProps) => {
 
   const [formState, setFormState] = useState<GuildEditInput>({
     guildId: guild.id,
-    name: guild.name ?? undefined,
-    private: guild.private ?? undefined,
+    name: guild.name,
+    private: guild.private,
     description: guild.description ?? undefined,
     messageBoard: guild.messageBoard ?? undefined,
     avatarId: guild.avatarId,
@@ -34,15 +37,55 @@ const EditGuildDialog = ({ onClose }: EditGuildDialogProps) => {
   })
 
   return (
-    <Dialog heading="Edit hunting group" isOpen onClose={onClose}>
+    <Dialog
+      heading="Edit hunting group"
+      isOpen
+      onClose={onClose}
+      className="grid gap-4 sm:min-w-[420px]"
+    >
+      <Input
+        label="Guild name"
+        value={formState.name}
+        onChange={(e) =>
+          setFormState((prev) => ({ ...prev, name: e.target.value }))
+        }
+      />
+
       <TextArea
-        label="New description"
+        label="Description"
         value={formState.description}
         className="min-h-[120px]"
         onChange={(e) =>
           setFormState((prev) => ({ ...prev, description: e.target.value }))
         }
         maxLength={600}
+      />
+
+      <TextArea
+        label="Message board (only seen by members)"
+        value={formState.messageBoard}
+        className="min-h-[120px]"
+        onChange={(e) =>
+          setFormState((prev) => ({ ...prev, messageBoard: e.target.value }))
+        }
+        maxLength={2048}
+      />
+
+      <Checkbox
+        label={
+          <span className="flex items-center gap-1.5">
+            Private group{' '}
+            <InfoTooltip
+              labelSize
+              content="A private group can be found, but its members will be hidden"
+            />
+          </span>
+        }
+        aria-label="Private group"
+        checked={formState.private}
+        onChange={() =>
+          setFormState((prev) => ({ ...prev, private: !formState.private }))
+        }
       />
 
       <div className="mt-4 flex items-center justify-end gap-4">
