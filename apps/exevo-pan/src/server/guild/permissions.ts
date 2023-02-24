bef676bac7ba16012aa66fa8db89b311e2a0fdf1
xandjiji <xandjiji@gmail.com>
2023-02-24 16:42:57 -0300

    feature: adding `manageApplications` to permission set

--- apps/exevo-pan/src/server/guild/permissions.ts
+++ apps/exevo-pan/src/server/guild/permissions.ts
@@ -3,6 +3,7 @@ import type { GUILD_MEMBER_ROLE } from '@prisma/client'
 type PermissionSet = {
   exclude: (role: GUILD_MEMBER_ROLE) => boolean
   editGuild: boolean
+  manageApplications: boolean
 }
 
 export const can: Record<GUILD_MEMBER_ROLE, PermissionSet> = {
@@ -10,13 +11,16 @@ export const can: Record<GUILD_MEMBER_ROLE, PermissionSet> = {
     exclude: (role) =>
       new Set<GUILD_MEMBER_ROLE>(['MODERATOR', 'USER']).has(role),
     editGuild: true,
+    manageApplications: true,
   },
   MODERATOR: {
     exclude: (role) => new Set<GUILD_MEMBER_ROLE>(['USER']).has(role),
     editGuild: true,
+    manageApplications: true,
   },
   USER: {
     exclude: (role) => new Set<GUILD_MEMBER_ROLE>([]).has(role),
     editGuild: false,
+    manageApplications: false,
   },
 }
