e58d58537f6089c0cba101140e2401efecf6f76d
xandjiji <xandjiji@gmail.com>
2023-02-26 12:11:17 -0300

    feature(useGuildData): adding setter

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/contexts/useGuildData.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/contexts/useGuildData.tsx
@@ -19,6 +19,7 @@ type GuildDataValues = {
   isMember: boolean
   isEditor: boolean
   isAdmin: boolean
+  setGuildData: React.Dispatch<React.SetStateAction<ServerSideGuildDataProps>>
 } & Omit<UseGuildDataProps, 'children'>
 
 const GuildDataContext = createContext<GuildDataValues>({} as GuildDataValues)
@@ -26,19 +27,24 @@ const GuildDataContext = createContext<GuildDataValues>({} as GuildDataValues)
 export const GuildDataConsumer = GuildDataContext.Consumer
 
 export const GuildDataProvider = ({
-  currentMember,
   children,
   ...propData
 }: UseGuildDataProps) => {
+  const [guildData, setGuildData] = useState<ServerSideGuildDataProps>(propData)
+
   const [inferredData] = useState({
-    isAdmin: currentMember ? currentMember.role === 'ADMIN' : false,
-    isMember: !!currentMember,
-    isEditor: currentMember ? can[currentMember.role].editGuild : false,
+    isAdmin: guildData.currentMember
+      ? guildData.currentMember.role === 'ADMIN'
+      : false,
+    isMember: !!guildData.currentMember,
+    isEditor: guildData.currentMember
+      ? can[guildData.currentMember.role].editGuild
+      : false,
   })
 
   return (
     <GuildDataContext.Provider
-      value={{ ...propData, ...inferredData, currentMember }}
+      value={{ ...guildData, ...inferredData, setGuildData }}
     >
       {children}
     </GuildDataContext.Provider>
