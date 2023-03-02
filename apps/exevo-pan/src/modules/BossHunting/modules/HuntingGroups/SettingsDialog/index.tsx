0505f777915b77d27f62c6408db88d0396cdd372
xandjiji <xandjiji@gmail.com>
2023-03-02 11:07:40 -0300

    feature(SettingsDialog): removing tabs

--- apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/SettingsDialog/index.tsx
+++ apps/exevo-pan/src/modules/BossHunting/modules/HuntingGroups/SettingsDialog/index.tsx
@@ -1,14 +1,22 @@
+import clsx from 'clsx'
 import { useState } from 'react'
-import { Dialog, Tabs, Button, Switch, Checkbox } from 'components/Atoms'
+import {
+  Dialog,
+  Alert,
+  LoadingAlert,
+  Button,
+  Switch,
+  Checkbox,
+} from 'components/Atoms'
+import { usePushNotifications } from 'hooks'
+import { toast } from 'react-hot-toast'
 import type { GuildMember } from '@prisma/client'
-import { RegisterDevice } from './RegisterDevice'
 import { useBlacklist, bossNames } from './useBlacklist'
 
 /* @ ToDo: i18n */
 
 /* 
 
-- ajustar dialog dimensions
 - trpc + button state
 
 */
@@ -18,7 +26,31 @@ type SettingsDialogProps = {
   currentMember: GuildMember
 }
 
+const AlertButton = ({
+  className,
+  ...props
+}: JSX.IntrinsicElements['button']) => (
+  <button
+    type="button"
+    className={clsx(
+      className,
+      'text-primaryHighlight cursor-pointer font-bold underline underline-offset-2',
+    )}
+    {...props}
+  />
+)
+
 const SettingsDialog = ({ onClose, currentMember }: SettingsDialogProps) => {
+  const {
+    isSupported,
+    permission,
+    subscribeDevice,
+    sendClientNotification,
+    isLoading: loadingDeviceSubscription,
+  } = usePushNotifications()
+
+  const registeredDevice = permission === 'granted'
+
   const [disabledNotifications, setDisabledNotifications] = useState(
     currentMember.disabledNotifications,
   )
@@ -26,36 +58,79 @@ const SettingsDialog = ({ onClose, currentMember }: SettingsDialogProps) => {
 
   return (
     <Dialog heading="Settings" isOpen onClose={onClose}>
-      <Tabs.Group>
-        <Tabs.Panel label="Register device">
-          <RegisterDevice />
-        </Tabs.Panel>
-        <Tabs.Panel label="Customize notifications">
-          <div className="grid gap-6">
-            <Switch
-              active={!disabledNotifications}
-              onClick={() => setDisabledNotifications((prev) => !prev)}
-            >
-              Receive notifications from this group
-            </Switch>
+      {loadingDeviceSubscription && <LoadingAlert>Loading...</LoadingAlert>}
 
-            <div className="grid gap-2">
-              <h5 className="text-s">Receive notifications for:</h5>
+      <div className="grid gap-6">
+        {isSupported ? (
+          <Alert variant="primary" noIcon>
+            {registeredDevice ? (
+              <>
+                <p>This device is registered!</p>
+                <p>
+                  How about trying a{' '}
+                  <AlertButton
+                    onClick={() =>
+                      sendClientNotification({
+                        title: 'Hey there 👋',
+                        text: 'Everything looking good!',
+                      })
+                    }
+                  >
+                    test
+                  </AlertButton>{' '}
+                  notification? 🔔
+                </p>
+              </>
+            ) : (
+              <>
+                Please{' '}
+                <AlertButton
+                  onClick={() =>
+                    subscribeDevice()
+                      .then(() =>
+                        toast.success(
+                          'This device was registered successfully!',
+                        ),
+                      )
+                      .catch(() => toast.error('Oops! Something went wrong'))
+                  }
+                >
+                  enable notifications
+                </AlertButton>{' '}
+                on this device
+              </>
+            )}
+          </Alert>
+        ) : (
+          <Alert variant="alert">
+            Web Push Notifications not supported by this device
+          </Alert>
+        )}
 
-              <div className="custom-scrollbar grid max-h-40 grid-cols-2 gap-x-4 gap-y-2 overflow-auto sm:grid-cols-3 md:max-h-60">
-                {bossNames.map((boss) => (
-                  <Checkbox
-                    key={boss}
-                    label={boss}
-                    checked={!blacklist.value.set.has(boss)}
-                    onChange={() => blacklist.toggleValue(boss)}
-                  />
-                ))}
-              </div>
-            </div>
+        <Switch
+          active={!disabledNotifications}
+          onClick={() => setDisabledNotifications((prev) => !prev)}
+          disabled={!registeredDevice}
+        >
+          Receive notifications from this group
+        </Switch>
+
+        <div className="grid gap-2">
+          <h5 className="text-s">Receive notifications for:</h5>
+
+          <div className="custom-scrollbar grid max-h-40 grid-cols-2 gap-x-4 gap-y-2 overflow-auto sm:grid-cols-3 md:max-h-60">
+            {bossNames.map((boss) => (
+              <Checkbox
+                key={boss}
+                label={boss}
+                checked={!blacklist.value.set.has(boss)}
+                onChange={() => blacklist.toggleValue(boss)}
+                disabled={!registeredDevice}
+              />
+            ))}
           </div>
-        </Tabs.Panel>
-      </Tabs.Group>
+        </div>
+      </div>
 
       <div className="mt-4 flex items-center justify-end gap-4">
         <Button hollow pill onClick={onClose}>
