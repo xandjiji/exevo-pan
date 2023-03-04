88d1930f70ca805b632709488e5cc7073c28382d
xandjiji <xandjiji@gmail.com>
2023-03-04 13:58:21 -0300

    feature(trpc): adding `TRPCRouteInputs` and `TRPCRouteOutputs`

--- apps/exevo-pan/src/pages/api/trpc/[trpc].ts
+++ apps/exevo-pan/src/pages/api/trpc/[trpc].ts
@@ -2,10 +2,14 @@ import * as trpcNext from '@trpc/server/adapters/next'
 import { router } from 'server/trpc'
 import { routes } from 'server/routes'
 import { createContext } from 'server/context'
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
 
 const appRouter = router(routes)
 
 export type AppRouter = typeof appRouter
+
+export type TRPCRouteInputs = inferRouterInputs<AppRouter>
+export type TRPCRouteOutputs = inferRouterOutputs<AppRouter>
 
 export default trpcNext.createNextApiHandler({
   router: appRouter,
