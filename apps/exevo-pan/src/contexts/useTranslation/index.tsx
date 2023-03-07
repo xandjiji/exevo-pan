6e154f66b2ef4be041461a3dda850c2d0e43893c
xandjiji <xandjiji@gmail.com>
2023-03-07 10:13:33 -0300

    feature(useTranslation): adding `templateString()`

--- apps/exevo-pan/src/contexts/useTranslation/index.tsx
+++ apps/exevo-pan/src/contexts/useTranslation/index.tsx
@@ -28,3 +28,22 @@ export const templateMessage = (
 
   return <>{templatedElements}</>
 }
+
+export const templateString = (
+  message: string,
+  template: Record<string, string>,
+): string => {
+  let messageTail = message
+  const templatedStrings: string[] = []
+
+  Object.entries(template).forEach(([key, string]) => {
+    const [prefix, tail] = messageTail.split(`{{${key}}}`)
+    templatedStrings.push(prefix)
+    templatedStrings.push(string)
+    messageTail = tail
+  })
+
+  if (messageTail) templatedStrings.push(messageTail)
+
+  return templatedStrings.join('')
+}
