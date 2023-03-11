7000bf3aea2baad8df0c1a10dd62135dba251682
xandjiji <xandjiji@gmail.com>
2023-03-11 15:04:37 -0300

    feature(services): adding `fetchCheckedBosses`

--- apps/exevo-pan/src/services/server/Bosses/index.tsx
+++ apps/exevo-pan/src/services/server/Bosses/index.tsx
@@ -1,3 +1,5 @@
+import { prisma } from 'lib/prisma'
+import { pluckPremiumBossData } from 'utils'
 import { endpoints, paths } from 'Constants'
 
 export default class BossesClient {
@@ -9,5 +11,43 @@ export default class BossesClient {
     const response = await fetch(`${this.bossChancesUrl}/${serverName}.json`)
 
     return response.json()
+  }
+
+  static async fetchCheckedBosses({
+    isPro,
+    guildId,
+    server,
+  }: {
+    isPro: boolean
+    guildId: string
+    server: string
+  }): Promise<CheckedBoss[]> {
+    const [bossChecks, allBossChances] = await Promise.all([
+      prisma.bossCheck.findMany({
+        where: { guildId },
+        include: { checkedBy: { select: { name: true } } },
+      }),
+      this.fetchServerBossChances(server),
+    ])
+
+    const bossChances: typeof allBossChances = isPro
+      ? allBossChances
+      : pluckPremiumBossData(allBossChances)
+
+    const checkedBosses: CheckedBoss[] = bossChances.bosses.map(
+      (boss): CheckedBoss => {
+        const lastCheck = bossChecks.find((check) => boss.name === check.boss)
+
+        return lastCheck
+          ? {
+              ...boss,
+              checkedAt: lastCheck.checkedAt,
+              checkedBy: lastCheck.checkedBy.name,
+            }
+          : boss
+      },
+    )
+
+    return checkedBosses
   }
 }
