dda167ab62ce733e66b6a8c62d9d5ed8c4e7b13d
xandjiji <xandjiji@gmail.com>
2023-03-15 10:52:23 -0300

    fix: Fixing route revalidation

--- apps/exevo-pan/src/server/admin/revalidatePage.ts
+++ apps/exevo-pan/src/server/admin/revalidatePage.ts
@@ -1,17 +1,12 @@
 import { z } from 'zod'
 import { adminProcedure } from 'server/trpc'
-import { links, endpoints } from 'Constants'
+import { PageRevalidationClient } from 'services/server'
 
 export const revalidatePage = adminProcedure
   .input(z.string().optional())
   .mutation(async ({ input }) => {
-    const endpoint = new URL(`${links.CANONICAL}${endpoints.REVALIDATE_PAGE}`)
-    endpoint.search = new URLSearchParams().toString()
-    endpoint.searchParams.set('route', input ?? '/')
-    endpoint.searchParams.set('secret', process.env.REVALIDATION_AUTH as string)
-
     try {
-      await fetch(endpoint.toString())
+      await PageRevalidationClient.revalidatePage(input)
       return true
     } catch (error) {
       return false
