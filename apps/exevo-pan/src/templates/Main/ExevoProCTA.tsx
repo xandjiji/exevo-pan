b77964a74815ad3ef90f16d17d2b8a50e0636a05
xandjiji <xandjiji@gmail.com>
2023-04-04 16:37:18 -0300

    feature(ExevoProCTA): adding icon

--- apps/exevo-pan/src/templates/Main/ExevoProCTA.tsx
+++ apps/exevo-pan/src/templates/Main/ExevoProCTA.tsx
@@ -2,6 +2,7 @@ import { useTranslations, templateMessage } from 'contexts/useTranslation'
 import { useSession } from 'next-auth/react'
 import Link from 'next/link'
 import { Shine } from 'components/Atoms'
+import { ExevoPanIcon } from 'assets/svgs'
 import { routes } from 'Constants'
 
 export const ExevoProCTA = () => {
@@ -16,18 +17,22 @@ export const ExevoProCTA = () => {
 
   return (
     <Link
-      className="bg-surface text-onSurface clickable animate-fadeIn z-71 border-1 border-separator/50 text-s fixed bottom-3 left-[calc(100vw-12px)] whitespace-nowrap rounded-md border-solid px-3 py-2 shadow md:bottom-6 md:left-[calc(100vw-24px)]"
+      className="bg-surface text-onSurface clickable animate-fadeIn z-71 border-1 border-separator/50 text-s fixed bottom-3 left-[calc(100vw-12px)] flex items-center gap-2 whitespace-nowrap rounded-md border-solid px-3 py-2 shadow md:bottom-6 md:left-[calc(100vw-24px)]"
       href={routes.EXEVOPRO}
       style={{ transform: 'translateX(-100%)' }}
     >
       <Shine animationIterationCount="infinite" width={60} />
-      {templateMessage(common.exevoProCTA, {
-        exevoPro: (
-          <strong className="rare-gradient-text ml-[1px] tracking-wide">
-            Exevo Pro
-          </strong>
-        ),
-      })}
+
+      <ExevoPanIcon width={18} height={18} />
+      <span>
+        {templateMessage(common.exevoProCTA, {
+          exevoPro: (
+            <strong className="rare-gradient-text ml-[1px] tracking-wide">
+              Exevo Pro
+            </strong>
+          ),
+        })}
+      </span>
     </Link>
   )
 }
