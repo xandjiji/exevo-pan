48939a39df85c877af5c18b9325fefac80283926
xandjiji <xandjiji@gmail.com>
2023-04-05 11:11:08 -0300

    feature: using default icon

--- apps/exevo-pan/src/server/guild/crud.ts
+++ apps/exevo-pan/src/server/guild/crud.ts
@@ -664,7 +664,6 @@ export const notifyGuildMembers = authedProcedure
               title: boss,
               body: guild.name,
               url: getGuildPermalink(guild.name, true),
-              badge: 'boss',
             },
             deleteInvalidDevices: false,
           }).catch(console.log),
