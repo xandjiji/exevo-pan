a202676e314d3bf45e2258e9c2a95d7529908fc1
xandjiji <xandjiji@gmail.com>
2023-04-10 17:51:25 -0300

    feature(Layout): adding navigation

--- apps/exevo-pan/src/modules/Dashboard/components/Layout.tsx
+++ apps/exevo-pan/src/modules/Dashboard/components/Layout.tsx
@@ -1,4 +1,7 @@
 import { useSession } from 'next-auth/react'
+import { SubHeader } from 'templates'
+import { PersonIcon, PapyrusIcon, BlogIcon, AlertIcon } from 'assets/svgs'
+import { routes } from 'Constants'
 import UserCard from './UserCard'
 
 type LayoutProps = {
@@ -9,23 +12,48 @@ const Layout = ({ children }: LayoutProps) => {
   const { data: session } = useSession()
 
   return (
-    <main className="inner-container relative grid gap-8 py-8 lg:block">
-      {/* ToDo: nav header */}
+    <>
+      {/* @ ToDo: i18n */}
+      <SubHeader
+        navItems={[
+          {
+            title: 'My account',
+            href: routes.DASHBOARD.ROOT,
+            icon: <PersonIcon />,
+          },
+          {
+            title: 'Transaction history',
+            href: routes.DASHBOARD.TRANSACTIONS,
+            icon: <PapyrusIcon />,
+          },
+          {
+            title: 'Notification devices',
+            href: routes.DASHBOARD.DEVICES,
+            icon: <BlogIcon />,
+          },
+          {
+            title: 'Auction notifications',
+            href: routes.DASHBOARD.AUCTION_NOTIFICATIONS,
+            icon: <AlertIcon />,
+          },
+        ]}
+      />
+      <main className="inner-container relative grid gap-8 py-8 lg:block">
+        {session ? (
+          <>
+            <section className="animate-fadeIn">
+              <UserCard user={session.user} />
+            </section>
 
-      {session ? (
-        <>
-          <section className="animate-fadeIn">
-            <UserCard user={session.user} />
-          </section>
-
-          {children}
-        </>
-      ) : (
-        <div className="absolute-centered">
-          <div className="loading-spinner h-8 w-8" role="alert" />
-        </div>
-      )}
-    </main>
+            {children}
+          </>
+        ) : (
+          <div className="absolute-centered">
+            <div className="loading-spinner h-8 w-8" role="alert" />
+          </div>
+        )}
+      </main>
+    </>
   )
 }
 
