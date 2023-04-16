d8cb927b38227e5e48c227fbd572f814307959c3
xandjiji <xandjiji@gmail.com>
2023-04-16 15:23:16 -0300

    fix: fixing relative time

--- apps/exevo-pan/src/modules/BossHunting/modules/Tracker/components/RecentlyAppeared/utils.ts
+++ apps/exevo-pan/src/modules/BossHunting/modules/Tracker/components/RecentlyAppeared/utils.ts
@@ -1,8 +1,9 @@
 import { MILLISECONDS_IN } from 'utils'
 
-const APPROX_TIME_OFFSET = MILLISECONDS_IN.DAY * 1.5
+const APPROX_TIME_OFFSET = MILLISECONDS_IN.DAY * 0.5
 
 export const hoursAgo = (lastAppearence = 0): number =>
   Math.round(
-    (+new Date() - APPROX_TIME_OFFSET - lastAppearence) / MILLISECONDS_IN.HOUR,
+    (+new Date() - (APPROX_TIME_OFFSET + lastAppearence)) /
+      MILLISECONDS_IN.HOUR,
   )
