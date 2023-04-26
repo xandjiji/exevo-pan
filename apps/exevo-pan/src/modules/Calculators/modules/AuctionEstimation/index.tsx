c229172970f69c20da67fe5fb24263a004318c64
xandjiji <xandjiji@gmail.com>
2023-04-26 10:39:33 -0300

    feature(AuctionEstimation): fetching data

--- apps/exevo-pan/src/modules/Calculators/modules/AuctionEstimation/index.tsx
+++ apps/exevo-pan/src/modules/Calculators/modules/AuctionEstimation/index.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import { Input, Button, NumericInput } from 'components/Atoms'
+import { Button, NumericInput } from 'components/Atoms'
 import { ChipGroup } from 'components/Organisms'
+import { trpc } from 'lib/trpc'
+import { vocation as vocationUtils } from 'data-dictionary/dist/dictionaries/vocations'
 import { TibiaIcons } from 'assets/svgs'
+import { auctionEstimations } from 'Constants'
 import {
   vocationOptions,
   skillOptions,
   pvpOptions,
   locationOptions,
 } from '../../options'
+import { Skill, Vocation } from '../../types'
 
 /* @ ToDo:
 
@@ -19,123 +23,164 @@ import {
     loading
     success
     failed
+    pro
 
 - reset button
 - similar results
 
 */
 
+const parseNumber = (value: string) => parseInt(value, 10)
+
 const AuctionEstimation = () => {
+  const [pvp, setPvp] = useState<string>()
+  const [location, setLocation] = useState<string>()
+  const [battleye, setBattleye] = useState<string>()
+
   const [vocation, setVocation] = useState<string>()
   const [skill, setSkill] = useState<string>()
   const [minSkill, setMinSkill] = useState<number>()
   const [maxSkill, setMaxSkill] = useState<number>()
   const [minLevel, setMinLevel] = useState<number>()
   const [maxLevel, setMaxLevel] = useState<number>()
-  const [pvp, setPvp] = useState<string>()
-  const [battleye, setBattleye] = useState<string>()
-  const [location, setLocation] = useState<string>()
+
+  const estimation = trpc.estimateAuctionPrice.useQuery(
+    {
+      filterOptions: {
+        pvp: pvp ? new Set([parseNumber(pvp)]) : undefined,
+        location: location ? new Set([parseNumber(location)]) : undefined,
+        battleye: battleye ? new Set([battleye === 'true']) : undefined,
+        vocation: vocation
+          ? new Set([vocationUtils.getIdByRegex(vocation)])
+          : undefined,
+        skillKey: skill
+          ? new Set(
+              {
+                melee: ['axe', 'club', 'sword'],
+                distance: ['distance'],
+                magic: ['magic'],
+              }[skill as Skill],
+            )
+          : undefined,
+        minSkill,
+        maxSkill,
+        minLevel,
+        maxLevel,
+      },
+    },
+    { enabled: false, keepPreviousData: true },
+  )
 
   return (
-    <div className="grid gap-8">
-      <div className="grid gap-3">
-        <ChipGroup
-          label="PvP"
-          toggleable
-          options={pvpOptions}
-          value={pvp}
-          onChange={(e) => setPvp(e.target.value)}
-        />
-        <ChipGroup
-          label="Localização do servidor"
-          toggleable
-          options={locationOptions}
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-        />
-        <ChipGroup
-          label="BattlEye"
-          toggleable
-          options={[
-            {
-              /* @ ToDo: i18n */
-              name: (
-                <>
-                  <TibiaIcons.BattlEye color="battleGreen" /> Verde
-                </>
-              ),
-              value: 'true',
-            },
-            {
-              name: (
-                <>
-                  <TibiaIcons.BattlEye color="battleYellow" /> Amarelo
-                </>
-              ),
-              value: 'false',
-            },
-          ]}
-          value={battleye}
-          onChange={(e) => setBattleye(e.target.value)}
-        />
-      </div>
+    <div className="flex flex-col gap-8 md:flex-row">
+      <div className="grid gap-8">
+        <div className="grid gap-3">
+          <ChipGroup
+            label="PvP"
+            toggleable
+            options={pvpOptions}
+            value={pvp}
+            onChange={(e) => setPvp(e.target.value)}
+          />
+          <ChipGroup
+            label="Localização do servidor"
+            toggleable
+            options={locationOptions}
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <ChipGroup
+            label="BattlEye"
+            toggleable
+            options={[
+              {
+                /* @ ToDo: i18n */
+                name: (
+                  <>
+                    <TibiaIcons.BattlEye color="battleGreen" /> Verde
+                  </>
+                ),
+                value: 'true',
+              },
+              {
+                name: (
+                  <>
+                    <TibiaIcons.BattlEye color="battleYellow" /> Amarelo
+                  </>
+                ),
+                value: 'false',
+              },
+            ]}
+            value={battleye}
+            onChange={(e) => setBattleye(e.target.value)}
+          />
+        </div>
 
-      <div className="grid gap-3">
-        <ChipGroup
-          label="Vocation"
-          toggleable
-          options={vocationOptions}
-          value={vocation}
-          onChange={(e) => setVocation(e.target.value)}
-        />
+        <div className="grid gap-3">
+          <ChipGroup
+            label="Vocation"
+            toggleable
+            options={vocationOptions}
+            value={vocation}
+            onChange={(e) => setVocation(e.target.value)}
+          />
 
-        <ChipGroup
-          label="Skill"
-          toggleable
-          options={skillOptions}
-          value={skill}
-          onChange={(e) => setSkill(e.target.value)}
-        />
+          <ChipGroup
+            label="Skill"
+            toggleable
+            options={skillOptions}
+            value={skill}
+            onChange={(e) => setSkill(e.target.value)}
+          />
 
-        <div className="xs:flex-row xs:gap-8 flex flex-col gap-3">
-          <div className="flex items-center gap-3">
-            <NumericInput
-              label="Min skill"
-              alwaysValid
-              step={5}
-              value={minSkill}
-              onChange={setMinSkill}
-              className="max-w-[64px]"
-            />
-            <NumericInput
-              label="Max skill"
-              alwaysValid
-              step={5}
-              value={maxSkill}
-              onChange={setMaxSkill}
-              className="max-w-[64px]"
-            />
-          </div>
-          <div className="flex gap-3">
-            <NumericInput
-              label="Min level"
-              alwaysValid
-              step={50}
-              value={minLevel}
-              onChange={setMinLevel}
-              className="max-w-[64px]"
-            />
-            <NumericInput
-              label="Max level"
-              alwaysValid
-              step={50}
-              value={maxLevel}
-              onChange={setMaxLevel}
-              className="max-w-[64px]"
-            />
+          <div className="xs:flex-row xs:gap-8 flex flex-col gap-3">
+            <div className="flex items-center gap-3">
+              <NumericInput
+                label="Min skill"
+                alwaysValid
+                step={5}
+                value={minSkill}
+                onChange={setMinSkill}
+                className="max-w-[64px]"
+              />
+              <NumericInput
+                label="Max skill"
+                alwaysValid
+                step={5}
+                value={maxSkill}
+                onChange={setMaxSkill}
+                className="max-w-[64px]"
+              />
+            </div>
+            <div className="flex gap-3">
+              <NumericInput
+                label="Min level"
+                alwaysValid
+                step={50}
+                value={minLevel}
+                onChange={setMinLevel}
+                className="max-w-[64px]"
+              />
+              <NumericInput
+                label="Max level"
+                alwaysValid
+                step={50}
+                value={maxLevel}
+                onChange={setMaxLevel}
+                className="max-w-[64px]"
+              />
+            </div>
           </div>
         </div>
+        <Button
+          onClick={() => estimation.refetch()}
+          loading={estimation.isFetching}
+        >
+          Go
+        </Button>
       </div>
+
+      <div>{estimation.data?.estimatedValue}</div>
     </div>
   )
 }
