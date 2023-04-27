f51166650c5647eac9aa88484385b6ca1e9df8bb
xandjiji <xandjiji@gmail.com>
2023-04-27 08:32:37 -0300

    fix: removing `TibiaIcons` from icon list

--- apps/exevo-pan/src/components/IconsList.tsx
+++ apps/exevo-pan/src/components/IconsList.tsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx'
 import * as Icons from 'assets/svgs'
 
+const { TibiaIcons, ...SvgIcons } = Icons
+
 const IconsList = ({ className, ...props }: JSX.IntrinsicElements['svg']) => (
   <div className="flex flex-wrap gap-4">
-    {Object.values(Icons).map((Icon) => (
+    {Object.values(SvgIcons).map((Icon) => (
       <div key={Icon.name} title={Icon.name} className="grid">
         <Icon
           className={clsx(className ?? 'fill-onSurface h-8 w-8')}
