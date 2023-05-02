a1430ab302a6ea40688638f2857690240067ca08
xandjiji <xandjiji@gmail.com>
2023-05-02 10:34:33 -0300

    feature(Scripts): adding `manual:bosses`

---
+++ apps/bazaar-scraper/src/Scripts/TrackBosses/manualUpdate.ts
@@ -0,0 +1,32 @@
+import { Timer, coloredText, broadcast } from 'logging'
+import ScrapServers from 'Scripts/ScrapServers'
+import { executeShell } from 'utils'
+import * as task from './tasks'
+
+const DEPLOY_SCRIPT = `${__dirname}/deployStatic.sh`
+
+const main = async (): Promise<void> => {
+  const { activeServers } = await ScrapServers()
+
+  const timer = new Timer()
+  broadcast(`Manually updating boss statistics`, 'success')
+
+  const bossDistributions = await task.generateBossDistributions()
+
+  await task.calculateBossChances({
+    activeServers,
+    bossDistributions,
+    wasUpdated: false,
+  })
+
+  await executeShell(DEPLOY_SCRIPT)
+
+  await task.revalidatePages(activeServers)
+
+  broadcast(
+    `Manually updated boss chances in ${timer.elapsedTime()}`,
+    'success',
+  )
+}
+
+main()
