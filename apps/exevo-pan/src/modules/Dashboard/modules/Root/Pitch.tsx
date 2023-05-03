8054ed624cfbd34fe97433d1ad9999a9ec46980a
xandjiji <xandjiji@gmail.com>
2023-05-03 08:48:21 -0300

    refactor: using translation variable

--- apps/exevo-pan/src/modules/Dashboard/modules/Root/Pitch.tsx
+++ apps/exevo-pan/src/modules/Dashboard/modules/Root/Pitch.tsx
@@ -45,50 +45,47 @@ const Pitch = ({ proStatus }: PitchProps) => {
   const {
     translations: { dashboard },
   } = useTranslations()
+  const i18n = dashboard.Pitch
 
   return (
     <div className="grid place-items-center gap-8">
       <div className="grid place-items-center gap-2">
         <Heading>
-          {proStatus ? dashboard.Pitch.thankYou : dashboard.Pitch.upgradeNow}{' '}
+          {proStatus ? i18n.thankYou : i18n.upgradeNow}{' '}
           <ExevoProLink className="text-2xl tracking-wide">🧙‍♂️</ExevoProLink>
         </Heading>
-        <p>
-          {proStatus
-            ? dashboard.Pitch.enjoyFeatures
-            : dashboard.Pitch.exclusiveFeatures}
-        </p>
+        <p>{proStatus ? i18n.enjoyFeatures : i18n.exclusiveFeatures}</p>
       </div>
 
       <div className="text-tsm grid gap-4">
         <ul className="grid gap-2">
-          <Li>{dashboard.Pitch.features.tcInvested}</Li>
+          <Li>{i18n.features.tcInvested}</Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.exclusiveFilters, {
+            {templateMessage(i18n.features.exclusiveFilters, {
               auctionFilters: (
                 <Tooltip
                   offset={[0, 6]}
                   content={
                     <div className="grid place-items-start justify-items-start gap-3">
                       <TooltipUl>
-                        <li>{dashboard.Pitch.filtersTooltip.tc}</li>
-                        <li>{dashboard.Pitch.filtersTooltip.store}</li>
-                        <li>{dashboard.Pitch.filtersTooltip.rareItems}</li>
-                        <li>{dashboard.Pitch.filtersTooltip.soulwar}</li>
-                        <li>{dashboard.Pitch.filtersTooltip.primalOrdeal}</li>
+                        <li>{i18n.filtersTooltip.tc}</li>
+                        <li>{i18n.filtersTooltip.store}</li>
+                        <li>{i18n.filtersTooltip.rareItems}</li>
+                        <li>{i18n.filtersTooltip.soulwar}</li>
+                        <li>{i18n.filtersTooltip.primalOrdeal}</li>
                       </TooltipUl>
                     </div>
                   }
                 >
                   <strong className="underline decoration-dashed underline-offset-4">
-                    {dashboard.Pitch.features.auctionFilters}
+                    {i18n.features.auctionFilters}
                   </strong>
                 </Tooltip>
               ),
             })}
           </Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.exclusiveBosses, {
+            {templateMessage(i18n.features.exclusiveBosses, {
               bossTracker: (
                 <Tooltip
                   offset={[0, 6]}
@@ -104,46 +101,43 @@ const Pitch = ({ proStatus }: PitchProps) => {
                   }
                 >
                   <strong className="underline decoration-dashed underline-offset-4">
-                    {dashboard.Pitch.features.bossTracker}
+                    {i18n.features.bossTracker}
                   </strong>
                 </Tooltip>
               ),
             })}
           </Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.discounts, {
+            {templateMessage(i18n.features.discounts, {
               auctionHighlighting: (
-                <strong>{dashboard.Pitch.features.auctionHighlighting}</strong>
+                <strong>{i18n.features.auctionHighlighting}</strong>
               ),
             })}
           </Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.bidNotifications, {
-              notifications: (
-                <strong>{dashboard.Pitch.features.notifications}</strong>
-              ),
+            {templateMessage(i18n.features.bidNotifications, {
+              notifications: <strong>{i18n.features.notifications}</strong>,
             })}
           </Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.privateGroups, {
-              private: <strong>{dashboard.Pitch.features.private}</strong>,
+            {templateMessage(i18n.features.privateGroups, {
+              private: <strong>{i18n.features.private}</strong>,
             })}
           </Li>
           <Li>
-            {templateMessage(dashboard.Pitch.features.estimate, {
+            {templateMessage(i18n.features.estimate, {
               maxEstimation: (
                 <Text.TibiaCoin value={auctionEstimations.MAX_FREE_VALUE} />
               ),
             })}
           </Li>
         </ul>
-        <p className="text-right">{dashboard.Pitch.more}</p>
+        <p className="text-right">{i18n.more}</p>
       </div>
 
       {!proStatus && (
         <Heading>
-          {dashboard.Pitch.payOnce}{' '}
-          <strong className="text-2xl">{dashboard.Pitch.forever} 🙌</strong>
+          {i18n.payOnce} <strong className="text-2xl">{i18n.forever} 🙌</strong>
         </Heading>
       )}
     </div>
