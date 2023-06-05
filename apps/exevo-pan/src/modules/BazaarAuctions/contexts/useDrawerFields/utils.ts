200a6131d3dd2a30b1c3b303f1e048dc00e12d3f
lucasquearis <lucasquearis@hotmail.com>
2023-06-05 12:28:00 -0300

    style: change sort

--- apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/utils.ts
+++ apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/utils.ts
@@ -1,15 +1,4 @@
 export const buildOption = (value: string): Option => ({ name: value, value })
 
-export const sortOptions = (a: Option, b: Option) => {
-  const nameA = a.name.toUpperCase()
-  const nameB = b.name.toUpperCase()
-
-  if (nameA < nameB) {
-    return -1
-  }
-
-  if (nameA > nameB) {
-    return 1
-  }
-  return 0
-}
+export const sortOptions = (a: Option, b: Option) =>
+  a.name.localeCompare(b.name)
