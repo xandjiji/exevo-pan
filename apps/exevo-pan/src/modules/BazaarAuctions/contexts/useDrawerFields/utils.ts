144e628b731ff91da8d725eea2d883245d6a3108
lucasquearis <lucasquearis@hotmail.com>
2023-06-05 11:21:12 -0300

    feat: add world filter in drawer fields

--- apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/utils.ts
+++ apps/exevo-pan/src/modules/BazaarAuctions/contexts/useDrawerFields/utils.ts
@@ -1,1 +1,15 @@
 export const buildOption = (value: string): Option => ({ name: value, value })
+
+export const sortOptions = (a: Option, b: Option) => {
+  const nameA = a.name.toUpperCase()
+  const nameB = b.name.toUpperCase()
+
+  if (nameA < nameB) {
+    return -1
+  }
+
+  if (nameA > nameB) {
+    return 1
+  }
+  return 0
+}
