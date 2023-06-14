daf92eb620d829ae08284f9078c7d302aa993e97
xandjiji <xandjiji@gmail.com>
2023-06-14 10:55:43 -0300

    fix: fixing wrong import

--- apps/exevo-pan/src/locales/es/exevopro.ts
+++ apps/exevo-pan/src/locales/es/exevopro.ts
@@ -1,5 +1,5 @@
 import { defaultComposer } from 'default-composer'
-import defaultTranslations from '../en/error'
+import defaultTranslations from '../en/exevopro'
 /* @ ToDo: i18n-es */
 
 export default defaultComposer(defaultTranslations, {})
