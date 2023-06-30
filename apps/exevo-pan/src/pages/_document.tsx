3329fb684aa8aa7623938bd64471305749b95409
xandjiji <xandjiji@gmail.com>
2023-06-30 17:34:10 -0300

    feature: adding Ad

--- apps/exevo-pan/src/pages/_document.tsx
+++ apps/exevo-pan/src/pages/_document.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-danger */
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
@@ -24,6 +24,38 @@ export default function Document() {
               'function getInitialTheme(){if("undefined"!=typeof window){const e=window.localStorage.getItem("data-theme");if(e)return e;if(window.matchMedia("(prefers-color-scheme: dark)").matches)return"dark"}return"light"}function injectCssVariables(e){const t="light"===e?"#3F51B5":"#8B63E3";document.documentElement.setAttribute("data-theme",e),document.getElementById("address-bar-1")?.setAttribute("content",t),document.getElementById("address-bar-2")?.setAttribute("content",t)}injectCssVariables(getInitialTheme());',
           }}
         />
+        <script
+          data-cfasync="false"
+          type="text/javascript"
+          id="clever-core"
+          dangerouslySetInnerHTML={{
+            __html: `/* <![CDATA[ */
+                                    (function (document, window) {
+                                        var a, c = document.createElement("script"), f = window.frameElement;
+
+                                        c.id = "CleverCoreLoader70802";
+                                        c.src = "https://scripts.cleverwebserver.com/766d56588bb3504688e266dcc4c359cd.js";
+
+                                        c.async = !0;
+                                        c.type = "text/javascript";
+                                        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+                                        c.setAttribute("data-callback", "put-your-callback-function-here");
+                                        c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
+                                        c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
+
+                                        try {
+                                            a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+                                        } catch (e) {
+                                            a = !1;
+                                        }
+
+                                        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+                                        a.parentNode.insertBefore(c, a);
+                                    })(document, window);
+                                    /* ]]> */`,
+          }}
+        />
+
         <Main />
         <NextScript />
       </body>
