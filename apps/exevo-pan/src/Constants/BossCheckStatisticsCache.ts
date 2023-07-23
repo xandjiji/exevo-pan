620c475a8417f0e67024ac776ba63cbb689b387b
xandjiji <xandjiji@gmail.com>
2023-07-23 08:45:24 -0300

    feature: increasing cached time

--- apps/exevo-pan/src/Constants/BossCheckStatisticsCache.ts
+++ apps/exevo-pan/src/Constants/BossCheckStatisticsCache.ts
@@ -3,5 +3,5 @@ import { MILLISECONDS_IN } from 'shared-utils/dist/time'
 export const BOSS_CHECK_STATISTICS_CACHE = {
   version: 1,
   MINIMUM_CACHE_ENTRIES: 1000,
-  MAX_AGE: MILLISECONDS_IN.HOUR * 8,
+  MAX_AGE: MILLISECONDS_IN.HOUR * 12,
 }
