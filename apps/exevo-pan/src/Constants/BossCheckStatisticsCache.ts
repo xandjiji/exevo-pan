26feda243d2f210c2943ee88ca656199717a3bc5
xandjiji <xandjiji@gmail.com>
2023-08-05 18:34:23 -0300

    feature: reducing `MINIMUM_CACHE_ENTRIES`

--- apps/exevo-pan/src/Constants/BossCheckStatisticsCache.ts
+++ apps/exevo-pan/src/Constants/BossCheckStatisticsCache.ts
@@ -2,6 +2,6 @@ import { MILLISECONDS_IN } from 'shared-utils/dist/time'
 
 export const BOSS_CHECK_STATISTICS_CACHE = {
   version: 1,
-  MINIMUM_CACHE_ENTRIES: 1000,
+  MINIMUM_CACHE_ENTRIES: 100,
   MAX_AGE: MILLISECONDS_IN.HOUR * 12,
 }
