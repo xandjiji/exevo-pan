349c1f3f196499a4c4e1c485bf71de8278853e95
xandjiji <xandjiji@gmail.com>
2023-08-12 17:43:20 -0300

    feature(hooks): creating `useDebounce`

--- apps/exevo-pan/src/hooks/index.ts
+++ apps/exevo-pan/src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useDebounce } from './useDebounce'
 export { default as useDrag } from './useDrag'
 export { default as useEscToClose } from './useEscToClose'
 export { default as useInfiniteScroll } from './useInfiniteScroll'
---
+++ apps/exevo-pan/src/hooks/useDebounce/index.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+export default <T>(value: T, delay = 300): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
