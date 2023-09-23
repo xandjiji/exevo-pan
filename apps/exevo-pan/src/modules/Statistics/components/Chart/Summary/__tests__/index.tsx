608375975bbfeb7fde8a8404d4a20aa434dbecb2
xandjiji <xandjiji@gmail.com>
2023-09-23 11:43:13 -0300

    test: updating snapshots

--- apps/exevo-pan/src/modules/Statistics/components/Chart/Summary/__tests__/index.tsx
+++ apps/exevo-pan/src/modules/Statistics/components/Chart/Summary/__tests__/index.tsx
@@ -32,16 +32,9 @@ describe('<Summary />', () => {
       <svg
         aria-label="Upwards trend"
         class="mr-1 h-4 w-4 fill-green"
-        height="24px"
         style="transform: none;"
         viewBox="0 0 24 24"
-        width="24px"
-        xmlns="http://www.w3.org/2000/svg"
       >
-        <path
-          d="M0 0h24v24H0z"
-          fill="none"
-        />
         <path
           d="M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6z"
         />
