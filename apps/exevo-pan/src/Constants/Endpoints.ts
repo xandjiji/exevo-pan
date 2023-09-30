c0cea3d93a64ad7de2e35ee274e65540ec807bd1
xandjiji <xandjiji@gmail.com>
2023-09-30 10:58:01 -0300

    chore: updating endpoints

--- apps/exevo-pan/src/Constants/Endpoints.ts
+++ apps/exevo-pan/src/Constants/Endpoints.ts
@@ -1,10 +1,8 @@
 export const endpoints = {
-  CURRENT_AUCTIONS: process.env.NEXT_PUBLIC_AUCTIONS_ENDPOINT as string,
-  HISTORY_AUCTIONS: process.env.NEXT_PUBLIC_HISTORY_ENDPOINT as string,
-  PRICE_ESTIMATION: `${
-    process.env.NEXT_PUBLIC_HISTORY_ENDPOINT as string
-  }/estimate`,
-  STATIC_DATA: process.env.NEXT_PUBLIC_STATIC_ENDPOINT as string,
+  CURRENT_AUCTIONS: process.env.AUCTIONS_ENDPOINT as string,
+  HISTORY_AUCTIONS: process.env.HISTORY_ENDPOINT as string,
+  PRICE_ESTIMATION: `${process.env.HISTORY_ENDPOINT as string}/estimate`,
+  STATIC_DATA: process.env.STATIC_ENDPOINT as string,
   BLOG_QUERY: process.env.NEXT_PUBLIC_BLOG_ENDPOINT as string,
   BLOG_STATIC: process.env.NEXT_PUBLIC_BLOG_STATIC_ENDPOINT as string,
   WAR_DATA: 'https://exevo-pan-war-data.netlify.app',
