72fe694d95b6bfcaa1a3d5cf6c26f984c7b2bdfd
xandjiji <xandjiji@gmail.com>
2024-03-29 11:48:58 -0300

    fix: more precise calculations

--- apps/exevo-pan/src/server/admin/proRevenue.ts
+++ apps/exevo-pan/src/server/admin/proRevenue.ts
@@ -1,18 +1,26 @@
 import { adminProcedure } from 'server/trpc'
 import { prisma } from 'lib/prisma'
 import {
-  exevoProOrdersToBrl,
+  formatBrlValue,
   INITIAL_TIMESTAMP,
   oneMonthAgo,
   toMonthlyAverage,
 } from './utils'
 
 export const proRevenue = adminProcedure.query(async () => {
-  const [monthly, total] = await Promise.all([
+  const [monthly, total40, currentTotal] = await Promise.all([
     prisma.user.count({
       where: {
         proSince: {
           gt: oneMonthAgo(),
+        },
+        paymentData: { confirmed: true, noBill: false },
+      },
+    }),
+    prisma.user.count({
+      where: {
+        proSince: {
+          lt: new Date('10/06/2023'),
         },
         paymentData: { confirmed: true, noBill: false },
       },
@@ -24,11 +32,14 @@ export const proRevenue = adminProcedure.query(async () => {
     }),
   ])
 
+  const total45 = currentTotal - total40
+  const totalBrl = total40 * 40 + total45 * 45
+
   return {
-    monthly: exevoProOrdersToBrl(monthly),
-    total: exevoProOrdersToBrl(total),
-    average: exevoProOrdersToBrl(
-      toMonthlyAverage(total, INITIAL_TIMESTAMP.EXEVO_PRO),
+    monthly: formatBrlValue(monthly * 45),
+    total: formatBrlValue(totalBrl),
+    average: formatBrlValue(
+      toMonthlyAverage(totalBrl, INITIAL_TIMESTAMP.EXEVO_PRO),
     ),
   }
 })
--- apps/exevo-pan/src/server/admin/utils.ts
+++ apps/exevo-pan/src/server/admin/utils.ts
@@ -9,18 +9,7 @@ const MONTH = DAYS_IN.MONTH * MILLISECONDS_IN.DAY
 
 export const oneMonthAgo = () => new Date(+new Date() - MONTH).toISOString()
 
-export const formatBrlValue = (value: number) => {
-  const stringValue = value.toFixed(2).replace('.', ',')
-
-  const [integerPart, decimalPart] = stringValue.split(',')
-
-  return `R$ ${integerPart.replace(/(\d)(\d{3})$/, '$1.$2')},${decimalPart}`
-}
-
 export const tcToBrl = (tc: number) => Math.round(tc * BRL_PRICE.TIBIA_COIN)
-
-export const exevoProOrdersToBrl = (count: number) =>
-  formatBrlValue(count * BRL_PRICE.EXEVO_PRO)
 
 export const INITIAL_TIMESTAMP = {
   HIGHLIGHT: 1670946432809,
@@ -32,4 +21,12 @@ export const toMonthlyAverage = (
 ): number => {
   const daysSinceStart = timestampDaysDiff(+new Date(), initialTimestamp)
   return (count / daysSinceStart) * DAYS_IN.MONTH
+}
+
+export const formatBrlValue = (value: number) => {
+  const stringValue = value.toFixed(2).replace('.', ',')
+
+  const [integerPart, decimalPart] = stringValue.split(',')
+
+  return `R$ ${integerPart.replace(/(\d)(\d{3})$/, '$1.$2')},${decimalPart}`
 }
