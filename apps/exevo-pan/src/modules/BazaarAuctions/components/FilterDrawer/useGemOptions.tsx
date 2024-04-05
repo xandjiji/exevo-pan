c4a1608f7a5ebb01b87ed27757ca5ead7331f273
xandjiji <xandjiji@gmail.com>
2024-04-05 19:30:58 -0300

    refactor: colocating gem file

--- apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/useGemOptions.tsx
+++ apps/exevo-pan/src/modules/BazaarAuctions/components/FilterDrawer/useGemOptions.tsx
@@ -1,16 +1,30 @@
 import { useMemo } from 'react'
 import { vocation } from 'data-dictionary/dist/dictionaries/vocations'
-import { vocationGemOptions } from './gemOptions'
+
+import { greaterGems } from 'data-dictionary/dist/dictionaries/gems'
+
+const buildOption = (value: string): Option => ({ name: value, value })
 
 const allOptions = [
   ...new Set([
-    ...vocationGemOptions.rook,
-    ...vocationGemOptions.knight,
-    ...vocationGemOptions.paladin,
-    ...vocationGemOptions.sorcerer,
-    ...vocationGemOptions.druid,
+    greaterGems.knight['+1.5% Critical Extra Damage'],
+    greaterGems.knight['+0.25% Dodge'],
+    greaterGems.knight['+1.2% Life Leech'],
+    greaterGems.knight['+0.4% Mana Leech'],
+    ...Object.keys(greaterGems.knight),
+    ...Object.keys(greaterGems.paladin),
+    ...Object.keys(greaterGems.sorcerer),
+    ...Object.keys(greaterGems.druid),
   ]),
-]
+].map(buildOption)
+
+export const vocationGemOptions = {
+  rook: [],
+  knight: Object.keys(greaterGems.knight).map(buildOption),
+  sorcerer: Object.keys(greaterGems.sorcerer).map(buildOption),
+  druid: Object.keys(greaterGems.druid).map(buildOption),
+  paladin: Object.keys(greaterGems.paladin).map(buildOption),
+}
 
 export const useGemOptions = (vocationSet: FilterOptions['vocation']) =>
   useMemo(() => {
