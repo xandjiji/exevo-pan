bd025c2cb0a600bee2dfd6fd3fdb46c3bc1b0873
xandjiji <xandjiji@gmail.com>
2024-04-11 19:12:36 -0300

    fix(db): using `Z` as timezone

--- packages/db/kysely/index.ts
+++ packages/db/kysely/index.ts
@@ -3,7 +3,11 @@ import { createPool } from 'mysql2'
 import type { DB } from './types'
 
 const dialect = new MysqlDialect({
-  pool: createPool({ uri: process.env.DATABASE_URL, connectionLimit: 30 }),
+  pool: createPool({
+    uri: process.env.DATABASE_URL,
+    connectionLimit: 30,
+    timezone: 'Z',
+  }),
 })
 
 export const db = new Kysely<DB>({ dialect })
