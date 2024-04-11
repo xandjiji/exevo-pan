485eaf3c99ed3ef1acd33833336c8000548adc14
xandjiji <xandjiji@gmail.com>
2024-04-11 13:00:57 -0300

    feature(db): importing database

--- packages/db/kysely/index.ts
+++ packages/db/kysely/index.ts
@@ -1,6 +1,6 @@
 import { Kysely, MysqlDialect } from 'kysely'
 import { createPool } from 'mysql2'
-import { DB } from './types'
+import type { DB } from './types'
 
 const dialect = new MysqlDialect({
   pool: createPool({ uri: process.env.DATABASE_URL, connectionLimit: 30 }),
