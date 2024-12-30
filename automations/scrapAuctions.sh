a1860ed5f45b05e9f8483dd5d31d084fceea9edf
xandjiji <xandjiji@gmail.com>
2024-12-30 09:32:44 -0300

    feat(automations): adding timeout

--- automations/scrapAuctions.sh
+++ automations/scrapAuctions.sh
@@ -7,13 +7,13 @@ cp --verbose ~/exevo-pan/apps/bazaar-scraper/Output/ActiveServers.json ~/exevo-p
 cp --verbose ~/exevo-pan/apps/bazaar-scraper/Output/ItemsData.json ~/exevo-pan/static
 
 cd ~/exevo-pan
-yarn deploy:static
+timeout 5m yarn deploy:static
 
 cp --verbose ~/exevo-pan/apps/bazaar-scraper/Output/CurrentAuctions.json ~/exevo-pan/apps/current-auctions-worker/src/Data
 cp --verbose ~/exevo-pan/apps/bazaar-scraper/Output/ServerData.json ~/exevo-pan/apps/current-auctions-worker/src/Data
 
 cd ~/exevo-pan/apps/current-auctions-worker
-yarn deploy
+timeout 5m yarn deploy
 
 sleep 10s
 cd ~/exevo-pan/apps/bazaar-scraper/
