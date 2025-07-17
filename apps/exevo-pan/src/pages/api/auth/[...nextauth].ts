e712350422034208fd57688da83698a5f8136234
xandjiji <xandjiji@gmail.com>
2025-07-17 13:19:05 -0300

    fix: type def

--- apps/exevo-pan/src/pages/api/auth/[...nextauth].ts
+++ apps/exevo-pan/src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export default NextAuth({
   session: {
     strategy: 'jwt',
   },
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma as any),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
