b035b91b53e81a09b1c6b070265d1df12ecb0ba8
xandjiji <xandjiji@gmail.com>
2025-11-15 11:57:15 -0300

    fix: fixing iframe dimensions

--- apps/exevo-pan/src/modules/Blog/components/Post/custom/BestiaryArenaBlogIframe.tsx
+++ apps/exevo-pan/src/modules/Blog/components/Post/custom/BestiaryArenaBlogIframe.tsx
@@ -2,7 +2,7 @@ const Component = ({ tag }: { tag: string }) => (
   <div style={{ position: 'relative', margin: '0 auto', height: 313 }}>
     <iframe
       src={`https://bestiaryarena.com/iframe-obs-4?tag=${tag}`}
-      width="276"
+      width="277"
       height="313"
       scrolling="no"
       loading="lazy"
