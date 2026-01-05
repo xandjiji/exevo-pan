11d10313925e697128526a8ecfd8d4c149fb030b
xandjiji <xandjiji@gmail.com>
2026-01-05 16:05:36 -0300

    feature: webhook review

--- apps/exevo-pan/src/pages/api/stripe.ts
+++ apps/exevo-pan/src/pages/api/stripe.ts
@@ -11,13 +11,13 @@ const color = 10617045
 export default async (request: VercelRequest, response: VercelResponse) => {
   const sig = request.headers['stripe-signature']
 
-  let event
   if (!sig) {
     console.log('Stripe webhook event with no signature')
     response.status(400).send('No signature')
     return
   }
 
+  let event: Stripe.Event
   try {
     event = stripe.webhooks.constructEvent(
       request.body,
@@ -26,9 +26,9 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     )
   } catch (err) {
     await DiscordAdmin.shout({
-      title: `Unknown error`,
-      color: 10617045,
-      description: JSON.stringify(event?.data.object),
+      title: `Webhook signature verification failed`,
+      color,
+      description: `Error: ${(err as any).message}`,
     })
 
     response.status(400).send(`Webhook Error: ${(err as any).message}`)
@@ -36,117 +36,143 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   }
 
   if (event.type === 'payment_intent.succeeded') {
-    const paymentIntent = event.data.object.id
+    const paymentIntent = event.data.object as Stripe.PaymentIntent
+    const paymentIntentId = paymentIntent.id
 
-    const sessionQuery = await stripe.checkout.sessions.list({
-      payment_intent: paymentIntent,
-      expand: ['data.line_items'],
-    })
-
-    const checkoutSession = sessionQuery.data[0]
-
-    if (!checkoutSession) {
-      await DiscordAdmin.shout({
-        title: `Missing checkout session not found: ${paymentIntent}`,
-        color,
-        description: JSON.stringify(event.data.object),
+    try {
+      const sessionQuery = await stripe.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+        expand: ['data.line_items'],
       })
-      response.status(202).send('')
-      return
-    }
 
-    if (sessionQuery.data.length > 1) {
-      await DiscordAdmin.shout({
-        title: `Multiple chekout sessions found: ${paymentIntent}`,
-        color,
-        description: JSON.stringify(event.data.object),
-      })
-      response.status(202).send('')
-      return
-    }
+      const checkoutSession = sessionQuery.data[0]
 
-    const userId = checkoutSession.client_reference_id
+      if (!checkoutSession) {
+        await DiscordAdmin.shout({
+          title: `Checkout session not found: ${paymentIntentId}`,
+          color,
+          description: JSON.stringify(paymentIntent),
+        })
+        response.status(202).send('')
+        return
+      }
 
-    if (!userId) {
-      await DiscordAdmin.shout({
-        title: `Missing client_reference_id: ${paymentIntent}`,
-        color,
-        description: JSON.stringify(event.data.object),
-      })
-      response.status(202).send('')
-      return
-    }
+      if (sessionQuery.data.length > 1) {
+        await DiscordAdmin.shout({
+          title: `Multiple checkout sessions found: ${paymentIntentId}`,
+          color,
+          description: JSON.stringify(paymentIntent),
+        })
+        response.status(202).send('')
+        return
+      }
 
-    const foundUser = await prisma.user.findUnique({ where: { id: userId } })
-    if (foundUser?.proStatus) {
-      response.status(208).send('')
-      return
-    }
+      const userId = checkoutSession.client_reference_id
 
-    const item = checkoutSession.line_items?.data[0]
+      if (!userId) {
+        await DiscordAdmin.shout({
+          title: `Missing client_reference_id: ${paymentIntentId}`,
+          color,
+          description: JSON.stringify(paymentIntent),
+        })
+        response.status(202).send('')
+        return
+      }
 
-    if (!item) {
-      await DiscordAdmin.shout({
-        title: `Invalid checkout: ${paymentIntent}`,
-        color,
-        description: JSON.stringify(event.data.object),
-      })
-      response.status(500).send('')
-      return
-    }
+      const foundUser = await prisma.user.findUnique({ where: { id: userId } })
 
-    const priceId = item.price?.id
-    if (!priceId) {
-      await DiscordAdmin.shout({
-        title: `Price id not found: ${paymentIntent}`,
-        color,
-        description: JSON.stringify(event.data.object),
-      })
-      response.status(500).send('')
-      return
-    }
+      if (!foundUser) {
+        await DiscordAdmin.shout({
+          title: `User not found: ${paymentIntentId}`,
+          color,
+          description: `userId: ${userId}`,
+        })
+        response.status(208).send('')
+        return
+      }
 
-    const currentDate = new Date().toISOString()
-    await prisma.$transaction(async (tx) => {
-      const user = await tx.user.update({
-        where: { id: userId },
-        data: {
-          proStatus: true,
-          proSince: currentDate,
-          paymentData: {
-            upsert: {
-              create: {
-                character: null,
-                confirmed: true,
-                lastUpdated: currentDate,
+      if (foundUser.proStatus) {
+        response.status(208).send('')
+        return
+      }
+
+      const item = checkoutSession.line_items?.data[0]
+
+      if (!item) {
+        await DiscordAdmin.shout({
+          title: `Invalid checkout: ${paymentIntentId}`,
+          color,
+          description: JSON.stringify(paymentIntent),
+        })
+        response.status(500).send('')
+        return
+      }
+
+      const priceId = item.price?.id
+      if (!priceId) {
+        await DiscordAdmin.shout({
+          title: `Price id not found: ${paymentIntentId}`,
+          color,
+          description: JSON.stringify(paymentIntent),
+        })
+        response.status(500).send('')
+        return
+      }
+
+      const amountPaid = paymentIntent.amount / 100
+      const currentDate = new Date()
+
+      await prisma.$transaction(async (tx) => {
+        const user = await tx.user.update({
+          where: { id: userId },
+          data: {
+            proStatus: true,
+            proSince: currentDate,
+            paymentData: {
+              upsert: {
+                create: {
+                  character: null,
+                  confirmed: true,
+                  lastUpdated: currentDate,
+                },
+                update: {
+                  confirmed: true,
+                  character: null,
+                  lastUpdated: currentDate,
+                },
               },
-              update: { confirmed: true, character: null },
             },
           },
-        },
+          include: { paymentData: true },
+        })
 
-        include: { paymentData: true },
+        await tx.transaction.create({
+          data: {
+            value: Math.round(amountPaid),
+            currency: 'BRL',
+            type: 'EXEVO_PRO',
+            date: currentDate,
+            user: { connect: { id: userId } },
+            exevoProPayment: { connect: { id: user.paymentData?.id } },
+          },
+        })
       })
 
-      await tx.transaction.create({
-        data: {
-          value: 50,
-          currency: 'BRL',
-          type: 'EXEVO_PRO',
-          date: currentDate,
-          user: { connect: { id: userId } },
-          exevoProPayment: { connect: { id: user.paymentData?.id } },
-        },
+      response.status(200).send('')
+      return
+    } catch (error) {
+      await DiscordAdmin.shout({
+        title: `Error processing payment: ${paymentIntentId}`,
+        color,
+        description: `Error: ${
+          error instanceof Error ? error.message : String(error)
+        }`,
       })
-    })
 
-    response.status(200).send('')
-    return
+      response.status(500).send('Internal server error')
+      return
+    }
   }
 
   response.status(204).send('')
-}
-
-export const config = {
-  runtime: 'experimental-edge',
 }
