8d0e15f2827d94f5d45de8d8e7c35fb89cd04330
xandjiji <xandjiji@gmail.com>
2026-01-05 15:17:48 -0300

    feature: adding services

---
+++ apps/exevo-pan/src/services/DiscordAdmin.ts
@@ -0,0 +1,19 @@
+export class DiscordAdmin {
+  static async shout(
+    embed: { title: string; description: string; color: number },
+    content?: string,
+  ) {
+    const body = JSON.stringify({
+      username: 'Exevo Pan Admin',
+      avatar_url: 'https://i.imgur.com/ek4nJl8.png',
+      embeds: [embed],
+      content,
+    })
+
+    return fetch(process.env.ADMIN_DISCORD_WEBHOOK_URL!, {
+      body,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+}
