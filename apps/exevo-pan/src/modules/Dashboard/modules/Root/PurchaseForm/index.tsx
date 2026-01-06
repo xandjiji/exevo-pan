b75de0a6fe10afd3acfa016f83cf6ffcf447fdb2
xandjiji <xandjiji@gmail.com>
2026-01-06 10:24:27 -0300

    feature: updating icon

--- apps/exevo-pan/src/modules/Dashboard/modules/Root/PurchaseForm/index.tsx
+++ apps/exevo-pan/src/modules/Dashboard/modules/Root/PurchaseForm/index.tsx
@@ -209,16 +209,13 @@ const PurchaseForm = ({
                   icon={
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="currentcolor"
                       className="text-onSurface"
                     >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <line x1="2" x2="22" y1="10" y2="10" />
+                      <path d="M880-720v480q0 33-23.5 56.5T800-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720Zm-720 80h640v-80H160v80Zm0 160v240h640v-240H160Zm0 240v-480 480Z" />
                     </svg>
                   }
                 >
