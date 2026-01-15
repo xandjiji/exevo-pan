ad4fd1849ab2eb49ce0313fd836d93e866e09a9b
xandjiji <xandjiji@gmail.com>
2026-01-15 17:04:27 -0300

    feature: removing mini cta

--- apps/exevo-pan/src/templates/Main/index.tsx
+++ apps/exevo-pan/src/templates/Main/index.tsx
@@ -21,24 +21,6 @@ export default ({ clean = false, children }: MainProps) => (
       <div className="z-71 fixed bottom-3 left-[calc(100vw-162px)] flex flex-col items-end gap-2 md:bottom-5 md:left-[calc(100vw-174px)] lg:left-[calc(100vw-319px)]">
         <div className="grid gap-2">
           <ExevoProCTA />
-
-          <a
-            className="clickable animate-fadeIn relative h-[36px] shadow lg:hidden"
-            title="Play Bestiary Arena on your browser!"
-            href={`${links.BESTIARY_ARENA}?tag=exevomini`}
-            target="_blank"
-            rel="noopener external nofollow noreferrer"
-          >
-            <Shine animationIterationCount="infinite" width={60} />
-
-            <img
-              alt="Bestiary Arena"
-              width={150}
-              height={36}
-              src="/bestiary-widget.png"
-              style={{ imageRendering: 'pixelated' }}
-            />
-          </a>
         </div>
         <BestiaryArenaCTA />
       </div>
